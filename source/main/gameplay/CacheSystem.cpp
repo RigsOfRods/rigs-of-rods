db8079245482627d8d2c869f1a0f5265dcb45d9b
Thomas Fischer <thomas@rigsofrods.com>
2012-08-29 16:45:42 +0200

    added error message when unable to write cache file

--- source/main/gameplay/CacheSystem.cpp
+++ source/main/gameplay/CacheSystem.cpp
@@ -1048,7 +1048,10 @@ void CacheSystem::writeGeneratedCache()
 
 	FILE *f = fopen(path.c_str(), "w");
 	if (!f)
-		return;
+	{
+		showError(_L("Fatal Error: Unable to write cache to disk"), _L("Unable to write file.\nPlease ensure the parent directories exists and that you have write access to this location:\n") + path);
+		exit(1337);
+	}
 	fprintf(f, "shaone=%s\n", const_cast<char*>(currentSHA1.c_str()));
 	fprintf(f, "modcount=%d\n", (int)entries.size());
 	fprintf(f, "cacheformat=%s\n", CACHE_FILE_FORMAT);
