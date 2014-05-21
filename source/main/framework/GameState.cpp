6b880207421f76c640a0f5e31364a5b520188e77
only_a_ptr <petr@ohlidal.cz>
2014-05-22 01:29:27 +0200

    [Refactor] Began removal of GameState class. Part of it's logic ported to MainThread

--- source/main/framework/GameState.cpp
+++ source/main/framework/GameState.cpp
@@ -17,26 +17,7 @@ GameState::GameState()
 
 void GameState::enter()
 {
-    LOG("Entering GameState...");
-
-	m_pSceneMgr = RoR::Application::GetOgreSubsystem()->GetOgreRoot()->createSceneManager(ST_EXTERIOR_CLOSE);
-	gEnv->sceneManager = m_pSceneMgr;
-
-	//CREATE CAMERA
-	LOG("Creating camera");
-	// Create the camera
-	m_pCamera = m_pSceneMgr->createCamera("PlayerCam");
-	gEnv->mainCamera = m_pCamera;
-
-	// Position it at 500 in Z direction
-	m_pCamera->setPosition(Vector3(128,25,128));
-	// Look back along -Z
-	m_pCamera->lookAt(Vector3(0,0,-300));
-	m_pCamera->setNearClipDistance( 0.5 );
-	m_pCamera->setFarClipDistance( 1000.0*1.733 );
-	m_pCamera->setFOVy(Degree(60));
-	m_pCamera->setAutoAspectRatio(true);
-	RoR::Application::GetOgreSubsystem()->GetViewport()->setCamera(m_pCamera);
+    
 	
 	// TO BE DONE:
 	//m_pSceneMgr->setCameraRelativeRendering(true);
@@ -73,9 +54,7 @@ void GameState::exit()
 		delete mFrameListener;
 	}
 
-    m_pSceneMgr->destroyCamera(m_pCamera);
-    if (m_pSceneMgr)
-        RoR::Application::GetOgreSubsystem()->GetOgreRoot()->destroySceneManager(m_pSceneMgr);
+
 }
 
 void GameState::createScene()
