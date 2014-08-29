5bcfa6f47717e9219f9a225c6be1505dba3b13ae
only_a_ptr <petr@ohlidal.cz>
2014-08-29 22:38:43 +0200

    [Tools] Code templates for CodeGenerator in MyGUI's LayoutEditor

---
+++ tools/MyGUI_LayoutEditor/RoR_LayoutTemplate.cpp
@@ -0,0 +1,13 @@
+#include "GUI_#{Panel_Name}.h"
+
+using namespace RoR;
+using namespace GUI;
+
+#{Panel_Name}::#{Panel_Name}(MyGUI::Widget* _parent)
+{
+	initialiseByAttributes(this, _parent);
+}
+
+#{Panel_Name}::~#{Panel_Name}()
+{
+}
