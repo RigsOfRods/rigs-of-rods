5ceeeaab92233cddff75f6faa2affeccafa6cbe2
only_a_ptr <petr@ohlidal.cz>
2014-09-17 22:16:31 +0200

    [Fix] BitFlags.h: Fixed embarrasing bug in Bitmask_SetBool()

--- source/common/BitFlags.h
+++ source/common/BitFlags.h
@@ -12,11 +12,17 @@
 
 #define BITMASK_SET_1( VAR, FLAGS ) ( (VAR) |= (FLAGS) )
 
-template<typename var_T, typename flags_T> void Bitmask_SetBool(bool & val, var_T var, flags_T flags)
+template<typename var_T, typename flags_T> void Bitmask_SetBool(bool val, var_T & _in_out_var, flags_T flags)
 {
-	var_T var_true  = BITMASK_SET_1(var, flags);
-	var_T var_false = BITMASK_SET_0(var, flags);
-	var = (val) ? var_true : var_false;
+	// This code is supposed to compile only as conditional assignments, not as branches.
+
+	var_T var_true  = _in_out_var;
+	BITMASK_SET_1(var_true, flags);
+
+	var_T var_false = _in_out_var;
+	BITMASK_SET_0(var_false, flags);
+
+	_in_out_var = (val) ? var_true : var_false;
 }
 
 #define BITMASK_IS_0( VAR, FLAGS )  ( ((VAR) & (FLAGS)) == 0 )
