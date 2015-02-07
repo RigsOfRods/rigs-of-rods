31ff3b5724d63bfc4f255ed97f0df5a5d9b5950d
Aperion <rethnor@gmail.com>
2015-02-07 15:08:26 -0800

    no networking

--- source/main/physics/Beam.cpp
+++ source/main/physics/Beam.cpp
@@ -6158,7 +6158,7 @@ Beam::Beam(
 	trucknum = truck_number;
 	freePositioned = freeposition;
 	usedSkin = skin;
-	networking = networking;
+	//networking = networking;
 	memset(truckname, 0, 256);
 	sprintf(truckname, "t%i", truck_number);
 	memset(uniquetruckid, 0, 256);
