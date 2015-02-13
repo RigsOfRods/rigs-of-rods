6fc72ecf9ebf8355849fd29095d29952fc794bca
only_a_ptr <petr@ohlidal.cz>
2015-02-13 17:50:49 +0100

    [Add] RigSerializer: 98% format support

--- source/rig_file_input_output/RigDef_Serializer.h
+++ source/rig_file_input_output/RigDef_Serializer.h
@@ -120,61 +120,37 @@ protected:
 	void ProcessCollisionBoxes(File::Module* module);
 	void ProcessManagedMaterialsAndOptions(File::Module* module);
 	void ProcessFlares2(File::Module* module);
-
+	void ProcessMaterialFlareBindings(File::Module* module);
+	void ProcessPropsAndAnimations(File::Module* module);
+	void ProcessFlexbodies(File::Module* module);
+	void ProcessDirectiveAddAnimation(RigDef::Animation & anim);
 	/* TODO: 
-	5.5 Look & Feel
-
-    5.5.5 Materialflarebindings
-    5.5.6 Props
-        5.5.6.1 Special Props
-    5.5.7 Add_animation
+	5.5.17 Camerarail
     5.5.8 Flexbodies
-        5.5.8.1 (sub-section) "Prop-like"
-        5.5.8.2 (sub-directive) forset
         5.5.8.3 (sub-directive) disable_flexbody_shadow
         5.5.8.4 (sub-directive) flexbody_camera_mode
-    5.5.9 Submesh
-        5.5.9.1 (sub-section) texcoords
-        5.5.9.2 (sub-section) cab
-        5.5.9.3 (sub-directive) backmesh
-    5.5.10 submesh_groundmodel
-    5.5.11 Exhausts
-    5.5.12 Sections
-    5.5.13 Guisettings
-    5.5.14 Set_skeleton_settings
-    5.5.15 Videocamera
-    5.5.16 Extcamera
-    5.5.17 Camerarail
-    5.5.18 Envmap
-
-5.6 Sounds
-
-    5.6.1 disabledefaultsounds
-    5.6.2 Soundsources
-    5.6.3 Soundsources2
-        5.6.3.1 Default
-            5.6.3.1.1 Engine (Diesel)
-            5.6.3.1.2 Engine (Gasoline)
-            5.6.3.1.3 Airplane (Prop)
-            5.6.3.1.4 Airplane (Jet)
-            5.6.3.1.5 Airplane (Piston)
-            5.6.3.1.6 Marine (Large)
-            5.6.3.1.7 Marine (Small)
-    5.6.4 Soundsources3
-
-5.7 Aircraft
-
-    5.7.1 Wings
-    5.7.2 Airbrakes
-    5.7.3 Turboprops
-    5.7.4 Fusedrag
-    5.7.5 Turbojets
-    5.7.6 Pistonprops
-
-5.8 Boats
-
-    5.8.1 Screwprops
 	*/
+
+	void ProcessSubmesh(File::Module* module);
+	void ProcessSubmeshGroundmodel(File::Module* module); // STUB!!!
+	void ProcessExhausts(File::Module* module);
+	void ProcessGuiSettings(File::Module* module);
+	void ProcessSetSkeletonSettings(File::Module* module);
+	void ProcessVideocamera(File::Module* module);
+	void ProcessExtCamera(File::Module* module);
+	void ProcessSoundsources(File::Module* module);
+	void ProcessSoundsources2(File::Module* module);
+
+	// Aerial
+	void ProcessWings(File::Module* module);
+	void ProcessAirbrakes(File::Module* module);
+	void ProcessTurboprops(File::Module* module);
+	void ProcessFusedrag(File::Module* module); // STUB!!!
+	void ProcessTurbojets(File::Module* module);
+	void ProcessPistonprops(File::Module* module);
+
+	// Marine
+	void ProcessScrewprops(File::Module* module);
 
 protected:
 
@@ -187,7 +163,6 @@ protected:
 	int                               m_node_id_width;
 	int                               m_command_key_width;
 	int                               m_inertia_function_width;
-
 	
 };
 
