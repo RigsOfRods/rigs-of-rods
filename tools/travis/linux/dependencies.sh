cc2b7f3656a2aa814c9510a968e40c0735bcbb59
Niklas Kersten <nkersten.dev@gmail.com>
2015-05-15 09:51:35 +0200

    [Tools] Travis fix and cleanup

--- tools/travis/linux/dependencies.sh
+++ tools/travis/linux/dependencies.sh
@@ -2,11 +2,12 @@
 
 #Precompiled dependencies
 #sudo apt-get update
-sudo apt-get install -qq subversion mercurial git automake cmake build-essential pkg-config doxygen \
- libfreetype6-dev libfreeimage-dev libzzip-dev scons libcurl4-openssl-dev \
- nvidia-cg-toolkit libgl1-mesa-dev libxrandr-dev libx11-dev libxt-dev libxaw7-dev \
- libglu1-mesa-dev libxxf86vm-dev uuid-dev libuuid1 libgtk2.0-dev \
- libopenal-dev libois-dev libssl-dev libwxgtk3.0-dev
+sudo apt-get install -qq build-essential git cmake pkg-config \
+libfreetype6-dev libfreeimage-dev libzzip-dev libois-dev \
+libgl1-mesa-dev libglu1-mesa-dev libopenal-dev  \
+libx11-dev libxt-dev libxaw7-dev libxrandr-dev \
+libssl-dev libcurl4-openssl-dev libgtk2.0-dev libwxgtk3.0-dev
+# libboost-all-dev (too old) nvidia-cg-toolkit (needed only at runtime, does not affect RoR build)
 
 cd ~/
 mkdir ~/ror-deps
@@ -14,18 +15,22 @@ mkdir ~/.rigsofrods
 cd ~/ror-deps
 
 #OGRE
-hg clone --quiet https://bitbucket.org/sinbad/ogre -b v1-8
-cd ogre
+wget -O ogre.zip http://bitbucket.org/sinbad/ogre/get/v1-8.zip
+unzip ogre.zip
+rm ogre.zip
+cd sinbad-ogre-*
 cmake -DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
 -DCMAKE_BUILD_TYPE:STRING=Release \
 -DOGRE_BUILD_SAMPLES:BOOL=OFF .
 make -s -j2
-sudo make install
+sudo make -s install
 cd ..
 
-#MyGUI
-svn co -q https://svn.code.sf.net/p/my-gui/code/trunk my-gui -r 4344
-cd my-gui
+#MyGUI (needs specific revision)
+wget -O mygui.zip https://github.com/MyGUI/mygui/archive/a790944c344c686805d074d7fc1d7fc13df98c37.zip
+unzip mygui.zip
+rm mygui.zip
+cd mygui-*
 cmake -DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
 -DCMAKE_BUILD_TYPE:STRING=Release \
 -DMYGUI_BUILD_DEMOS:BOOL=OFF \
@@ -34,7 +39,7 @@ cmake -DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
 -DMYGUI_BUILD_TOOLS:BOOL=OFF \
 -DMYGUI_BUILD_PLUGINS:BOOL=OFF .
 make -s -j2
-sudo make install
+sudo make -s install
 cd ..
 
 #Paged Geometry
@@ -43,15 +48,17 @@ cd ogre-paged
 cmake -DCMAKE_BUILD_TYPE:STRING=Release \
 -DPAGEDGEOMETRY_BUILD_SAMPLES:BOOL=OFF .
 make -s -j2
-sudo make install
+sudo make -s install
 cd ..
 
 #Caelum (needs specific revision for OGRE-1.8)
-hg clone --quiet -r 3b0f1afccf5cb75c65d812d0361cce61b0e82e52 https://caelum.googlecode.com/hg/ caelum
-cd caelum
+wget -O caelum.zip http://caelum.googlecode.com/archive/3b0f1afccf5cb75c65d812d0361cce61b0e82e52.zip
+unzip caelum.zip
+rm caelum.zip
+cd caelum-*
 cmake -DCaelum_BUILD_SAMPLES:BOOL=OFF .
 make -s -j2
-sudo make install
+sudo make -s install
 cd .. 
 # important step, so the plugin can load:
 sudo ln -s /usr/local/lib/libCaelum.so /usr/local/lib/OGRE/
@@ -60,7 +67,7 @@ sudo ln -s /usr/local/lib/libCaelum.so /usr/local/lib/OGRE/
 git clone -q --depth=1 https://github.com/Hiradur/mysocketw.git
 cd mysocketw
 make shared -s -j2
-sudo make install
+sudo make -s install
 cd ..
 
 #Angelscript
@@ -72,7 +79,7 @@ cd sdk/angelscript/projects/gnuc
 SHARED=1 VERSION=2.22.1 make --silent -j2 
 # sudo make install fails when making the symbolic link, this removes the existing versions
 rm -f ../../lib/*
-sudo SHARED=1 VERSION=2.22.1 make install 
+sudo SHARED=1 VERSION=2.22.1 make -s install 
 #cleanup files made by root
 rm -f ../../lib/*
 cd ../../../../../
