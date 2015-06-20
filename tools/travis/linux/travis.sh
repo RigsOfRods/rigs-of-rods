8c8a44cc76bbb6501461f8e65dc9d30d6f6c49c9
Niklas Kersten <nkersten.dev@gmail.com>
2015-06-20 19:07:48 +0200

    [Tools] Travis: Integrate static code analysis

---
+++ tools/travis/linux/travis.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Build script for travis-ci.org builds to handle compiles 
+# and static analysis when ANALYZE=true.
+#
+if [ $ANALYZE = "true" ]; then
+        cppcheck --version
+        cppcheck \
+          --template "{file}({line}): {severity} ({id}): {message}" \
+          --enable=information --enable=performance \
+          --force --std=c++11 -j2 . 2> cppcheck.txt
+        if [ -s cppcheck.txt ]; then
+            cat cppcheck.txt
+            exit 1
+        fi
+    fi
+else # no static analysis, do regular build
+    ./tools/travis/linux/dependencies.sh
+    ./tools/travis/linux/build.sh
+fi
