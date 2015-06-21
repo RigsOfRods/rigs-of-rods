e80125d45fd34a2b80b39769267a378dd00622d3
Hiradur <nkersten.dev@gmail.com>
2015-06-21 09:58:07 +0200

    [Tools] Travis/cppcheck: Don't return 1, reroute stdout
    - route stdout to /dev/null since it contains no interesting information
    - return 0 even if defects were defected so the Travis build doesn't show up as failed
    
    [ci skip]

--- tools/travis/linux/travis.sh
+++ tools/travis/linux/travis.sh
@@ -8,10 +8,11 @@ if [ $ANALYZE = "true" ]; then
         cppcheck \
           --template "{file}({line}): {severity} ({id}): {message}" \
           --enable=information --enable=performance \
-          --force --std=c++11 -j2 ./source 2> cppcheck.txt
+          --force --std=c++11 -j2 ./source \
+          1> /dev/null 2> cppcheck.txt
         if [ -s cppcheck.txt ]; then
             cat cppcheck.txt
-            exit 1
+            exit 0
         fi
 else # no static analysis, do regular build
     ./tools/travis/linux/dependencies.sh
