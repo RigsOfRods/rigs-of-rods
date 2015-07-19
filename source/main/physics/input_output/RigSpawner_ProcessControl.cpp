5a8cf692382c831a3739ec72d099115cfaf88dd0
Petr Ohlidal <petr@ohlidal.cz>
2015-07-19 21:44:31 +0200

    [fix] RigSpawner: railgroups + slidenodes working
    Fixes #146

--- source/main/physics/input_output/RigSpawner_ProcessControl.cpp
+++ source/main/physics/input_output/RigSpawner_ProcessControl.cpp
@@ -242,9 +242,6 @@ rig_t *RigSpawner::SpawnRig()
 	// Section 'triggers'
 	PROCESS_SECTION_IN_ALL_MODULES(RigDef::File::KEYWORD_TRIGGERS, triggers, ProcessTrigger);
 
-	// Section 'slidenodes'
-	PROCESS_SECTION_IN_ALL_MODULES(RigDef::File::KEYWORD_SLIDENODES, slidenodes, ProcessSlidenode);
-
 	// Section 'materialflarebindings'
 	PROCESS_SECTION_IN_ALL_MODULES(RigDef::File::KEYWORD_MATERIALFLAREBINDINGS, material_flare_bindings, ProcessMaterialFlareBinding);
 
@@ -278,6 +275,9 @@ rig_t *RigSpawner::SpawnRig()
 
 	// Section 'railgroups'
 	PROCESS_SECTION_IN_ALL_MODULES(RigDef::File::KEYWORD_RAILGROUPS, railgroups, ProcessRailGroup);
+
+	// Section 'slidenodes'
+	PROCESS_SECTION_IN_ALL_MODULES(RigDef::File::KEYWORD_SLIDENODES, slidenodes, ProcessSlidenode);
 
 	// Section 'ropes'
 	PROCESS_SECTION_IN_ALL_MODULES(RigDef::File::KEYWORD_ROPES, ropes, ProcessRope);
