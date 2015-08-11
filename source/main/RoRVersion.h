fd61ac1ad4c27622228a40360d1883194fa4d427
Max98 <moncefbenslimane@yahoo.fr>
2015-08-11 21:05:13 +0100

    [Release] RoR 0.4.5.0

--- source/main/RoRVersion.h
+++ source/main/RoRVersion.h
@@ -32,7 +32,7 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 // $Rev:$
 
 #define ROR_VERSION_STRING_SHORT      "0.4"
-#define ROR_VERSION_STRING            "0.4.5.0-dev"
+#define ROR_VERSION_STRING            "0.4.5.0"
 #define ROR_VERSION_STRING_WIN         0,4,5,0
 
 #endif //RORVERSION_H__
