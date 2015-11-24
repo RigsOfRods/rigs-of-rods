8e2a317424511f39e38d3c28ec16288320f84c10
Niklas Kersten <nkersten.dev@gmail.com>
2015-11-24 17:17:03 +0100

    [Tools] Adjust and add helper scripts for Configurator l10n
    The Configurator uses its own gettext macro and thus needs special handling

---
+++ tools/linux/l10n/compile_mo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Compiles translated *.po files to binary *.mo files for gettext
+# Setup:
+# ./compile_mo.sh (this script)
+# ./languages/<lang_code>/LC_MESSAGES/ror.po (po file to be baked into *.mo file)
+
+cd languages
+for dir in *
+do
+	cd $dir
+	cd LC_MESSAGES
+	echo "Now compiling $dir"
+	msgfmt -o ror.mo ror.po
+	cd ../../
+done
