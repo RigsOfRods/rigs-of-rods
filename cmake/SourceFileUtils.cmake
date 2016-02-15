a5e854eb3dbe219e0578d94f82e3738c538b63cf
mikadou <mikadou@users.noreply.github.com>
2016-02-15 18:48:45 +0100

    [cmake] Group sources for IDEs

--- cmake/SourceFileUtils.cmake
+++ cmake/SourceFileUtils.cmake
@@ -69,3 +69,66 @@ function( expand_file_extensions RESULT )
   set( ${RESULT} ${EXPANDED_SOURCES} PARENT_SCOPE )
 endfunction()
 
+
+
+# ================================================================================================ #
+#  GENERATE_SOURCE_GROUPS( SOURCE_FILES )                                                          #
+#                                                                                                  #
+# In CMake the source_group() command can be used to provide guidance for IDEs such as Visual
+# Studio or XCode how to organize source files.
+# This function takes a list of source files and creates sensible source groups based on the
+# path provided for each file. In addition header and source files are sorted into separate super
+# groups. For example:
+#
+#   path/to/file.cpp
+#   path/to/file.h
+#
+# might be displayed in an IDE somewhat like this
+#
+#   |-Header Files
+#   ||-path
+#   | |-to
+#   |  |-file.h
+#   |-Source Files
+#   ||-path
+#   | |-to
+#   |  |-file.cpp
+#
+
+function( generate_source_groups )
+
+  set( SOURCES_GROUP "Source Files" )
+  set( HEADERS_GROUP "Header Files" )
+
+  foreach( f IN LISTS ARGN )
+	# Split filename into its components
+        get_filename_component( filename ${f} NAME )
+        get_filename_component( filedir ${f} DIRECTORY )
+
+	# Extract group name from directory path
+	if( filedir AND NOT ARG_FLAT_STRUCTURE )
+	  string( REPLACE "/" "\\\\" groupdir ${filedir} )
+	else()
+          set( groupdir "" )
+        endif()
+        list( APPEND GROUPDIRS ${groupdir} )
+       
+        # sort according to filetype
+        if( ${filename} MATCHES ".*\\.(c|cc|cpp|cxx)" )
+            list( APPEND ${groupdir}_SOURCES ${f} )
+        elseif( ${filename} MATCHES ".*\\.(h|hpp|hxx|txx)" )
+            list( APPEND ${groupdir}_HEADERS ${f} )       
+        else()
+            message( FATAL_ERROR "Unknown file type: ${filename}" )
+        endif()
+    endforeach()
+
+    list( REMOVE_DUPLICATES GROUPDIRS )
+
+    foreach( groupdir IN LISTS GROUPDIRS )
+      source_group( "${SOURCES_GROUP}\\${groupdir}" FILES ${${groupdir}_SOURCES} )
+      source_group( "${HEADERS_GROUP}\\${groupdir}" FILES ${${groupdir}_HEADERS} )
+    endforeach()
+   
+endfunction()
+
