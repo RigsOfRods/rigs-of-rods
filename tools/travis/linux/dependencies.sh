84ae4621a25244d34cf19ac7c42e47f2a9378ea0
mikadou <mikadou@users.noreply.github.com>
2016-02-15 16:09:13 +0100

    [Travis] Use packages addon for toolchain

--- tools/travis/linux/dependencies.sh
+++ tools/travis/linux/dependencies.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#Precompiled dependencies
-#sudo apt-get update
-sudo apt-get install -qq build-essential git cmake pkg-config \
-libfreetype6-dev libfreeimage-dev libzzip-dev libois-dev \
-libgl1-mesa-dev libglu1-mesa-dev libopenal-dev  \
-libx11-dev libxt-dev libxaw7-dev libxrandr-dev \
-libssl-dev libcurl4-openssl-dev libgtk2.0-dev libwxgtk3.0-dev
-
-# dependencies for building documentation
-sudo apt-get install doxygen graphviz
-
 cd ~/
 mkdir ~/ror-deps
 mkdir ~/.rigsofrods
