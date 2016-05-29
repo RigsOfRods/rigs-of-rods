0cb511d69a83ce3fa60e5bf4fb885423516c5e3d
ulteq <ulteq@web.de>
2016-05-29 20:54:29 +0200

    [Cleanup] Header cleanup

--- source/main/gfx/HydraxWater.cpp
+++ source/main/gfx/HydraxWater.cpp
@@ -1,32 +1,27 @@
-///WILL BE CLEANED UP
-
 /*
 This source file is part of Rigs of Rods
 Copyright 2005-2012 Pierre-Michel Ricordel
 Copyright 2007-2012 Thomas Fischer
-Copyright 2013-2014 Petr Ohlidal
+Copyright 2013-2016 Petr Ohlidal
+
 For more information, see http://www.rigsofrods.com/
 Rigs of Rods is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License version 3, as
 published by the Free Software Foundation.
+
 Rigs of Rods is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
-along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
+along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-/*
-Based on HydraxWater.cpp/.h
-*/
-
 
 #include "HydraxWater.h"
 
 #include "Application.h"
 #include "OgreSubsystem.h"
-#include "Settings.h"
 #include "SkyManager.h"
 
 #ifdef USE_CAELUM
