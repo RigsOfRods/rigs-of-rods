b1009a5b379b7d2a378105d3165e0db2fa7ceda3
Petr Ohlídal <petr@ohlidal.cz>
2016-08-27 11:59:55 +0200

    [Codechange] Formatting only: GlobalEnvironment.h

--- source/main/GlobalEnvironment.h
+++ source/main/GlobalEnvironment.h
@@ -1,22 +1,22 @@
 /*
-	This source file is part of Rigs of Rods
-	Copyright 2005-2012 Pierre-Michel Ricordel
-	Copyright 2007-2012 Thomas Fischer
-	Copyright 2013-2014 Petr Ohlidal
+    This source file is part of Rigs of Rods
+    Copyright 2005-2012 Pierre-Michel Ricordel
+    Copyright 2007-2012 Thomas Fischer
+    Copyright 2013-2016 Petr Ohlidal
 
-	For more information, see http://www.rigsofrods.org/
+    For more information, see http://www.rigsofrods.org/
 
-	Rigs of Rods is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License version 3, as
-	published by the Free Software Foundation.
+    Rigs of Rods is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3, as
+    published by the Free Software Foundation.
 
-	Rigs of Rods is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    Rigs of Rods is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
@@ -27,34 +27,33 @@ class GlobalEnvironment
 {
 public:
 
-	GlobalEnvironment() :
-		  cameraManager(0)
-		, collisions(0)
-		, frameListener(0)
-		, mainCamera(0)
-		, mrTime(0)
-		, multiplayer(false)
-		, player(0)
-		, sceneManager(0)
-		, sky(0)
-		, surveyMap(0)
-		, terrainManager(0)
-		, threadPool(0)
-	{
-	}
+    GlobalEnvironment() :
+          cameraManager(0)
+        , collisions(0)
+        , frameListener(0)
+        , mainCamera(0)
+        , mrTime(0)
+        , multiplayer(false)
+        , player(0)
+        , sceneManager(0)
+        , sky(0)
+        , surveyMap(0)
+        , terrainManager(0)
+        , threadPool(0)
+    {}
 
-	Ogre::Camera *mainCamera;
-	Ogre::SceneManager *sceneManager;
+    Ogre::Camera*       mainCamera;
+    Ogre::SceneManager* sceneManager;
 
-	CameraManager *cameraManager;
-	Character *player;
-	Collisions *collisions;
-	SurveyMapManager *surveyMap;
-	RoRFrameListener *frameListener;
-	SkyManager *sky;
-	TerrainManager *terrainManager;
-	ThreadPool *threadPool;
+    CameraManager*      cameraManager;
+    Character*          player;
+    Collisions*         collisions;
+    SurveyMapManager*   surveyMap;
+    RoRFrameListener*   frameListener;
+    SkyManager*         sky;
+    TerrainManager*     terrainManager;
+    ThreadPool*         threadPool;
 
-	float mrTime;
-	bool multiplayer;
+    float               mrTime;
+    bool                multiplayer;
 };
