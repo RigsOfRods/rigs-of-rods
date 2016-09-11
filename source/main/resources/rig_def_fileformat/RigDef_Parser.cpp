864bdc63bfb000389a61da3b4ee01f2faa4d715c
Petr Ohlídal <petr@ohlidal.cz>
2016-09-11 22:50:20 +0200

    [Fix] directive `set_beam_defaults_scale`

--- source/main/resources/rig_def_fileformat/RigDef_Parser.cpp
+++ source/main/resources/rig_def_fileformat/RigDef_Parser.cpp
@@ -1443,6 +1443,7 @@ void Parser::ParseDirectiveSetManagedMaterialsOptions()
 
 void Parser::ParseDirectiveSetBeamDefaultsScale()
 {
+    this->TokenizeCurrentLine();
     if (! this->CheckNumArguments(5)) { return; }
     
     BeamDefaults* b = new BeamDefaults(*m_user_beam_defaults);
