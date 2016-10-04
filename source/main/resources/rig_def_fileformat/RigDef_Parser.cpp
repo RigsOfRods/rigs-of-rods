300620c29b69f85b13ced67a0ab1ee822f7510d1
ulteq <ulteq@web.de>
2016-10-04 11:51:44 +0200

    [Fix] Truly replicate old BeamDefaults behaviour
    Reference: https://github.com/only-a-ptr/ror-legacy-svn-trunk/blob/0a55a993899d6a06cdc58fd98b5b675b371eb89a/source/main/physics/input_output/SerializedRig.cpp#L1242-L1246

--- source/main/resources/rig_def_fileformat/RigDef_Parser.cpp
+++ source/main/resources/rig_def_fileformat/RigDef_Parser.cpp
@@ -1472,12 +1472,12 @@ void Parser::ParseDirectiveSetBeamDefaults()
     if (m_num_args > 6) { d.beam_material_name     = this->GetArgStr  (6); }
     if (m_num_args > 7) { d.plastic_deform_coef    = this->GetArgFloat(7); }
 
-    if (d.springiness           < 0.f) { d.springiness           = DEFAULT_SPRING;        }
-    if (d.damping_constant      < 0.f) { d.damping_constant      = DEFAULT_DAMP;          }
-    if (d.deformation_threshold < 0.f) { d.deformation_threshold = BEAM_DEFORM;           }
-    if (d.breaking_threshold    < 0.f) { d.breaking_threshold    = BEAM_BREAK;            }
-    if (d.visual_beam_diameter  < 0.f) { d.visual_beam_diameter  = DEFAULT_BEAM_DIAMETER; }
-    if (d.plastic_deform_coef   < 0.f) { d.plastic_deform_coef   = 0.f;                   }
+    if (d.springiness           < 0.f) { d.springiness           = DEFAULT_SPRING;                              }
+    if (d.damping_constant      < 0.f) { d.damping_constant      = DEFAULT_DAMP;                                }
+    if (d.deformation_threshold < 0.f) { d.deformation_threshold = BEAM_DEFORM;                                 }
+    if (d.breaking_threshold    < 0.f) { d.breaking_threshold    = BEAM_BREAK;                                  }
+    if (d.visual_beam_diameter  < 0.f) { d.visual_beam_diameter  = DEFAULT_BEAM_DIAMETER;                       }
+    if (d.plastic_deform_coef   < 0.f) { d.plastic_deform_coef   = (*m_user_beam_defaults).plastic_deform_coef; }
 
     m_user_beam_defaults = std::shared_ptr<BeamDefaults>( new BeamDefaults(d) );
     return;
