a6a8f3057951e451d3db81865450b460ab2155ba
ulteq <ulteq@web.de>
2016-10-22 20:45:28 +0200

    [Bugfix] Fixed a typo
    Typo was introduced by this commit: fa85f6a412c6c8999b59c0a6c2a15cb7c252b0f8

--- source/main/physics/collision/DynamicCollisions.cpp
+++ source/main/physics/collision/DynamicCollisions.cpp
@@ -291,7 +291,7 @@ void intraTruckCollisions(const float dt, PointColDetector &intraPointCD,
 
         if (collision)
         {
-            intra_collcabrate[i].rate = 0;
+            intra_collcabrate[i].rate = -20000;
         } else
         {
             intra_collcabrate[i].rate++;
