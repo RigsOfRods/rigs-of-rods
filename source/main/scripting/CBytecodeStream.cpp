7564aad62a21ddd8d5544667de1d7eb909b40aec
Petr Ohlídal <petr@ohlidal.cz>
2016-11-19 16:35:15 +0100

    :triangular_ruler: Major formatting cleanup: scripting dir
    Tabs->spaces, removed old comment-outs, unified header blocks, removed header guards in favor of #pragma once
    Used tools: ReSharper C++; VisualStudio "untabify selected lines", manual adjustments

--- source/main/scripting/CBytecodeStream.cpp
+++ source/main/scripting/CBytecodeStream.cpp
@@ -1,50 +1,55 @@
 /*
-This source file is part of Rigs of Rods
-Copyright 2005-2012 Pierre-Michel Ricordel
-Copyright 2007-2012 Thomas Fischer
+    This source file is part of Rigs of Rods
+    Copyright 2005-2012 Pierre-Michel Ricordel
+    Copyright 2007-2012 Thomas Fischer
 
-For more information, see http://www.rigsofrods.org/
+    For more information, see http://www.rigsofrods.org/
 
-Rigs of Rods is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as
-published by the Free Software Foundation.
+    Rigs of Rods is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3, as
+    published by the Free Software Foundation.
 
-Rigs of Rods is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    Rigs of Rods is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
 */
-// created on 15th of May 2011 by Thomas Fischer
+
+/// @file
+/// @author Thomas Fischer
+/// @date   15th of May 2011
 
 #include "CBytecodeStream.h"
 
 CBytecodeStream::CBytecodeStream(std::string filename) : f(0)
 {
-	f = fopen(filename.c_str(), "wb");
+    f = fopen(filename.c_str(), "wb");
 }
 
 CBytecodeStream::~CBytecodeStream()
 {
-	if (f)
-		fclose(f);
+    if (f)
+        fclose(f);
 }
 
-void CBytecodeStream::Write(const void *ptr, AngelScript::asUINT size)
+void CBytecodeStream::Write(const void* ptr, AngelScript::asUINT size)
 {
-	if (!f) return;
-	size_t result = fwrite(ptr, size, 1, f);
+    if (!f)
+        return;
+    size_t result = fwrite(ptr, size, 1, f);
 }
 
-void CBytecodeStream::Read(void *ptr, AngelScript::asUINT size)
+void CBytecodeStream::Read(void* ptr, AngelScript::asUINT size)
 {
-	if (!f) return;
-	size_t result = fread(ptr, size, 1, f);
+    if (!f)
+        return;
+    size_t result = fread(ptr, size, 1, f);
 }
 
 bool CBytecodeStream::Existing()
 {
-	return (f != 0);
+    return (f != 0);
 }
--- source/main/scripting/ScriptEngine.h
+++ source/main/scripting/ScriptEngine.h
@@ -1,27 +1,29 @@
 /*
-This source file is part of Rigs of Rods
-Copyright 2005-2012 Pierre-Michel Ricordel
-Copyright 2007-2012 Thomas Fischer
+    This source file is part of Rigs of Rods
+    Copyright 2005-2012 Pierre-Michel Ricordel
+    Copyright 2007-2012 Thomas Fischer
+    Copyright 2013+     Petr Ohlidal & contributors
 
-For more information, see http://www.rigsofrods.org/
+    For more information, see http://www.rigsofrods.org/
 
-Rigs of Rods is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as
-published by the Free Software Foundation.
+    Rigs of Rods is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3, as
+    published by the Free Software Foundation.
 
-Rigs of Rods is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    Rigs of Rods is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
 */
-// created on 24th of February 2009 by Thomas Fischer
+
+/// @file
+/// @author Thomas Fischer
+/// @date   24th of February 2009
 
 #pragma once
-#ifndef SCRIPTENGINE_H__
-#define SCRIPTENGINE_H__
 
 #ifdef USE_ANGELSCRIPT
 
@@ -47,166 +49,163 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 class GameScript;
+
 /**
  *  @brief This class represents the angelscript scripting interface. It can load and execute scripts.
  */
 class ScriptEngine : public RoRSingletonNoCreation<ScriptEngine>, public Ogre::LogListener, public ZeroedMemoryAllocator
 {
-	friend class GameScript;
+    friend class GameScript;
 
 public:
 
-	ScriptEngine(Collisions *_coll = nullptr);
-	~ScriptEngine();
+    ScriptEngine(Collisions* _coll = nullptr);
+    ~ScriptEngine();
 
-	void setCollisions(Collisions *_coll) { coll=_coll; };
+    void setCollisions(Collisions* _coll) { coll = _coll; };
 
-	/**
-	 * Loads a script
-	 * @param scriptname filename to load
-	 * @return 0 on success, everything else on error
-	 */
-	int loadScript(Ogre::String scriptname);
+    /**
+     * Loads a script
+     * @param scriptname filename to load
+     * @return 0 on success, everything else on error
+     */
+    int loadScript(Ogre::String scriptname);
 
-	/**
-	 * Calls the script's framestep function to be able to use timed things inside the script
-	 * @param dt time passed since the last call to this function in seconds
-	 * @return 0 on success, everything else on error
-	 */
-	int framestep(Ogre::Real dt);
+    /**
+     * Calls the script's framestep function to be able to use timed things inside the script
+     * @param dt time passed since the last call to this function in seconds
+     * @return 0 on success, everything else on error
+     */
+    int framestep(Ogre::Real dt);
 
-	void activateLogging();
+    void activateLogging();
 
-	unsigned int eventMask;                              //!< filter mask for script events
-	
-	/**
-	 * triggers an event. Not to be used by the end-user
-	 * @param eventValue \see enum scriptEvents
-	 */
-	void triggerEvent(int scriptEvents, int value=0);
+    unsigned int eventMask; //!< filter mask for script events
 
-	/**
-	 * executes a string (useful for the console)
-	 * @param command string to execute
-	 */
-	int executeString(Ogre::String command);
-	
-	/**
-	 * Queues a string for execution.
-	 * Use this when you want to execute a script statement from another thread.
-	 * @param command string to queue for execution
-	 * @see executeString
-	 */
-	void queueStringForExecution(const Ogre::String command);
-	
-	/**
-	 * Adds a global function to the script
-	 * @param arg A declaration for the function.
-	*/
-	int addFunction(const Ogre::String& arg);
-	
-	/**
-	 * Checks if a global function exists
-	 * @param arg A declaration for the function.
-	*/
-	int functionExists(const Ogre::String& arg);
-	
-	/**
-	 * Deletes a global function from the script
-	 * @param arg A declaration for the function.
-	*/
-	int deleteFunction(const Ogre::String& arg);
-	
-	/**
-	 * Adds a global variable to the script
-	 * @param arg A declaration for the variable.
-	*/
-	int addVariable(const Ogre::String& arg);
-	
-	/**
-	 * Deletes a global variable from the script
-	 * @param arg A declaration for the variable.
-	*/
-	int deleteVariable(const Ogre::String& arg);
+    /**
+     * triggers an event. Not to be used by the end-user
+     * @param eventValue \see enum scriptEvents
+     */
+    void triggerEvent(int scriptEvents, int value = 0);
 
-	Ogre::StringVector getAutoComplete(Ogre::String command);
+    /**
+     * executes a string (useful for the console)
+     * @param command string to execute
+     */
+    int executeString(Ogre::String command);
 
-	int fireEvent(std::string instanceName, float intensity);
+    /**
+     * Queues a string for execution.
+     * Use this when you want to execute a script statement from another thread.
+     * @param command string to queue for execution
+     * @see executeString
+     */
+    void queueStringForExecution(const Ogre::String command);
 
-	int envokeCallback(int functionPtr, eventsource_t *source, node_t *node=0, int type=0);
+    /**
+     * Adds a global function to the script
+     * @param arg A declaration for the function.
+    */
+    int addFunction(const Ogre::String& arg);
 
-	AngelScript::asIScriptEngine *getEngine() { return engine; };
+    /**
+     * Checks if a global function exists
+     * @param arg A declaration for the function.
+    */
+    int functionExists(const Ogre::String& arg);
 
-	Ogre::String getScriptName() { return scriptName; };
-	Ogre::String getScriptHash() { return scriptHash; };
+    /**
+     * Deletes a global function from the script
+     * @param arg A declaration for the function.
+    */
+    int deleteFunction(const Ogre::String& arg);
 
-	// method from Ogre::LogListener
+    /**
+     * Adds a global variable to the script
+     * @param arg A declaration for the variable.
+    */
+    int addVariable(const Ogre::String& arg);
+
+    /**
+     * Deletes a global variable from the script
+     * @param arg A declaration for the variable.
+    */
+    int deleteVariable(const Ogre::String& arg);
+
+    Ogre::StringVector getAutoComplete(Ogre::String command);
+
+    int fireEvent(std::string instanceName, float intensity);
+
+    int envokeCallback(int functionPtr, eventsource_t* source, node_t* node = 0, int type = 0);
+
+    AngelScript::asIScriptEngine* getEngine() { return engine; };
+
+    Ogre::String getScriptName() { return scriptName; };
+    Ogre::String getScriptHash() { return scriptHash; };
+
+    // method from Ogre::LogListener
 #if OGRE_VERSION < ((1 << 16) | (8 << 8 ) | 0)
 		void messageLogged( const Ogre::String& message, Ogre::LogMessageLevel lml, bool maskDebug, const Ogre::String &logName);
 #else
-		void messageLogged( const Ogre::String& message, Ogre::LogMessageLevel lml, bool maskDebug, const Ogre::String &logName, bool& skipThisMessage);
+    void messageLogged(const Ogre::String& message, Ogre::LogMessageLevel lml, bool maskDebug, const Ogre::String& logName, bool& skipThisMessage);
 #endif // OGRE_VERSION
 
-	// deprecated
-	void exploreScripts();
+    // deprecated
+    void exploreScripts();
 
+    Ogre::Log* scriptLog;
 
-	Ogre::Log *scriptLog;
-
-	void SetFrameListener(RoRFrameListener* frame_listener)
-	{
-		mefl = frame_listener;
-	}
+    void SetFrameListener(RoRFrameListener* frame_listener)
+    {
+        mefl = frame_listener;
+    }
 
     RoRFrameListener* GetFrameListener() { return mefl; }
 
 protected:
 
-    RoRFrameListener *mefl;                 //!< local RoRFrameListener instance, used as proxy for many functions
-	Collisions *coll;
-    AngelScript::asIScriptEngine *engine;   //!< instance of the scripting engine
-	AngelScript::asIScriptContext *context; //!< context in which all scripting happens
-	int frameStepFunctionPtr;               //!< script function pointer to the frameStep function
-	int wheelEventFunctionPtr;              //!< script function pointer
-	int eventCallbackFunctionPtr;           //!< script function pointer to the event callback function
-	int defaultEventCallbackFunctionPtr;    //!< script function pointer for spawner events
-	Ogre::String scriptName;
-	Ogre::String scriptHash;
-	std::map <std::string , std::vector<int> > callbacks;
-	
-	InterThreadStoreVector< Ogre::String > stringExecutionQueue;     //!< The string execution queue \see queueStringForExecution
+    RoRFrameListener* mefl; //!< local RoRFrameListener instance, used as proxy for many functions
+    Collisions* coll;
+    AngelScript::asIScriptEngine* engine; //!< instance of the scripting engine
+    AngelScript::asIScriptContext* context; //!< context in which all scripting happens
+    int frameStepFunctionPtr; //!< script function pointer to the frameStep function
+    int wheelEventFunctionPtr; //!< script function pointer
+    int eventCallbackFunctionPtr; //!< script function pointer to the event callback function
+    int defaultEventCallbackFunctionPtr; //!< script function pointer for spawner events
+    Ogre::String scriptName;
+    Ogre::String scriptHash;
+    std::map<std::string, std::vector<int>> callbacks;
 
-	static const char *moduleName;
+    InterThreadStoreVector<Ogre::String> stringExecutionQueue; //!< The string execution queue \see queueStringForExecution
 
+    static const char* moduleName;
 
-	/**
-	 * This function initialzies the engine and registeres all types
-	 */
+    /**
+     * This function initialzies the engine and registeres all types
+     */
     void init();
 
-	/**
-	 * This is the callback function that gets called when script error occur.
-	 * When the script crashes, this function will provide you with more detail
-	 * @param msg arguments that contain details about the crash
-	 * @param param unkown?
-	 */
-    void msgCallback(const AngelScript::asSMessageInfo *msg);
+    /**
+     * This is the callback function that gets called when script error occur.
+     * When the script crashes, this function will provide you with more detail
+     * @param msg arguments that contain details about the crash
+     * @param param unkown?
+     */
+    void msgCallback(const AngelScript::asSMessageInfo* msg);
 
-	/**
-	 * This function reads a file into the provided string.
-	 * @param filename filename of the file that should be loaded into the script string
-	 * @param script reference to a string where the contents of the file is written to
-	 * @param hash reference to a string where the hash of the contents is written to
-	 * @return 0 on success, everything else on error
-	 */
-	int loadScriptFile(const char *fileName, std::string &script, std::string &hash);
+    /**
+     * This function reads a file into the provided string.
+     * @param filename filename of the file that should be loaded into the script string
+     * @param script reference to a string where the contents of the file is written to
+     * @param hash reference to a string where the hash of the contents is written to
+     * @return 0 on success, everything else on error
+     */
+    int loadScriptFile(const char* fileName, std::string& script, std::string& hash);
 
-	// undocumented debugging functions below, not working.
-	void ExceptionCallback(AngelScript::asIScriptContext *ctx, void *param);
-	void PrintVariables(AngelScript::asIScriptContext *ctx, int stackLevel);
-	void LineCallback(AngelScript::asIScriptContext *ctx, unsigned long *timeOut);
+    // undocumented debugging functions below, not working.
+    void ExceptionCallback(AngelScript::asIScriptContext* ctx, void* param);
+    void PrintVariables(AngelScript::asIScriptContext* ctx, int stackLevel);
+    void LineCallback(AngelScript::asIScriptContext* ctx, unsigned long* timeOut);
 };
 
 #endif // USE_ANGELSCRIPT
-
-#endif //SCRIPTENGINE_H__
