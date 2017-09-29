RigsOfRods/rigs-of-rods

commit f541d75352a50f90e3947c7d52b122a7f48abec7

:triangular_ruler: AngelScript: Removed obsolete+unused 'scriptstring'
The "scriptstring" addon was obsoleted by AngelScript in favor of "scriptstdstring". RoR doesn't use it anymore.

source/main/scripting/ScriptEngine.cpp
@@ -31,7 +31,6 @@
 #include "scriptany/scriptany.h"
 #include "scriptarray/scriptarray.h"
 #include "scripthelper/scripthelper.h"
-#include "scriptstring/scriptstring.h"
 // AS addons end
 
 #ifdef USE_CURL
@@ -259,8 +258,6 @@ void ScriptEngine::init()
     AngelScript::RegisterScriptMath(engine);
     AngelScript::RegisterScriptAny(engine);
     AngelScript::RegisterScriptDictionary(engine);
-    //AngelScript::RegisterScriptString(engine);
-    //AngelScript::RegisterScriptStringUtils(engine);
 
     // register some Ogre objects like the vector3 and the quaternion
     registerOgreObjects(engine);

source/angelscript_addons/scriptstring/scriptstring.h
@@ -1,64 +1,0 @@
-//
-// CScriptString
-//
-// This class is used to pass strings between the application and the script engine.
-// It is basically a container for the normal std::string, with the addition of a
-// reference counter so that the script can use object handles to hold the type.
-//
-// Because the class is reference counted it cannot be stored locally in the
-// application functions, nor be received or returned by value. Instead it should
-// be manipulated through pointers or references.
-//
-// Note, because the internal buffer is placed at the beginning of the class
-// structure it is infact possible to receive this type as a reference or pointer
-// to a normal std::string where the reference counter doesn't have to be manipulated.
-//
-
-#ifndef SCRIPTSTRING_H
-#define SCRIPTSTRING_H
-
-#include <angelscript.h>
-#include <string>
-
-BEGIN_AS_NAMESPACE
-
-class CScriptString
-{
-public:
-	CScriptString();
-	CScriptString(const CScriptString &other);
-	CScriptString(const char *s, unsigned int len);
-	CScriptString(const std::string &s);
-
-	void AddRef() const;
-	void Release() const;
-
-	CScriptString &operator=(const CScriptString &other);
-	CScriptString &operator+=(const CScriptString &other);
-	friend CScriptString *operator+(const CScriptString &a, const CScriptString &b);
-
-	std::string buffer;
-
-protected:
-	~CScriptString();
-	mutable int refCount;
-};
-
-// This function will determine the configuration of the engine
-// and use one of the two functions below to register the string type
-void RegisterScriptString(asIScriptEngine *engine);
-
-// Call this function to register the string type
-// using native calling conventions
-void RegisterScriptString_Native(asIScriptEngine *engine);
-
-// Use this one instead if native calling conventions
-// are not supported on the target platform
-void RegisterScriptString_Generic(asIScriptEngine *engine);
-
-// This function will register utility functions for the script string
-void RegisterScriptStringUtils(asIScriptEngine *engine);
-
-END_AS_NAMESPACE
-
-#endif

source/angelscript_addons/scriptstring/scriptstring_utils.cpp
@@ -1,374 +1,0 @@
-#include <assert.h>
-#include "scriptstring.h"
-#include "../scriptarray/scriptarray.h"
-#include <string.h> // strstr
-
-
-BEGIN_AS_NAMESPACE
-
-// This function returns a string containing the substring of the input string
-// determined by the starting index and count of characters.
-//
-// AngelScript signature:
-// string@ substring(const string &in str, int start, int count)
-void StringSubString_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    int start = *(int*)gen->GetAddressOfArg(1);
-    int count = *(int*)gen->GetAddressOfArg(2);
-
-    // Create the substring
-    CScriptString *sub = new CScriptString();
-    sub->buffer = str->buffer.substr(start,count);
-
-    // Return the substring
-    *(CScriptString**)gen->GetAddressOfReturnLocation() = sub;
-}
-
-
-
-// This function returns the index of the first position where the substring
-// exists in the input string. If the substring doesn't exist in the input
-// string -1 is returned.
-//
-// AngelScript signature:
-// int findFirst(const string &in str, const string &in sub, int start)
-void StringFindFirst_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *sub = *(CScriptString**)gen->GetAddressOfArg(1);
-    int start = *(int*)gen->GetAddressOfArg(2);
-
-    // Find the substring
-    int loc = (int)str->buffer.find(sub->buffer, start);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-// TODO: Angelscript should permit default parameters
-void StringFindFirst0_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *sub = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Find the substring
-    int loc = (int)str->buffer.find(sub->buffer);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-
-
-
-// This function returns the index of the last position where the substring
-// exists in the input string. If the substring doesn't exist in the input
-// string -1 is returned.
-//
-// AngelScript signature:
-// int findLast(const string &in str, const string &in sub, int start)
-void StringFindLast_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *sub = *(CScriptString**)gen->GetAddressOfArg(1);
-    int start = *(int*)gen->GetAddressOfArg(2);
-
-    // Find the substring
-    int loc = (int)str->buffer.rfind(sub->buffer, start);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-void StringFindLast0_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *sub = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Find the substring
-    int loc = (int)str->buffer.rfind(sub->buffer);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-
-
-
-// This function returns the index of the first character that is in
-// the specified set of characters. If no such character is found -1 is
-// returned.
-//
-// AngelScript signature:
-// int findFirstOf(const string &in str, const string &in chars, int start)
-void StringFindFirstOf_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-    int start = *(int*)gen->GetAddressOfArg(2);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_first_of(chars->buffer, start);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-void StringFindFirstOf0_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_first_of(chars->buffer);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-
-
-// This function returns the index of the first character that is not in
-// the specified set of characters. If no such character is found -1 is
-// returned.
-//
-// AngelScript signature:
-// int findFirstNotOf(const string &in str, const string &in chars, int start)
-void StringFindFirstNotOf_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-    int start = *(int*)gen->GetAddressOfArg(2);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_first_not_of(chars->buffer, start);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-void StringFindFirstNotOf0_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_first_not_of(chars->buffer);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-
-
-
-// This function returns the index of the last character that is in
-// the specified set of characters. If no such character is found -1 is
-// returned.
-//
-// AngelScript signature:
-// int findLastOf(const string &in str, const string &in chars, int start)
-void StringFindLastOf_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-    int start = *(int*)gen->GetAddressOfArg(2);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_last_of(chars->buffer, start);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-void StringFindLastOf0_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_last_of(chars->buffer);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-
-
-
-// This function returns the index of the last character that is not in
-// the specified set of characters. If no such character is found -1 is
-// returned.
-//
-// AngelScript signature:
-// int findLastNotOf(const string &in str, const string &in chars, int start)
-void StringFindLastNotOf_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-    int start = *(int*)gen->GetAddressOfArg(2);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_last_not_of(chars->buffer, start);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-void StringFindLastNotOf0_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptString *str = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *chars = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Find the substring
-    int loc = (int)str->buffer.find_last_not_of(chars->buffer);
-
-    // Return the result
-    *(int*)gen->GetAddressOfReturnLocation() = loc;
-}
-
-
-
-// This function takes an input string and splits it into parts by looking
-// for a specified delimiter. Example:
-//
-// string str = "A|B||D";
-// array<string@>@ array = split(str, "|");
-//
-// The resulting array has the following elements:
-//
-// {"A", "B", "", "D"}
-//
-// AngelScript signature:
-// array<string@>@ split(const string &in str, const string &in delim)
-void StringSplit_Generic(asIScriptGeneric *gen)
-{
-    // Obtain a pointer to the engine
-    asIScriptContext *ctx = asGetActiveContext();
-    asIScriptEngine *engine = ctx->GetEngine();
-
-    // TODO: This should only be done once
-	// TODO: This assumes that CScriptArray was already registered
-	asIObjectType *arrayType = engine->GetObjectTypeById(engine->GetTypeIdByDecl("array<string@>"));
-
-    // Create the array object
-    CScriptArray *array = new CScriptArray(0, arrayType);
-
-    // Get the arguments
-    CScriptString *str   = *(CScriptString**)gen->GetAddressOfArg(0);
-    CScriptString *delim = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Find the existence of the delimiter in the input string
-    int pos = 0, prev = 0, count = 0;
-    while( (pos = (int)str->buffer.find(delim->buffer, prev)) != (int)std::string::npos )
-    {
-        // Add the part to the array
-        CScriptString *part = new CScriptString();
-        part->buffer.assign(&str->buffer[prev], pos-prev);
-        array->Resize(array->GetSize()+1);
-        *(CScriptString**)array->At(count) = part;
-
-        // Find the next part
-        count++;
-        prev = pos + (int)delim->buffer.length();
-    }
-
-    // Add the remaining part
-    CScriptString *part = new CScriptString();
-    part->buffer.assign(&str->buffer[prev]);
-    array->Resize(array->GetSize()+1);
-    *(CScriptString**)array->At(count) = part;
-
-    // Return the array by handle
-    *(CScriptArray**)gen->GetAddressOfReturnLocation() = array;
-}
-
-
-
-// This function takes as input an array of string handles as well as a
-// delimiter and concatenates the array elements into one delimited string.
-// Example:
-//
-// array<string@> array = {"A", "B", "", "D"};
-// string str = join(array, "|");
-//
-// The resulting string is:
-//
-// "A|B||D"
-//
-// AngelScript signature:
-// string@ join(const array<string@> &in array, const string &in delim)
-void StringJoin_Generic(asIScriptGeneric *gen)
-{
-    // Get the arguments
-    CScriptArray  *array = *(CScriptArray**)gen->GetAddressOfArg(0);
-    CScriptString *delim = *(CScriptString**)gen->GetAddressOfArg(1);
-
-    // Create the new string
-    CScriptString *str = new CScriptString();
-    int n;
-    for( n = 0; n < (int)array->GetSize() - 1; n++ )
-    {
-        CScriptString *part = *(CScriptString**)array->At(n);
-        str->buffer += part->buffer;
-        str->buffer += delim->buffer;
-    }
-
-    // Add the last part
-    CScriptString *part = *(CScriptString**)array->At(n);
-    str->buffer += part->buffer;
-
-    // Return the string
-    *(CScriptString**)gen->GetAddressOfReturnLocation() = str;
-}
-
-
-
-// TODO: Implement the following functions
-//
-//       int64    parseInt(const string &in str, int &out bytesParsed);
-//       double   parseDouble(const string &in str, int &out bytesParsed);
-//       string @ formatString(int64, const string &in format);  // should use sprintf to format the string
-//       string @ formatDouble(double, const string &in format);
-//
-//       int16    byteStringToInt16(const string &in str, int start);
-//       int32    byteStringToInt32(const string &in str, int start);
-//       int64    byteStringtoInt64(const string &in str, int start);
-//       float    byteStringToFloat(const string &in str, int start);
-//       double   byteStringToDouble(const string &in str, int start);
-//       string @ int16ToByteString(int16);
-//       string @ int32ToByteString(int32);
-//       string @ int64ToByteString(int64);
-//       string @ floatToByteString(float);
-//       string @ doubleToByteString(double);
-
-
-
-
-// This is where the utility functions are registered.
-// The string type must have been registered first.
-void RegisterScriptStringUtils(asIScriptEngine *engine)
-{
-    int r;
-
-    r = engine->RegisterGlobalFunction("string@ substring(const string &in, int, int)", asFUNCTION(StringSubString_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findFirst(const string &in, const string &in)", asFUNCTION(StringFindFirst0_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findFirst(const string &in, const string &in, int)", asFUNCTION(StringFindFirst_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findLast(const string &in, const string &in)", asFUNCTION(StringFindLast0_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findLast(const string &in, const string &in, int)", asFUNCTION(StringFindLast_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findFirstOf(const string &in, const string &in)", asFUNCTION(StringFindFirstOf0_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findFirstOf(const string &in, const string &in, int)", asFUNCTION(StringFindFirstOf_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findFirstNotOf(const string &in, const string &in)", asFUNCTION(StringFindFirstNotOf0_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findFirstNotOf(const string &in, const string &in, int)", asFUNCTION(StringFindFirstNotOf_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findLastOf(const string &in, const string &in)", asFUNCTION(StringFindLastOf0_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findLastOf(const string &in, const string &in, int)", asFUNCTION(StringFindLastOf_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findLastNotOf(const string &in, const string &in)", asFUNCTION(StringFindLastNotOf0_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("int findLastNotOf(const string &in, const string &in, int)", asFUNCTION(StringFindLastNotOf_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("array<string@>@ split(const string &in, const string &in)", asFUNCTION(StringSplit_Generic), asCALL_GENERIC); assert(r >= 0);
-    r = engine->RegisterGlobalFunction("string@ join(const array<string@> &in, const string &in)", asFUNCTION(StringJoin_Generic), asCALL_GENERIC); assert(r >= 0);
-}
-
-END_AS_NAMESPACE

source/main/scripting/GameScript.cpp
@@ -35,7 +35,6 @@
 #include "scripthelper/scripthelper.h"
 #include "scriptmath/scriptmath.h"
 #include "scriptstdstring/scriptstdstring.h"
-#include "scriptstring/scriptstring.h"
 // AS addons end
 
 #include "Application.h"

commit 0173bc2fca300747d41880dc83dee6b2d5ff8be8

:triangular_ruler: ScriptEngine: Removed dead `callbacks` hash table.
This hashtable listed available callbacks, but it was never read from or otherwise used. Purpose is unknown. All the callbacks are being used via pointers (except 'on_terrain_loading()'). So the hashtable may be some unfinished upgrade attempt aiming to replace the pointers with hashtable of vectors.

NOTE: The hashtable was already broken by recent commit f541d75352a50f90e3947c7d52b122a7f48abec7 which removed initialization of the table entries.

source/main/scripting/ScriptEngine.cpp
@@ -632,26 +632,16 @@ int ScriptEngine::addFunction(const String &arg)
         {
             if (frameStepFunctionPtr == nullptr)
                 frameStepFunctionPtr = func;
-
-            callbacks["frameStep"].push_back(func);
         }
         else if (func == mod->GetFunctionByDecl("void eventCallback(int, int)"))
         {
             if (eventCallbackFunctionPtr == nullptr)
                 eventCallbackFunctionPtr = func;
-
-            callbacks["eventCallback"].push_back(func);
         }
         else if (func == mod->GetFunctionByDecl("void defaultEventCallback(int, string, string, int)"))
         {
             if (defaultEventCallbackFunctionPtr == nullptr)
                 defaultEventCallbackFunctionPtr = func;
-
-            callbacks["defaultEventCallback"].push_back(func);
-        }
-        else if (func == mod->GetFunctionByDecl("void on_terrain_loading(string lines)"))
-        {
-            callbacks["on_terrain_loading"].push_back(func);
         }
     }
 
@@ -711,12 +701,7 @@ int ScriptEngine::deleteFunction(const String &arg)
         engine->GarbageCollect();
 
         // Check if we removed a "special" function
-        for (auto it=callbacks.begin(); it!=callbacks.end(); it++)
-        {
-            auto found_itor = std::find(it->second.begin(), it->second.end(), func);
-            if (found_itor != it->second.end())
-                it->second.erase(found_itor);
-        }
+
         if ( frameStepFunctionPtr == func )
             frameStepFunctionPtr = nullptr;
 
@@ -860,20 +845,10 @@ int ScriptEngine::loadScript(String _scriptName)
 
     // get some other optional functions
     frameStepFunctionPtr = mod->GetFunctionByDecl("void frameStep(float)");
-    if (frameStepFunctionPtr != nullptr)
-        callbacks["frameStep"].push_back(frameStepFunctionPtr);
 
     eventCallbackFunctionPtr = mod->GetFunctionByDecl("void eventCallback(int, int)");
-    if (eventCallbackFunctionPtr != nullptr)
-        callbacks["eventCallback"].push_back(eventCallbackFunctionPtr);
 
     defaultEventCallbackFunctionPtr = mod->GetFunctionByDecl("void defaultEventCallback(int, string, string, int)");
-    if (defaultEventCallbackFunctionPtr != nullptr)
-        callbacks["defaultEventCallback"].push_back(defaultEventCallbackFunctionPtr);
-
-    AngelScript::asIScriptFunction* callback_fn = mod->GetFunctionByDecl("void on_terrain_loading(string lines)");
-    if (callback_fn != nullptr)
-        callbacks["on_terrain_loading"].push_back(callback_fn);
 
     // Find the function that is to be called.
     auto main_func = mod->GetFunctionByDecl("void main()");

source/main/scripting/ScriptEngine.h
@@ -165,7 +165,6 @@ protected:
     AngelScript::asIScriptFunction* defaultEventCallbackFunctionPtr; //!< script function pointer for spawner events
     Ogre::String scriptName;
     Ogre::String scriptHash;
-    std::map<std::string, std::vector<AngelScript::asIScriptFunction*>> callbacks;
     Ogre::Log* scriptLog;
 
     InterThreadStoreVector<Ogre::String> stringExecutionQueue; //!< The string execution queue \see queueStringForExecution