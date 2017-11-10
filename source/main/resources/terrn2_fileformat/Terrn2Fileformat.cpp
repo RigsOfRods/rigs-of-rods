cb035c3ae54ea4751abfeed739f2f24ca26b182e
Petr Ohlidal <petr@ohlidal.cz>
2017-11-10 10:51:15 +0100

    :sparkles: Custom material support for terrain.
    New parameter 'CustomMaterial' added to Terrn2 format, section [General].

--- source/main/resources/terrn2_fileformat/Terrn2Fileformat.cpp
+++ source/main/resources/terrn2_fileformat/Terrn2Fileformat.cpp
@@ -67,6 +67,7 @@ bool Terrn2Parser::LoadTerrn2(Terrn2Def& def, Ogre::DataStreamPtr &ds)
     def.traction_map_file    = file.GetStringEx   ("TractionMap",      "General");
     def.water_height         = file.GetFloat      ("WaterLine",        "General");
     def.water_bottom_height  = file.GetFloat      ("WaterBottomLine",  "General");
+    def.custom_material_name = file.GetStringEx   ("CustomMaterial",   "General");
 
     def.start_position       = StringConverter::parseVector3(file.GetStringEx("StartPosition", "General"), Vector3(512.0f, 0.0f, 512.0f));
 
