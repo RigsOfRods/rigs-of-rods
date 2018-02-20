2e2cd2866829214467f77f72598ad490af038053
Petr Ohlidal <petr@ohlidal.cz>
2018-02-20 17:49:19 +0100

    :triangular_ruler: Differentials: removed deadcode

--- source/main/physics/Differentials.h
+++ source/main/physics/Differentials.h
@@ -24,7 +24,6 @@
 
 #include "RoRPrerequisites.h"
 
-#define MAX_DIFFS 3
 struct differential_data_t
 {
     Ogre::Real speed[2];
@@ -37,20 +36,14 @@ struct differential_data_t
 enum DiffType
 {
     SPLIT_DIFF = 0,
-    VISCOUS_DIFF,
-    TC_DIFF,
     OPEN_DIFF,
     LOCKED_DIFF
 };
 
 typedef void (*diff_callback)( differential_data_t& diff_data);
 
-//! class created from a struct will be ugly for a while.
-// ugly code is not merged. fixed that up -- thomas
 class Axle : public ZeroedMemoryAllocator
 {
-    friend class RigInspector;
-
 public:
     Axle();
 
@@ -62,34 +55,22 @@ public:
     Ogre::Real torsion_rate;
     Ogre::Real torsion_damp;
 
-    //! average wheel speed,
-    Ogre::Real avg_speed;
-    //! not used ATM will be for future use
-    Ogre::Real gear_ratio;
-    //! which group this axle belong to, not used ATM
-    unsigned int axle_group;
-
     void addDiffType(DiffType diff);
-    const std::vector<DiffType>& availableDiffs();
     void toggleDiff();
     void calcTorque( differential_data_t& diff_data );
     Ogre::UTFString getDiffTypeName();
 
     //! a differential that always splits the torque evenly, this is the original method
     static void calcSeperatedDiff( differential_data_t& diff_data);
-    //! viscous coupler, the greater the difference in speed the more torque is transfered
-    static void calcViscousDiff( differential_data_t& diff_data );
-    //! initial attempt at traction controll
-    static void calcTCDiff( differential_data_t& diff_data );
     //! more power goes to the faster spining wheel
     static void calcOpenDiff( differential_data_t& diff_data );
     //! ensures both wheels rotate at the the same speed
     static void calcLockedDiff( differential_data_t& diff_data );
+
 private:
 
     //! type of differential
     unsigned int which_diff;
-    unsigned int free_diff;
     diff_callback current_callback;
 
     //! available diffs
