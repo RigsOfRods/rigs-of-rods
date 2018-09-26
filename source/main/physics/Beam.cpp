RigsOfRods/rigs-of-rods

commit fe74eeb8e13e8924a19f31b12605ab49c6261154

:video_game: No more MAX_SHOCKS (was 5000)

source/main/physics/Beam.cpp
@@ -5141,7 +5141,7 @@ void Beam::updateDashBoards(float dt)
     }
 
     // active shocks / roll correction
-    if (free_active_shock)
+    if (this->has_active_shocks)
     {
         // TOFIX: certainly not working:
         float roll_corr = - stabratio * 10.0f;
@@ -6112,11 +6112,6 @@ bool Beam::LoadTruck(
     // print some truck memory stats
     int mem = 0, memr = 0, tmpmem = 0;
     LOG("BEAM: memory stats following");
-
-    tmpmem = free_shock * sizeof(shock_t);
-    mem += tmpmem;
-    memr += MAX_SHOCKS * sizeof(beam_t);
-    LOG("BEAM: shock memory: " + TOSTRING(tmpmem) + " B (" + TOSTRING(free_shock) + " x " + TOSTRING(sizeof(shock_t)) + " B) / " + TOSTRING(MAX_SHOCKS * sizeof(shock_t)));
 
     tmpmem = free_prop * sizeof(prop_t);
     mem += tmpmem;

source/main/datatypes/rig_t.h
@@ -1,9 +1,23 @@
 /*
- * rig.h
- *
- *  Created on: Dec 29, 2012
- *      Author: chris
- */
+    This source file is part of Rigs of Rods
+    Copyright 2005-2012 Pierre-Michel Ricordel
+    Copyright 2007-2012 Thomas Fischer
+    Copyright 2016-2017 Petr Ohlidal & contributors
+
+    For more information, see http://www.rigsofrods.org/
+
+    Rigs of Rods is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3, as
+    published by the Free Software Foundation.
+
+    Rigs of Rods is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
@@ -16,19 +30,26 @@
 
 struct rig_t ///< A simulation actor; typically a vehicle
 {
-    rig_t(): nodes(nullptr), free_node(0), beams(nullptr), free_beam(0) {}
+    rig_t(): nodes(nullptr), free_node(0), beams(nullptr), free_beam(0),
+             shocks(nullptr), free_shock(0), has_active_shocks(false)
+    {}
 
     ~rig_t()
     {
         delete nodes;
         delete beams;
+        delete shocks;
     }
 
     node_t* nodes;
-    int free_node; ///< Number of nodes; name is historical (free index in static array)
+    int free_node;            ///< Number of nodes; name is historical (free index in static array)
 
     beam_t* beams;
-    int free_beam; ///< Number of beams; name is historical (free index in static array)
+    int free_beam;            ///< Number of beams; name is historical (free index in static array)
+
+    shock_t* shocks;          ///< Shock absorbers
+    int free_shock;           ///< Number of shock absorbers; name is historical (free index in static array)
+    bool has_active_shocks;   ///< Are there active stabilizer shocks?
 
     std::vector<beam_t*> interTruckBeams;
 
@@ -58,10 +79,6 @@ struct rig_t ///< A simulation actor; typically a vehicle
     prop_t props[MAX_PROPS];
     prop_t *driverSeat;
     int free_prop;
-
-    shock_t shocks[MAX_SHOCKS];
-    int free_shock;
-    int free_active_shock; //!< this has no array associated with it. its just to determine if there are active shocks!
 
     std::vector < exhaust_t > exhausts;
 

source/main/datatypes/shock_t.h
@@ -1,9 +1,23 @@
 /*
- * shock.h
- *
- *  Created on: Dec 29, 2012
- *      Author: chris
- */
+    This source file is part of Rigs of Rods
+    Copyright 2005-2012 Pierre-Michel Ricordel
+    Copyright 2007-2012 Thomas Fischer
+    Copyright 2016-2017 Petr Ohlidal & contributors
+
+    For more information, see http://www.rigsofrods.org/
+
+    Rigs of Rods is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3, as
+    published by the Free Software Foundation.
+
+    Rigs of Rods is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
@@ -12,6 +26,8 @@
 */
 struct shock_t
 {
+    shock_t() { memset(this, 0, sizeof(shock_t)); }
+
     int beamid;
     int flags;
     float lastpos;

source/main/physics/BeamConstants.h
@@ -26,7 +26,6 @@ static const int   MAX_WHEELS                 = 64;              //!< maximum nu
 static const int   MAX_SUBMESHES              = 500;             //!< maximum number of submeshes per truck
 static const int   MAX_TEXCOORDS              = 3000;            //!< maximum number of texture coordinates per truck
 static const int   MAX_CABS                   = 3000;            //!< maximum number of cabs per truck
-static const int   MAX_SHOCKS                 = 5000;            //!< maximum number of shocks per truck
 static const int   MAX_ROPES                  = 64;              //!< maximum number of ropes per truck
 static const int   MAX_ROPABLES               = 64;              //!< maximum number of ropables per truck
 static const int   MAX_TIES                   = 64;              //!< maximum number of ties per truck

source/main/physics/BeamForcesEuler.cpp
@@ -657,11 +657,8 @@ void Beam::calcForcesEulerCompute(int doUpdate, Real dt, int step, int maxsteps)
     BES_STOP(BES_CORE_Wheels);
     BES_START(BES_CORE_Shocks);
 
-    //update position
-    //		if (free_node != 0)
-    //			aposition/=(Real)(free_node);
     //variable shocks for stabilization
-    if (free_active_shock && stabcommand)
+    if (this->has_active_shocks && stabcommand)
     {
         if ((stabcommand == 1 && stabratio < 0.1) || (stabcommand == -1 && stabratio > -0.1))
             stabratio = stabratio + (float)stabcommand * dt * STAB_RATE;
@@ -675,7 +672,7 @@ void Beam::calcForcesEulerCompute(int doUpdate, Real dt, int step, int maxsteps)
         }
     }
     //auto shock adjust
-    if (free_active_shock && doUpdate)
+    if (this->has_active_shocks && doUpdate)
     {
         stabsleep -= dt * maxsteps;
 

source/main/physics/RigSpawner.cpp
@@ -160,7 +160,8 @@ void RigSpawner::CalcMemoryRequirements(ActorMemoryRequirements& req, RigDef::Fi
     req.num_beams += module_def->hydros.size();
 
     // 'triggers'
-    req.num_beams += module_def->triggers.size();
+    req.num_beams  += module_def->triggers.size();
+    req.num_shocks += module_def->triggers.size();
 
     // 'animators'
     req.num_beams += module_def->animators.size();
@@ -170,8 +171,10 @@ void RigSpawner::CalcMemoryRequirements(ActorMemoryRequirements& req, RigDef::Fi
     req.num_beams += module_def->cinecam.size() * 8;
 
     // 'shocks' and 'shocks2'
-    req.num_beams += module_def->shocks.size();
-    req.num_beams += module_def->shocks_2.size();
+    req.num_beams  += module_def->shocks.size();
+    req.num_shocks += module_def->shocks.size();
+    req.num_beams  += module_def->shocks_2.size();
+    req.num_shocks += module_def->shocks_2.size();
 
     // 'commands' and 'commands2' (unified)
     req.num_beams += module_def->commands_2.size();
@@ -225,6 +228,8 @@ void RigSpawner::InitializeRig()
     // Allocate memory as needed
     m_rig->beams = new beam_t[req.num_beams];
     m_rig->nodes = new node_t[req.num_nodes];
+    if (req.num_shocks > 0)
+        m_rig->shocks = new shock_t[req.num_shocks];
 
     // clear rig parent structure
     memset(m_rig->contacters, 0, sizeof(contacter_t) * MAX_CONTACTERS);
@@ -254,9 +259,6 @@ void RigSpawner::InitializeRig()
     m_rig->free_flare = 0;
     memset(m_rig->props, 0, sizeof(prop_t) * MAX_PROPS);
     m_rig->free_prop = 0;
-    memset(m_rig->shocks, 0, sizeof(shock_t) * MAX_SHOCKS);
-    m_rig->free_shock = 0;
-    m_rig->free_active_shock = 0;
     m_rig->exhausts.clear();
     memset(m_rig->cparticles, 0, sizeof(cparticle_t) * MAX_CPARTICLES);
     m_rig->free_cparticle = 0;
@@ -3859,11 +3861,6 @@ void RigSpawner::ProcessShock2(RigDef::Shock2 & def)
 {
     SPAWNER_PROFILE_SCOPED();
 
-    if (! CheckShockLimit(1)) // TODO: remove the limit! See `RigSpawner::CalcMemoryRequirements()` ~ only_a_ptr, 06/2017
-    {
-        return;
-    }
-
     node_t & node_1 = GetNode(def.nodes[0]);
     node_t & node_2 = GetNode(def.nodes[1]);
     float short_bound = def.short_bound;
@@ -3951,11 +3948,6 @@ void RigSpawner::ProcessShock(RigDef::Shock & def)
 {
     SPAWNER_PROFILE_SCOPED();
 
-    if (! CheckShockLimit(1)) // TODO: remove the limit! See `RigSpawner::CalcMemoryRequirements()` ~ only_a_ptr, 06/2017
-    {
-        return;
-    }
-
     node_t & node_1 = GetNode(def.nodes[0]);
     node_t & node_2 = GetNode(def.nodes[1]);
     float short_bound = def.short_bound;
@@ -3972,13 +3964,13 @@ void RigSpawner::ProcessShock(RigDef::Shock & def)
     {
         BITMASK_SET_0(shock_flags, SHOCK_FLAG_NORMAL); /* Not normal anymore */
         BITMASK_SET_1(shock_flags, SHOCK_FLAG_LACTIVE);
-        m_rig->free_active_shock++; /* This has no array associated with it. its just to determine if there are active shocks! */
+        m_rig->has_active_shocks = true;
     }
     if (BITMASK_IS_1(def.options, RigDef::Shock::OPTION_R_ACTIVE_RIGHT))
     {
         BITMASK_SET_0(shock_flags, SHOCK_FLAG_NORMAL); /* Not normal anymore */
         BITMASK_SET_1(shock_flags, SHOCK_FLAG_RACTIVE);
-        m_rig->free_active_shock++; /* This has no array associated with it. its just to determine if there are active shocks! */
+        m_rig->has_active_shocks = true;
     }
     if (BITMASK_IS_1(def.options, RigDef::Shock::OPTION_m_METRIC))
     {
@@ -6448,20 +6440,6 @@ void RigSpawner::ProcessGlobals(RigDef::Globals & def)
 /* -------------------------------------------------------------------------- */
 /* Limits.
 /* -------------------------------------------------------------------------- */
-
-bool RigSpawner::CheckShockLimit(unsigned int count)
-{
-    SPAWNER_PROFILE_SCOPED();
-
-    if ((m_rig->free_shock + count) > MAX_SHOCKS)
-    {
-        std::stringstream msg;
-        msg << "Shock limit (" << MAX_SHOCKS << ") exceeded";
-        AddMessage(Message::TYPE_ERROR, msg.str());
-        return false;
-    }
-    return true;
-}
 
 bool RigSpawner::CheckRotatorLimit(unsigned int count)
 {

source/main/physics/RigSpawner.h
@@ -206,7 +206,8 @@ private:
         ActorMemoryRequirements() { memset(this,0, sizeof(ActorMemoryRequirements)); }
 
         size_t num_nodes;
-        size_t num_beams; // ... more to come ...
+        size_t num_beams;
+        size_t num_shocks; // ... more to come ...
     };
 
 /* -------------------------------------------------------------------------- */
@@ -573,13 +574,6 @@ private:
 /* -------------------------------------------------------------------------- */
 /* Limits.                                                                    */
 /* -------------------------------------------------------------------------- */
-
-    /**
-    * Checks there is still space left in rig_t::shocks array.
-    * @param count Required number of free slots.
-    * @return True if there is space left.
-    */
-    bool CheckShockLimit(unsigned int count);
 
     /**
     * Checks there is still space left in rig_t::hydro array.

commit 5ea9bf40514202bd49f5bea9e685a072f8bff31e

:bug: Fixed segfault in 'calcShocks2'
Probably caused by: https://github.com/RigsOfRods/rigs-of-rods/commit/fe74eeb8e13e8924a19f31b12605ab49c6261154

Fixes: https://github.com/RigsOfRods/rigs-of-rods/issues/1577

source/main/physics/Beam.cpp
@@ -2568,7 +2568,7 @@ void Actor::calcShocks2(int beam_i, Real difftoBeamL, Real& k, Real& d, Real dt,
                     ar_beams[i].shock->trigger_switch_state = 0.0f;
                 if (ar_beams[i].shock->flags & SHOCK_FLAG_TRG_BLOCKER) // this is an enabled blocker and past boundary
                 {
-                    for (int scount = i + 1; scount <= i + ar_beams[i].shock->trigger_cmdshort; scount++) // (cycle blockerbeamID +1) to (blockerbeamID + beams to lock)
+                    for (int scount = i + 1; scount <= std::min(i + ar_beams[i].shock->trigger_cmdshort, ar_num_beams - 1); scount++) // (cycle blockerbeamID +1) to (blockerbeamID + beams to lock)
                     {
                         if (ar_beams[scount].shock && (ar_beams[scount].shock->flags & SHOCK_FLAG_ISTRIGGER)) // don't mess anything up if the user set the number too big
                         {
@@ -2583,7 +2583,7 @@ void Actor::calcShocks2(int beam_i, Real difftoBeamL, Real& k, Real& d, Real dt,
                 }
                 else if (ar_beams[i].shock->flags & SHOCK_FLAG_TRG_BLOCKER_A) // this is an enabled inverted blocker and inside boundary
                 {
-                    for (int scount = i + 1; scount <= i + ar_beams[i].shock->trigger_cmdlong; scount++) // (cycle blockerbeamID + 1) to (blockerbeamID + beams to release)
+                    for (int scount = i + 1; scount <= std::min(i + ar_beams[i].shock->trigger_cmdlong, ar_num_beams - 1); scount++) // (cycle blockerbeamID + 1) to (blockerbeamID + beams to release)
                     {
                         if (ar_beams[scount].shock && (ar_beams[scount].shock->flags & SHOCK_FLAG_ISTRIGGER)) // don't mess anything up if the user set the number too big
                         {
@@ -2742,7 +2742,7 @@ void Actor::calcShocks2(int beam_i, Real difftoBeamL, Real& k, Real& d, Real dt,
                 }
                 else if (ar_beams[i].shock->flags & SHOCK_FLAG_TRG_BLOCKER) // this is an enabled blocker and inside boundary
                 {
-                    for (int scount = i + 1; scount <= i + ar_beams[i].shock->trigger_cmdlong; scount++) // (cycle blockerbeamID + 1) to (blockerbeamID + beams to release)
+                    for (int scount = i + 1; scount <= std::min(i + ar_beams[i].shock->trigger_cmdlong, ar_num_beams - 1); scount++) // (cycle blockerbeamID + 1) to (blockerbeamID + beams to release)
                     {
                         if (ar_beams[scount].shock && (ar_beams[scount].shock->flags & SHOCK_FLAG_ISTRIGGER)) // don't mess anything up if the user set the number too big
                         {
@@ -2757,7 +2757,7 @@ void Actor::calcShocks2(int beam_i, Real difftoBeamL, Real& k, Real& d, Real dt,
                 }
                 else if (ar_beams[i].shock->flags & SHOCK_FLAG_TRG_BLOCKER_A) // this is an enabled reverse blocker and past boundary
                 {
-                    for (int scount = i + 1; scount <= i + ar_beams[i].shock->trigger_cmdshort; scount++) // (cylce blockerbeamID +1) to (blockerbeamID + beams tob lock)
+                    for (int scount = i + 1; scount <= std::min(i + ar_beams[i].shock->trigger_cmdshort, ar_num_beams - 1); scount++) // (cylce blockerbeamID +1) to (blockerbeamID + beams tob lock)
                     {
                         if (ar_beams[scount].shock && (ar_beams[scount].shock->flags & SHOCK_FLAG_ISTRIGGER)) // dont mess anything up if the user set the number too big
                         {