628c0a7e077e17cc1647f0791829c386bc028421
ulteq <ulteq@web.de>
2018-11-16 20:03:17 +0100

    :bug: Fixed segfault on remote actor deletion

--- source/main/physics/BeamFactory.cpp
+++ source/main/physics/BeamFactory.cpp
@@ -399,7 +399,7 @@ void ActorManager::RemoveStreamSource(int sourceid)
 
         if (actor->ar_net_source_id == sourceid)
         {
-            this->DeleteActorInternal(actor);
+            App::GetSimController()->QueueActorRemove(actor);
         }
     }
 }
@@ -478,7 +478,7 @@ void ActorManager::HandleActorStreamData(std::vector<RoR::Networking::recv_packe
             Actor* b = this->GetActorByNetworkLinks(packet.header.source, packet.header.streamid);
             if (b && b->ar_sim_state == Actor::SimState::NETWORKED_OK)
             {
-                this->DeleteActorInternal(b);
+                App::GetSimController()->QueueActorRemove(b);
             }
             auto search = m_stream_mismatches.find(packet.header.source);
             if (search != m_stream_mismatches.end())
