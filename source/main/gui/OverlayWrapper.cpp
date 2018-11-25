RigsOfRods/rigs-of-rods

commit f58a8f6e574be00e2d3fdb129d241571dc654976

:bug: Fixed various issues reported by Coverity Scan

source/main/gui/OverlayWrapper.cpp
@@ -180,6 +180,8 @@ int OverlayWrapper::init()
     MaterialPtr m = MaterialManager::getSingleton().getByName("tracks/pressureneedle_mat");
     if (!m.isNull())
         pressuretexture = m->getTechnique(0)->getPass(0)->getTextureUnitState(0);
+    else
+        pressuretexture = nullptr;
 
     resizePanel(loadOverlayElement("tracks/machineinstructions"));
     resizePanel(loadOverlayElement("tracks/machinehelppanel"));
@@ -532,8 +534,7 @@ bool OverlayWrapper::mouseMoved(const OIS::MouseEvent& _arg)
         if (element)
         {
             res = true;
-            char name[256];
-            strcpy(name, element->getName().c_str());
+            auto name = element->getName().c_str();
             if (!strncmp(name, "tracks/thrust1", 14))
                 player_actor->ar_aeroengines[0]->setThrottle(1.0f - ((mouseY - thrtop - throffset) / thrheight));
             if (!strncmp(name, "tracks/thrust2", 14) && player_actor->ar_num_aeroengines > 1)
@@ -548,8 +549,7 @@ bool OverlayWrapper::mouseMoved(const OIS::MouseEvent& _arg)
         if (element2)
         {
             res = true;
-            char name[256];
-            strcpy(name, element2->getName().c_str());
+            auto name = element->getName().c_str();
             //LogManager::getSingleton().logMessage("element "+element2->getName());
             if (!strncmp(name, "tracks/engstart1", 16))
                 player_actor->ar_aeroengines[0]->flipStart();

source/main/audio/MumbleIntegration.cpp
@@ -42,6 +42,8 @@ MumbleIntegration::MumbleIntegration() : lm(NULL)
 
 MumbleIntegration::~MumbleIntegration()
 {
+    if (lm != nullptr)
+        delete lm;
 }
 
 void MumbleIntegration::initMumble()

source/main/gameplay/AutoPilot.cpp
@@ -30,11 +30,14 @@ using namespace Ogre;
 using namespace RoR;
 
 Autopilot::Autopilot(int actor_id):
-    m_actor_id(actor_id)
+    m_actor_id(actor_id),
+    m_ils_runway_distance(0.0f)
 {
     ref_l = nullptr;
     ref_r = nullptr;
     ref_b = nullptr;
+    ref_c = nullptr;
+    ref_span = 1.0f;
     reset();
 }
 

source/main/gameplay/BeamEngine.cpp
@@ -45,6 +45,7 @@ EngineSim::EngineSim(float _min_rpm, float _max_rpm, float torque, std::vector<f
     , m_cur_gear(0)
     , m_cur_gear_range(0)
     , m_cur_wheel_revolutions(0.0f)
+    , m_ref_wheel_revolutions(0.0f)
     , m_diff_ratio(dratio)
     , m_tcase_ratio(1.0f)
     , m_engine_torque(torque)
@@ -145,8 +146,8 @@ void EngineSim::SetTurboOptions(int type, float tinertiaFactor, int nturbos, flo
     }
     else
     {
-        m_turbo_max_psi = param1; //maxPSI
-        m_max_turbo_rpm = m_turbo_max_psi * 10000;
+        float turbo_max_psi = param1; //maxPSI
+        m_max_turbo_rpm = turbo_max_psi * 10000;
 
         //Duh
         if (param3 == 1)
@@ -594,8 +595,7 @@ void EngineSim::UpdateEngineSim(float dt, int doUpdate)
     if (doUpdate && !m_shifting && !m_post_shifting)
     {
         // gear hack
-        m_abs_velocity = m_actor->ar_nodes[0].Velocity.length();
-        float velocity = m_abs_velocity;
+        float velocity = m_actor->ar_nodes[0].Velocity.length();
 
         Vector3 hdir = m_actor->GetCameraDir();
         if (hdir != Vector3::ZERO)

source/main/gameplay/BeamEngine.h
@@ -147,7 +147,6 @@ private:
 
     // Vehicle
     Actor*         m_actor;
-    float          m_abs_velocity;          //!< Vehicle; current velocity of the vehicle
 
     // Gearbox
     float          m_ref_wheel_revolutions; //!< Gears; estimated wheel revolutions based on current vehicle speed along the longi. axis
@@ -222,7 +221,6 @@ private:
     int            m_max_turbo_rpm;
     float          m_engine_addi_torque[MAXTURBO];
     float          m_turbo_engine_rpm_operation;
-    float          m_turbo_max_psi;
     bool           m_turbo_has_bov;
     float          m_turbo_bov_rpm[MAXTURBO];
     int            m_min_bov_psi;

source/main/gameplay/Character.cpp
@@ -47,12 +47,14 @@ Character::Character(int source, unsigned int streamid, int color_number, bool i
     , m_character_rotation(0.0f)
     , m_character_h_speed(2.0f)
     , m_character_v_speed(0.0f)
+    , m_character_visible(false)
     , m_color_number(color_number)
     , m_anim_time(0.f)
     , m_hide_own_net_label(BSETTING("HideOwnNetLabel", false))
     , m_net_username("")
     , m_is_remote(is_remote)
     , m_source_id(source)
+    , m_survey_map_entity(nullptr)
     , m_stream_id(streamid)
     , m_gfx_character(nullptr)
     , m_driving_anim_length(0.f)

source/main/gameplay/Landusemap.cpp
@@ -35,6 +35,7 @@ using namespace RoR;
 
 Landusemap::Landusemap(String configFilename) :
     data(0)
+    , default_ground_model(nullptr)
     , mapsize(Vector3::ZERO)
 {
     mapsize = App::GetSimTerrain()->getMaxTerrainSize();

source/main/gameplay/Replay.cpp
@@ -32,6 +32,9 @@ Replay::Replay(Actor* actor, int _numFrames)
     numBeams = actor->ar_num_beams;
     numFrames = _numFrames;
 
+    curFrameTime = 0;
+    curOffset = 0;
+
     replayTimer = new Timer();
 
     // DO NOT get memory here, get memory when we use it first time!

commit 544ba837df38b30784bf4bee2f66441eb15ec864

:bug: Fixed typo
Introduced with: https://github.com/RigsOfRods/rigs-of-rods/commit/f58a8f6e574be00e2d3fdb129d241571dc654976

source/main/gui/OverlayWrapper.cpp
@@ -550,7 +550,7 @@ bool OverlayWrapper::mouseMoved(const OIS::MouseEvent& _arg)
         if (element2)
         {
             res = true;
-            auto name = element->getName().c_str();
+            auto name = element2->getName().c_str();
             //LogManager::getSingleton().logMessage("element "+element2->getName());
             if (!strncmp(name, "tracks/engstart1", 16))
                 player_actor->ar_aeroengines[0]->flipStart();