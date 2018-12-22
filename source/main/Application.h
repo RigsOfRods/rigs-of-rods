e739639baf62be43c2265bcb543c94131aadaeb0
ulteq <ulteq@web.de>
2018-12-22 18:12:07 +0100

    :sparkles: Customizable extra mod path

--- source/main/Application.h
+++ source/main/Application.h
@@ -659,6 +659,7 @@ extern GVarPod_APS<bool>       app_async_physics;
 extern GVarPod_APS<int>        app_num_workers;
 extern GVarStr_AP<50>          app_screenshot_format;
 extern GVarStr_A<100>          app_rendersys_override;
+extern GVarStr_A<300>          app_extra_mod_path;
 
 // Simulation
 extern GVarEnum_AP<SimState>   sim_state;
