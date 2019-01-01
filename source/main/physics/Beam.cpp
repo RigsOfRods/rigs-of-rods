RigsOfRods/rigs-of-rods

commit a1a297f076cd445bd65c4c9a1031a4c9bcec3baa

:video_game: Advanced contacter logic

source/main/physics/Beam.cpp
@@ -2262,9 +2262,9 @@ void Actor::CalcAnimators(const int flag_state, float& cstate, int& div, Real ti
 
 void Actor::CalcCabCollisions()
 {
-    for (int i = 0; i < ar_num_contacters; i++)
+    for (int i = 0; i < ar_num_nodes; i++)
     {
-        ar_nodes[ar_contacters[i]].nd_has_mesh_contact = false;
+        ar_nodes[i].nd_has_mesh_contact = false;
     }
     if (m_intra_point_col_detector != nullptr)
     {
@@ -4322,6 +4322,8 @@ Actor::Actor(
     , ar_airbrakes{} // Init array to nullptr
     , ar_cabs{} // Init array to 0
     , ar_num_cabs(0)
+    , ar_num_contactable_nodes(0)
+    , ar_num_contacters(0)
     , ar_screwprops{} // Init array to nullptr
     , ar_num_screwprops(0)
     , ar_num_camera_rails(0)
@@ -4329,8 +4331,6 @@ Actor::Actor(
     , ar_num_aeroengines() // Zero-init
     , ar_pressure_beams() // Zero-init array
     , ar_free_pressure_beam() // Zero-init
-    , ar_contacters() // memset() the array to zero
-    , ar_num_contacters() // zero-init
     , ar_wheels() // array
     , ar_num_wheels() // int
     , m_avg_proped_wheel_radius(0.2f)

source/main/physics/RigSpawner.cpp
@@ -415,6 +415,20 @@ void ActorSpawner::FinalizeRig()
         }
     }
 
+    // Precalculate number of contacters / contactable nodes
+    for (int i = 0; i < m_actor->ar_num_nodes; i++)
+    {
+        if (m_actor->ar_nodes[i].nd_contacter)
+        {
+            m_actor->ar_num_contactable_nodes++;
+            m_actor->ar_num_contacters++;
+        }
+        else if (!m_actor->ar_nodes[i].nd_no_ground_contact)
+        {
+            m_actor->ar_num_contactable_nodes++;
+        }
+    }
+
     //calculate gwps height offset
     //get a starting value
     m_actor->ar_posnode_spawn_height=m_actor->ar_nodes[0].RelPosition.y;
@@ -3230,8 +3244,7 @@ void ActorSpawner::ProcessTrigger(RigDef::Trigger & def)
 void ActorSpawner::ProcessContacter(RigDef::Node::Ref & node_ref)
 {
     unsigned int node_index = GetNodeIndexOrThrow(node_ref);
-    m_actor->ar_contacters[m_actor->ar_num_contacters] = node_index;
-    m_actor->ar_num_contacters++;
+    m_actor->ar_nodes[node_index].nd_contacter = true;
 };
 
 void ActorSpawner::ProcessRotator(RigDef::Rotator & def)
@@ -4048,10 +4061,8 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
         outer_node.volume_coef   = def.node_defaults->volume;
         outer_node.surface_coef  = def.node_defaults->surface;
         outer_node.iswheel       = WHEEL_FLEXBODY;
+        outer_node.nd_contacter  = true;
         AdjustNodeBuoyancy(outer_node, def.node_defaults);
-
-        m_actor->ar_contacters[m_actor->ar_num_contacters] = outer_node.pos;
-        m_actor->ar_num_contacters++;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
 
@@ -4066,10 +4077,8 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
         inner_node.volume_coef   = def.node_defaults->volume;
         inner_node.surface_coef  = def.node_defaults->surface;
         inner_node.iswheel       = WHEEL_FLEXBODY;
+        inner_node.nd_contacter  = true;
         AdjustNodeBuoyancy(inner_node, def.node_defaults);
-
-        m_actor->ar_contacters[m_actor->ar_num_contacters] = inner_node.pos;
-        m_actor->ar_num_contacters++;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
 
@@ -4451,12 +4460,10 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
 
         node_t & outer_node = GetFreeNode();
         InitNode(outer_node, ray_point, node_defaults);
-        outer_node.mass    = wheel_mass / (2.f * num_rays);
-        outer_node.iswheel = (set_param_iswheel) ? WHEEL_DEFAULT : NOWHEEL;
+        outer_node.mass          = wheel_mass / (2.f * num_rays);
+        outer_node.iswheel       = (set_param_iswheel) ? WHEEL_DEFAULT : NOWHEEL;
+        outer_node.nd_contacter  = true;
         AdjustNodeBuoyancy(outer_node, node_defaults);
-
-        m_actor->ar_contacters[m_actor->ar_num_contacters] = outer_node.pos;
-        m_actor->ar_num_contacters++;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
 
@@ -4466,12 +4473,10 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
 
         node_t & inner_node = GetFreeNode();
         InitNode(inner_node, ray_point, node_defaults);
-        inner_node.mass    = wheel_mass / (2.f * num_rays);
-        inner_node.iswheel = (set_param_iswheel) ? WHEEL_DEFAULT : NOWHEEL;
+        inner_node.mass          = wheel_mass / (2.f * num_rays);
+        inner_node.iswheel       = (set_param_iswheel) ? WHEEL_DEFAULT : NOWHEEL;
+        inner_node.nd_contacter  = true;
         AdjustNodeBuoyancy(inner_node, node_defaults);
-
-        m_actor->ar_contacters[m_actor->ar_num_contacters] = inner_node.pos;
-        m_actor->ar_num_contacters++;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
 
@@ -4774,9 +4779,7 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
         outer_node.friction_coef = wheel.wh_width * WHEEL_FRICTION_COEF;
         outer_node.volume_coef   = wheel_2_def.node_defaults->volume;
         outer_node.surface_coef  = wheel_2_def.node_defaults->surface;
-
-        m_actor->ar_contacters[m_actor->ar_num_contacters] = outer_node.pos;
-        m_actor->ar_num_contacters++;
+        outer_node.nd_contacter  = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
 
@@ -4790,9 +4793,7 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
         inner_node.friction_coef = wheel.wh_width * WHEEL_FRICTION_COEF;
         inner_node.volume_coef   = wheel_2_def.node_defaults->volume;
         inner_node.surface_coef  = wheel_2_def.node_defaults->surface;
-
-        m_actor->ar_contacters[m_actor->ar_num_contacters] = inner_node.pos;
-        m_actor->ar_num_contacters++;
+        inner_node.nd_contacter  = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
 
@@ -6269,9 +6270,9 @@ void ActorSpawner::UpdateCollcabContacterNodes()
     for (int i=0; i<m_actor->ar_num_collcabs; i++)
     {
         int tmpv = m_actor->ar_collcabs[i] * 3;
-        m_actor->ar_nodes[m_actor->ar_cabs[tmpv]].nd_contacter = true;
-        m_actor->ar_nodes[m_actor->ar_cabs[tmpv+1]].nd_contacter = true;
-        m_actor->ar_nodes[m_actor->ar_cabs[tmpv+2]].nd_contacter = true;
+        m_actor->ar_nodes[m_actor->ar_cabs[tmpv]].nd_cab_node = true;
+        m_actor->ar_nodes[m_actor->ar_cabs[tmpv+1]].nd_cab_node = true;
+        m_actor->ar_nodes[m_actor->ar_cabs[tmpv+2]].nd_cab_node = true;
     }
 }
 

source/main/physics/collision/PointColDetector.cpp
@@ -35,7 +35,7 @@ void PointColDetector::UpdateIntraPoint(Actor* actor)
     {
         m_actors = {actor};
         m_object_list_size = actor->ar_num_contacters;
-        update_structures_for_contacters();
+        update_structures_for_contacters(false);
     }
 
     m_kdtree[0].ref = NULL;
@@ -55,7 +55,7 @@ void PointColDetector::UpdateInterPoint(Actor* actor, bool ignorestate)
         if (t != actor && (ignorestate || t->ar_update_physics) && actor->ar_bounding_box.intersects(t->ar_bounding_box))
         {
             actors.push_back(t);
-            contacters_size += t->ar_num_contacters;
+            contacters_size += t->ar_num_contactable_nodes;
             if (actor->ar_nodes[0].Velocity.squaredDistance(t->ar_nodes[0].Velocity) > 16)
             {
                 for (int i = 0; i < actor->ar_num_collcabs; i++)
@@ -78,7 +78,7 @@ void PointColDetector::UpdateInterPoint(Actor* actor, bool ignorestate)
     {
         m_actors = actors;
         m_object_list_size = contacters_size;
-        update_structures_for_contacters();
+        update_structures_for_contacters(true);
     }
 
     m_kdtree[0].ref = NULL;
@@ -86,7 +86,7 @@ void PointColDetector::UpdateInterPoint(Actor* actor, bool ignorestate)
     m_kdtree[0].end = -m_object_list_size;
 }
 
-void PointColDetector::update_structures_for_contacters()
+void PointColDetector::update_structures_for_contacters(bool inter)
 {
     m_ref_list.resize(m_object_list_size);
     m_pointid_list.resize(m_object_list_size);
@@ -95,13 +95,16 @@ void PointColDetector::update_structures_for_contacters()
     int refi = 0;
     for (auto actor : m_actors)
     {
-        for (int i = 0; i < actor->ar_num_contacters; i++)
+        for (int i = 0; i < actor->ar_num_nodes; i++)
         {
-            m_pointid_list[refi].actor = actor;
-            m_pointid_list[refi].node_id = actor->ar_contacters[i];
-            m_ref_list[refi].pidref = &m_pointid_list[refi];
-            m_ref_list[refi].point = actor->ar_nodes[actor->ar_contacters[i]].AbsPosition.ptr();
-            refi++;
+            if (actor->ar_nodes[i].nd_contacter || (inter && !actor->ar_nodes[i].nd_no_ground_contact))
+            {
+                m_pointid_list[refi].actor = actor;
+                m_pointid_list[refi].node_id = i;
+                m_ref_list[refi].pidref = &m_pointid_list[refi];
+                m_ref_list[refi].point = actor->ar_nodes[i].AbsPosition.ptr();
+                refi++;
+            }
         }
     }
 

source/main/physics/collision/PointColDetector.h
@@ -68,5 +68,5 @@ private:
     void queryrec(int kdindex, int axis);
     void build_kdtree_incr(int axis, int index);
     void partintwo(const int start, const int median, const int end, const int axis, float& minex, float& maxex);
-    void update_structures_for_contacters();
+    void update_structures_for_contacters(bool inter);
 };

source/main/datatypes/node_t.h
@@ -57,7 +57,8 @@ struct node_t
     ground_model_t* nd_last_collision_gm;    //!< Physics state; last collision 'ground model' (surface definition)
 
     // Bit flags
-    bool            nd_contacter:1;          //!< Attr; This node is part of collision triangle
+    bool            nd_cab_node:1;           //!< Attr; This node is part of collision triangle
+    bool            nd_contacter:1;          //!< Attr; User-defined
     bool            nd_has_ground_contact:1; //!< Physics state
     bool            nd_has_mesh_contact:1;   //!< Physics state
     bool            nd_immovable:1;          //!< Attr; User-defined

source/main/physics/Beam.h
@@ -215,8 +215,8 @@ public:
     std::vector<std::vector<int>>  ar_node_to_beam_connections;
     std::vector<Ogre::AxisAlignedBox>  ar_collision_bounding_boxes; //!< smart bounding boxes, used for determining the state of an actor (every box surrounds only a subset of nodes)
     std::vector<Ogre::AxisAlignedBox>  ar_predicted_coll_bounding_boxes;
-    int               ar_contacters[MAX_CONTACTERS];
-    int               ar_num_contacters;
+    int               ar_num_contactable_nodes; //!< Total number of nodes which can contact ground or cabs
+    int               ar_num_contacters; //!< Total number of nodes which can selfcontact cabs
     wheel_t           ar_wheels[MAX_WHEELS];
     int               ar_num_wheels;
     command_t         ar_command_key[MAX_COMMANDS + 10]; // 0 for safety

source/main/physics/BeamConstants.h
@@ -19,7 +19,6 @@
 
 /* maximum limits */
 static const int   MAX_ACTORS                 = 5000;            //!< maximum number of actors per game session
-static const int   MAX_CONTACTERS             = 2000;            //!< maximum number of contacters per actor
 static const int   MAX_WHEELS                 = 64;              //!< maximum number of wheels per actor
 static const int   MAX_SUBMESHES              = 500;             //!< maximum number of submeshes per actor
 static const int   MAX_TEXCOORDS              = 3000;            //!< maximum number of texture coordinates per actor

source/main/physics/BeamForcesEuler.cpp
@@ -1324,7 +1324,7 @@ void Actor::CalcBeams(bool trigger_hooks)
                     //Break the beam only when it is not connected to a node
                     //which is a part of a collision triangle and has 2 "live" beams or less
                     //connected to it.
-                    if (!((ar_beams[i].p1->nd_contacter && GetNumActiveConnectedBeams(ar_beams[i].p1->pos) < 3) || (ar_beams[i].p2->nd_contacter && GetNumActiveConnectedBeams(ar_beams[i].p2->pos) < 3)))
+                    if (!((ar_beams[i].p1->nd_contacter && GetNumActiveConnectedBeams(ar_beams[i].p1->pos) < 3) || (ar_beams[i].p2->nd_cab_node && GetNumActiveConnectedBeams(ar_beams[i].p2->pos) < 3)))
                     {
                         slen = 0.0f;
                         ar_beams[i].bm_broken = true;
@@ -1499,7 +1499,7 @@ void Actor::CalcBeamsInterActor()
                     //Break the beam only when it is not connected to a node
                     //which is a part of a collision triangle and has 2 "live" beams or less
                     //connected to it.
-                    if (!((ar_inter_beams[i]->p1->nd_contacter && GetNumActiveConnectedBeams(ar_inter_beams[i]->p1->pos) < 3) || (ar_inter_beams[i]->p2->nd_contacter && GetNumActiveConnectedBeams(ar_inter_beams[i]->p2->pos) < 3)))
+                    if (!((ar_inter_beams[i]->p1->nd_cab_node && GetNumActiveConnectedBeams(ar_inter_beams[i]->p1->pos) < 3) || (ar_inter_beams[i]->p2->nd_contacter && GetNumActiveConnectedBeams(ar_inter_beams[i]->p2->pos) < 3)))
                     {
                         slen = 0.0f;
                         ar_inter_beams[i]->bm_broken = true;

commit 57eedc18358b233609183fe473858c3b2e40c3ad

:wrench: Fixed the contacter logic on locked trucks
Prevents unstable submesh collision when submeshed loads are tightly
locked together.

The issue was introduced with: https://github.com/RigsOfRods/rigs-of-rods/commit/a1a297f076cd445bd65c4c9a1031a4c9bcec3baa

source/main/physics/Beam.cpp
@@ -812,10 +812,10 @@ Vector3 Actor::calculateCollisionOffset(Vector3 direction)
     direction.normalise();
 
     if (m_intra_point_col_detector)
-        m_intra_point_col_detector->UpdateIntraPoint(this);
+        m_intra_point_col_detector->UpdateIntraPoint();
 
     if (m_inter_point_col_detector)
-        m_inter_point_col_detector->UpdateInterPoint(this, true);
+        m_inter_point_col_detector->UpdateInterPoint(true);
 
     // collision displacement
     Vector3 collision_offset = Vector3::ZERO;
@@ -2297,7 +2297,7 @@ void Actor::CalcCabCollisions()
     }
     if (m_intra_point_col_detector != nullptr)
     {
-        m_intra_point_col_detector->UpdateIntraPoint(this);
+        m_intra_point_col_detector->UpdateIntraPoint();
         ResolveIntraActorCollisions(PHYSICS_DT,
             *m_intra_point_col_detector,
             ar_num_collcabs,

source/main/physics/BeamFactory.cpp
@@ -1110,7 +1110,7 @@ void ActorManager::UpdatePhysicsSimulation()
                 {
                     auto func = std::function<void()>([this, actor]()
                         {
-                            actor->m_inter_point_col_detector->UpdateInterPoint(actor);
+                            actor->m_inter_point_col_detector->UpdateInterPoint();
                             if (actor->ar_collision_relevant)
                             {
                                 ResolveInterActorCollisions(PHYSICS_DT,

source/main/physics/RigSpawner.cpp
@@ -338,12 +338,12 @@ void ActorSpawner::InitializeRig()
 
     if (!App::sim_no_collisions.GetActive())
     {
-        m_actor->m_inter_point_col_detector = new PointColDetector();
+        m_actor->m_inter_point_col_detector = new PointColDetector(m_actor);
     }
 
     if (!App::sim_no_self_collisions.GetActive())
     {
-        m_actor->m_intra_point_col_detector = new PointColDetector();
+        m_actor->m_intra_point_col_detector = new PointColDetector(m_actor);
     }
 
     m_actor->ar_submesh_ground_model = gEnv->collisions->defaultgm;

source/main/physics/collision/PointColDetector.cpp
@@ -26,16 +26,13 @@
 
 using namespace Ogre;
 
-void PointColDetector::UpdateIntraPoint(Actor* actor)
+void PointColDetector::UpdateIntraPoint()
 {
-    if (!actor)
-        return;
-
-    if (actor->ar_num_contacters != m_object_list_size)
+    if (m_actor->ar_num_contacters != m_object_list_size)
     {
-        m_actors = {actor};
-        m_object_list_size = actor->ar_num_contacters;
-        update_structures_for_contacters(false);
+        m_actors = {m_actor};
+        m_object_list_size = m_actor->ar_num_contacters;
+        update_structures_for_contacters();
     }
 
     m_kdtree[0].ref = NULL;
@@ -43,42 +40,43 @@ void PointColDetector::UpdateIntraPoint(Actor* actor)
     m_kdtree[0].end = -m_object_list_size;
 }
 
-void PointColDetector::UpdateInterPoint(Actor* actor, bool ignorestate)
+void PointColDetector::UpdateInterPoint(bool ignorestate)
 {
-    if (!actor)
-        return;
+    auto links = m_actor->GetAllLinkedActors();
 
-    std::vector<Actor*> actors;
     int contacters_size = 0;
-    for (auto t : RoR::App::GetSimController()->GetActors())
+    std::vector<Actor*> actors;
+    for (auto actor : RoR::App::GetSimController()->GetActors())
     {
-        if (t != actor && (ignorestate || t->ar_update_physics) && actor->ar_bounding_box.intersects(t->ar_bounding_box))
+        if (actor != m_actor && (ignorestate || actor->ar_update_physics) &&
+                m_actor->ar_bounding_box.intersects(actor->ar_bounding_box))
         {
-            actors.push_back(t);
-            contacters_size += t->ar_num_contactable_nodes;
-            if (actor->ar_nodes[0].Velocity.squaredDistance(t->ar_nodes[0].Velocity) > 16)
+            actors.push_back(actor);
+            bool intra_vehicle_collision = std::find(links.begin(), links.end(), actor) != links.end();
+            contacters_size += intra_vehicle_collision ? m_actor->ar_num_contacters : actor->ar_num_contactable_nodes;
+            if (m_actor->ar_nodes[0].Velocity.squaredDistance(actor->ar_nodes[0].Velocity) > 16)
             {
+                for (int i = 0; i < m_actor->ar_num_collcabs; i++)
+                {
+                    m_actor->ar_intra_collcabrate[i].rate = 0;
+                    m_actor->ar_inter_collcabrate[i].rate = 0;
+                }
                 for (int i = 0; i < actor->ar_num_collcabs; i++)
                 {
                     actor->ar_intra_collcabrate[i].rate = 0;
                     actor->ar_inter_collcabrate[i].rate = 0;
                 }
-                for (int i = 0; i < t->ar_num_collcabs; i++)
-                {
-                    t->ar_intra_collcabrate[i].rate = 0;
-                    t->ar_inter_collcabrate[i].rate = 0;
-                }
             }
         }
     }
 
-    actor->ar_collision_relevant = (contacters_size > 0);
+    m_actor->ar_collision_relevant = (contacters_size > 0);
 
     if (actors != m_actors || contacters_size != m_object_list_size)
     {
         m_actors = actors;
         m_object_list_size = contacters_size;
-        update_structures_for_contacters(true);
+        update_structures_for_contacters();
     }
 
     m_kdtree[0].ref = NULL;
@@ -86,18 +84,21 @@ void PointColDetector::UpdateInterPoint(Actor* actor, bool ignorestate)
     m_kdtree[0].end = -m_object_list_size;
 }
 
-void PointColDetector::update_structures_for_contacters(bool inter)
+void PointColDetector::update_structures_for_contacters()
 {
     m_ref_list.resize(m_object_list_size);
     m_pointid_list.resize(m_object_list_size);
+
+    auto links = m_actor->GetAllLinkedActors();
 
     // Insert all contacters into the list of points to consider when building the kdtree
     int refi = 0;
     for (auto actor : m_actors)
     {
+        bool intra_vehicle_collision = (actor == m_actor) || std::find(links.begin(), links.end(), actor) != links.end();
         for (int i = 0; i < actor->ar_num_nodes; i++)
         {
-            if (actor->ar_nodes[i].nd_contacter || (inter && !actor->ar_nodes[i].nd_no_ground_contact))
+            if (actor->ar_nodes[i].nd_contacter || (!intra_vehicle_collision && !actor->ar_nodes[i].nd_no_ground_contact))
             {
                 m_pointid_list[refi].actor = actor;
                 m_pointid_list[refi].node_id = i;

source/main/physics/collision/PointColDetector.h
@@ -33,10 +33,10 @@ public:
 
     std::vector<pointid_t*> hit_list;
 
-    PointColDetector(): m_object_list_size(-1) {};
+    PointColDetector(Actor* actor): m_actor(actor), m_object_list_size(-1) {};
 
-    void UpdateIntraPoint(Actor* actor);
-    void UpdateInterPoint(Actor* actor, bool ignorestate = false);
+    void UpdateIntraPoint();
+    void UpdateInterPoint(bool ignorestate = false);
     void query(const Ogre::Vector3& vec1, const Ogre::Vector3& vec2, const Ogre::Vector3& vec3, const float enlargeBB);
 
 private:
@@ -57,6 +57,7 @@ private:
         int begin;
     };
 
+    Actor*                 m_actor;
     std::vector<Actor*>    m_actors;
     std::vector<refelem_t> m_ref_list;
     std::vector<pointid_t> m_pointid_list;
@@ -68,5 +69,5 @@ private:
     void queryrec(int kdindex, int axis);
     void build_kdtree_incr(int axis, int index);
     void partintwo(const int start, const int median, const int end, const int axis, float& minex, float& maxex);
-    void update_structures_for_contacters(bool inter);
+    void update_structures_for_contacters();
 };