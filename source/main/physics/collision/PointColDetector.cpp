RigsOfRods/rigs-of-rods

commit 84ff51ae9d0cdd5250695d5dea513985c51805f7

:wrench: Fixed the contacter logic on locked trucks
Prevents unstable submesh collision when submeshed loads are tightly
locked together.

The issue was introduced with: https://github.com/RigsOfRods/rigs-of-rods/commit/a1a297f076cd445bd65c4c9a1031a4c9bcec3baa

source/main/physics/collision/PointColDetector.cpp
@@ -26,16 +26,13 @@
 
 using namespace Ogre;
 
-void PointColDetector::UpdateIntraPoint(Actor* actor)
+void PointColDetector::UpdateIntraPoint()
 {
-    if (!actor)
-        return;
-
-    if (actor->ar_num_contacters != m_object_list_size)
+    if (m_actor->ar_num_contacters != m_object_list_size)
     {
-        m_actors = {actor};
-        m_object_list_size = actor->ar_num_contacters;
-        update_structures_for_contacters(false);
+        m_actors = {m_actor};
+        m_object_list_size = m_actor->ar_num_contacters;
+        update_structures_for_contacters();
     }
 
     m_kdtree[0].ref = NULL;
@@ -43,42 +40,43 @@ void PointColDetector::UpdateIntraPoint(Actor* actor)
     m_kdtree[0].end = -m_object_list_size;
 }
 
-void PointColDetector::UpdateInterPoint(Actor* actor, bool ignorestate)
+void PointColDetector::UpdateInterPoint(bool ignorestate)
 {
-    if (!actor)
-        return;
+    auto links = m_actor->GetAllLinkedActors();
 
-    std::vector<Actor*> actors;
     int contacters_size = 0;
-    for (auto t : RoR::App::GetSimController()->GetActors())
+    std::vector<Actor*> actors;
+    for (auto actor : RoR::App::GetSimController()->GetActors())
     {
-        if (t != actor && (ignorestate || t->ar_update_physics) && actor->ar_bounding_box.intersects(t->ar_bounding_box))
+        if (actor != m_actor && (ignorestate || actor->ar_update_physics) &&
+                m_actor->ar_bounding_box.intersects(actor->ar_bounding_box))
         {
-            actors.push_back(t);
-            contacters_size += t->ar_num_contactable_nodes;
-            if (actor->ar_nodes[0].Velocity.squaredDistance(t->ar_nodes[0].Velocity) > 16)
+            actors.push_back(actor);
+            bool intra_vehicle_collision = std::find(links.begin(), links.end(), actor) != links.end();
+            contacters_size += intra_vehicle_collision ? m_actor->ar_num_contacters : actor->ar_num_contactable_nodes;
+            if (m_actor->ar_nodes[0].Velocity.squaredDistance(actor->ar_nodes[0].Velocity) > 16)
             {
+                for (int i = 0; i < m_actor->ar_num_collcabs; i++)
+                {
+                    m_actor->ar_intra_collcabrate[i].rate = 0;
+                    m_actor->ar_inter_collcabrate[i].rate = 0;
+                }
                 for (int i = 0; i < actor->ar_num_collcabs; i++)
                 {
                     actor->ar_intra_collcabrate[i].rate = 0;
                     actor->ar_inter_collcabrate[i].rate = 0;
                 }
-                for (int i = 0; i < t->ar_num_collcabs; i++)
-                {
-                    t->ar_intra_collcabrate[i].rate = 0;
-                    t->ar_inter_collcabrate[i].rate = 0;
-                }
             }
         }
     }
 
-    actor->ar_collision_relevant = (contacters_size > 0);
+    m_actor->ar_collision_relevant = (contacters_size > 0);
 
     if (actors != m_actors || contacters_size != m_object_list_size)
     {
         m_actors = actors;
         m_object_list_size = contacters_size;
-        update_structures_for_contacters(true);
+        update_structures_for_contacters();
     }
 
     m_kdtree[0].ref = NULL;
@@ -86,18 +84,21 @@ void PointColDetector::UpdateInterPoint(Actor* actor, bool ignorestate)
     m_kdtree[0].end = -m_object_list_size;
 }
 
-void PointColDetector::update_structures_for_contacters(bool inter)
+void PointColDetector::update_structures_for_contacters()
 {
     m_ref_list.resize(m_object_list_size);
     m_pointid_list.resize(m_object_list_size);
+
+    auto links = m_actor->GetAllLinkedActors();
 
     // Insert all contacters into the list of points to consider when building the kdtree
     int refi = 0;
     for (auto actor : m_actors)
     {
+        bool intra_vehicle_collision = (actor == m_actor) || std::find(links.begin(), links.end(), actor) != links.end();
         for (int i = 0; i < actor->ar_num_nodes; i++)
         {
-            if (actor->ar_nodes[i].nd_contacter || (inter && !actor->ar_nodes[i].nd_no_ground_contact))
+            if (actor->ar_nodes[i].nd_contacter || (!intra_vehicle_collision && !actor->ar_nodes[i].nd_no_ground_contact))
             {
                 m_pointid_list[refi].actor = actor;
                 m_pointid_list[refi].node_id = i;

source/main/physics/Beam.cpp
@@ -812,10 +812,10 @@ Vector3 Actor::calculateCollisionOffset(Vector3 direction)
     direction.normalise();
 
     if (m_intra_point_col_detector)
-        m_intra_point_col_detector->UpdateIntraPoint(this);
+        m_intra_point_col_detector->UpdateIntraPoint();
 
     if (m_inter_point_col_detector)
-        m_inter_point_col_detector->UpdateInterPoint(this, true);
+        m_inter_point_col_detector->UpdateInterPoint(true);
 
     // collision displacement
     Vector3 collision_offset = Vector3::ZERO;
@@ -2297,7 +2297,7 @@ void Actor::CalcCabCollisions()
     }
     if (m_intra_point_col_detector != nullptr)
     {
-        m_intra_point_col_detector->UpdateIntraPoint(this);
+        m_intra_point_col_detector->UpdateIntraPoint();
         ResolveIntraActorCollisions(PHYSICS_DT,
             *m_intra_point_col_detector,
             ar_num_collcabs,

source/main/physics/BeamFactory.cpp
@@ -1110,7 +1110,7 @@ void ActorManager::UpdatePhysicsSimulation()
                 {
                     auto func = std::function<void()>([this, actor]()
                         {
-                            actor->m_inter_point_col_detector->UpdateInterPoint(actor);
+                            actor->m_inter_point_col_detector->UpdateInterPoint();
                             if (actor->ar_collision_relevant)
                             {
                                 ResolveInterActorCollisions(PHYSICS_DT,

source/main/physics/RigSpawner.cpp
@@ -338,12 +338,12 @@ void ActorSpawner::InitializeRig()
 
     if (!App::sim_no_collisions.GetActive())
     {
-        m_actor->m_inter_point_col_detector = new PointColDetector();
+        m_actor->m_inter_point_col_detector = new PointColDetector(m_actor);
     }
 
     if (!App::sim_no_self_collisions.GetActive())
     {
-        m_actor->m_intra_point_col_detector = new PointColDetector();
+        m_actor->m_intra_point_col_detector = new PointColDetector(m_actor);
     }
 
     m_actor->ar_submesh_ground_model = gEnv->collisions->defaultgm;

source/main/physics/collision/PointColDetector.h
@@ -33,10 +33,10 @@ public:
 
     std::vector<pointid_t*> hit_list;
 
-    PointColDetector(): m_object_list_size(-1) {};
+    PointColDetector(Actor* actor): m_actor(actor), m_object_list_size(-1) {};
 
-    void UpdateIntraPoint(Actor* actor);
-    void UpdateInterPoint(Actor* actor, bool ignorestate = false);
+    void UpdateIntraPoint();
+    void UpdateInterPoint(bool ignorestate = false);
     void query(const Ogre::Vector3& vec1, const Ogre::Vector3& vec2, const Ogre::Vector3& vec3, const float enlargeBB);
 
 private:
@@ -57,6 +57,7 @@ private:
         int begin;
     };
 
+    Actor*                 m_actor;
     std::vector<Actor*>    m_actors;
     std::vector<refelem_t> m_ref_list;
     std::vector<pointid_t> m_pointid_list;
@@ -68,5 +69,5 @@ private:
     void queryrec(int kdindex, int axis);
     void build_kdtree_incr(int axis, int index);
     void partintwo(const int start, const int median, const int end, const int axis, float& minex, float& maxex);
-    void update_structures_for_contacters(bool inter);
+    void update_structures_for_contacters();
 };

commit b528489aaafdffc37f98ec228af6bac63c28a3fd

:bug: Fixed a nasty typo
Introduced with: https://github.com/RigsOfRods/rigs-of-rods/commit/84ff51ae9d0cdd5250695d5dea513985c51805f7

Fixes: https://github.com/RigsOfRods/rigs-of-rods/issues/2023

source/main/physics/collision/PointColDetector.cpp
@@ -53,7 +53,7 @@ void PointColDetector::UpdateInterPoint(bool ignorestate)
         {
             actors.push_back(actor);
             bool intra_vehicle_collision = std::find(links.begin(), links.end(), actor) != links.end();
-            contacters_size += intra_vehicle_collision ? m_actor->ar_num_contacters : actor->ar_num_contactable_nodes;
+            contacters_size += intra_vehicle_collision ? actor->ar_num_contacters : actor->ar_num_contactable_nodes;
             if (m_actor->ar_nodes[0].Velocity.squaredDistance(actor->ar_nodes[0].Velocity) > 16)
             {
                 for (int i = 0; i < m_actor->ar_num_collcabs; i++)