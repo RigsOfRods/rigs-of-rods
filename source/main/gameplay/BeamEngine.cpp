f071dbf520f9d6537451ab13e7227f11fb2f2c3b
ulteq <ulteq@web.de>
2019-01-05 02:24:30 +0100

    :video_game: Take g-force into account when shifting

--- source/main/gameplay/BeamEngine.cpp
+++ source/main/gameplay/BeamEngine.cpp
@@ -746,7 +746,8 @@ void EngineSim::UpdateEngineSim(float dt, int doUpdate)
             if (newGear < m_cur_gear && std::abs(m_cur_wheel_revolutions * (m_gear_ratios[newGear + 1] - m_gear_ratios[m_cur_gear + 1])) > m_one_third_rpm_range / 6.0f ||
                 newGear > m_cur_gear && std::abs(m_cur_wheel_revolutions * (m_gear_ratios[newGear + 1] - m_gear_ratios[m_cur_gear + 1])) > m_one_third_rpm_range / 3.0f)
             {
-                shiftTo(newGear);
+                if (std::abs(m_actor->GetGForcesCur().y) < 0.25f)
+                    shiftTo(newGear);
             }
 
             if (m_accs.size() > 200)
