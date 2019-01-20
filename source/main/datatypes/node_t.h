RigsOfRods/rigs-of-rods

commit 2679b2c3bab874927bbc1ea4a8d2f74566220be0

:bug: Fixed rim node synchronization in multiplayer
All wheel (tyre & rim) nodes now carry the 'iswheel' flag

All rim nodes now carry the 'nd_rim_node' flag

Multiplayer: Rim nodes are now treated similar to tyre nodes

source/main/gfx/GfxActor.cpp
@@ -603,7 +603,7 @@ void RoR::GfxActor::UpdateParticles(float dt_sec)
                 break;
 
             case Collisions::FX_HARD:
-                if (n.iswheel != 0) // This is a wheel => skidmarks and tyre smoke
+                if (n.iswheel != NOWHEEL && !n.nd_rim_node) // This is a wheel => skidmarks and tyre smoke
                 {
                     const float SMOKE_THRESHOLD = 10.f;
                     const float SCREECH_THRESHOLD = 5.f;

source/main/physics/Beam.cpp
@@ -507,6 +507,18 @@ void Actor::CalcNetwork()
             ar_wheels[i].wh_nodes[j * 2 + 1]->AbsPosition = ar_wheels[i].wh_axis_node_1->AbsPosition + uray;
             ar_wheels[i].wh_nodes[j * 2 + 1]->RelPosition = ar_wheels[i].wh_nodes[j * 2 + 1]->AbsPosition - ar_origin;
         }
+        ray.normalise();
+        ray *= ar_wheels[i].wh_rim_radius;
+        for (int j = 0; j < ar_wheels[i].wh_num_rim_nodes / 2; j++)
+        {
+            Vector3 uray = Quaternion(Radian(rp - drp * j), axis) * ray;
+
+            ar_wheels[i].wh_rim_nodes[j * 2 + 0]->AbsPosition = ar_wheels[i].wh_axis_node_0->AbsPosition + uray;
+            ar_wheels[i].wh_rim_nodes[j * 2 + 0]->RelPosition = ar_wheels[i].wh_rim_nodes[j * 2]->AbsPosition - ar_origin;
+
+            ar_wheels[i].wh_rim_nodes[j * 2 + 1]->AbsPosition = ar_wheels[i].wh_axis_node_1->AbsPosition + uray;
+            ar_wheels[i].wh_rim_nodes[j * 2 + 1]->RelPosition = ar_wheels[i].wh_rim_nodes[j * 2 + 1]->AbsPosition - ar_origin;
+        }
     }
     this->UpdateBoundingBoxes();
 
@@ -627,7 +639,7 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
     //reset
     for (int i = 0; i < ar_num_nodes; i++)
     {
-        if (!ar_nodes[i].iswheel)
+        if (!ar_nodes[i].iswheel || ar_nodes[i].nd_rim_node)
         {
             if (!ar_nodes[i].nd_loaded_mass)
             {
@@ -646,9 +658,9 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
         if (ar_beams[i].bm_type != BEAM_VIRTUAL)
         {
             Real half_newlen = ar_beams[i].L / 2.0;
-            if (!(ar_beams[i].p1->iswheel))
+            if (!(ar_beams[i].p1->iswheel && !ar_nodes[i].nd_rim_node))
                 len += half_newlen;
-            if (!(ar_beams[i].p2->iswheel))
+            if (!(ar_beams[i].p2->iswheel && !ar_nodes[i].nd_rim_node))
                 len += half_newlen;
         }
     }
@@ -660,9 +672,9 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
             if (ar_beams[i].bm_type != BEAM_VIRTUAL)
             {
                 Real half_mass = ar_beams[i].L * total / len / 2.0f;
-                if (!(ar_beams[i].p1->iswheel))
+                if (!(ar_beams[i].p1->iswheel && !ar_nodes[i].nd_rim_node))
                     ar_beams[i].p1->mass += half_mass;
-                if (!(ar_beams[i].p2->iswheel))
+                if (!(ar_beams[i].p2->iswheel && !ar_nodes[i].nd_rim_node))
                     ar_beams[i].p2->mass += half_mass;
             }
         }
@@ -685,7 +697,7 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
     {
         //LOG("Nodemass "+TOSTRING(i)+"-"+TOSTRING(ar_nodes[i].mass));
         //for stability
-        if (!ar_nodes[i].iswheel && ar_nodes[i].mass < m_minimass)
+        if ((!ar_nodes[i].iswheel || ar_nodes[i].nd_rim_node) && ar_nodes[i].mass < m_minimass)
         {
             if (App::diag_truck_mass.GetActive())
             {

source/main/physics/BeamFactory.cpp
@@ -241,7 +241,7 @@ void ActorManager::SetupActor(Actor* actor, ActorSpawnRequest rq, std::shared_pt
     actor->m_wheel_node_count = 0;
     for (int i = 0; i < actor->ar_num_nodes; i++)
     {
-        if (actor->ar_nodes[i].iswheel != NOWHEEL)
+        if (actor->ar_nodes[i].iswheel != NOWHEEL && !actor->ar_nodes[i].nd_rim_node)
             actor->m_wheel_node_count++;
     }
 
@@ -249,7 +249,7 @@ void ActorManager::SetupActor(Actor* actor, ActorSpawnRequest rq, std::shared_pt
     actor->m_net_first_wheel_node = actor->ar_num_nodes;
     for (int i = 0; i < actor->ar_num_nodes; i++)
     {
-        if (actor->ar_nodes[i].iswheel == WHEEL_DEFAULT)
+        if (actor->ar_nodes[i].iswheel != NOWHEEL || actor->ar_nodes[i].nd_rim_node)
         {
             actor->m_net_first_wheel_node = i;
             break;

source/main/physics/RigSpawner.cpp
@@ -433,7 +433,7 @@ void ActorSpawner::FinalizeRig()
     
     m_actor->m_has_axles_section = m_actor->m_num_wheel_diffs > 0;
 
-    // Calculate mass of each wheels
+    // Calculate mass of each wheel (without rim)
     for (int i = 0; i < m_actor->ar_num_wheels; i++)
     {
         m_actor->ar_wheels[i].wh_mass = 0.0f;
@@ -4000,7 +4000,6 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
     Ogre::Quaternion rim_ray_rotator = Ogre::Quaternion(Ogre::Degree(-360.f / (def.num_rays * 2)), axis_vector);
 
     // Rim nodes
-    // NOTE: node.iswheel is not used for rim nodes
     for (unsigned int i = 0; i < def.num_rays; i++)
     {
         float node_mass = def.mass / (4.f * def.num_rays);
@@ -4014,6 +4013,7 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
 
         outer_node.mass          = node_mass;
         outer_node.friction_coef = def.node_defaults->friction;
+        outer_node.iswheel       = WHEEL_FLEXBODY;
         outer_node.nd_rim_node   = true;
         AdjustNodeBuoyancy(outer_node, def.node_defaults);
 
@@ -4028,14 +4028,15 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
 
         inner_node.mass          = node_mass;
         inner_node.friction_coef = def.node_defaults->friction;
+        inner_node.iswheel       = WHEEL_FLEXBODY;
         inner_node.nd_rim_node   = true;
         AdjustNodeBuoyancy(inner_node, def.node_defaults);
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
 
         // Wheel object
-        wheel.wh_nodes[i * 2]       = & outer_node;
-        wheel.wh_nodes[(i * 2) + 1] = & inner_node;
+        wheel.wh_rim_nodes[i * 2]       = & outer_node;
+        wheel.wh_rim_nodes[(i * 2) + 1] = & inner_node;
     }
 
     Ogre::Vector3 tyre_ray_vector = axis_vector.perpendicular() * def.tyre_radius;
@@ -4193,9 +4194,11 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
     wheel.wh_braking = this->TranslateBrakingDef(def.braking);
     wheel.wh_propulsed = def.propulsion;
     wheel.wh_num_nodes = 2 * def.num_rays;
+    wheel.wh_num_rim_nodes = wheel.wh_num_nodes;
     wheel.wh_axis_node_0 = axis_node_1;
     wheel.wh_axis_node_1 = axis_node_2;
     wheel.wh_radius = def.tyre_radius;
+    wheel.wh_rim_radius = def.rim_radius;
     wheel.wh_arm_node = this->GetNodePointer(def.reference_arm_node);
 
     if (def.propulsion != RigDef::Wheels::PROPULSION_NONE)
@@ -4403,7 +4406,6 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
     RigDef::Wheels::Braking braking,
     std::shared_ptr<RigDef::NodeDefaults> node_defaults,
     float wheel_mass,
-    bool set_param_iswheel, /* Default: true */
     float wheel_width       /* Default: -1.f */
 )
 {
@@ -4457,7 +4459,7 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
         node_t & outer_node = GetFreeNode();
         InitNode(outer_node, ray_point, node_defaults);
         outer_node.mass          = wheel_mass / (2.f * num_rays);
-        outer_node.iswheel       = (set_param_iswheel) ? WHEEL_DEFAULT : NOWHEEL;
+        outer_node.iswheel       = WHEEL_DEFAULT;
         outer_node.nd_contacter  = true;
         AdjustNodeBuoyancy(outer_node, node_defaults);
 
@@ -4470,7 +4472,7 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
         node_t & inner_node = GetFreeNode();
         InitNode(inner_node, ray_point, node_defaults);
         inner_node.mass          = wheel_mass / (2.f * num_rays);
-        inner_node.iswheel       = (set_param_iswheel) ? WHEEL_DEFAULT : NOWHEEL;
+        inner_node.iswheel       = WHEEL_DEFAULT;
         inner_node.nd_contacter  = true;
         AdjustNodeBuoyancy(inner_node, node_defaults);
 
@@ -4610,7 +4612,6 @@ unsigned int ActorSpawner::AddWheel(RigDef::Wheel & wheel_def)
         wheel_def.braking,
         wheel_def.node_defaults,
         wheel_def.mass,
-        true,
         -1.f // Set width to axis length (width in definition is ignored)
     );
 
@@ -4697,26 +4698,28 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
         /* Outer ring */
         Ogre::Vector3 ray_point = axis_node_1->RelPosition + rim_ray_vector;
 
-        node_t & outer_node = GetFreeNode();
+        node_t & outer_node    = GetFreeNode();
         InitNode(outer_node, ray_point, wheel_2_def.node_defaults);
-        outer_node.mass    = node_mass;
-        outer_node.iswheel = WHEEL_2;
+        outer_node.mass        = node_mass;
+        outer_node.iswheel     = WHEEL_2;
+        outer_node.nd_rim_node = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
 
         /* Inner ring */
         ray_point = axis_node_2->RelPosition + rim_ray_vector;
 
-        node_t & inner_node = GetFreeNode();
+        node_t & inner_node    = GetFreeNode();
         InitNode(inner_node, ray_point, wheel_2_def.node_defaults);
-        inner_node.mass    = node_mass;
-        inner_node.iswheel = WHEEL_2;
+        inner_node.mass        = node_mass;
+        inner_node.iswheel     = WHEEL_2;
+        inner_node.nd_rim_node = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
 
         /* Wheel object */
-        wheel.wh_nodes[i * 2] = & outer_node;
-        wheel.wh_nodes[(i * 2) + 1] = & inner_node;
+        wheel.wh_rim_nodes[i * 2] = & outer_node;
+        wheel.wh_rim_nodes[(i * 2) + 1] = & inner_node;
 
         rim_ray_vector = rim_ray_rotator * rim_ray_vector;
     }
@@ -4836,9 +4839,11 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
     wheel.wh_braking       = this->TranslateBrakingDef(wheel_2_def.braking);
     wheel.wh_propulsed     = wheel_2_def.propulsion;
     wheel.wh_num_nodes     = 2 * wheel_2_def.num_rays;
+    wheel.wh_num_rim_nodes = wheel.wh_num_nodes;
     wheel.wh_axis_node_0   = axis_node_1;
     wheel.wh_axis_node_1   = axis_node_2;
     wheel.wh_radius        = wheel_2_def.tyre_radius;
+    wheel.wh_rim_radius    = wheel_2_def.rim_radius;
     wheel.wh_arm_node      = this->GetNodePointer(wheel_2_def.reference_arm_node);
 
     if (wheel_2_def.propulsion != RigDef::Wheels::PROPULSION_NONE)

source/main/physics/collision/DynamicCollisions.cpp
@@ -232,7 +232,7 @@ void ResolveIntraActorCollisions(const float dt, PointColDetector &intraPointCD,
                 const auto hitnode = &nodes[h->node_id];
 
                 //ignore wheel/chassis self contact
-                if (hitnode->iswheel) continue;
+                if (hitnode->iswheel && !hitnode->nd_rim_node) continue;
                 if (no == hitnode || na == hitnode || nb == hitnode) continue;
 
                 // transform point to triangle local coordinates

source/main/datatypes/wheel_t.h
@@ -39,6 +39,8 @@ struct wheel_t
 
     int         wh_num_nodes;
     node_t*     wh_nodes[50];             // TODO: remove limit, make this dyn-allocated ~ only_a_ptr, 08/2017
+    int         wh_num_rim_nodes;
+    node_t*     wh_rim_nodes[50];         // TODO: remove limit, make this dyn-allocated ~ only_a_ptr, 08/2017
     BrakeCombo  wh_braking;
     node_t*     wh_arm_node;
     node_t*     wh_near_attach_node;
@@ -46,6 +48,7 @@ struct wheel_t
     node_t*     wh_axis_node_1;
     int         wh_propulsed;             // TODO: add enum ~ only_a_ptr, 08/2017
     Ogre::Real  wh_radius;
+    Ogre::Real  wh_rim_radius;
     Ogre::Real  wh_speed;             //<! Current wheel speed in m/s
     Ogre::Real  wh_avg_speed;         //<! Internal physics state; Do not read from this
     Ogre::Real  wh_alb_coef;          //<! Sim state; Current anti-lock  brake modulation ratio

source/main/physics/RigSpawner.h
@@ -978,7 +978,6 @@ private:
         RigDef::Wheels::Braking braking,
         std::shared_ptr<RigDef::NodeDefaults> node_defaults,
         float wheel_mass,
-        bool set_param_iswheel = true,
         float wheel_width = -1.f
     );
 

commit 6dac60093f2952743fafe2601825322f913c64c0

:bug: Fixed the wheel node mass calculation
The bug was introduced with: https://github.com/RigsOfRods/rigs-of-rods/pull/2078

source/main/datatypes/node_t.h
@@ -46,19 +46,14 @@ struct node_t
     Ogre::Real surface_coef;
     Ogre::Real volume_coef;
 
-    int16_t iswheel;          //!< 0=NOWHEEL, 1=WHEEL_DEFAULT, 2=WHEEL_2  3=WHEEL_FLEXBODY
     int16_t pos;              //!< This node's index in Actor::ar_nodes array.
     int16_t nd_coll_bbox_id;  //!< Optional attribute (-1 = none) - multiple collision bounding boxes defined in truckfile
     int16_t nd_lockgroup;     //!< Optional attribute (-1 = default, 9999 = deny lock) - used in the hook lock logic
 
-    Ogre::Real      nd_avg_collision_slip;   //!< Physics state; average slip velocity across the last few physics frames
-    Ogre::Vector3   nd_last_collision_slip;  //!< Physics state; last collision slip vector
-    Ogre::Vector3   nd_last_collision_force; //!< Physics state; last collision force
-    ground_model_t* nd_last_collision_gm;    //!< Physics state; last collision 'ground model' (surface definition)
-
     // Bit flags
     bool            nd_cab_node:1;           //!< Attr; This node is part of collision triangle
-    bool            nd_rim_node:1;           //!< Attr; This node is part of the rim
+    bool            nd_rim_node:1;           //!< Attr; This node is part of a rim
+    bool            nd_tyre_node:1;          //!< Attr; This node is part of a tyre
     bool            nd_contacter:1;          //!< Attr; User-defined
     bool            nd_contactable:1;        //!< Attr; This node will be treated as contacter on inter truck collisions
     bool            nd_has_ground_contact:1; //!< Physics state
@@ -69,4 +64,9 @@ struct node_t
     bool            nd_override_mass:1;      //!< User defined attr; mass is user-specified rather than calculated (override the calculation)
     bool            nd_under_water:1;        //!< State; GFX hint
     bool            nd_no_mouse_grab:1;      //!< Attr; User-defined
+
+    Ogre::Real      nd_avg_collision_slip;   //!< Physics state; average slip velocity across the last few physics frames
+    Ogre::Vector3   nd_last_collision_slip;  //!< Physics state; last collision slip vector
+    Ogre::Vector3   nd_last_collision_force; //!< Physics state; last collision force
+    ground_model_t* nd_last_collision_gm;    //!< Physics state; last collision 'ground model' (surface definition)
 };

source/main/gfx/GfxActor.cpp
@@ -603,7 +603,7 @@ void RoR::GfxActor::UpdateParticles(float dt_sec)
                 break;
 
             case Collisions::FX_HARD:
-                if (n.iswheel != NOWHEEL && !n.nd_rim_node) // This is a wheel => skidmarks and tyre smoke
+                if (n.nd_tyre_node) // skidmarks and tyre smoke
                 {
                     const float SMOKE_THRESHOLD = 8.f;
                     const float SCREECH_THRESHOLD = 5.f;

source/main/physics/Beam.cpp
@@ -638,7 +638,7 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
     //reset
     for (int i = 0; i < ar_num_nodes; i++)
     {
-        if (!ar_nodes[i].iswheel || ar_nodes[i].nd_rim_node)
+        if (!ar_nodes[i].nd_tyre_node)
         {
             if (!ar_nodes[i].nd_loaded_mass)
             {
@@ -657,9 +657,9 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
         if (ar_beams[i].bm_type != BEAM_VIRTUAL)
         {
             Real half_newlen = ar_beams[i].L / 2.0;
-            if (!(ar_beams[i].p1->iswheel && !ar_nodes[i].nd_rim_node))
+            if (!ar_beams[i].p1->nd_tyre_node)
                 len += half_newlen;
-            if (!(ar_beams[i].p2->iswheel && !ar_nodes[i].nd_rim_node))
+            if (!ar_beams[i].p2->nd_tyre_node)
                 len += half_newlen;
         }
     }
@@ -671,9 +671,9 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
             if (ar_beams[i].bm_type != BEAM_VIRTUAL)
             {
                 Real half_mass = ar_beams[i].L * total / len / 2.0f;
-                if (!(ar_beams[i].p1->iswheel && !ar_nodes[i].nd_rim_node))
+                if (!ar_beams[i].p1->nd_tyre_node)
                     ar_beams[i].p1->mass += half_mass;
-                if (!(ar_beams[i].p2->iswheel && !ar_nodes[i].nd_rim_node))
+                if (!ar_beams[i].p2->nd_tyre_node)
                     ar_beams[i].p2->mass += half_mass;
             }
         }
@@ -696,7 +696,7 @@ void Actor::RecalculateNodeMasses(Real total, bool reCalc)
     {
         //LOG("Nodemass "+TOSTRING(i)+"-"+TOSTRING(ar_nodes[i].mass));
         //for stability
-        if ((!ar_nodes[i].iswheel || ar_nodes[i].nd_rim_node) && ar_nodes[i].mass < m_minimass)
+        if (!ar_nodes[i].nd_tyre_node && ar_nodes[i].mass < m_minimass)
         {
             if (App::diag_truck_mass.GetActive())
             {

source/main/physics/BeamData.h
@@ -183,13 +183,6 @@ enum {
     DEFAULT_DETACHER_GROUP  = 0, // default for detaching beam group
 };
 
-enum {
-    NOWHEEL,
-    WHEEL_DEFAULT,
-    WHEEL_2,
-    WHEEL_FLEXBODY
-};
-
 enum class FlareType: char
 {
     NONE           = 0,

source/main/physics/BeamFactory.cpp
@@ -246,7 +246,7 @@ void ActorManager::SetupActor(Actor* actor, ActorSpawnRequest rq, std::shared_pt
     actor->m_wheel_node_count = 0;
     for (int i = 0; i < actor->ar_num_nodes; i++)
     {
-        if (actor->ar_nodes[i].iswheel != NOWHEEL && !actor->ar_nodes[i].nd_rim_node)
+        if (actor->ar_nodes[i].nd_tyre_node)
             actor->m_wheel_node_count++;
     }
 
@@ -254,7 +254,7 @@ void ActorManager::SetupActor(Actor* actor, ActorSpawnRequest rq, std::shared_pt
     actor->m_net_first_wheel_node = actor->ar_num_nodes;
     for (int i = 0; i < actor->ar_num_nodes; i++)
     {
-        if (actor->ar_nodes[i].iswheel != NOWHEEL || actor->ar_nodes[i].nd_rim_node)
+        if (actor->ar_nodes[i].nd_tyre_node || actor->ar_nodes[i].nd_rim_node)
         {
             actor->m_net_first_wheel_node = i;
             break;

source/main/physics/RigSpawner.cpp
@@ -4013,7 +4013,6 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
 
         outer_node.mass          = node_mass;
         outer_node.friction_coef = def.node_defaults->friction;
-        outer_node.iswheel       = WHEEL_FLEXBODY;
         outer_node.nd_rim_node   = true;
         AdjustNodeBuoyancy(outer_node, def.node_defaults);
 
@@ -4028,7 +4027,6 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
 
         inner_node.mass          = node_mass;
         inner_node.friction_coef = def.node_defaults->friction;
-        inner_node.iswheel       = WHEEL_FLEXBODY;
         inner_node.nd_rim_node   = true;
         AdjustNodeBuoyancy(inner_node, def.node_defaults);
 
@@ -4057,8 +4055,8 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
         outer_node.friction_coef = def.node_defaults->friction;
         outer_node.volume_coef   = def.node_defaults->volume;
         outer_node.surface_coef  = def.node_defaults->surface;
-        outer_node.iswheel       = WHEEL_FLEXBODY;
         outer_node.nd_contacter  = true;
+        outer_node.nd_tyre_node  = true;
         AdjustNodeBuoyancy(outer_node, def.node_defaults);
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
@@ -4073,8 +4071,8 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
         inner_node.friction_coef = def.node_defaults->friction;
         inner_node.volume_coef   = def.node_defaults->volume;
         inner_node.surface_coef  = def.node_defaults->surface;
-        inner_node.iswheel       = WHEEL_FLEXBODY;
         inner_node.nd_contacter  = true;
+        inner_node.nd_tyre_node  = true;
         AdjustNodeBuoyancy(inner_node, def.node_defaults);
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
@@ -4459,8 +4457,8 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
         node_t & outer_node = GetFreeNode();
         InitNode(outer_node, ray_point, node_defaults);
         outer_node.mass          = wheel_mass / (2.f * num_rays);
-        outer_node.iswheel       = WHEEL_DEFAULT;
         outer_node.nd_contacter  = true;
+        outer_node.nd_tyre_node  = true;
         AdjustNodeBuoyancy(outer_node, node_defaults);
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
@@ -4472,8 +4470,8 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
         node_t & inner_node = GetFreeNode();
         InitNode(inner_node, ray_point, node_defaults);
         inner_node.mass          = wheel_mass / (2.f * num_rays);
-        inner_node.iswheel       = WHEEL_DEFAULT;
         inner_node.nd_contacter  = true;
+        inner_node.nd_tyre_node  = true;
         AdjustNodeBuoyancy(inner_node, node_defaults);
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
@@ -4485,9 +4483,9 @@ unsigned int ActorSpawner::BuildWheelObjectAndNodes(
 #ifdef DEBUG_TRUCKPARSER2013
         // TRUCK PARSER 2013 DEBUG
         int modifier = 0;
-        msg << "\nDBG\tN1: index=" << outer_node.pos + modifier << ", iswheel=" << outer_node.iswheel 
+        msg << "\nDBG\tN1: index=" << outer_node.pos + modifier << ", iswheel=" << WHEEL_DEFAULT 
             <<", X=" << outer_node.AbsPosition.x <<", Y=" << outer_node.AbsPosition.y <<", Z=" << outer_node.AbsPosition.z << std::endl
-            << "DBG\tN2: index=" << inner_node.pos + modifier << ", iswheel=" << inner_node.iswheel 
+            << "DBG\tN2: index=" << inner_node.pos + modifier << ", iswheel=" << WHEEL_DEFAULT 
             <<", X=" << inner_node.AbsPosition.x <<", Y=" << inner_node.AbsPosition.y <<", Z=" << inner_node.AbsPosition.z;
         // END
 #endif
@@ -4701,7 +4699,6 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
         node_t & outer_node    = GetFreeNode();
         InitNode(outer_node, ray_point, wheel_2_def.node_defaults);
         outer_node.mass        = node_mass;
-        outer_node.iswheel     = WHEEL_2;
         outer_node.nd_rim_node = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
@@ -4712,7 +4709,6 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
         node_t & inner_node    = GetFreeNode();
         InitNode(inner_node, ray_point, wheel_2_def.node_defaults);
         inner_node.mass        = node_mass;
-        inner_node.iswheel     = WHEEL_2;
         inner_node.nd_rim_node = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
@@ -4737,11 +4733,11 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
         node_t & outer_node = GetFreeNode();
         InitNode(outer_node, ray_point);
         outer_node.mass          = (0.67f * wheel_2_def.mass) / (2.f * wheel_2_def.num_rays);
-        outer_node.iswheel       = WHEEL_2;
         outer_node.friction_coef = wheel.wh_width * WHEEL_FRICTION_COEF;
         outer_node.volume_coef   = wheel_2_def.node_defaults->volume;
         outer_node.surface_coef  = wheel_2_def.node_defaults->surface;
         outer_node.nd_contacter  = true;
+        outer_node.nd_tyre_node  = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(outer_node.pos)));
 
@@ -4751,11 +4747,11 @@ unsigned int ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
         node_t & inner_node = GetFreeNode();
         InitNode(inner_node, ray_point);
         inner_node.mass          = (0.33f * wheel_2_def.mass) / (2.f * wheel_2_def.num_rays);
-        inner_node.iswheel       = WHEEL_2;
         inner_node.friction_coef = wheel.wh_width * WHEEL_FRICTION_COEF;
         inner_node.volume_coef   = wheel_2_def.node_defaults->volume;
         inner_node.surface_coef  = wheel_2_def.node_defaults->surface;
         inner_node.nd_contacter  = true;
+        inner_node.nd_tyre_node  = true;
 
         m_gfx_nodes.push_back(GfxActor::NodeGfx(static_cast<uint16_t>(inner_node.pos)));
 
@@ -5655,7 +5651,6 @@ void ActorSpawner::ProcessNode(RigDef::Node & def)
     node.AbsPosition = node_position; 
     node.RelPosition = node_position - m_actor->ar_origin;
 
-    node.iswheel = NOWHEEL;
     node.friction_coef = def.node_defaults->friction;
     node.volume_coef = def.node_defaults->volume;
     node.surface_coef = def.node_defaults->surface;

source/main/physics/collision/DynamicCollisions.cpp
@@ -232,7 +232,7 @@ void ResolveIntraActorCollisions(const float dt, PointColDetector &intraPointCD,
                 const auto hitnode = &nodes[h->node_id];
 
                 //ignore wheel/chassis self contact
-                if (hitnode->iswheel && !hitnode->nd_rim_node) continue;
+                if (hitnode->nd_tyre_node) continue;
                 if (no == hitnode || na == hitnode || nb == hitnode) continue;
 
                 // transform point to triangle local coordinates