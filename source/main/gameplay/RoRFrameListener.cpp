RigsOfRods/rigs-of-rods

commit 3c0e95bcd2a37c1686374377627a11f45b18addc

:wrench: Server terrain name completion
'./rorserver -terrain f1_testtrack' is enough to set '2af11UID-f1_testtrack.terrn2'

source/main/gameplay/RoRFrameListener.cpp
@@ -2005,10 +2005,16 @@ bool SimController::SetupGameplayLoop()
     // Loading map
     // ============================================================================
 
-    // Terrain name lookup
     if (!App::diag_preset_terrain.IsActiveEmpty())
     {
-        String name = App::diag_preset_terrain.GetActive();
+        App::sim_terrain_name.SetPending(App::diag_preset_terrain.GetActive());
+        App::diag_preset_terrain.SetActive("");
+    }
+
+    // Terrain name lookup
+    if (!App::sim_terrain_name.IsPendingEmpty())
+    {
+        String name = App::sim_terrain_name.GetPending();
         StringUtil::toLowerCase(name);
         for (const auto& entry : *App::GetCacheSystem()->GetEntries())
         {
@@ -2016,7 +2022,7 @@ bool SimController::SetupGameplayLoop()
             StringUtil::toLowerCase(fname);
             if (entry.fext == "terrn2" && fname.find(name) != std::string::npos) 
             {
-                App::diag_preset_terrain.SetActive(entry.fname.c_str());
+                App::sim_terrain_name.SetPending(entry.fname.c_str());
                 break;
             }
         }
@@ -2024,17 +2030,12 @@ bool SimController::SetupGameplayLoop()
 
     if (App::sim_load_savegame.GetActive())
     {
-        if (!App::diag_preset_terrain.IsActiveEmpty())
+        if (!App::sim_terrain_name.IsPendingEmpty())
         {
-            String filename = m_actor_manager.GetQuicksaveFilename(App::diag_preset_terrain.GetActive());
+            String filename = m_actor_manager.GetQuicksaveFilename(App::sim_terrain_name.GetPending());
             App::sim_savegame.SetActive(filename.c_str());
-            App::diag_preset_terrain.SetActive("");
         }
         m_actor_manager.LoadScene(App::sim_savegame.GetActive());
-    } else if (!App::diag_preset_terrain.IsActiveEmpty())
-    {
-        App::sim_terrain_name.SetPending(App::diag_preset_terrain.GetActive());
-        App::diag_preset_terrain.SetActive("");
     } else if (App::sim_terrain_name.IsPendingEmpty())
     {
         CacheEntry* selected_map = RoR::App::GetGuiManager()->GetMainSelector()->GetSelectedEntry();

commit bf345dc952e9269465973d3e2c00a688e1010186

:bug: Fixed bug in the savegame logic
The bug was introduced with: https://github.com/RigsOfRods/rigs-of-rods/commit/3c0e95bcd2a37c1686374377627a11f45b18addc

source/main/gameplay/RoRFrameListener.cpp
@@ -2002,7 +2002,6 @@ bool SimController::SetupGameplayLoop()
     if (!App::diag_preset_terrain.IsActiveEmpty())
     {
         App::sim_terrain_name.SetPending(App::diag_preset_terrain.GetActive());
-        App::diag_preset_terrain.SetActive("");
     }
 
     // Terrain name lookup
@@ -2038,10 +2037,9 @@ bool SimController::SetupGameplayLoop()
         }
     }
 
-
     if (App::sim_load_savegame.GetActive())
     {
-        if (!App::sim_terrain_name.IsPendingEmpty())
+        if (!App::diag_preset_terrain.IsActiveEmpty())
         {
             String filename = m_actor_manager.GetQuicksaveFilename(App::sim_terrain_name.GetPending());
             App::sim_savegame.SetActive(filename.c_str());
@@ -2062,6 +2060,8 @@ bool SimController::SetupGameplayLoop()
         App::GetGuiManager()->SetVisible_LoadingWindow(false);
         return false;
     }
+
+    App::diag_preset_terrain.SetActive("");
 
     if (! this->LoadTerrain())
     {