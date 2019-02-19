RigsOfRods/rigs-of-rods

commit 5aa586c015ebb91cb5792c7a064db8975b752f60

:construction: Project AsyncScene: Flexbodies
First prototype - flexbodies are updated by GfxScene but still managed by Actor.

CODECHANGES:
- FlexBody.h/cpp - read softbody data from simbuffer instead of live physics data.
- BeamFactory.h/cpp - removed flexbody update API
- RoRFrameListener.cpp [class SimController] - Removed all Flexbody updates
- GfxActor.cpp/h - added flexbody update API
- DustManager.cpp [class GfxScene] - Added flexbody updates

source/main/gfx/GfxActor.cpp
@@ -2322,3 +2322,13 @@ void RoR::GfxActor::UpdatePropAnimations(const float dt)
     }
 }
 
+void RoR::GfxActor::UpdateFlexbodies()
+{
+    m_actor->UpdateFlexbodiesPrepare(this);
+}
+
+void RoR::GfxActor::FinishFlexbodyTasks()
+{
+    m_actor->UpdateFlexbodiesFinal();
+}
+

source/main/gfx/GfxActor.h
@@ -254,6 +254,8 @@ public:
     void                      CalculateDriverPos (Ogre::Vector3& out_pos, Ogre::Quaternion& out_rot);
     void                      UpdateWheelVisuals ();
     void                      FinishWheelUpdates ();
+    void                      UpdateFlexbodies   ();
+    void                      FinishFlexbodyTasks();
     void                      SetWheelsVisible   (bool value);
     void                      UpdateDebugView    ();
     void                      CycleDebugViews    ();

source/main/physics/flex/FlexBody.cpp
@@ -25,6 +25,7 @@
 #include "ApproxMath.h"
 #include "BeamData.h"
 #include "FlexFactory.h"
+#include "GfxActor.h"
 #include "RigDef_File.h"
 #include "RigLoadingProfilerControl.h"
 
@@ -51,7 +52,6 @@ FlexBody::FlexBody(
     , m_node_y(ny)
     , m_is_enabled(true)
     , m_has_texture_blend(true)
-    , m_nodes(all_nodes)
     , m_scene_node(nullptr)
     , m_scene_entity(ent)
     , m_has_texture(true)
@@ -68,6 +68,8 @@ FlexBody::FlexBody(
     Vector3 normal = Vector3::UNIT_Y;
     Vector3 position = Vector3::ZERO;
     Quaternion orientation = Quaternion::ZERO;
+
+    node_t* m_nodes = all_nodes;
 
     if (ref >= 0)
     {
@@ -636,45 +638,49 @@ void FlexBody::printMeshInfo(Mesh* mesh)
     }
 }
 
-bool FlexBody::flexitPrepare()
+bool FlexBody::flexitPrepare(RoR::GfxActor* gfx_actor)
 {
     if (!m_is_enabled) return false; // TODO: This is a horrid mechanism! Fix it! ~ only_a_ptr, 02/2017
-    if (m_has_texture_blend) updateBlend();
+    // // if (m_has_texture_blend) updateBlend(); Disabled for {AsyncScene} refactor ~ only_a_ptr, 08/2018
 
     // compute the local center
     Ogre::Vector3 flexit_normal;
 
+    RoR::GfxActor::NodeData* nodes = gfx_actor->GetSimNodeBuffer();
+
     if (m_node_center >= 0)
     {
-        Vector3 diffX = m_nodes[m_node_x].AbsPosition - m_nodes[m_node_center].AbsPosition;
-        Vector3 diffY = m_nodes[m_node_y].AbsPosition - m_nodes[m_node_center].AbsPosition;
+        Vector3 diffX = nodes[m_node_x].AbsPosition - nodes[m_node_center].AbsPosition;
+        Vector3 diffY = nodes[m_node_y].AbsPosition - nodes[m_node_center].AbsPosition;
         flexit_normal = fast_normalise(diffY.crossProduct(diffX));
 
-        m_flexit_center = m_nodes[m_node_center].AbsPosition + m_center_offset.x * diffX + m_center_offset.y * diffY;
+        m_flexit_center = nodes[m_node_center].AbsPosition + m_center_offset.x * diffX + m_center_offset.y * diffY;
         m_flexit_center += m_center_offset.z * flexit_normal;
     }
     else
     {
         flexit_normal = Vector3::UNIT_Y;
-        m_flexit_center = m_nodes[0].AbsPosition;
+        m_flexit_center = nodes[0].AbsPosition;
     }
 
     return true;
-}	
+}
 
-void FlexBody::flexitCompute()
+void FlexBody::flexitCompute(RoR::GfxActor* gfx_actor)
 {
+    RoR::GfxActor::NodeData* nodes = gfx_actor->GetSimNodeBuffer();
+
     for (int i=0; i<(int)m_vertex_count; i++)
     {
-        Vector3 diffX = m_nodes[m_locators[i].nx].AbsPosition - m_nodes[m_locators[i].ref].AbsPosition;
-        Vector3 diffY = m_nodes[m_locators[i].ny].AbsPosition - m_nodes[m_locators[i].ref].AbsPosition;
+        Vector3 diffX = nodes[m_locators[i].nx].AbsPosition - nodes[m_locators[i].ref].AbsPosition;
+        Vector3 diffY = nodes[m_locators[i].ny].AbsPosition - nodes[m_locators[i].ref].AbsPosition;
         Vector3 nCross = fast_normalise(diffX.crossProduct(diffY)); //nCross.normalise();
 
         m_dst_pos[i].x = diffX.x * m_locators[i].coords.x + diffY.x * m_locators[i].coords.y + nCross.x * m_locators[i].coords.z;
         m_dst_pos[i].y = diffX.y * m_locators[i].coords.x + diffY.y * m_locators[i].coords.y + nCross.y * m_locators[i].coords.z;
         m_dst_pos[i].z = diffX.z * m_locators[i].coords.x + diffY.z * m_locators[i].coords.y + nCross.z * m_locators[i].coords.z;
 
-        m_dst_pos[i] += m_nodes[m_locators[i].ref].AbsPosition - m_flexit_center;
+        m_dst_pos[i] += nodes[m_locators[i].ref].AbsPosition - m_flexit_center;
 
         m_dst_normals[i].x = diffX.x * m_src_normals[i].x + diffY.x * m_src_normals[i].y + nCross.x * m_src_normals[i].z;
         m_dst_normals[i].y = diffX.y * m_src_normals[i].x + diffY.y * m_src_normals[i].y + nCross.y * m_src_normals[i].z;
@@ -682,21 +688,6 @@ void FlexBody::flexitCompute()
 
         m_dst_normals[i] = fast_normalise(m_dst_normals[i]);
     }
-#if 0
-    for (int i=0; i<(int)m_vertex_count; i++)
-    {
-        Matrix3 mat;
-        Vector3 diffX = m_nodes[m_locators[i].nx].AbsPosition - m_nodes[m_locators[i].ref].AbsPosition;
-        Vector3 diffY = m_nodes[m_locators[i].ny].AbsPosition - m_nodes[m_locators[i].ref].AbsPosition;
-
-        mat.SetColumn(0, diffX);
-        mat.SetColumn(1, diffY);
-        mat.SetColumn(2, fast_normalise(diffX.crossProduct(diffY))); // Old version: mat.SetColumn(2, m_nodes[loc.nz].AbsPosition-m_nodes[loc.ref].AbsPosition);
-
-        m_dst_pos[i] = mat * m_locators[i].coords + m_nodes[m_locators[i].ref].AbsPosition - m_flexit_center;
-        m_dst_normals[i] = fast_normalise(mat * m_src_normals[i]);
-    }
-#endif
 }
 
 Vector3 FlexBody::flexitFinal()
@@ -749,6 +740,9 @@ void FlexBody::writeBlend()
     }
 }
 
+/* Disabled for {AsyncScene} refactor ~ only_a_ptr, 08/2018
+
+
 void FlexBody::updateBlend() //so easy!
 {
     if (!m_is_enabled) return;
@@ -771,3 +765,4 @@ void FlexBody::updateBlend() //so easy!
     }
     if (changed) writeBlend();
 }
+*/

source/main/physics/flex/FlexBody.h
@@ -39,7 +39,7 @@ namespace RoR
     struct FlexBodyCacheData;
 }
 
-class FlexBody : public Flexable
+class FlexBody // OLD: public Flexable
 {
     friend class RoR::FlexFactory;
     friend class RoR::FlexBodyFileIO;
@@ -63,7 +63,7 @@ public:
 
     void printMeshInfo(Ogre::Mesh* mesh);
     void reset();
-    void updateBlend();
+    // // void updateBlend(); Disabled for {AsyncScene} refactor ~ only_a_ptr, 08/2018
     void writeBlend();
     Ogre::SceneNode *getSceneNode() { return m_scene_node; };
 
@@ -77,15 +77,14 @@ public:
     int getCameraMode() { return m_camera_mode; };
 
     // Flexable
-    bool flexitPrepare();
-    void flexitCompute();
+    bool flexitPrepare(RoR::GfxActor* gfx_actor);
+    void flexitCompute(RoR::GfxActor* gfx_actor);
     Ogre::Vector3 flexitFinal();
 
     void setVisible(bool visible);
 
 private:
 
-    node_t*           m_nodes;
     size_t            m_vertex_count;
     Ogre::Vector3     m_flexit_center; ///< Updated per frame
 

source/main/gameplay/RoRFrameListener.cpp
@@ -302,8 +302,6 @@ void SimController::UpdateInputEvents(float dt)
         RoR::App::GetGuiManager()->HideNotification();
         RoR::App::GetGuiManager()->SetMouseCursorVisibility(RoR::GUIManager::MouseCursorVisibility::HIDDEN);
 
-        m_actor_manager.UpdateFlexbodiesFinal(); // Waits until all flexbody tasks are finished
-
         if (App::app_screenshot_format.GetActive() == "png")
         {
             // add some more data into the image
@@ -826,7 +824,6 @@ void SimController::UpdateInputEvents(float dt)
                         scale *= RoR::App::GetInputEngine()->isKeyDown(OIS::KC_LSHIFT) ? 3.0f : 1.0f;
                         scale *= RoR::App::GetInputEngine()->isKeyDown(OIS::KC_LCONTROL) ? 10.0f : 1.0f;
 
-                        m_player_actor->UpdateFlexbodiesFinal();
                         m_player_actor->displace(translation * scale, rotation * scale);
 
                         m_advanced_vehicle_repair_timer = 0.0f;
@@ -1512,8 +1509,6 @@ void SimController::FinalizeActorSpawning(Actor* local_actor, Actor* prev_actor)
             this->SetPlayerActor(local_actor);
         }
 
-        local_actor->UpdateFlexbodiesPrepare();
-        local_actor->UpdateFlexbodiesFinal();
         local_actor->updateVisual();
     }
 }
@@ -1543,13 +1538,6 @@ void SimController::UpdateSimulation(float dt)
 
     RoR::App::GetInputEngine()->Capture();
     auto s = App::sim_state.GetActive();
-
-    //if (gEnv->collisions) 	printf("> ground model used: %s\n", gEnv->collisions->last_used_ground_model->name);
-    //
-    if ((simRUNNING(s) || simEDITOR(s)) && !simPAUSED(s))
-    {
-        m_actor_manager.UpdateFlexbodiesPrepare(); // Pushes all flexbody tasks into the thread pool 
-    }
 
     if (OutProtocol::getSingletonPtr())
     {
@@ -1644,12 +1632,9 @@ void SimController::UpdateSimulation(float dt)
 
         if (!simPAUSED(s))
         {
-            m_actor_manager.JoinFlexbodyTasks(); // Waits until all flexbody tasks are finished
             m_gfx_scene.BufferSimulationData();
 
             m_actor_manager.UpdateActors(m_player_actor, dt); // *** Start new physics tasks. No reading from Actor N/B beyond this point.
-
-            m_actor_manager.UpdateFlexbodiesFinal(); // Updates the harware buffers
         }
 
         if (simRUNNING(s) && (App::sim_state.GetPending() == SimState::PAUSED))
@@ -2169,8 +2154,6 @@ bool SimController::SetupGameplayLoop()
             Vector3 translation = pos - actor->GetRotationCenter();
             actor->ResetPosition(actor->ar_nodes[0].AbsPosition + Vector3(translation.x, 0.0f, translation.z), true);
 
-            actor->UpdateFlexbodiesPrepare();
-            actor->UpdateFlexbodiesFinal();
             actor->updateVisual();
 
             if (App::diag_preset_veh_enter.GetActive() && actor->ar_num_nodes > 0)

source/main/gfx/DustManager.cpp
@@ -97,6 +97,7 @@ void RoR::GfxScene::UpdateScene(float dt_sec)
     for (GfxActor* gfx_actor: m_live_gfx_actors)
     {
         gfx_actor->UpdateWheelVisuals(); // Push flexwheel tasks to threadpool
+        gfx_actor->UpdateFlexbodies(); // Push flexbody tasks to threadpool
     }
 
     // Var
@@ -238,6 +239,7 @@ void RoR::GfxScene::UpdateScene(float dt_sec)
     for (GfxActor* gfx_actor: m_live_gfx_actors)
     {
         gfx_actor->FinishWheelUpdates();
+        gfx_actor->FinishFlexbodyTasks();
     }
 }
 

source/main/physics/Beam.cpp
@@ -2983,14 +2983,14 @@ void Actor::UpdateSoundSources()
 #endif //OPENAL
 }
 
-void Actor::UpdateFlexbodiesPrepare()
+void Actor::UpdateFlexbodiesPrepare(RoR::GfxActor*    gfx_actor)
 {
     if (gEnv->threadPool)
     {
         m_flexbody_prepare.reset();
         for (int i = 0; i < ar_num_flexbodies; i++)
         {
-            m_flexbody_prepare.set(i, ar_flexbodies[i]->flexitPrepare());
+            m_flexbody_prepare.set(i, ar_flexbodies[i]->flexitPrepare(gfx_actor));
         }
 
         // Push tasks into thread pool
@@ -2998,9 +2998,9 @@ void Actor::UpdateFlexbodiesPrepare()
         {
             if (m_flexbody_prepare[i])
             {
-                auto func = std::function<void()>([this, i]()
+                auto func = std::function<void()>([this, i, gfx_actor]()
                     {
-                        ar_flexbodies[i]->flexitCompute();
+                        ar_flexbodies[i]->flexitCompute(gfx_actor);
                     });
                 auto task_handle = gEnv->threadPool->RunTask(func);
                 m_flexbody_tasks.push_back(task_handle);
@@ -3011,9 +3011,9 @@ void Actor::UpdateFlexbodiesPrepare()
     {
         for (int i = 0; i < ar_num_flexbodies; i++)
         {
-            if (ar_flexbodies[i]->flexitPrepare())
+            if (ar_flexbodies[i]->flexitPrepare(gfx_actor))
             {
-                ar_flexbodies[i]->flexitCompute();
+                ar_flexbodies[i]->flexitCompute(gfx_actor);
                 ar_flexbodies[i]->flexitFinal();
             }
         }

source/main/physics/Beam.h
@@ -136,7 +136,7 @@ public:
     /// - Particles: updates particles (dust, exhausts, custom)
     /// - Display: updates wings; updates props; updates rig-skeleton + cab fade effect; updates debug overlay
     void              updateVisual(float dt=0);
-    void              UpdateFlexbodiesPrepare();
+    void              UpdateFlexbodiesPrepare(RoR::GfxActor*    gfx_actor);
     void              UpdateFlexbodiesFinal();
     void              JoinFlexbodyTasks();                 //!< Waits until all flexbody tasks are finished, but does not update the hardware buffers
     void              UpdateActorNetLabels(float dt=0);    //!< Gfx;

commit 43ca6535274443013dff289c8c4b1887b36b06a8

:bug: Fixed the damage texture blending
They broke in: https://github.com/RigsOfRods/rigs-of-rods/commit/5aa586c015ebb91cb5792c7a064db8975b752f60

source/main/gfx/GfxActor.cpp
@@ -1798,7 +1798,10 @@ void RoR::GfxActor::UpdateSimDataBuffer()
     const int num_nodes = m_actor->ar_num_nodes;
     for (int i = 0; i < num_nodes; ++i)
     {
-        m_simbuf.simbuf_nodes.get()[i].AbsPosition = m_actor->ar_nodes[i].AbsPosition;
+        auto node = m_actor->ar_nodes[i];
+        m_simbuf.simbuf_nodes.get()[i].AbsPosition = node.AbsPosition;
+        m_simbuf.simbuf_nodes.get()[i].nd_has_contact = node.nd_has_ground_contact || node.nd_has_mesh_contact;
+        m_simbuf.simbuf_nodes.get()[i].nd_is_wet = (m_gfx_nodes[i].nx_wet_time_sec != -1.f);
     }
 
     // beams

source/main/gfx/GfxActor.h
@@ -130,8 +130,9 @@ public:
     /// Copy of node simulation state
     struct NodeData
     {
-        // Members intentionally have same names as in `node_t`
         Ogre::Vector3 AbsPosition;
+        bool nd_has_contact:1;
+        bool nd_is_wet:1;
     };
 
     /// Visuals of softbody beam (`beam_t` struct); Partially updated along with SimBuffer

source/main/physics/collision/DynamicCollisions.cpp
@@ -182,6 +182,9 @@ void ResolveInterActorCollisions(const float dt, PointColDetector &interPointCD,
 
                     hitnode->nd_last_collision_gm = &submesh_ground_model;
                     hitnode->nd_has_mesh_contact = true;
+                    na->nd_has_mesh_contact = true;
+                    nb->nd_has_mesh_contact = true;
+                    no->nd_has_mesh_contact = true;
                 }
             }
         }

source/main/physics/flex/FlexBody.cpp
@@ -53,6 +53,7 @@ FlexBody::FlexBody(
     , m_scene_entity(ent)
     , m_shared_buf_num_verts(0)
     , m_has_texture(true)
+    , m_blend_changed(false)
     , m_locators(nullptr)
     , m_src_normals(nullptr)
     , m_dst_normals(nullptr)
@@ -605,7 +606,7 @@ void FlexBody::printMeshInfo(Mesh* mesh)
 
 void FlexBody::ComputeFlexbody()
 {
-    // // if (m_has_texture_blend) updateBlend(); Disabled for {AsyncScene} refactor ~ only_a_ptr, 08/2018
+    if (m_has_texture_blend) updateBlend();
 
     RoR::GfxActor::NodeData* nodes = m_gfx_actor->GetSimNodeBuffer();
 
@@ -666,6 +667,12 @@ void FlexBody::UpdateFlexbodyVertexBuffers()
         npt += m_submesh_vbufs_vertex_counts[i];
     }
 
+    if (m_blend_changed)
+    {
+        writeBlend();
+        m_blend_changed = false;
+    }
+
     m_scene_node->setPosition(m_flexit_center);
 }
 
@@ -694,31 +701,23 @@ void FlexBody::writeBlend()
     }
 }
 
-/* Disabled for {AsyncScene} refactor ~ only_a_ptr, 08/2018
-
-
 void FlexBody::updateBlend() //so easy!
 {
-    bool changed = false;
+    RoR::GfxActor::NodeData* nodes = m_gfx_actor->GetSimNodeBuffer();
     for (int i=0; i<(int)m_vertex_count; i++)
     {
-        // TODO: temporarily disabled during project "AsyncScene" - the GfxActor::NodeData doesn't carry the 'has_contact' flag ~ only_a_ptr, 06/2018
-        node_t *nd = &nodes[m_locators[i].ref];
+        RoR::GfxActor::NodeData *nd = &nodes[m_locators[i].ref];
         ARGB col = m_src_colors[i];
         if (nd->nd_has_contact && !(col&0xFF000000))
         {
             m_src_colors[i]=col|0xFF000000;
-            changed = true;
+            m_blend_changed = true;
         }
-        
-
-        // Disabled while refactoring particles ~ only_a_ptr, 04/2018
-        //if ((nd->wetstate!=DRY) ^ ((col&0x000000FF)>0))
-        //{
-        //    m_src_colors[i]=(col&0xFFFFFF00)+0x000000FF*(nd->wetstate!=DRY);
-        //    changed = true;
-        //}
+        if (nd->nd_is_wet ^ ((col&0x000000FF)>0))
+        {
+            m_src_colors[i]=(col&0xFFFFFF00)+0x000000FF*nd->nd_is_wet;
+            m_blend_changed = true;
+        }
     }
-    if (changed) writeBlend();
 }
-*/
+

source/main/physics/flex/FlexBody.h
@@ -55,7 +55,7 @@ public:
 
     void printMeshInfo(Ogre::Mesh* mesh);
     void reset();
-    // // void updateBlend(); Disabled for {AsyncScene} refactor ~ only_a_ptr, 08/2018
+    void updateBlend();
     void writeBlend();
     Ogre::SceneNode *getSceneNode() { return m_scene_node; };
 
@@ -107,4 +107,5 @@ private:
     bool m_uses_shared_vertex_data;
     bool m_has_texture;
     bool m_has_texture_blend;
+    bool m_blend_changed;
 };