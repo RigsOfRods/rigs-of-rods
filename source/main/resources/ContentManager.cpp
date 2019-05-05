2560e7b157141599c798bc6abdca4f26f467f123
Petr Ohlídal <petr@ohlidal.cz>
2019-05-05 22:19:28 +0200

    :triangular_ruler: Deleted unused code

--- source/main/resources/ContentManager.cpp
+++ source/main/resources/ContentManager.cpp
@@ -146,7 +146,6 @@ void ContentManager::AddResourcePack(ResourcePack const& resource_pack, std::str
 void ContentManager::InitContentManager()
 {
     Ogre::ScriptCompilerManager::getSingleton().setListener(this);
-    Ogre::ResourceGroupManager::getSingleton().addResourceGroupListener(this);
 
     // Initialize "managed materials" first
     //   These are base materials referenced by user content
@@ -310,20 +309,6 @@ bool ContentManager::resourceCollision(Ogre::Resource* resource, Ogre::ResourceM
     RoR::LogFormat("[RoR|ContentManager] Skipping resource with duplicate name: '%s' (origin: '%s')",
         resource->getName().c_str(), resource->getOrigin().c_str());
     return false; // Instruct OGRE to drop the new resource and keep the original.
-}
-
-void ContentManager::scriptParseStarted(const String& scriptName, bool& skipThisScript)
-{
-    assert(m_curr_parsed_script.empty());
-
-    m_curr_parsed_script = scriptName;
-}
-
-void ContentManager::scriptParseEnded(const String& scriptName, bool skipped)
-{
-    assert(scriptName == m_curr_parsed_script);
-
-    m_curr_parsed_script.clear();
 }
 
 /// Workaround for OGRE script compiler not properly checking that material name is not empty.
