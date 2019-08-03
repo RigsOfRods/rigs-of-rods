9ea454beeefe0ef0f34fc2db6340509c60758b24
Edgar <Edgar@AnotherFoxGuy.com>
2019-08-03 17:32:10 +0200

    :wrench: Update PMM to 1.5

--- cmake/DependenciesConfig.cmake
+++ cmake/DependenciesConfig.cmake
@@ -8,7 +8,6 @@ set(ROR_FEAT_TIMING OFF)
 set(ROR_BUILD_SIM ON)
 
 if (USE_PACKAGE_MANAGER)
-
     set(ROR_USE_OPENAL TRUE)
     set(ROR_USE_SOCKETW TRUE)
     set(ROR_USE_PAGED TRUE)
@@ -17,45 +16,35 @@ if (USE_PACKAGE_MANAGER)
     set(ROR_USE_CURL TRUE)
     set(ROR_USE_MOFILEREADER TRUE)
 
-    if (USE_PMM)
-        include(pmm)
+    include(pmm)
+    pmm(CONAN REMOTES ror-dependencies https://api.bintray.com/conan/anotherfoxguy/ror-dependencies BINCRAFTERS
+        CMakeCM ROLLING)
 
-        pmm(CONAN REMOTES ror-dependencies https://api.bintray.com/conan/anotherfoxguy/ror-dependencies BINCRAFTERS)
-
-    else ()
-        include(conan)
-
-        conan_check()
-        conan_add_remote(NAME ror-dependencies URL https://api.bintray.com/conan/anotherfoxguy/ror-dependencies)
-        conan_add_remote(NAME bincrafters URL https://api.bintray.com/conan/bincrafters/public-conan)
-
-        conan_cmake_run(CONANFILE conanfile.txt
-                        BASIC_SETUP CMAKE_TARGETS
-                        BUILD missing
-            )
-
-    endif ()
+    include(cotire)
 
 else (USE_PACKAGE_MANAGER)
+    include(pmm)
+    pmm(CMakeCM ROLLING)
+    include(cotire)
 
-  # components
-  set(ROR_USE_OPENAL "TRUE" CACHE BOOL "use OPENAL")
-  set(ROR_USE_SOCKETW "TRUE" CACHE BOOL "use SOCKETW")
-  set(ROR_USE_PAGED "TRUE" CACHE BOOL "use paged-geometry")
-  set(ROR_USE_CAELUM "TRUE" CACHE BOOL "use caelum sky")
-  set(ROR_USE_ANGELSCRIPT "TRUE" CACHE BOOL "use angelscript")
-  set(ROR_USE_CURL "TRUE" CACHE BOOL "use curl, required for communication with online services")
-  set(ROR_USE_MOFILEREADER "TRUE" CACHE BOOL "use mofilereader")
+    # components
+    set(ROR_USE_OPENAL "TRUE" CACHE BOOL "use OPENAL")
+    set(ROR_USE_SOCKETW "TRUE" CACHE BOOL "use SOCKETW")
+    set(ROR_USE_PAGED "TRUE" CACHE BOOL "use paged-geometry")
+    set(ROR_USE_CAELUM "TRUE" CACHE BOOL "use caelum sky")
+    set(ROR_USE_ANGELSCRIPT "TRUE" CACHE BOOL "use angelscript")
+    set(ROR_USE_CURL "TRUE" CACHE BOOL "use curl, required for communication with online services")
+    set(ROR_USE_MOFILEREADER "TRUE" CACHE BOOL "use mofilereader")
 
-  # find packages
-  find_package(OGRE 1.11 REQUIRED COMPONENTS Bites Overlay Paging RTShaderSystem MeshLodGenerator Terrain)
-  find_package(OpenAL)
-  find_package(OIS REQUIRED)
-  find_package(MyGUI REQUIRED)
-  find_package(SocketW)
-  find_package(AngelScript)
-  find_package(CURL)
-  find_package(Caelum)
-  find_package(MoFileReader)
+    # find packages
+    find_package(OGRE 1.11 REQUIRED COMPONENTS Bites Overlay Paging RTShaderSystem MeshLodGenerator Terrain)
+    find_package(OpenAL)
+    find_package(OIS REQUIRED)
+    find_package(MyGUI REQUIRED)
+    find_package(SocketW)
+    find_package(AngelScript)
+    find_package(CURL)
+    find_package(Caelum)
+    find_package(MoFileReader)
 
 endif (USE_PACKAGE_MANAGER)
