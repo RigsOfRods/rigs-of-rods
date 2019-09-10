7f9b25263ee30fa96d4f889714d3d6535a16847c
only-a-ptr <petr@ohlidal.cz>
2019-09-11 00:50:06 +0200

    :triangular_ruler: Removed dead function declarations

--- source/main/utils/Utils.cpp
+++ source/main/utils/Utils.cpp
@@ -42,6 +42,18 @@
 
 using namespace Ogre;
 
+#ifdef USE_DISCORD_RPC
+void DiscordErrorCallback(int, const char *error)
+{
+    RoR::LogFormat("Discord Error: %s", error);
+}
+
+void DiscordReadyCallback(const DiscordUser *user)
+{
+    RoR::LogFormat("Discord Ready: %s", user->username);
+}
+#endif
+
 void InitDiscord()
 {
 #ifdef USE_DISCORD_RPC
@@ -49,26 +61,14 @@ void InitDiscord()
     {
         DiscordEventHandlers handlers;
         memset(&handlers, 0, sizeof(handlers));
-        handlers.ready = handleDiscordReady;
-        handlers.errored = handleDiscordError;
+        handlers.ready = DiscordReadyCallback;
+        handlers.errored = DiscordErrorCallback;
 
         // Discord_Initialize(const char* applicationId, DiscordEventHandlers* handlers, int autoRegister, const char* optionalSteamId)
         Discord_Initialize("492484203435393035", &handlers, 1, "1234");
     }
 #endif
 }
-
-#ifdef USE_DISCORD_RPC
-void handleDiscordError(int, const char *error)
-{
-    RoR::LogFormat("Discord Error: %s", error);
-}
-
-void handleDiscordReady(const DiscordUser *user)
-{
-    RoR::LogFormat("Discord Ready: %s", user->username);
-}
-#endif
 
 void UpdatePresence()
 {
