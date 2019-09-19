186ff27c39ebd527bc95e5e75bedf6c082a86203
tritonas00 <tritonas00@gmail.com>
2019-09-19 20:34:57 +0300

    reworked notifications panel

--- source/main/gui/panels/GUI_MultiplayerClientList.cpp
+++ source/main/gui/panels/GUI_MultiplayerClientList.cpp
@@ -303,7 +303,7 @@ void MpClientList::update()
     int slotid = 0;
 
     MyGUI::IntSize gui_area = MyGUI::RenderManager::getInstance().getViewSize();
-    int x = gui_area.width - sidebarWidth, y = 30;
+    int x = gui_area.width - sidebarWidth, y = 65;
     mpPanel->setPosition(x, y);
 
     // add local player to first slot always
