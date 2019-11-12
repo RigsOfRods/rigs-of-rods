4a3655ff3b15424db11dfe0e437b7a92c3f180c2
Petr Ohlidal <petr@ohlidal.cz>
2019-11-12 15:05:46 +0100

    :video_game: v0.3x-era netchat is back, using DearIMGUI
    Changes:
    - netchat box enlarged and made invisible, messages aligned to bottom, fixed expiration.
    - ConsoleView refactor: use stuct (for composition) instead of base class - preparation for multi-tab console Window (to be done after OGRE 1.12x upgrade)

--- source/main/gui/panels/GUI_ConsoleView.cpp
+++ source/main/gui/panels/GUI_ConsoleView.cpp
@@ -35,51 +35,65 @@ using namespace Ogre;
 
 void GUI::ConsoleView::DrawConsoleMessages()
 {
-    Console::MsgLockGuard lock(App::GetConsole());
-    const size_t disp_count = std::min(MSG_DISP_LIMIT, lock.messages.size());
+    m_display_list.clear();
+    Console::MsgLockGuard lock(App::GetConsole()); // RAII: Scoped lock
+    const size_t disp_max = std::min(cvw_max_lines, lock.messages.size());
     GUIManager::GuiTheme& theme = App::GetGuiManager()->GetTheme();
     auto disp_endi = lock.messages.end();
-    auto disp_itor = disp_endi - disp_count;
+    auto disp_itor = disp_endi - disp_max;
     for (; disp_itor != disp_endi; ++disp_itor)
     {
         Console::Message const& m = *disp_itor;
         if (this->MessageFilter(m))
         {
-            if (m.cm_net_userid != 0)
-            {
-                RoRnet::UserInfo user;
-                RoR::Networking::GetUserInfo(m.cm_net_userid, user);
-                Ogre::ColourValue col = RoR::Networking::GetPlayerColor(user.colournum);
-                ImGui::TextColored(ImVec4(col.r, col.g, col.b, col.a), "%s: ", user.username);
-                ImGui::SameLine();
-            }
+            m_display_list.push_back(&m);
+        }
+    }
 
-            switch (m.cm_type)
-            {
-            case Console::Console::CONSOLE_TITLE:
-                ImGui::TextColored(theme.highlight_text_color, "%s", m.cm_text.c_str());
-                break;
+    if (cvw_align_bottom)
+    {
+        for (size_t i = m_display_list.size(); i < cvw_max_lines; ++i)
+        {
+            ImGui::NewLine();
+        }
+    }
 
-            case Console::Console::CONSOLE_SYSTEM_ERROR:
-                ImGui::TextColored(theme.error_text_color, "%s", m.cm_text.c_str());
-                break;
+    for (const Console::Message* dm: m_display_list)
+    {
+        if (dm->cm_net_userid != 0)
+        {
+            RoRnet::UserInfo user;
+            RoR::Networking::GetUserInfo(dm->cm_net_userid, user);
+            Ogre::ColourValue col = RoR::Networking::GetPlayerColor(user.colournum);
+            ImGui::TextColored(ImVec4(col.r, col.g, col.b, col.a), "%s: ", user.username);
+            ImGui::SameLine();
+        }
 
-            case Console::CONSOLE_SYSTEM_WARNING:
-                ImGui::TextColored(theme.warning_text_color, "%s", m.cm_text.c_str());
-                break;
+        switch (dm->cm_type)
+        {
+        case Console::Console::CONSOLE_TITLE:
+            ImGui::TextColored(theme.highlight_text_color, "%s", dm->cm_text.c_str());
+            break;
 
-            case Console::Console::CONSOLE_SYSTEM_REPLY:
-                ImGui::TextColored(theme.success_text_color, "%s", m.cm_text.c_str());
-                break;
+        case Console::Console::CONSOLE_SYSTEM_ERROR:
+            ImGui::TextColored(theme.error_text_color, "%s", dm->cm_text.c_str());
+            break;
 
-            case Console::Console::CONSOLE_HELP:
-                ImGui::TextColored(theme.help_text_color, "%s", m.cm_text.c_str());
-                break;
+        case Console::CONSOLE_SYSTEM_WARNING:
+            ImGui::TextColored(theme.warning_text_color, "%s", dm->cm_text.c_str());
+            break;
 
-            default:
-                ImGui::Text("%s", m.cm_text.c_str());
-                break;
-            }
+        case Console::Console::CONSOLE_SYSTEM_REPLY:
+            ImGui::TextColored(theme.success_text_color, "%s", dm->cm_text.c_str());
+            break;
+
+        case Console::Console::CONSOLE_HELP:
+            ImGui::TextColored(theme.help_text_color, "%s", dm->cm_text.c_str());
+            break;
+
+        default:
+            ImGui::Text("%s", dm->cm_text.c_str());
+            break;
         }
     }
 }
@@ -89,16 +103,16 @@ void GUI::ConsoleView::DrawFilteringPopup(const char* name)
     if (ImGui::BeginPopup(name))
     {
         ImGui::TextDisabled(_LC("Console", "By area:"));
-        ImGui::MenuItem(_LC("Console", "Logfile echo"), "", &m_filter_area_echo);
-        ImGui::MenuItem(_LC("Console", "Scripting"),    "", &m_filter_area_script);
-        ImGui::MenuItem(_LC("Console", "Actors"),       "", &m_filter_area_actor);
-        ImGui::MenuItem(_LC("Console", "Terrain"),      "", &m_filter_area_terrn);
+        ImGui::MenuItem(_LC("Console", "Logfile echo"), "", &cvw_filter_area_echo);
+        ImGui::MenuItem(_LC("Console", "Scripting"),    "", &cvw_filter_area_script);
+        ImGui::MenuItem(_LC("Console", "Actors"),       "", &cvw_filter_area_actor);
+        ImGui::MenuItem(_LC("Console", "Terrain"),      "", &cvw_filter_area_terrn);
 
         ImGui::Separator();
         ImGui::TextDisabled(_LC("Console", "By level:"));
-        ImGui::MenuItem(_LC("Console", "Notices"),  "", &m_filter_type_notice);
-        ImGui::MenuItem(_LC("Console", "Warnings"), "", &m_filter_type_warning);
-        ImGui::MenuItem(_LC("Console", "Errors"),   "", &m_filter_type_error);
+        ImGui::MenuItem(_LC("Console", "Notices"),  "", &cvw_filter_type_notice);
+        ImGui::MenuItem(_LC("Console", "Warnings"), "", &cvw_filter_type_warning);
+        ImGui::MenuItem(_LC("Console", "Errors"),   "", &cvw_filter_type_error);
 
         ImGui::EndPopup();
     }
@@ -106,24 +120,24 @@ void GUI::ConsoleView::DrawFilteringPopup(const char* name)
 
 bool GUI::ConsoleView::MessageFilter(Console::Message const& m)
 {
-    const bool area_ok = 
+    const bool area_ok =
         (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_INFO) ||
-        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_LOG    && m_filter_area_echo) ||
-        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_ACTOR  && m_filter_area_actor) ||
-        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_TERRN  && m_filter_area_terrn) ||
-        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_SCRIPT && m_filter_area_script);
+        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_LOG    && cvw_filter_area_echo) ||
+        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_ACTOR  && cvw_filter_area_actor) ||
+        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_TERRN  && cvw_filter_area_terrn) ||
+        (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_SCRIPT && cvw_filter_area_script);
 
     const bool type_ok =
         (m.cm_type == Console::CONSOLE_HELP) ||
         (m.cm_type == Console::CONSOLE_TITLE) ||
         (m.cm_type == Console::CONSOLE_SYSTEM_REPLY) ||
-        (m.cm_type == Console::CONSOLE_SYSTEM_ERROR   && m_filter_type_error) ||
-        (m.cm_type == Console::CONSOLE_SYSTEM_WARNING && m_filter_type_warning) ||
-        (m.cm_type == Console::CONSOLE_SYSTEM_NOTICE  && m_filter_type_notice);
+        (m.cm_type == Console::CONSOLE_SYSTEM_ERROR   && cvw_filter_type_error) ||
+        (m.cm_type == Console::CONSOLE_SYSTEM_WARNING && cvw_filter_type_warning) ||
+        (m.cm_type == Console::CONSOLE_SYSTEM_NOTICE  && cvw_filter_type_notice);
 
     const bool time_ok =
-        m_filter_expired ||
-        m.cm_timestamp <= Ogre::Root::getSingleton().getTimer()->getMilliseconds();
+        (cvw_filter_duration_ms == 0) ||
+        m.cm_timestamp + cvw_filter_duration_ms >= Ogre::Root::getSingleton().getTimer()->getMilliseconds();
 
     return type_ok && area_ok && time_ok;
 }
