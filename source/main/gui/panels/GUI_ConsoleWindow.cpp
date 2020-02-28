6ba08b52e7277570da6df1842e90a488748497ce
tritonas00 <tritonas00@gmail.com>
2020-02-28 18:04:24 +0200

    fixed auto scrolling

--- source/main/gui/panels/GUI_ConsoleWindow.cpp
+++ source/main/gui/panels/GUI_ConsoleWindow.cpp
@@ -69,12 +69,8 @@ void GUI::ConsoleWindow::Draw()
     if (this->GetNewestMsgTime() > m_autoscroll_time) // New message arrived?
     {
         m_autoscroll_time = this->GetNewestMsgTime();
-        if  (m_autoscroll_pos >= 1.f) // Only autoscroll if previous position is rougly at bottom (imgui yields ~1.15 at full bottom)
-        {
-            ImGui::SetScrollHere(1.f); // Bottom of last text item
-        }
+        ImGui::SetScrollFromPosY(9999); // Force to bottom
     }
-    m_autoscroll_pos = ImGui::GetScrollY() / ImGui::GetScrollMaxY();
 
     ImGui::EndChild();
     ImGui::Separator();
