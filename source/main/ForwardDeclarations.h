RigsOfRods/rigs-of-rods

commit e23ea80ad43e7650c0aae5de8a5ed25be18be043

:video_game: Remade FPS box with DearIMGUI
Also cleaned leftovers from notification-box.

source/main/ForwardDeclarations.h
@@ -70,7 +70,6 @@ namespace RoR
         class  MainSelector;
         class  MpClientList;
         class  MultiplayerSelector;
-        class  SimUtils;
         class  TopMenubar;
     }
 } // namespace RoR

source/main/gameplay/RoRFrameListener.cpp
@@ -63,7 +63,6 @@
 #include "GUI_LoadingWindow.h"
 #include "GUI_MainSelector.h"
 #include "GUI_MultiplayerClientList.h"
-#include "GUI_SimUtils.h"
 
 #include "SurveyMapManager.h"
 
@@ -391,7 +390,6 @@ void SimController::UpdateInputEvents(float dt)
         String ssmsg = _L("Screenshot:") + String(" ") + fn_name + fn_suffix;
         LOG(ssmsg);
         RoR::App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE, ssmsg, "camera.png", 10000, false);
-
         m_screenshot_request = false;
     }
 
@@ -399,7 +397,6 @@ void SimController::UpdateInputEvents(float dt)
     {
         // Nasty workaround to avoid calling 'GetRenderWindow()->update()' which overwrites the (ImGUI) skeleton view
         m_screenshot_request = true;
-        RoR::App::GetGuiManager()->HideNotification();
         RoR::App::GetGuiManager()->SetMouseCursorVisibility(RoR::GUIManager::MouseCursorVisibility::HIDDEN);
     }
 
@@ -1467,7 +1464,7 @@ void SimController::UpdateInputEvents(float dt)
 
     if ((simRUNNING(s) || simPAUSED(s) || simEDITOR(s)) && RoR::App::GetInputEngine()->getEventBoolValueBounce(EV_COMMON_TOGGLE_STATS))
     {
-        gui_man->GetSimUtils()->SetFPSBoxVisible(! gui_man->GetSimUtils()->IsFPSBoxVisible());
+        gui_man->SetVisible_SimPerfStats(!gui_man->IsVisible_SimPerfStats());
     }
 
     if (RoR::App::GetInputEngine()->getEventBoolValueBounce(EV_COMMON_TOGGLE_MAT_DEBUG))
@@ -1799,8 +1796,6 @@ void SimController::UpdateSimulation(float dt)
         }
 
         this->UpdateForceFeedback();
-
-        RoR::App::GetGuiManager()->UpdateSimUtils(dt, m_player_actor);
 
         m_scene_mouse.UpdateSimulation();
 

source/main/gui/GUIManager.cpp
@@ -51,7 +51,7 @@
 #include "GUI_MainSelector.h"
 #include "GUI_NodeBeamUtils.h"
 #include "GUI_SimActorStats.h"
-#include "GUI_SimUtils.h"
+#include "GUI_SimPerfStats.h"
 #include "GUI_TextureToolWindow.h"
 #include "GUI_TopMenubar.h"
 #include "GUI_VehicleDescription.h"
@@ -78,7 +78,7 @@ struct GuiManagerImpl
     GUI::GamePauseMenu          panel_GamePauseMenu;
     GUI::GameSettings           panel_GameSettings;
     GUI::SimActorStats          panel_SimActorStats;
-    GUI::SimUtils               panel_SimUtils;
+    GUI::SimPerfStats           panel_SimPerfStats;
     GUI::MessageBoxDialog       panel_MessageBox;
     GUI::MultiplayerSelector    panel_MultiplayerSelector;
     GUI::MainSelector           panel_MainSelector;
@@ -137,6 +137,7 @@ void GUIManager::SetVisible_Console             (bool v) { m_impl->panel_Console
 void GUIManager::SetVisible_GameSettings        (bool v) { m_impl->panel_GameSettings       .SetVisible(v); }
 void GUIManager::SetVisible_NodeBeamUtils       (bool v) { m_impl->panel_NodeBeamUtils      .SetVisible(v); }
 void GUIManager::SetVisible_SimActorStats       (bool v) { m_impl->panel_SimActorStats      .SetVisible(v); }
+void GUIManager::SetVisible_SimPerfStats        (bool v) { m_impl->panel_SimPerfStats       .SetVisible(v); }
 
 bool GUIManager::IsVisible_GameMainMenu         () { return m_impl->panel_GameMainMenu       .IsVisible(); }
 bool GUIManager::IsVisible_GameAbout            () { return m_impl->panel_GameAbout          .IsVisible(); }
@@ -153,6 +154,7 @@ bool GUIManager::IsVisible_GameSettings         () { return m_impl->panel_GameSe
 bool GUIManager::IsVisible_TopMenubar           () { return m_impl->panel_TopMenubar         .IsVisible(); }
 bool GUIManager::IsVisible_NodeBeamUtils        () { return m_impl->panel_NodeBeamUtils      .IsVisible(); }
 bool GUIManager::IsVisible_SimActorStats        () { return m_impl->panel_SimActorStats      .IsVisible(); }
+bool GUIManager::IsVisible_SimPerfStats         () { return m_impl->panel_SimPerfStats       .IsVisible(); }
 
 // GUI GetInstance*()
 GUI::MainSelector*          GUIManager::GetMainSelector()      { return &m_impl->panel_MainSelector        ; }
@@ -162,7 +164,6 @@ GUI::LoadingWindow*         GUIManager::GetLoadingWindow()     { return &m_impl-
 GUI::MpClientList*          GUIManager::GetMpClientList()      { return &m_impl->panel_MpClientList        ; }
 GUI::MultiplayerSelector*   GUIManager::GetMpSelector()        { return &m_impl->panel_MultiplayerSelector ; }
 GUI::FrictionSettings*      GUIManager::GetFrictionSettings()  { return &m_impl->panel_FrictionSettings    ; }
-GUI::SimUtils*              GUIManager::GetSimUtils()          { return &m_impl->panel_SimUtils            ; }
 GUI::TopMenubar*            GUIManager::GetTopMenubar()        { return &m_impl->panel_TopMenubar          ; }
 
 GUIManager::GUIManager() :
@@ -257,7 +258,6 @@ bool GUIManager::frameEnded(const Ogre::FrameEvent& evt)
 
 void GUIManager::DrawSimulationGui(float dt)
 {
-    m_impl->panel_SimUtils.FrameStepSimGui(dt);
     if (App::app_state.GetActive() == AppState::SIMULATION)
     {
         m_impl->panel_TopMenubar.Update();
@@ -310,11 +310,11 @@ void GUIManager::DrawSimGuiBuffered(GfxActor* player_gfx_actor)
     {
         m_impl->panel_ChatBox.Draw();
     }
-}
 
-void GUIManager::HideNotification()
-{
-    m_impl->panel_SimUtils.HideNotificationBox();
+    if (this->IsVisible_SimPerfStats())
+    {
+        m_impl->panel_SimPerfStats.Draw();
+    }
 }
 
 void GUIManager::windowResized(Ogre::RenderWindow* rw)
@@ -359,24 +359,13 @@ void GUIManager::SetSceneManagerForGuiRendering(Ogre::SceneManager* scene_manage
     m_impl->mygui_platform->getRenderManagerPtr()->setSceneManager(scene_manager);
 }
 
-void GUIManager::UpdateSimUtils(float dt, Actor *truck)
-{
-    if (m_impl->panel_SimUtils.IsBaseVisible()) //Better to update only when it's visible.
-    {
-        m_impl->panel_SimUtils.UpdateStats(dt, truck);
-    }
-}
-
 void GUIManager::hideGUI(bool hidden)
 {
     if (hidden)
     {
-        m_impl->panel_SimUtils.HideNotificationBox();
-        m_impl->panel_SimUtils.SetFPSBoxVisible(false);
-        m_impl->panel_SimActorStats.SetVisible(false);
+        m_impl->panel_SimPerfStats.SetVisible(false);
         m_impl->panel_ChatBox.SetVisible(false);
     }
-    m_impl->panel_SimUtils.DisableNotifications(hidden);
 }
 
 void GUIManager::SetMouseCursorVisibility(MouseCursorVisibility visi)
@@ -412,13 +401,12 @@ void GUIManager::ReflectGameState()
         m_impl->panel_TextureToolWindow  .SetVisible(false);
         m_impl->panel_VehicleDescription .SetVisible(false);
         m_impl->panel_SimActorStats      .SetVisible(false);
-        m_impl->panel_SimUtils           .SetBaseVisible(false);
+        m_impl->panel_SimPerfStats       .SetVisible(false);
         m_impl->panel_MpClientList       .SetVisible(mp_state == MpState::CONNECTED);
         return;
     }
     if (app_state == AppState::SIMULATION)
     {
-        m_impl->panel_SimUtils           .SetBaseVisible(true);
         m_impl->panel_GameMainMenu       .SetVisible(false);
         return;
     }

source/main/gui/GUIManager.h
@@ -85,6 +85,7 @@ public:
     void SetVisible_LoadingWindow       (bool visible);
     void SetVisible_Console             (bool visible);
     void SetVisible_SimActorStats       (bool visible);
+    void SetVisible_SimPerfStats        (bool visible);
 
     // GUI IsVisible*()
     bool IsVisible_GameMainMenu         ();
@@ -103,6 +104,7 @@ public:
     bool IsVisible_LoadingWindow        ();
     bool IsVisible_Console              ();
     bool IsVisible_SimActorStats        ();
+    bool IsVisible_SimPerfStats         ();
 
     // GUI GetInstance*()
     GUI::MainSelector* GetMainSelector();
@@ -112,15 +114,12 @@ public:
     GUI::MpClientList* GetMpClientList();
     GUI::MultiplayerSelector* GetMpSelector();
     GUI::FrictionSettings* GetFrictionSettings();
-    GUI::SimUtils* GetSimUtils();
     GUI::TopMenubar* GetTopMenubar();
 
     // GUI manipulation
     void ShowMessageBox(const char* title, const char* text, bool allow_close = true, const char* btn1_text = "OK", const char* btn2_text = nullptr);
     void UnfocusGui();
-    void HideNotification();
 
-    void UpdateSimUtils(float dt, Actor* truck);
     void NewImGuiFrame(float dt);
     void DrawMainMenuGui();
     void DrawSimulationGui(float dt); //!< Touches live data; must be called in sync with sim. thread

resources/mygui/Utils.layout
@@ -1,56 +1,0 @@
-﻿<?xml version="1.0" encoding="UTF-8"?>
-<MyGUI type="Layout" version="3.2.0">
-    <Widget type="ImageBox" skin="ImageBox" position="0 115 1024 795" align="Stretch" layer="Back" name="_Main">
-        <Widget type="ImageBox" skin="ImageBox" position="5 625 260 160" align="Left Bottom" name="fpscounter_box">
-            <Property key="ImageTexture" value="FpsCounter.png"/>
-            <Property key="Visible" value="false"/>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="10 5 155 20" name="cur_fps">
-                <Property key="Caption" value="Current FPS: 150.000"/>
-            </Widget>
-            <Widget type="ScrollBar" skin="SliderV" position="10 25 240 1"/>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="10 35 150 15" name="avg_fps">
-                <Property key="Caption" value="Average FPS:"/>
-                <Property key="FontHeight" value="14"/>
-                <Property key="FontName" value="DefaultSmall"/>
-            </Widget>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="10 50 150 15" name="worst_fps">
-                <Property key="Caption" value="Worst FPS:"/>
-                <Property key="FontHeight" value="14"/>
-                <Property key="FontName" value="DefaultSmall"/>
-            </Widget>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="10 65 150 15" name="best_fps">
-                <Property key="Caption" value="Best FPS:"/>
-                <Property key="FontHeight" value="14"/>
-            </Widget>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="10 80 150 15" name="triangle_count">
-                <Property key="Caption" value="Triangle count:"/>
-                <Property key="FontHeight" value="14"/>
-                <Property key="FontName" value="DefaultSmall"/>
-            </Widget>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="10 95 155 15" name="batch_count">
-                <Property key="Caption" value="Batch count:"/>
-                <Property key="FontHeight" value="14"/>
-                <Property key="FontName" value="DefaultSmall"/>
-            </Widget>
-        </Widget>
-        
-        <Widget type="ImageBox" skin="ImageBox" position="535 5 485 80" align="Top Center" name="notification">
-            <Property key="ImageTexture" value="NotificationBg.png"/>
-            <Property key="Visible" value="false"/>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="5 5 470 20" name="not_title">
-                <Property key="Caption" value="Title"/>
-            </Widget>
-            <Widget type="EditBox" skin="WordWrapEmpty" position="5 25 475 70" name="not_text">
-                <Property key="Caption" value="Text"/>
-                <Property key="FontHeight" value="14"/>
-                <Property key="FontName" value="DefaultSmall"/>
-            </Widget>
-        </Widget>
-    </Widget>
-    <CodeGeneratorSettings>
-        <Property key="IncludeDirectory" value="C:\Users\Max\Documents\GitHub\ror_nextstable_gui\source\main\gui\panels"/>
-        <Property key="PanelName" value="SimUtilsLayout"/>
-        <Property key="PanelNamespace" value="lol"/>
-        <Property key="SourceDirectory" value="C:\Users\Max\Documents\GitHub\ror_nextstable_gui\source\main\gui\panels"/>
-    </CodeGeneratorSettings>
-</MyGUI>

source/main/CMakeLists.txt
@@ -149,8 +149,7 @@ set(SOURCE_FILES
         gui/panels/GUI_MultiplayerClientList.{h,cpp}
         gui/panels/GUI_NodeBeamUtils.{h,cpp}
         gui/panels/GUI_SimActorStats.{h,cpp}
-        gui/panels/GUI_SimUtils.{h,cpp}
-        gui/panels/GUI_SimUtilsLayout.{h,cpp}
+        gui/panels/GUI_SimPerfStats.{h,cpp}
         gui/panels/GUI_VehicleDescription.{h,cpp}
         network/Network.{h,cpp}
         physics/ApproxMath.h

commit 5c06ee9140071795cf89da2d33144e6ce3336a43

:bug: Fixed actor stats not being updated
Introduced in e23ea80ad43e7650c0aae5de8a5ed25be18be043

source/main/ForwardDeclarations.h
@@ -70,6 +70,7 @@ namespace RoR
         class  MainSelector;
         class  MpClientList;
         class  MultiplayerSelector;
+        class  SimActorStats;
         class  SurveyMap;
         class  TopMenubar;
     }

source/main/gameplay/RoRFrameListener.cpp
@@ -35,6 +35,7 @@
 #include "DashBoardManager.h"
 #include "GfxScene.h"
 #include "GUIManager.h"
+#include "GUI_SimActorStats.h"
 #include "GUI_SurveyMap.h"
 #include "ForceFeedback.h"
 #include "InputEngine.h"
@@ -1793,6 +1794,11 @@ void SimController::UpdateSimulation(float dt)
         }
 
         this->UpdateForceFeedback();
+
+        if (m_player_actor)
+        {
+            App::GetGuiManager()->GetSimActorStats()->UpdateStats(dt, m_player_actor);
+        }
 
         m_scene_mouse.UpdateSimulation();
 

source/main/gui/GUIManager.cpp
@@ -155,6 +155,7 @@ GUI::MultiplayerSelector*   GUIManager::GetMpSelector()        { return &m_impl-
 GUI::FrictionSettings*      GUIManager::GetFrictionSettings()  { return &m_impl->panel_FrictionSettings    ; }
 GUI::TopMenubar*            GUIManager::GetTopMenubar()        { return &m_impl->panel_TopMenubar          ; }
 GUI::SurveyMap*             GUIManager::GetSurveyMap()         { return &m_impl->panel_SurveyMap           ; }
+GUI::SimActorStats*         GUIManager::GetSimActorStats()     { return &m_impl->panel_SimActorStats       ; }
 
 GUIManager::GUIManager() :
     m_renderwindow_closed(false),

source/main/gui/GUIManager.h
@@ -120,6 +120,7 @@ public:
     GUI::FrictionSettings* GetFrictionSettings();
     GUI::TopMenubar* GetTopMenubar();
     GUI::SurveyMap* GetSurveyMap();
+    GUI::SimActorStats* GetSimActorStats();
 
     // GUI manipulation
     void ShowMessageBox(const char* title, const char* text, bool allow_close = true, const char* btn1_text = "OK", const char* btn2_text = nullptr);