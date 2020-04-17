787f3a4fb7fff0354685c178bba160c4c7836534
Petr Ohlídal <petr@ohlidal.cz>
2020-04-17 22:55:18 +0200

    :wrench: Console: custom soft-clipped msg rendering
    Scrolling accomplished using ImGui::Dummy()

--- source/main/gui/panels/GUI_ConsoleView.cpp
+++ source/main/gui/panels/GUI_ConsoleView.cpp
@@ -36,48 +36,82 @@ using namespace Ogre;
 
 inline void color2i(ImVec4 v, int&r, int&g, int&b) { r=(int)(v.x*255); g=(int)(v.y*255); b=(int)(v.z*255); }
 
+static const int LINE_BUF_MAX = 5000;
+
 void GUI::ConsoleView::DrawConsoleMessages()
 {
+    // Update pre-filtered message list
     this->UpdateMessages();
 
-    if (cvw_align_bottom)
+    // Gather visible (non-expired) messages
+    const unsigned long curr_timestamp = App::GetConsole()->QueryMessageTimer();
+    m_display_messages.clear();
+    for (Console::Message& m: m_filtered_messages)
     {
-        ImVec2 text_size = ImGui::CalcTextSize("A");
-        for (size_t i = m_filtered_messages.size(); i < cvw_max_lines; ++i)
+        if (cvw_msg_duration_ms == 0 || curr_timestamp <= m.cm_timestamp + cvw_msg_duration_ms)
         {
-            this->NewLine(text_size);
+            m_display_messages.push_back(&m);
         }
     }
 
-    GUIManager::GuiTheme& theme = App::GetGuiManager()->GetTheme();
-    const unsigned long curr_timestamp = App::GetConsole()->QueryMessageTimer();
-    for (const Console::Message& m: m_filtered_messages)
+    // Add a dummy sized as messages to facilitate builtin scrolling
+    float line_h = ImGui::CalcTextSize("").y + (2 * cvw_background_padding.y) + cvw_line_spacing;
+    float dummy_h = line_h * (float)m_display_messages.size();
+    ImGui::Dummy(ImVec2(1, dummy_h));
+
+    int msg_start = 0, msg_count = 0;
+    ImVec2 cursor = ImGui::GetWindowPos();
+    if (ImGui::GetScrollMaxY() < 0)
     {
-        // Skip expired messages
-        if (cvw_msg_duration_ms != 0 && curr_timestamp > m.cm_timestamp + cvw_msg_duration_ms)
-        {
-            continue;
-        }
+        // No scrolling
+        cursor += ImVec2(0, 
+            ImGui::GetWindowHeight() - (float)m_display_messages.size() * (line_h));
+        msg_count = (int)m_display_messages.size();
+    }
+    else
+    {
+        // Scrolling
+        const float scroll_rel = ImGui::GetScrollY()/ImGui::GetScrollMaxY();
+        const float scroll_offset = ((dummy_h - ImGui::GetWindowHeight()) *scroll_rel);
+        msg_start = (int)(scroll_offset/line_h);
+        msg_start = std::max(0, msg_start);
+
+        msg_count = std::min((int)(ImGui::GetWindowHeight() / line_h)+2, // Bias (2) for partially visible messages (1 top, 1 bottom)
+                             (int)m_display_messages.size() - msg_start);
+
+        const float line_offset = scroll_offset/line_h;
+        cursor -= ImVec2(0, (line_offset - (float)(int)line_offset)*line_h);
+    }
+
+    // Draw the messages
+    GUIManager::GuiTheme& theme = App::GetGuiManager()->GetTheme();
+    ImDrawList* drawlist = ImGui::GetWindowDrawList();
+    drawlist->ChannelsSplit(2); // 2 layers: 0=background, 1=text
+    Str<LINE_BUF_MAX> line;
+    for (int i = msg_start; i < msg_start + msg_count; i++)
+    {
+        const Console::Message& m = *m_display_messages[i];
 
         // Draw icons based on filters
+        Ogre::TexturePtr icon;
         if (m.cm_area == Console::MessageArea::CONSOLE_MSGTYPE_SCRIPT)
         {
-            this->DrawIcon(Ogre::TextureManager::getSingleton().load("script.png", "IconsRG"));
+            icon = Ogre::TextureManager::getSingleton().load("script.png", "IconsRG");
         }
         else if (m.cm_type == Console::CONSOLE_SYSTEM_NOTICE)
         {
-            this->DrawIcon(Ogre::TextureManager::getSingleton().load("information.png", "IconsRG"));
+            icon = Ogre::TextureManager::getSingleton().load("information.png", "IconsRG");
         }
         else if (m.cm_type == Console::CONSOLE_SYSTEM_WARNING)
         {
-            this->DrawIcon(Ogre::TextureManager::getSingleton().load("error.png", "IconsRG"));
+            icon = Ogre::TextureManager::getSingleton().load("error.png", "IconsRG");
         }
         else if (m.cm_type == Console::CONSOLE_SYSTEM_NETCHAT)
         {
-            this->DrawIcon(Ogre::TextureManager::getSingleton().load("comment.png", "IconsRG"));
+            icon = Ogre::TextureManager::getSingleton().load("comment.png", "IconsRG");
         }
 
-        std::string line = m.cm_text;
+        // Add colored multiplayer username
         RoRnet::UserInfo user;
         if (m.cm_net_userid != 0 && RoR::Networking::GetAnyUserInfo((int)m.cm_net_userid, user))
         {
@@ -86,36 +120,32 @@ void GUI::ConsoleView::DrawConsoleMessages()
             int r,g,b;
             color2i(ImVec4(col.r, col.g, col.b, col.a), r,g,b);
             snprintf(prefix, 400, "#%02x%02x%02x%s: #000000", r, g, b, user.username);
-            line = std::string(prefix) + line;
-        }        
 
+            line.Clear();
+            line << prefix << m.cm_text;
+        }
+        else
+        {
+            line = m.cm_text;
+        }
+
+        // Colorize text by type
+        ImVec4 base_color = ImGui::GetStyle().Colors[ImGuiCol_Text];
         switch (m.cm_type)
         {
-        case Console::Console::CONSOLE_TITLE:
-            this->DrawColorMarkedText(theme.highlight_text_color, line);
-            break;
-
-        case Console::Console::CONSOLE_SYSTEM_ERROR:
-            this->DrawColorMarkedText(theme.error_text_color, line);
-            break;
-
-        case Console::CONSOLE_SYSTEM_WARNING:
-            this->DrawColorMarkedText(theme.warning_text_color, line);
-            break;
-
-        case Console::Console::CONSOLE_SYSTEM_REPLY:
-            this->DrawColorMarkedText(theme.success_text_color, line);
-            break;
-
-        case Console::Console::CONSOLE_HELP:
-            this->DrawColorMarkedText(theme.help_text_color, line);
-            break;
-
-        default:
-            this->DrawColorMarkedText(ImGui::GetStyle().Colors[ImGuiCol_Text], line);
-            break;
+        case Console::CONSOLE_TITLE:          base_color = theme.highlight_text_color;  break;
+        case Console::CONSOLE_SYSTEM_ERROR:   base_color = theme.error_text_color;      break;
+        case Console::CONSOLE_SYSTEM_WARNING: base_color = theme.warning_text_color;    break;
+        case Console::CONSOLE_SYSTEM_REPLY:   base_color = theme.success_text_color;    break;
+        case Console::CONSOLE_HELP:           base_color = theme.help_text_color;       break;
+        default:;
         }
+
+        this->DrawColorMarkedText(cursor, icon, base_color, line.ToCStr());
+        cursor += ImVec2(0.f, line_h);
     }
+
+    drawlist->ChannelsMerge();
 }
 
 void GUI::ConsoleView::DrawFilteringOptions()
@@ -156,16 +186,28 @@ bool GUI::ConsoleView::MessageFilter(Console::Message const& m)
     return type_ok && area_ok;
 }
 
-void GUI::ConsoleView::DrawColorMarkedText(ImVec4 default_color, std::string const& line)
+void GUI::ConsoleView::DrawColorMarkedText(ImVec2 bg_cursor, Ogre::TexturePtr icon, ImVec4 default_color, std::string const& line)
 {
     ImDrawList* drawlist = ImGui::GetWindowDrawList();
-    drawlist->ChannelsSplit(2); // 2 layers
-    ImVec2 bg_cursor = ImGui::GetCursorScreenPos();
+
+    // Print icon
+    drawlist->ChannelsSetCurrent(1); // Text layer
     ImVec2 text_cursor = bg_cursor + cvw_background_padding;
-    ImVec2 total_text_size;
+    ImVec2 total_text_size(0,0);
+    float text_h = ImGui::CalcTextSize("").y;
+    if (cvw_enable_icons && icon)
+    {
+        ImVec2 icon_size(icon->getWidth(), icon->getHeight());
+        ImVec2 tl = ImVec2(text_cursor.x, text_cursor.y + (text_h / 2) - (icon_size.y / 2));
+        ImVec2 br = tl + icon_size;
+        drawlist->AddImage(reinterpret_cast<ImTextureID>(icon->getHandle()), tl, br);
+        const float ICON_GAP = 8;
+        total_text_size += ImVec2(icon_size.x + ICON_GAP, text_h);
+        text_cursor.x += icon_size.x + ICON_GAP;
+    }
 
     // Print colored line segments
-    drawlist->ChannelsSetCurrent(1); // Text layer
+
     int r,g,b;
     color2i(default_color, r,g,b);
     std::smatch color_match;
@@ -176,9 +218,9 @@ void GUI::ConsoleView::DrawColorMarkedText(ImVec4 default_color, std::string con
         std::string::const_iterator seg_end = color_match[0].first;
         if (seg_start != seg_end)
         {
-            std::string text(seg_start, seg_end); // TODO: optimize!
-            ImVec2 text_size = ImGui::CalcTextSize(text.c_str());           
-            drawlist->AddText(text_cursor, ImColor(r,g,b), text.c_str());
+            Str<LINE_BUF_MAX> seg_text(seg_start, seg_end);
+            ImVec2 text_size = ImGui::CalcTextSize(seg_text.ToCStr());
+            drawlist->AddText(text_cursor, ImColor(r,g,b), seg_text.ToCStr());
             total_text_size.x += text_size.x;
             total_text_size.y = std::max(total_text_size.y, text_size.y);
             text_cursor.x += text_size.x;
@@ -207,15 +249,6 @@ void GUI::ConsoleView::DrawColorMarkedText(ImVec4 default_color, std::string con
     ImVec2 bg_rect_size = total_text_size + (cvw_background_padding * 2);
     drawlist->AddRectFilled(bg_cursor, bg_cursor + bg_rect_size,
         ImColor(cvw_background_color), ImGui::GetStyle().FrameRounding);
-
-    // Finalize
-    drawlist->ChannelsMerge();
-    this->NewLine(total_text_size);
-}
-
-void GUI::ConsoleView::NewLine(ImVec2 text_size)
-{
-    ImGui::SetCursorPosY(ImGui::GetCursorPosY() + (text_size + (cvw_background_padding * 2)).y + cvw_line_spacing);
 }
 
 bool GUI::ConsoleView::DrawIcon(Ogre::TexturePtr tex)
--- source/main/gui/panels/GUI_ConsoleView.h
+++ source/main/gui/panels/GUI_ConsoleView.h
@@ -56,20 +56,20 @@ struct ConsoleView
 
     // Misc options
     size_t cvw_msg_duration_ms = 0u; //!< Message expiration; 0 means unlimited
-    size_t cvw_max_lines = 100u;
     bool   cvw_align_bottom = false;
+    bool   cvw_enable_icons = false;
     ImVec4 cvw_background_color = ImVec4(0,0,0,0); //!< Text-background color
     ImVec2 cvw_background_padding = ImVec2(0,0);
     float  cvw_line_spacing = 1.f;
 
 private:
     bool MessageFilter(Console::Message const& m); //!< Returns true if message should be displayed
-    void DrawColorMarkedText(ImVec4 default_color, std::string const& line);
-    void NewLine(ImVec2 text_size);
+    void DrawColorMarkedText(ImVec2 text_cursor, Ogre::TexturePtr icon, ImVec4 default_color, std::string const& line);
     bool DrawIcon(Ogre::TexturePtr tex);
     void UpdateMessages();
 
-    std::vector<Console::Message> m_filtered_messages;
+    std::vector<Console::Message> m_filtered_messages; //!< Updated as needed
+    std::vector<const Console::Message*> m_display_messages; //!< Rebuilt every frame; kept as member to reuse allocated memory
     unsigned long                 m_newest_msg_time = 0;      // Updated by `DrawConsoleMessages()`
     std::regex                    m_text_color_regex = std::regex(R"(#[a-fA-F\d]{6})");
     bool                          m_reload_messages = false;
