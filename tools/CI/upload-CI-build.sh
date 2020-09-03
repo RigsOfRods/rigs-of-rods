15e3ba62f842c48f5605581c9a919090260e4112
tritonas00 <tritonas00@gmail.com>
2020-09-03 05:14:05 +0300

    fixed libpng

--- tools/CI/upload-CI-build.sh
+++ tools/CI/upload-CI-build.sh
@@ -13,10 +13,10 @@ then
 	cp /usr/lib/x86_64-linux-gnu/libwebp.so.6               ./redist/lib/
 	cp /usr/lib/x86_64-linux-gnu/libwebpmux.so.3            ./redist/lib/
 	cp /usr/lib/x86_64-linux-gnu/libIlmImf-2_2.so.22        ./redist/lib/
-	# cp /usr/lib/x86_64-linux-gnu/libjasper.so.1             ./redist/lib/
+	# cp /usr/lib/x86_64-linux-gnu/libjasper.so.1           ./redist/lib/
 	cp /usr/lib/x86_64-linux-gnu/libHalf.so.12              ./redist/lib/
 	cp /usr/lib/x86_64-linux-gnu/libIex-2_2.so.12           ./redist/lib/
-	cp /lib/x86_64-linux-gnu/libpng16.so.0                  ./redist/lib/
+	cp /lib/x86_64-linux-gnu/libpng16.so                    ./redist/lib/
 	cp /usr/lib/x86_64-linux-gnu/libIlmThread-2_2.so.12     ./redist/lib/
 	cp /usr/lib/x86_64-linux-gnu/libCg.so                   ./redist/lib/
 	cp /usr/lib/x86_64-linux-gnu/libjpeg.so.8               ./redist/lib/
