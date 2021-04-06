5ca6b6f21eb19888314710446a7dace234d1bac7
Edgar <Edgar@AnotherFoxGuy.com>
2021-04-06 12:58:13 +0200

    :rocket: Simplified cmake install logic

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -1,82 +1,4 @@
-macro(copy_files srcDir destDir)
-    message(STATUS "Copying ${srcDir} -> ${destDir} ...")
-    #make_directory(${destDir})
-
-    file(GLOB files RELATIVE ${srcDir} ${srcDir}/*)
-    foreach (filename ${files})
-        set(srcTemplatePath ${srcDir}/${filename})
-        if (NOT IS_DIRECTORY ${srcTemplatePath})
-            message(STATUS "Copying file ${filename}")
-            configure_file(
-                    ${srcTemplatePath}
-                    ${destDir}/${filename}
-                    COPYONLY)
-        endif (NOT IS_DIRECTORY ${srcTemplatePath})
-    endforeach (filename)
-endmacro(copy_files)
-
-macro(copy_files_for_target TRGT SRC)
-    set(mysrc ${SRC})
-    set(mydst ${EXECUTABLE_OUTPUT_PATH}/$(ConfigurationName)/)
-    if (WIN32)
-        STRING(REGEX REPLACE "/" "\\\\" mysrc ${mysrc})
-        STRING(REGEX REPLACE "/" "\\\\" mydst ${mydst})
-        add_custom_command(
-                TARGET ${TRGT}
-                POST_BUILD
-                COMMAND xcopy /S /Y ${mysrc} ${mydst}
-                COMMENT "Copying ${mysrc} to ${mydst}"
-        )
-    else ()
-        set(mydst ${EXECUTABLE_OUTPUT_PATH}/)
-        add_custom_command(
-                TARGET ${TRGT}
-                POST_BUILD
-                COMMAND cp -r ${mysrc} ${mydst}
-                COMMENT "Copying ${mysrc} to ${mydst}"
-        )
-    endif ()
-endmacro(copy_files_for_target)
-
-macro(copy_lib_files_for_target TRGT SRC)
-    copy_files_for_target("${TRGT}" "${ROR_DEPENDENCIES_DIR}/bin/${ARCH_DIR}/${SRC}/$(ConfigurationName)/*" ${SRC})
-endmacro(copy_lib_files_for_target)
-
-macro(setup_lib name)
-    if (ROR_USE_${name})
-        include_directories(${${name}_INCLUDE_DIRS})
-        link_directories(${${name}_LIBRARY_DIRS})
-        add_definitions("-DUSE_${name}")
-        set(optional_libs ${optional_libs};${${name}_LIBRARIES})
-        message(STATUS "${name} Enabled:      \tYES")
-        message(STATUS "${name}_INCLUDE_DIRS: \t${${name}_INCLUDE_DIRS}")
-        #if(${${name}_LIBRARY_DIRS})
-        message(STATUS "${name}_LIBRARY_DIRS: \t${${name}_LIBRARY_DIRS}")
-        #endif(${${name}_LIBRARY_DIRS})
-        message(STATUS "${name}_LIBRARIES:    \t${${name}_LIBRARIES}")
-    else ()
-        message(STATUS "${name} Enabled:      \tNO")
-    endif (ROR_USE_${name})
-endmacro(setup_lib)
-
-macro(add_sub_dir BINNAME name)
-    FILE(GLOB_RECURSE ${BINNAME}_${name}_source ${RoR_Main_SOURCE_DIR}/${name}/*.cpp ${RoR_Main_SOURCE_DIR}/${name}/*.c)
-    FILE(GLOB_RECURSE ${BINNAME}_${name}_header ${RoR_Main_SOURCE_DIR}/${name}/*.h)
-    SOURCE_GROUP("${name}" FILES ${${BINNAME}_${name}_source} ${${BINNAME}_${name}_header})
-    include_directories(${RoR_Main_SOURCE_DIR}/${name})
-    set(${BINNAME}_sources ${${BINNAME}_sources} ${${BINNAME}_${name}_source})
-    set(${BINNAME}_headers ${${BINNAME}_headers} ${${BINNAME}_${name}_header})
-endmacro(add_sub_dir)
-
-macro(add_main_dir BINNAME)
-    FILE(GLOB ${BINNAME}_main_source ${RoR_Main_SOURCE_DIR}/*.cpp ${RoR_Main_SOURCE_DIR}/*.c)
-    FILE(GLOB ${BINNAME}_main_header ${RoR_Main_SOURCE_DIR}/*.h)
-    SOURCE_GROUP("globals" FILES ${${BINNAME}_main_source} ${${BINNAME}_main_header})
-    set(${BINNAME}_sources ${${BINNAME}_sources} ${${BINNAME}_main_source})
-    set(${BINNAME}_headers ${${BINNAME}_headers} ${${BINNAME}_main_header})
-endmacro(add_main_dir)
-
-macro(get_sub_dirs result curdir)
+function(get_sub_dirs result curdir)
     FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
     SET(dirlist "")
     FOREACH (child ${children})
@@ -85,9 +7,10 @@ macro(get_sub_dirs result curdir)
         ENDIF ()
     ENDFOREACH ()
     SET(${result} ${dirlist})
-endmacro(get_sub_dirs)
+endfunction(get_sub_dirs)
 
-macro(recursive_zip_folder name in_dir out_dir result)
+function(recursive_zip_folder in_dir out_dir)
+    get_filename_component(FROM_DIR_NAME ${in_dir} NAME)
     set(TMP_FILE_DIR ${CMAKE_BINARY_DIR}/tmp/${name})
     get_sub_dirs(SUB_DIRS "${in_dir}")
     file(MAKE_DIRECTORY ${out_dir})
@@ -104,9 +27,9 @@ macro(recursive_zip_folder name in_dir out_dir result)
         foreach (ZIP_FILE ${ZIP_FILES})
             file(RELATIVE_PATH REL_PATH "${in_dir}/${ZIP_DIR}" "${ZIP_FILE}")
             set(ZIP_FILES_TXT "${ZIP_FILES_TXT}${REL_PATH}\n")
-            if(NOT IS_DIRECTORY ${ZIP_FILE})
+            if (NOT IS_DIRECTORY ${ZIP_FILE})
                 list(APPEND ZIP_FILES_DEP ${ZIP_FILE})
-            endif()
+            endif ()
         endforeach ()
         file(WRITE "${TMP_FILE_DIR}/${ZIP_DIR}-filelist.txt" ${ZIP_FILES_TXT})
 
@@ -117,6 +40,74 @@ macro(recursive_zip_folder name in_dir out_dir result)
                 COMMAND ${CMAKE_COMMAND} -E tar c ${ZIP_NAME} --format=zip --files-from="${TMP_FILE_DIR}/${ZIP_DIR}-filelist.txt"
                 WORKING_DIRECTORY "${in_dir}/${ZIP_DIR}"
         )
-        list(APPEND ${result} ${ZIP_NAME})
+        list(APPEND ALL_ZIPS ${ZIP_NAME})
     endforeach ()
-endmacro(recursive_zip_folder)
+
+    add_custom_target(
+            zip_folder_${FROM_DIR_NAME} ALL
+            DEPENDS ${ALL_ZIPS}
+    )
+endfunction(recursive_zip_folder)
+
+function(fast_copy FROM_DIR TO_DIR)
+    file(GLOB_RECURSE files "${FROM_DIR}/*")
+    get_filename_component(FROM_DIR_NAME ${FROM_DIR} NAME)
+
+    foreach (file IN LISTS files)
+        file(RELATIVE_PATH output_file ${FROM_DIR} ${file})
+        set(dest "${TO_DIR}/${output_file}")
+
+        add_custom_command(
+                COMMENT "Copying ${output_file}"
+                OUTPUT ${dest}
+                DEPENDS ${file}
+                COMMAND ${CMAKE_COMMAND} -E copy ${file} ${dest}
+        )
+        list(APPEND ALL_FILES ${dest})
+    endforeach ()
+
+    add_custom_target(
+            fast_copy_${FROM_DIR_NAME} ALL
+            DEPENDS ${ALL_FILES}
+    )
+endfunction()
+
+function(extract_pot source_files_list)
+    set(FILELIST ${CMAKE_BINARY_DIR}/tmp/xgettext-filelist.txt)
+    set(ROR_POT "${CMAKE_SOURCE_DIR}/languages/ror.pot")
+
+    list(JOIN source_files_list "\n" source_files)
+    file(WRITE "${FILELIST}" "${source_files}")
+
+    add_custom_target(
+            extract_pot
+            COMMENT "Extract pot"
+            OUTPUT ${ROR_POT}
+            COMMAND xgettext -i -F -k_L -k_LC:1c,2 -c -o${ROR_POT} -f ${FILELIST}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+endfunction()
+
+function(compile_mo)
+    file(GLOB_RECURSE potfiles ${CMAKE_SOURCE_DIR}/languages/*/*.po CONFIGURE_DEPENDS)
+
+    foreach (file IN LISTS potfiles)
+        get_filename_component(dir ${file} DIRECTORY)
+        get_filename_component(filename ${file} NAME_WE)
+        get_filename_component(name "${dir}" NAME)
+        set(out "${dir}/${filename}.mo")
+        add_custom_command(
+                COMMENT "Compiling ${name}"
+                OUTPUT ${out}
+                DEPENDS ${file}
+                COMMAND msgfmt -o${out} ${file}
+                WORKING_DIRECTORY ${dir}
+        )
+        list(APPEND MO_FILES ${out})
+    endforeach ()
+
+    add_custom_target(
+            compile_mo
+            DEPENDS ${MO_FILES}
+    )
+endfunction()
