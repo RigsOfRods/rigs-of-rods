a79c0555df20786aed4bda12424cee2660f6b4d2
Petr Ohlidal <petr@ohlidal.cz>
2021-04-27 23:07:30 +0200

    :books: RoRnet - fixed comment.

--- source/main/network/RoRnet.h
+++ source/main/network/RoRnet.h
@@ -30,7 +30,7 @@ namespace RoRnet {
 #define RORNET_MAX_PEERS            64     //!< maximum clients connected at the same time
 #define RORNET_MAX_MESSAGE_LENGTH   8192   //!< maximum size of a RoR message. 8192 bytes = 8 kibibytes
 #define RORNET_LAN_BROADCAST_PORT   13000  //!< port used to send the broadcast announcement in LAN mode
-#define RORNET_MAX_USERNAME_LEN     40     //!< port used to send the broadcast announcement in LAN mode
+#define RORNET_MAX_USERNAME_LEN     40     //!< bytes.
 
 #define RORNET_VERSION              "RoRnet_2.44"
 
