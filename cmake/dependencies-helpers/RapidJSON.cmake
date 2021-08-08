e6eef362cf518b6a4d3ce60e121d1a4cb17e15d0
Edgar <Edgar@AnotherFoxGuy.com>
2021-08-08 10:33:29 +0000

    :hammer: Added support for finding deps with pkg config

--- cmake/dependencies-helpers/RapidJSON.cmake
+++ cmake/dependencies-helpers/RapidJSON.cmake
@@ -1,7 +1,9 @@
 # Add the RapidJSON target
+
 if (${_PREFIX}USE_RAPIDJSON STREQUAL "SYSTEM")
-    if (NOT TARGET RapidJSON::RapidJSON)
+    if (NOT TARGET RapidJSON::RapidJSON AND TARGET rapidjson)
         message(STATUS "Adding RapidJSON::RapidJSON target")
+        set_target_properties(rapidjson PROPERTIES IMPORTED_GLOBAL TRUE)
         add_library(RapidJSON::RapidJSON ALIAS rapidjson)
     endif ()
 endif ()
