5a07586972315fe5961213ebd0b9b69bfb2ab6ab
Edgar <Edgar@AnotherFoxGuy.com>
2021-08-08 10:39:27 +0200

    :bug: Fixed build without conan on windows

---
+++ cmake/dependencies-helpers/RapidJSON.cmake
@@ -0,0 +1,7 @@
+# Add the RapidJSON target
+if (${_PREFIX}USE_RAPIDJSON STREQUAL "SYSTEM")
+    if (NOT TARGET RapidJSON::RapidJSON)
+        message(STATUS "Adding RapidJSON::RapidJSON target")
+        add_library(RapidJSON::RapidJSON ALIAS rapidjson)
+    endif ()
+endif ()
