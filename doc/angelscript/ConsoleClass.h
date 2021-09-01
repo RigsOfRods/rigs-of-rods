9b88295f69d1e5b50eb81229477a56fe212c5b51
Petr Ohlidal <petr@ohlidal.cz>
2021-09-01 10:08:55 +0200

    :books: Added scripting doc for console + cvars

---
+++ doc/angelscript/ConsoleClass.h
@@ -0,0 +1,35 @@
+
+
+/**
+ * @brief A class that gives you access to console variables (cvars), usually defined in RoR.cfg file.
+ * @note This object is created automatically as global variable `console`.
+ */
+class ConsoleClass
+{
+public:
+    /**
+     * @brief Add CVar and parse default value if specified
+     */
+    CVar* cVarCreate(std::string const& name, std::string const& long_name,
+        int flags, std::string const& val = std::string());
+
+    /**
+     * Parse value by cvar type
+     */
+    void cVarAssign(CVar* cvar, std::string const& value);
+
+    /**
+     * Find cvar by short/long name
+     */
+    CVar* cVarFind(std::string const& input_name);
+
+    /**
+     * Set existing cvar by short/long name. Return the modified cvar (or NULL if not found)
+     */
+    CVar* cVarSet(std::string const& input_name, std::string const& input_val);
+
+    /**
+     * Get cvar by short/long name, or create new one using input as short name.
+     */
+    CVar* cVarGet(std::string const& input_name, int flags);
+};
