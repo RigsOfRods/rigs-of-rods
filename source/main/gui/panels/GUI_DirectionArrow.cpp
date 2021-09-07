1a349dc417b6582b7b516502b93be5a513c692dd
Petr Ohlidal <petr@ohlidal.cz>
2021-09-07 21:17:36 +0200

    moved race box to top menubar

--- source/main/gui/panels/GUI_DirectionArrow.cpp
+++ source/main/gui/panels/GUI_DirectionArrow.cpp
@@ -74,46 +74,6 @@ void GUI::DirectionArrow::Update(RoR::GfxActor* player_vehicle)
 
         // Update arrow direction
         m_node->lookAt(data.simbuf_dir_arrow_target, Ogre::Node::TS_WORLD, Ogre::Vector3::UNIT_Y);
-
-        // Update distance
-        float distance = 0.0f;
-        if (player_vehicle != nullptr && player_vehicle->GetSimDataBuffer().simbuf_live_local)
-        {
-            distance = player_vehicle->GetSimDataBuffer().simbuf_pos.distance(data.simbuf_dir_arrow_target);
-        }
-        else
-        {
-            distance = data.simbuf_character_pos.distance(data.simbuf_dir_arrow_target);
-        }
-
-        GUIManager::GuiTheme const& theme = App::GetGuiManager()->GetTheme();
-
-        ImGuiWindowFlags flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar;
-        std::string text_a = data.simbuf_dir_arrow_text;
-        std::string text_b = fmt::format("{:.1f} {}", distance, _LC("DirectionArrow", "meter")); 
-        const float content_width = ImGui::CalcTextSize(text_a.c_str()).x + ImGui::CalcTextSize(text_b.c_str()).x;
-        ImGui::SetNextWindowContentWidth(content_width);
-        ImGui::SetNextWindowPos(ImVec2((ImGui::GetIO().DisplaySize.x/2.f) - (content_width / 2.f), theme.screen_edge_padding.y));
-        ImGui::PushStyleColor(ImGuiCol_WindowBg, theme.semitransparent_window_bg);
-        ImGui::Begin("Arrow", nullptr, flags);
-        ImGui::Text(text_a.c_str());
-        ImGui::SameLine();
-
-        if (distance < 50)
-        {
-            ImGui::TextColored(theme.success_text_color,"%s", text_b.c_str());
-        }
-        else if (distance > 200)
-        {
-            ImGui::TextColored(theme.warning_text_color,"%s", text_b.c_str());
-        }
-        else
-        {
-            ImGui::TextColored(theme.value_blue_text_color,"%s", text_b.c_str());
-        }
-
-        ImGui::End();
-        ImGui::PopStyleColor(1); // WindowBg
     }
     else
     {
