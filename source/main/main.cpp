RigsOfRods/rigsofrods-next-stable

use X for remove button, mute/unmute sounds, alligned buttons

source/main/main.cpp
@@ -639,6 +639,7 @@ int main(int argc, char *argv[])
                         actor->GetGfxActor()->SetAllMeshesVisible(false);
                         actor->GetGfxActor()->SetCastShadows(false);
                         actor->GetGfxActor()->SetRodsVisible(false);
+                        actor->StopAllSounds(); // Stop sounds
                     }
                     break;
 
@@ -654,6 +655,7 @@ int main(int argc, char *argv[])
                         actor->GetGfxActor()->SetAllMeshesVisible(true);
                         actor->GetGfxActor()->SetCastShadows(true);
                         actor->GetGfxActor()->SetRodsVisible(true);
+                        actor->UnmuteAllSounds(); // Unmute sounds
                     }
                     break;
 

RigsOfRods/rigs-of-rods

source/main/main.cpp
@@ -642,6 +642,7 @@ int main(int argc, char *argv[])
                         actor->GetGfxActor()->SetAllMeshesVisible(false);
                         actor->GetGfxActor()->SetCastShadows(false);
                         actor->GetGfxActor()->SetRodsVisible(false);
+                        actor->muteAllSounds(); // Stop sounds
                     }
                     break;
 
@@ -657,6 +658,7 @@ int main(int argc, char *argv[])
                         actor->GetGfxActor()->SetAllMeshesVisible(true);
                         actor->GetGfxActor()->SetCastShadows(true);
                         actor->GetGfxActor()->SetRodsVisible(true);
+                        actor->unmuteAllSounds(); // Unmute sounds
                     }
                     break;
 