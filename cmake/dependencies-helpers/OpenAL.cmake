368e073264f0eec77f538f72d481998f85d07f8d
Pavel Rojtberg <rojtberg@gmail.com>
2021-12-07 18:23:29 +0100

    fix build using system libs

--- cmake/dependencies-helpers/OpenAL.cmake
+++ cmake/dependencies-helpers/OpenAL.cmake
@@ -1,7 +1,12 @@
 # Refind OpenAL to add the OpenAL::OpenAL target
 
 if (${_PREFIX}USE_OPENAL STREQUAL "SYSTEM")
+    find_package(OpenAL QUIET)
     if (NOT TARGET OpenAL::OpenAL)
-        find_package(OpenAL QUIET CONFIG)
+        add_library(OpenAL::OpenAL INTERFACE IMPORTED)
+        set_target_properties(OpenAL::OpenAL PROPERTIES
+                INTERFACE_LINK_LIBRARIES "${OPENAL_LIBRARY}"
+                INTERFACE_INCLUDE_DIRECTORIES "${OPENAL_INCLUDE_DIR}"
+                )
     endif ()
 endif ()
