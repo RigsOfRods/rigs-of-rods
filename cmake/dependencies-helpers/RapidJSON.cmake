4cd184a9606851ba89e13591c768f27239085e4b
Pavel Rojtberg <rojtberg@gmail.com>
2021-12-07 18:23:29 +0100

    fix build using system libs

--- cmake/dependencies-helpers/RapidJSON.cmake
+++ cmake/dependencies-helpers/RapidJSON.cmake
@@ -1,9 +1,8 @@
 # Add the RapidJSON target
 
 if (${_PREFIX}USE_RAPIDJSON STREQUAL "SYSTEM")
-    if (NOT TARGET RapidJSON::RapidJSON AND TARGET rapidjson)
+    if (NOT TARGET RapidJSON::RapidJSON)
         message(STATUS "Adding RapidJSON::RapidJSON target")
-        set_target_properties(rapidjson PROPERTIES IMPORTED_GLOBAL TRUE)
-        add_library(RapidJSON::RapidJSON ALIAS rapidjson)
+        add_library(RapidJSON::RapidJSON INTERFACE IMPORTED)
     endif ()
 endif ()
