1b00b871e81959859187b7bf6f5d7bdae321ca4e
tritonas00 <tritonas00@gmail.com>
2022-03-03 12:54:18 +0200

    continue using mygui 3.4.0, 3.4.1 has rendering issues

--- resources/mygui/MyGUI_Ogre_VP.hlsl
+++ resources/mygui/MyGUI_Ogre_VP.hlsl
@@ -1,13 +1,15 @@
 void main(
-	in float4 inPosition : POSITION0,
+	in float3 inPosition : POSITION0,
 	in float4 inColor : COLOR0,
 	in float2 inTexcoord : TEXCOORD0,
-	uniform float4x4 worldViewProj,
+	uniform float YFlipScale,
 	out float4 outPosition : SV_POSITION,
 	out float4 outColor : TEXCOORD0,
 	out float2 outTexcoord : TEXCOORD1 )
 {
-	outPosition = mul(worldViewProj, inPosition);
+	float4 vpos = float4(inPosition, 1.0f);
+	vpos.y *= YFlipScale;
+	outPosition = vpos;
 	outColor = inColor;
 	outTexcoord = inTexcoord;
 }
--- resources/mygui/MyGUI_OpenGL3_FP.glsl
+++
@@ -1,8 +0,0 @@
-#version 130
-in vec4 Color;
-in vec2 TexCoord;
-uniform sampler2D Texture;
-void main(void)
-{
-	gl_FragColor = texture2D(Texture, TexCoord) * Color;
-}
