b770465f5597f2894522789cac0ebe3eae6be13b
Petr Ohlidal <petr@ohlidal.cz>
2022-04-07 13:49:18 +0200

    Doxygen: added missing @defgroup-s, improved formatting.

--- doc/doxygen/Modules.h
+++ doc/doxygen/Modules.h
@@ -1,3 +1,8 @@
+
+// Please maintain structure matching the HTML output
+// (alphabetical order and hierarchy).
+// ---------------------------------------------------
+
 /// @defgroup Application Application
 /// Integration with host OS, hardware and drivers
 
@@ -7,35 +12,50 @@
 /// @defgroup GameState Game state
 /// Makes all gameplay features work together.
 
-/// @defgroup MsgQueue Message queue
-/// Ensures everything gets executed at right time & in right order.
+    /// @defgroup MsgQueue Message queue
+    /// Ensures everything gets executed at right time & in right order.
 
 /// @defgroup Physics Physics
 /// Soft-body physics and associated simulations.
 
-/// @defgroup Collisions Collisions
-/// Static & dynamic collision detection system
-
-/// @defgroup Trucks Land vehicles
-/// Cars, trucks, rovers, ATVs, anything.
-
-/// @defgroup Aerial Aerial
-/// Airplanes and helicopters.
-
-/// @defgroup Character Character
-/// Wandering the terrain on foot.
-
-/// @defgroup Marine Marine
-/// Ships and boats of any size and function.
+    /// @defgroup Collisions Collisions
+    /// Static & dynamic collision detection system
 
 /// @defgroup Console Console
 /// Centerpiece of the application; parses command line & config, maintains cvars, logs messages.
 
-/// @defgroup ConsoleCmd Console commmands
-/// Built-in console commands.
+    /// @defgroup ConsoleCmd Console commmands
+    /// Built-in console commands.
 
 /// @defgroup Scripting Scripting
 /// Game scripting subsystem using AngelScript.
 
 /// @defgroup Network Network
 /// Multiplayer and integration with remote services.
+
+/// @defgroup Gameplay Gameplay
+/// Game logic.
+
+    /// @defgroup Trucks Land vehicles
+    /// Cars, trucks, rovers, ATVs, anything.
+
+    /// @defgroup Aerial Aerial
+    /// Airplanes and helicopters.
+
+    /// @defgroup Character Character
+    /// Wandering the terrain on foot.
+
+    /// @defgroup Marine Marine
+    /// Ships and boats of any size and function.
+
+/// @defgroup Terrain Terrain
+/// Everything related to terrain and objects - game logic, visualization, scripting.
+
+/// @defgroup Gfx Gfx
+/// Visual-only objects or visual couterparts to @ref Gameplay objects.
+
+    /// @defgroup Hydrax Hydrax
+    /// Nice looking water, originally a 3rd party component.
+    
+    /// @defgroup Flex Flex
+    /// Meshes deforming in real time: flexbodies, wings, wheels, cab mesh.
