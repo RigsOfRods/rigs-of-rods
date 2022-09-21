7725e9add0cd57784b94fcb08d9a7d32c38644c0
Edgar <Edgar@AnotherFoxGuy.com>
2022-09-21 13:54:15 +0200

    :bug: Fixed missing MyGUI shaders

--- resources/mygui/MyGUI_Ogre_VP.glsl
+++ resources/mygui/MyGUI_Ogre_VP.glsl
@@ -6,17 +6,15 @@ precision highp float;
 in vec4 position;
 in vec4 uv0;
 in vec4 colour;
-uniform float YFlipScale;
+uniform mat4 worldViewProj;
 
 out vec4 outUV0;
 out vec4 outColor;
 
-// Texturing vertex program for GLSL ES
+// Texturing vertex program for GLSL
 void main()
 {
-	vec4 vpos = position;
-	vpos.y *= YFlipScale;
-	gl_Position = vpos;
+	gl_Position = worldViewProj * position;
 	outUV0 = uv0;
 	outColor = colour;
 }
