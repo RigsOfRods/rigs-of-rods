84956d278e4763e7cdb89a94c0ffbd84404c2ff0
Edgar <Edgar@AnotherFoxGuy.com>
2022-10-23 11:41:42 +0200

    :construction: Build with conan V2

--- cmake/dependencies-helpers/CURL.cmake
+++
@@ -1,7 +0,0 @@
-# Fixup broken CURL target
-
-if (${_PREFIX}USE_CURL STREQUAL "CONAN")
-    if (NOT TARGET CURL::libcurl)
-        add_library(CURL::libcurl ALIAS CONAN_PKG::CURL)
-    endif ()
-endif ()
