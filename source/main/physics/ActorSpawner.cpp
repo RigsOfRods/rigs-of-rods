RigsOfRods/rigs-of-rods

commit 0e8e64644c685ccdd1904c53260269d1473406a0

:truck: Parser: improved 'managedmaterials' processing.
Per guidelines, type enum made strong+standalone, added parsing function GetArg*().
Also, removed redundant helpers `Has*()`, resource check moved to spawner where it belongs.

source/main/physics/ActorSpawner.cpp
@@ -2217,6 +2217,26 @@ void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
         return;
     }
 
+    // Check all textures exist
+    Ogre::ResourceGroupManager& rgm = Ogre::ResourceGroupManager::getSingleton();
+    if (!rgm.resourceExists(m_custom_resource_group, def.diffuse_map))
+    {
+        this->AddMessage(Message::TYPE_WARNING, "Skipping managed material, missing texture file: " + def.diffuse_map);
+        return;
+    }
+    if (def.damaged_diffuse_map != "" &&
+        !rgm.resourceExists(m_custom_resource_group, def.damaged_diffuse_map))
+    {
+        this->AddMessage(Message::TYPE_WARNING, "Skipping managed material, missing texture file: " + def.damaged_diffuse_map);
+        return;
+    }
+    if (def.specular_map != "" &&
+        !rgm.resourceExists(m_custom_resource_group, def.specular_map))
+    {
+        this->AddMessage(Message::TYPE_WARNING, "Skipping managed material, missing texture file: " + def.specular_map);
+        return;
+    }
+
     // Create temporary placeholder
     // This is necessary to load meshes with original material names (= unchanged managed mat names)
     // - if not found, OGRE substitutes them with 'BaseWhite' which breaks subsequent processing.
@@ -2227,16 +2247,16 @@ void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
 
     std::string custom_name = def.name + ACTOR_ID_TOKEN + TOSTRING(m_actor->ar_instance_id);
     Ogre::MaterialPtr material;
-    if (def.type == RigDef::ManagedMaterial::TYPE_FLEXMESH_STANDARD || def.type == RigDef::ManagedMaterial::TYPE_FLEXMESH_TRANSPARENT)
+    if (def.type == RigDef::ManagedMaterialType::FLEXMESH_STANDARD || def.type == RigDef::ManagedMaterialType::FLEXMESH_TRANSPARENT)
     {
         std::string mat_name_base
-            = (def.type == RigDef::ManagedMaterial::TYPE_FLEXMESH_STANDARD)
+            = (def.type == RigDef::ManagedMaterialType::FLEXMESH_STANDARD)
             ? "managed/flexmesh_standard"
             : "managed/flexmesh_transparent";
 
-        if (def.HasDamagedDiffuseMap())
+        if (def.damaged_diffuse_map != "")
         {
-            if (def.HasSpecularMap())
+            if (def.specular_map != "")
             {
                 /* FLEXMESH, damage, specular */
                 if (App::gfx_classic_shaders->getBool())
@@ -2279,7 +2299,7 @@ void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
         }
         else
         {
-            if (def.HasSpecularMap())
+            if (def.specular_map != "")
             {
                 /* FLEXMESH, no_damage, specular */
                 if (App::gfx_classic_shaders->getBool())
@@ -2318,14 +2338,14 @@ void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
             }
         }
     }
-    else if (def.type == RigDef::ManagedMaterial::TYPE_MESH_STANDARD || def.type == RigDef::ManagedMaterial::TYPE_MESH_TRANSPARENT)
+    else if (def.type == RigDef::ManagedMaterialType::MESH_STANDARD || def.type == RigDef::ManagedMaterialType::MESH_TRANSPARENT)
     {
         Ogre::String mat_name_base
-            = (def.type == RigDef::ManagedMaterial::TYPE_MESH_STANDARD)
+            = (def.type == RigDef::ManagedMaterialType::MESH_STANDARD)
             ? "managed/mesh_standard"
             : "managed/mesh_transparent";
 
-        if (def.HasSpecularMap())
+        if (def.specular_map != "")
         {
             /* MESH, specular */
             if (App::gfx_classic_shaders->getBool())
@@ -2365,12 +2385,12 @@ void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
         }
     }
 
-    if (def.type != RigDef::ManagedMaterial::TYPE_INVALID)
+    if (def.type != RigDef::ManagedMaterialType::INVALID)
     {
         if (def.options.double_sided)
         {
             material->getTechnique("BaseTechnique")->getPass("BaseRender")->setCullingMode(Ogre::CULL_NONE);
-            if (def.HasSpecularMap())
+            if (def.specular_map != "")
             {
                 if (App::gfx_classic_shaders->getBool())
                 {

source/main/resources/rig_def_fileformat/RigDef_File.h
@@ -57,6 +57,9 @@ namespace RigDef {
 
 extern const char* ROOT_MODULE_NAME;
 
+// --------------------------------
+// Enums which only carry value
+
 // IMPORTANT! If you add a value here, you must also modify Regexes::IDENTIFY_KEYWORD, it relies on numeric values of this enum.
 enum class Keyword
 {
@@ -193,6 +196,15 @@ enum class SpecialProp
     BEACON,
     REDBEACON,
     LIGHTBAR,
+};
+
+enum class ManagedMaterialType
+{
+    INVALID,
+    FLEXMESH_STANDARD,
+    FLEXMESH_TRANSPARENT,
+    MESH_STANDARD,
+    MESH_TRANSPARENT,
 };
 
 // --------------------------------
@@ -1000,37 +1012,12 @@ struct ManagedMaterialsOptions
 
 struct ManagedMaterial
 {
-    /* IMPORTANT! Order of these values must match Regexes::IDENTIFY_MANAGED_MATERIAL_TYPE enum from Regexes.h */
-    enum Type
-    {
-        TYPE_FLEXMESH_STANDARD = 1,
-        TYPE_FLEXMESH_TRANSPARENT,
-        TYPE_MESH_STANDARD,
-        TYPE_MESH_TRANSPARENT,
-
-        TYPE_INVALID = 0xFFFFFFFF
-    };
-
     Ogre::String name;
-    
-    /* Attributes */
-    Type type;
+    ManagedMaterialType type = ManagedMaterialType::INVALID;
     ManagedMaterialsOptions options;
-    
-    /* Textures */
     Ogre::String diffuse_map;
     Ogre::String damaged_diffuse_map;
     Ogre::String specular_map;
-
-    bool HasDamagedDiffuseMap()
-    {
-        return (damaged_diffuse_map.length() != 0 && damaged_diffuse_map[0] != '-');
-    }
-
-    bool HasSpecularMap()
-    {
-        return (specular_map.length() != 0 && specular_map[0] != '-');
-    }
 };
 
 struct MaterialFlareBinding

source/main/resources/rig_def_fileformat/RigDef_Parser.cpp
@@ -2360,61 +2360,30 @@ void Parser::ParseManagedMaterials()
     if (! this->CheckNumArguments(2)) { return; }
 
     ManagedMaterial managed_mat;
-    
     managed_mat.options = m_current_managed_material_options;
     managed_mat.name    = this->GetArgStr(0);
+    managed_mat.type    = this->GetArgManagedMatType(1);
 
-    const std::string type_str = this->GetArgStr(1);
-    if (type_str == "mesh_standard" || type_str == "mesh_transparent")
+    if (managed_mat.type != ManagedMaterialType::INVALID)
     {
         if (! this->CheckNumArguments(3)) { return; }
 
-        managed_mat.type = (type_str == "mesh_standard")
-            ? ManagedMaterial::TYPE_MESH_STANDARD
-            : ManagedMaterial::TYPE_MESH_TRANSPARENT;
-        
         managed_mat.diffuse_map = this->GetArgStr(2);
-        
-        if (m_num_args > 3) { managed_mat.specular_map = this->GetArgManagedTex(3); }
-    }
-    else if (type_str == "flexmesh_standard" || type_str == "flexmesh_transparent")
-    {
-        if (! this->CheckNumArguments(3)) { return; }
 
-        managed_mat.type = (type_str == "flexmesh_standard")
-            ? ManagedMaterial::TYPE_FLEXMESH_STANDARD
-            : ManagedMaterial::TYPE_FLEXMESH_TRANSPARENT;
-            
-        managed_mat.diffuse_map = this->GetArgStr(2);
-        
-        if (m_num_args > 3) { managed_mat.damaged_diffuse_map = this->GetArgManagedTex(3); }
-        if (m_num_args > 4) { managed_mat.specular_map        = this->GetArgManagedTex(4); }
-    }
-    else
-    {
-        this->LogMessage(Console::CONSOLE_SYSTEM_WARNING, type_str + " is an unkown effect");
-        return;
-    }
+        if (managed_mat.type == ManagedMaterialType::MESH_STANDARD ||
+            managed_mat.type == ManagedMaterialType::MESH_TRANSPARENT)
+        {
+            if (m_num_args > 3) { managed_mat.specular_map = this->GetArgManagedTex(3); }
+        }
+        else if (managed_mat.type == ManagedMaterialType::FLEXMESH_STANDARD ||
+            managed_mat.type == ManagedMaterialType::FLEXMESH_TRANSPARENT)
+        {
+            if (m_num_args > 3) { managed_mat.damaged_diffuse_map = this->GetArgManagedTex(3); }
+            if (m_num_args > 4) { managed_mat.specular_map        = this->GetArgManagedTex(4); }
+        }
 
-    Ogre::ResourceGroupManager& rgm = Ogre::ResourceGroupManager::getSingleton();
-
-    if (!rgm.resourceExists(m_resource_group, managed_mat.diffuse_map))
-    {
-        this->LogMessage(Console::CONSOLE_SYSTEM_WARNING, "Missing texture file: " + managed_mat.diffuse_map);
-        return;
+        m_current_module->managedmaterials.push_back(managed_mat);
     }
-    if (managed_mat.HasDamagedDiffuseMap() && !rgm.resourceExists(m_resource_group, managed_mat.damaged_diffuse_map))
-    {
-        this->LogMessage(Console::CONSOLE_SYSTEM_WARNING, "Missing texture file: " + managed_mat.damaged_diffuse_map);
-        managed_mat.damaged_diffuse_map = "-";
-    }
-    if (managed_mat.HasSpecularMap() && !rgm.resourceExists(m_resource_group, managed_mat.specular_map))
-    {
-        this->LogMessage(Console::CONSOLE_SYSTEM_WARNING, "Missing texture file: " + managed_mat.specular_map);
-        managed_mat.specular_map = "-";
-    }
-
-    m_current_module->managedmaterials.push_back(managed_mat);
 }
 
 void Parser::ParseLockgroups()
@@ -3273,6 +3242,19 @@ EngineType Parser::GetArgEngineType(int index)
             fmt::format("invalid EngineType '{}', falling back to 't' (truck)", c);
             return EngineType::t_TRUCK;
     }
+}
+
+ManagedMaterialType Parser::GetArgManagedMatType(int index)
+{
+    std::string str = this->GetArgStr(index);
+    if (str == "mesh_standard")        return ManagedMaterialType::MESH_STANDARD;
+    if (str == "mesh_transparent")     return ManagedMaterialType::MESH_TRANSPARENT;
+    if (str == "flexmesh_standard")    return ManagedMaterialType::FLEXMESH_STANDARD;
+    if (str == "flexmesh_transparent") return ManagedMaterialType::FLEXMESH_TRANSPARENT;
+
+    this->LogMessage(Console::CONSOLE_SYSTEM_WARNING,
+        fmt::format("Not a valid ManagedMaterialType: '{}'", str));
+    return ManagedMaterialType::INVALID;
 }
 
 int Parser::TokenizeCurrentLine()

source/main/resources/rig_def_fileformat/RigDef_Parser.h
@@ -222,6 +222,7 @@ private:
     BitMask_t          GetArgNodeOptions  (int index);
     SpecialProp        GetArgSpecialProp  (int index);
     EngineType         GetArgEngineType   (int index);
+    ManagedMaterialType GetArgManagedMatType(int index);
 
     float              ParseArgFloat      (const char* str);
     int                ParseArgInt        (const char* str);

source/main/resources/rig_def_fileformat/RigDef_Serializer.cpp
@@ -766,16 +766,16 @@ void Serializer::ProcessManagedMaterialsAndOptions(Document::Module* module)
         // Type
         switch (def.type)
         {
-        case ManagedMaterial::TYPE_FLEXMESH_STANDARD:
+        case ManagedMaterialType::FLEXMESH_STANDARD:
             m_stream << "flexmesh_standard ";
             break;
-        case ManagedMaterial::TYPE_FLEXMESH_TRANSPARENT:
+        case ManagedMaterialType::FLEXMESH_TRANSPARENT:
             m_stream << "flexmesh_transparent ";
             break;
-        case ManagedMaterial::TYPE_MESH_STANDARD:
+        case ManagedMaterialType::MESH_STANDARD:
             m_stream << "mesh_standard ";
             break;
-        case ManagedMaterial::TYPE_MESH_TRANSPARENT:
+        case ManagedMaterialType::MESH_TRANSPARENT:
             m_stream << "mesh_transparent ";
             break;
         default:
@@ -784,7 +784,7 @@ void Serializer::ProcessManagedMaterialsAndOptions(Document::Module* module)
         // Diffuse texture filename
         m_stream << def.diffuse_map << " ";
         // Diffuse damage-texture filename
-        if (def.type == ManagedMaterial::TYPE_FLEXMESH_STANDARD || def.type == ManagedMaterial::TYPE_FLEXMESH_TRANSPARENT)
+        if (def.type == ManagedMaterialType::FLEXMESH_STANDARD || def.type == ManagedMaterialType::FLEXMESH_TRANSPARENT)
         {
             m_stream << (def.damaged_diffuse_map.empty() ? "-" : def.damaged_diffuse_map) << " ";
         }

commit 3eebb12362299c20195d32f349f471ff80698ab2

Fixed white managedmat if specular/damage texture missing
This was a bug introduced with 0e8e64644c685ccdd1904c53260269d1473406a0 - managedmaterial processing got refactored and new logic didn't match the old logic - new checks for missing textures were overzealous and disabled the entire material instead of disabling just the related effect.

Fixes #3005

source/main/physics/ActorSpawner.cpp
@@ -2261,17 +2261,19 @@ void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
         this->AddMessage(Message::TYPE_WARNING, "Skipping managed material, missing texture file: " + def.diffuse_map);
         return;
     }
+
     if (def.damaged_diffuse_map != "" &&
         !rgm.resourceExists(m_custom_resource_group, def.damaged_diffuse_map))
     {
-        this->AddMessage(Message::TYPE_WARNING, "Skipping managed material, missing texture file: " + def.damaged_diffuse_map);
-        return;
+        this->AddMessage(Message::TYPE_WARNING, "Damage texture not found: " + def.damaged_diffuse_map);
+        def.damaged_diffuse_map = "";
     }
+
     if (def.specular_map != "" &&
         !rgm.resourceExists(m_custom_resource_group, def.specular_map))
     {
-        this->AddMessage(Message::TYPE_WARNING, "Skipping managed material, missing texture file: " + def.specular_map);
-        return;
+        this->AddMessage(Message::TYPE_WARNING, "Specular texture not found: " + def.specular_map);
+        def.specular_map = "";
     }
 
     // Create temporary placeholder