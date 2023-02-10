a23f7e93decde4ea8c243dc8fe560fc6a3ae9f5c
Edgar <Edgar@AnotherFoxGuy.com>
2023-02-10 11:51:40 +0100

    :wrench: Switch conan generator to CMakeToolchain

--- cmake/dependencies-helpers/RapidJSON.cmake
+++
@@ -1,8 +0,0 @@
-# Add the RapidJSON target
-
-if (${_PREFIX}USE_RAPIDJSON STREQUAL "SYSTEM")
-    if (NOT TARGET RapidJSON::RapidJSON)
-        message(STATUS "Adding RapidJSON::RapidJSON target")
-        add_library(RapidJSON::RapidJSON INTERFACE IMPORTED)
-    endif ()
-endif ()
