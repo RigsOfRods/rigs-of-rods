317db14fee6bc6f77eb5ce861306ecb045990a5e
Petr Ohlidal <petr@ohlidal.cz>
2023-02-26 18:32:00 +0100

    Scripting: LocalStorage made consistent with other objects.
    *Renamed `LocalStorage`->`LocalStorageClass` - now ALL script objects are consistent!
    *Upgraded to RefCountingObject<> - the previous refcounting was actually valid (although overcomplicated) but why not do everything the same way.
    *Function `operator=` remade to `copyFrom()` because it was flaky - it only added items, didn't clear the old ones, so it didn't behave as expected.
    * updated builtin script 'races.as' to use the modified API.

--- source/main/scripting/LocalStorage.h
+++ source/main/scripting/LocalStorage.h
@@ -22,8 +22,12 @@
 #pragma once
 
 #include "Application.h"
-#include <angelscript.h>
 #include "ImprovedConfigFile.h"
+#include "RefCountingObject.h"
+
+#include <angelscript.h>
+
+namespace RoR {
 
 /// @addtogroup Scripting
 /// @{
@@ -31,66 +35,54 @@
 /**
  *  @brief A class that allows scripts to store data persistently
  */
-class LocalStorage : public ImprovedConfigFile
+class LocalStorage : public ImprovedConfigFile, public RefCountingObject<LocalStorage>
 {
 public:
-    // Memory management
-    void AddRef() const;
-    void Release() const;
-
-    LocalStorage(AngelScript::asIScriptEngine* engine, std::string fileName_in, const std::string& sectionName_in);
-    LocalStorage(AngelScript::asIScriptEngine* engine_in);
+    LocalStorage(std::string filename, const std::string& section_name, const std::string& rg_name /* = RGN_CACHE*/);
     ~LocalStorage();
 
-    LocalStorage& operator =(LocalStorage& other);
+    void copyFrom(LocalStoragePtr other);
     void changeSection(const std::string& section);
 
-    std::string get(std::string& key);
-    void set(std::string& key, const std::string& value);
+    std::string get(std::string key);
+    void set(std::string key, const std::string& value);
 
-    int getInt(std::string& key);
-    void set(std::string& key, const int value);
+    int getInt(std::string key);
+    void set(std::string key, const int value);
 
-    float getFloat(std::string& key);
-    void set(std::string& key, const float value);
+    float getFloat(std::string key);
+    void set(std::string key, const float value);
 
-    bool getBool(std::string& key);
-    void set(std::string& key, const bool value);
+    bool getBool(std::string key);
+    void set(std::string key, const bool value);
 
-    Ogre::Vector3 getVector3(std::string& key);
-    void set(std::string& key, const Ogre::Vector3& value);
+    Ogre::Vector3 getVector3(std::string key);
+    void set(std::string key, const Ogre::Vector3& value);
 
-    Ogre::Quaternion getQuaternion(std::string& key);
-    void set(std::string& key, const Ogre::Quaternion& value);
+    Ogre::Quaternion getQuaternion(std::string key);
+    void set(std::string key, const Ogre::Quaternion& value);
 
-    Ogre::Radian getRadian(std::string& key);
-    void set(std::string& key, const Ogre::Radian& value);
+    Ogre::Radian getRadian(std::string key);
+    void set(std::string key, const Ogre::Radian& value);
 
-    Ogre::Degree getDegree(std::string& key);
-    void set(std::string& key, const Ogre::Degree& value);
+    Ogre::Degree getDegree(std::string key);
+    void set(std::string key, const Ogre::Degree& value);
 
     void saveDict();
     // int extendDict();
     bool loadDict();
 
     // removes a key and its associated value
-    void eraseKey(std::string& key);
+    void eraseKey(std::string key);
 
     // Returns true if the key is set
-    bool exists(std::string& key);
+    bool exists(std::string key);
 
     // Deletes all keys
     void deleteAll();
 
     // parses a key
-    void parseKey(std::string& key, std::string& section);
-
-    // Garbage collections behaviours
-    int GetRefCount();
-    void SetGCFlag();
-    bool GetGCFlag();
-    void EnumReferences(AngelScript::asIScriptEngine* engine);
-    void ReleaseAllReferences(AngelScript::asIScriptEngine* engine);
+    void parseKey(std::string& inout_key, std::string& out_section);
 
     SettingsBySection getSettings() { return mSettingsPtr; }
     std::string getFilename() { return m_filename; }
@@ -98,13 +90,11 @@ public:
 
 protected:
     std::string m_filename;
+    std::string m_resource_group;
     bool saved; //!< Inverted 'dirty flag'
     std::string sectionName;
-
-    // Our properties
-    AngelScript::asIScriptEngine* engine;
-    static int refCount;
-    bool cgflag;
 };
 
 /// @}   //addtogroup Scripting
+
+} // namespace RoR
--- source/main/scripting/bindings/LocalStorageAngelscript.cpp
+++ source/main/scripting/bindings/LocalStorageAngelscript.cpp
@@ -21,91 +21,67 @@
 
 #include "LocalStorage.h"
 #include "AngelScriptBindings.h"
+
 #include <angelscript.h>
 
 using namespace AngelScript;
+using namespace RoR;
 
-void scriptLocalStorageFactory_Generic(asIScriptGeneric *gen)
+static LocalStorage* LocalStorageFactory(std::string filename, const std::string& section_name, const std::string& rg_name)
 {
-    std::string filename	= **(std::string**)gen->GetAddressOfArg(0);
-    std::string sectionname = **(std::string**)gen->GetAddressOfArg(1);
-    
-    *(LocalStorage**)gen->GetAddressOfReturnLocation() = new LocalStorage(gen->GetEngine(), filename, sectionname);
+    return new LocalStorage(filename, section_name, rg_name);
 }
-
-void scriptLocalStorageFactory2_Generic(asIScriptGeneric *gen)
-{
-    std::string filename	= **(std::string**)gen->GetAddressOfArg(0);	
-    *(LocalStorage**)gen->GetAddressOfReturnLocation() = new LocalStorage(gen->GetEngine(), filename, "common");
-}
-
-void scriptLocalStorageFactory3_Generic(asIScriptGeneric *gen)
-{	
-    *(LocalStorage**)gen->GetAddressOfReturnLocation() = new LocalStorage(gen->GetEngine());
-}
-
 
 void RoR::RegisterLocalStorage(asIScriptEngine *engine)
 {
+    LocalStorage::RegisterRefCountingObject(engine, "LocalStorageClass");
+    LocalStoragePtr::RegisterRefCountingObjectPtr(engine, "LocalStorageClassPtr", "LocalStorageClass");
+
     int r;
+    r = engine->RegisterObjectBehaviour("LocalStorageClass", asBEHAVE_FACTORY, "LocalStorageClass@+ f(string, const string&in = \"common\", const string&in = \"Cache\")", asFUNCTION(LocalStorageFactory), asCALL_CDECL); ROR_ASSERT(r >= 0);
 
-    r = engine->RegisterObjectType("LocalStorage", sizeof(LocalStorage), asOBJ_REF | asOBJ_GC); ROR_ASSERT( r >= 0 );
-    // Use the generic interface to construct the object since we need the engine pointer, we could also have retrieved the engine pointer from the active context
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_FACTORY, "LocalStorage@ f(const string &in, const string &in)", asFUNCTION(scriptLocalStorageFactory_Generic), asCALL_GENERIC); ROR_ASSERT( r>= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_FACTORY, "LocalStorage@ f(const string &in)", asFUNCTION(scriptLocalStorageFactory2_Generic), asCALL_GENERIC); ROR_ASSERT( r>= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_FACTORY, "LocalStorage@ f()", asFUNCTION(scriptLocalStorageFactory3_Generic), asCALL_GENERIC); ROR_ASSERT( r>= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_ADDREF, "void f()",  asMETHOD(LocalStorage,AddRef), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_RELEASE, "void f()", asMETHOD(LocalStorage,Release), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void copyFrom(LocalStorageClassPtr@)", asMETHOD(LocalStorage,copyFrom), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void changeSection(const string &in)", asMETHOD(LocalStorage,changeSection), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "LocalStorage &opAssign(LocalStorage &in)", asMETHODPR(LocalStorage, operator=, (LocalStorage &), LocalStorage&), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void changeSection(const string &in)",     asMETHODPR(LocalStorage,changeSection,(const std::string&), void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "string get(string)",                 asMETHODPR(LocalStorage,get,(std::string), std::string), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "string getString(string)",           asMETHODPR(LocalStorage,get,(std::string), std::string), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, const string &in)", asMETHODPR(LocalStorage,set,(std::string, const std::string&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setString(string, const string &in)", asMETHODPR(LocalStorage,set,(std::string, const std::string&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "string get(string &in)",                 asMETHODPR(LocalStorage,get,(std::string&), std::string), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "string getString(string &in)",           asMETHODPR(LocalStorage,get,(std::string&), std::string), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, const string &in)", asMETHODPR(LocalStorage,set,(std::string&, const std::string&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setString(string &in, const string &in)", asMETHODPR(LocalStorage,set,(std::string&, const std::string&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "float getFloat(string)",  asMETHODPR(LocalStorage,getFloat,(std::string), float), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, float)", asMETHODPR(LocalStorage,set,(std::string, const float),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setFloat(string, float)", asMETHODPR(LocalStorage,set,(std::string, const float),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "float getFloat(string &in)",  asMETHODPR(LocalStorage,getFloat,(std::string&), float), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, float)", asMETHODPR(LocalStorage,set,(std::string&, const float),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setFloat(string &in, float)", asMETHODPR(LocalStorage,set,(std::string&, const float),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "vector3 getVector3(string)",          asMETHODPR(LocalStorage,getVector3,(std::string), Ogre::Vector3), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, const vector3 &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Vector3&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setVector3(string, const vector3 &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Vector3&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "vector3 getVector3(string &in)",          asMETHODPR(LocalStorage,getVector3,(std::string&), Ogre::Vector3), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, const vector3 &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Vector3&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setVector3(string &in, const vector3 &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Vector3&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "radian getRadian(string)",           asMETHODPR(LocalStorage,getRadian,(std::string), Ogre::Radian), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, const radian &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Radian&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setRadian(string, const radian &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Radian&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "radian getRadian(string &in)",           asMETHODPR(LocalStorage,getRadian,(std::string&), Ogre::Radian), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, const radian &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Radian&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setRadian(string &in, const radian &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Radian&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "degree getDegree(string)",           asMETHODPR(LocalStorage,getDegree,(std::string), Ogre::Degree), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, const degree &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Degree&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setDegree(string, const degree &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Degree&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "degree getDegree(string &in)",           asMETHODPR(LocalStorage,getDegree,(std::string&), Ogre::Degree), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, const degree &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Degree&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setDegree(string &in, const degree &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Degree&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "quaternion getQuaternion(string)",       asMETHODPR(LocalStorage,getQuaternion,(std::string), Ogre::Quaternion), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, const quaternion &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Quaternion&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setQuaternion(string, const quaternion &in)", asMETHODPR(LocalStorage,set,(std::string, const Ogre::Quaternion&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "quaternion getQuaternion(string &in)",       asMETHODPR(LocalStorage,getQuaternion,(std::string&), Ogre::Quaternion), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, const quaternion &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Quaternion&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setQuaternion(string &in, const quaternion &in)", asMETHODPR(LocalStorage,set,(std::string&, const Ogre::Quaternion&),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "bool getBool(string)",             asMETHODPR(LocalStorage,getBool,(std::string), bool), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, const bool &in)", asMETHODPR(LocalStorage,set,(std::string, const bool),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setBool(string, const bool &in)", asMETHODPR(LocalStorage,set,(std::string, const bool),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "bool getBool(string &in)",             asMETHODPR(LocalStorage,getBool,(std::string&), bool), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, const bool &in)", asMETHODPR(LocalStorage,set,(std::string&, const bool),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setBool(string &in, const bool &in)", asMETHODPR(LocalStorage,set,(std::string&, const bool),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "int getInt(string)",     asMETHODPR(LocalStorage,getInt,(std::string), int), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "int getInteger(string)", asMETHODPR(LocalStorage,getInt,(std::string), int), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void set(string, int)",  asMETHODPR(LocalStorage,set,(std::string, const int),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setInt(string, int)",  asMETHODPR(LocalStorage,set,(std::string, const int),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void setInteger(string, int)",  asMETHODPR(LocalStorage,set,(std::string, const int),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "int getInt(string &in)",     asMETHODPR(LocalStorage,getInt,(std::string&), int), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "int getInteger(string &in)", asMETHODPR(LocalStorage,getInt,(std::string&), int), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void set(string &in, int)",  asMETHODPR(LocalStorage,set,(std::string&, const int),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setInt(string &in, int)",  asMETHODPR(LocalStorage,set,(std::string&, const int),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void setInteger(string &in, int)",  asMETHODPR(LocalStorage,set,(std::string&, const int),void), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void save()",   asMETHOD(LocalStorage,saveDict), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "bool reload()", asMETHOD(LocalStorage,loadDict), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
-    r = engine->RegisterObjectMethod("LocalStorage", "void save()",   asMETHOD(LocalStorage,saveDict), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "bool reload()", asMETHOD(LocalStorage,loadDict), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-
-    r = engine->RegisterObjectMethod("LocalStorage", "bool exists(string &in) const", asMETHOD(LocalStorage,exists), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectMethod("LocalStorage", "void delete(string &in)",       asMETHOD(LocalStorage,eraseKey), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-
-    // Register GC behaviours
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_GETREFCOUNT, "int f()", asMETHOD(LocalStorage,GetRefCount), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_SETGCFLAG, "void f()",  asMETHOD(LocalStorage,SetGCFlag), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_GETGCFLAG, "bool f()",  asMETHOD(LocalStorage,GetGCFlag), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_ENUMREFS, "void f(int&in)", asMETHOD(LocalStorage,EnumReferences), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
-    r = engine->RegisterObjectBehaviour("LocalStorage", asBEHAVE_RELEASEREFS, "void f(int&in)", asMETHOD(LocalStorage,ReleaseAllReferences), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "bool exists(string &in) const", asMETHOD(LocalStorage,exists), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
+    r = engine->RegisterObjectMethod("LocalStorageClass", "void delete(string &in)",       asMETHOD(LocalStorage,eraseKey), asCALL_THISCALL); ROR_ASSERT( r >= 0 );
 
 }
