4395686932de1499fdabd45fd51c166e24256ad2
Edgar <Edgar@AnotherFoxGuy.com>
2023-03-28 12:16:16 +0200

    :wrench: Updated caelum version

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class RoR(ConanFile):
         self.requires("fmt/8.0.1")
         self.requires("libcurl/7.79.1")
         self.requires("mygui/3.4.0@anotherfoxguy/stable")
-        self.requires("ogre3d-caelum/0.6.3@rigs-of-rods/stable")
+        self.requires("ogre3d-caelum/0.6.3.1@anotherfoxguy/stable")
         self.requires("ogre3d-pagedgeometry/1.2.0@anotherfoxguy/stable")
         self.requires("ogre3d/1.11.6.1@anotherfoxguy/stable", force=True)
         self.requires("ois/1.4.1@rigsofrods/custom")
