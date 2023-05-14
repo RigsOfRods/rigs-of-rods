d128f013d9b8a4673f9e2ace61f0c839d60223d1
Petr Ohlidal <petr@ohlidal.cz>
2023-05-14 11:07:40 +0200

    valgrind memcheck: fixed "[RoR|Actor|Warning]  (): Selected module not found: "
    (Missing empty string check :|)
    
    ==129104== Conditional jump or move depends on uninitialised value(s)
    ==129104==    at 0x43456C: RoR::ActorSpawner::AddMessage(RoR::ActorSpawner::Message, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) (ActorSpawner.cpp:5594)
    ==129104==    by 0x43606F: RoR::ActorSpawner::ConfigureSections(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&, std::shared_ptr<RigDef::Document>) (ActorSpawner.cpp:101)
    ==129104==    by 0x42D4A0: RoR::ActorManager::CreateNewActor(RoR::ActorSpawnRequest, std::shared_ptr<RigDef::Document>) (ActorManager.cpp:89)
    ==129104==    by 0x1DD506: RoR::GameContext::SpawnActor(RoR::ActorSpawnRequest&) (GameContext.cpp:235)
    ==129104==    by 0x5AFD78: RoR::GameScript::spawnTruckAI(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&, Ogre::Vector<3, float>&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&, int) (GameScript.cpp:959)
    ==129104==    by 0x63EC07: X64_CallFunction(unsigned long const*, int, unsigned long (*)(), unsigned long&, bool) (as_callfunc_x64_gcc.cpp:74)

--- source/main/physics/ActorSpawner.h
+++ source/main/physics/ActorSpawner.h
@@ -463,7 +463,7 @@ private:
     int                            m_first_wing_index;
     std::vector<CabTexcoord>       m_oldstyle_cab_texcoords;
     std::vector<CabSubmesh>        m_oldstyle_cab_submeshes;    
-    RigDef::Keyword                m_current_keyword; //!< For error reports    
+    RigDef::Keyword                m_current_keyword = RigDef::Keyword::INVALID; //!< For error reports    
     std::map<Ogre::String, unsigned int> m_named_nodes;
     /// @}
 
