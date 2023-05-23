f3817f5eaf1dc52e1a505fbe1cbc14b58b4241e5
Edgar <Edgar@AnotherFoxGuy.com>
2023-05-23 11:34:23 +0200

    :arrow_up: Updated dependencies

--- conanfile.py
+++ conanfile.py
@@ -7,16 +7,20 @@ class RoR(ConanFile):
     name = "Rigs of Rods"
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
+    default_options = {
+        "ogre3d*:nodeless_positioning": "True",
+        "ogre3d*:resourcemanager_strict": "off"
+    }
 
     def requirements(self):
         self.requires("angelscript/2.35.1")
         self.requires("discord-rpc/3.4.0@anotherfoxguy/stable")
         self.requires("fmt/8.0.1")
         self.requires("libcurl/7.79.1")
-        self.requires("mygui/3.4.0@anotherfoxguy/stable")
-        self.requires("ogre3d-caelum/0.6.3.1@anotherfoxguy/stable")
-        self.requires("ogre3d-pagedgeometry/1.2.0@anotherfoxguy/stable")
-        self.requires("ogre3d/1.11.6.1@anotherfoxguy/stable", force=True)
+        self.requires("mygui/3.4.2@anotherfoxguy/stable")
+        self.requires("ogre3d-caelum/2022.08@anotherfoxguy/stable")
+        self.requires("ogre3d-pagedgeometry/2022.08@anotherfoxguy/stable")
+        self.requires("ogre3d/13.6.4@anotherfoxguy/stable", force=True)
         self.requires("ois/1.4.1@rigsofrods/custom")
         self.requires("openal-soft/1.22.2")
         self.requires("openssl/1.1.1s", force=True)
@@ -26,6 +30,7 @@ class RoR(ConanFile):
         self.requires("libpng/1.6.39", override=True)
         self.requires("libwebp/1.3.0", override=True)
         self.requires("zlib/1.2.13", override=True)
+        self.requires("xz_utils/5.4.2", override=True)
 
     def generate(self):
         for dep in self.dependencies.values():
