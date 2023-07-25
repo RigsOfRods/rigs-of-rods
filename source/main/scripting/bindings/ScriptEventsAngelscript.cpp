0284250e330121a453b5acb8fe253e91a7e82743
Petr Ohlidal <petr@ohlidal.cz>
2023-07-26 01:23:05 +0200

    :angel: Script: added `SE_ANGELSCRIPT_LINECALLBACK` for extra diag.
        * LineCallback = Optional callback which receives diagnostic info for every executed statement.
        * https://www.angelcode.com/angelscript/sdk/docs/manual/classas_i_script_context.html#lineCallback
        * see included 'example_angelscript_linecallback.as'

--- source/main/scripting/bindings/ScriptEventsAngelscript.cpp
+++ source/main/scripting/bindings/ScriptEventsAngelscript.cpp
@@ -57,6 +57,7 @@ void RoR::RegisterScriptEvents(asIScriptEngine *engine)
 
     result = engine->RegisterEnumValue("scriptEvents", "SE_ANGELSCRIPT_MANIPULATIONS", SE_ANGELSCRIPT_MANIPULATIONS); ROR_ASSERT(result>=0);
     result = engine->RegisterEnumValue("scriptEvents", "SE_ANGELSCRIPT_MSGCALLBACK", SE_ANGELSCRIPT_MSGCALLBACK); ROR_ASSERT(result>=0);
+    result = engine->RegisterEnumValue("scriptEvents", "SE_ANGELSCRIPT_LINECALLBACK", SE_ANGELSCRIPT_LINECALLBACK); ROR_ASSERT(result>=0);
     result = engine->RegisterEnumValue("scriptEvents", "SE_ANGELSCRIPT_THREAD_STATUS", SE_ANGELSCRIPT_THREAD_STATUS); ROR_ASSERT(result>=0);
 
     result = engine->RegisterEnumValue("scriptEvents", "SE_GENERIC_MESSAGEBOX_CLICK", SE_GENERIC_MESSAGEBOX_CLICK); ROR_ASSERT(result>=0);
