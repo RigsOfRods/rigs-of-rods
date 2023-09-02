RigsOfRods/rigs-of-rods

commit 3111ec97917d6c7eea461d6873687701f6cdb8b2

Codechange: exposed `ar_linked_actors`
CHANGES:
* Actor.h: exposed `ar_linked_actors`, deleted `getAllLinkedActors()` - lots of find&replace
* PointColDetector.h/cpp: removed the local copy (it was likely a multithreading hack, but now that we have MSG_SIM_ACTOR_LINKING_REQUESTED it's meaningless)
* Find&replaced `for (ActorPtr actor` to `for (ActorPtr& actor` to reduce refcount overhead
* some corrections related to `auto` keyword.

source/main/GameContext.cpp
@@ -391,15 +391,14 @@ void GameContext::DeleteActor(ActorPtr actor)
     }
 
     // Find linked actors and un-tie if tied
-    auto linked_actors = actor->getAllLinkedActors();
     for (auto actorx : m_actor_manager.GetLocalActors())
     {
-        if (actorx->isTied() && std::find(linked_actors.begin(), linked_actors.end(), actorx) != linked_actors.end())
+        if (actorx->isTied() && std::find(actor->ar_linked_actors.begin(), actor->ar_linked_actors.end(), actorx) != actor->ar_linked_actors.end())
         {
             actorx->tieToggle(); // OK to invoke here - processing MSG_SIM_DELETE_ACTOR_REQUESTED
         }
 
-        if (actorx->isLocked() && std::find(linked_actors.begin(), linked_actors.end(), actorx) != linked_actors.end())
+        if (actorx->isLocked() && std::find(actor->ar_linked_actors.begin(), actor->ar_linked_actors.end(), actorx) != actor->ar_linked_actors.end())
         {
             actorx->hookToggle(); // OK to invoke here - processing MSG_SIM_DELETE_ACTOR_REQUESTED
         }
@@ -610,7 +609,7 @@ void GameContext::ShowLoaderGUI(int type, const Ogre::String& instance, const Og
     if (!(App::mp_state->getEnum<MpState>() == MpState::CONNECTED))
     {
         collision_box_t* spawnbox = m_terrain->GetCollisions()->getBox(instance, box);
-        for (ActorPtr actor : this->GetActorManager()->GetActors())
+        for (ActorPtr& actor : this->GetActorManager()->GetActors())
         {
             for (int i = 0; i < actor->ar_num_nodes; i++)
             {
@@ -845,12 +844,13 @@ void GameContext::TeleportPlayer(float x, float z)
 
     Ogre::Vector3 translation = Ogre::Vector3(x, y, z) - this->GetPlayerActor()->ar_nodes[0].AbsPosition;
 
-    auto actors = this->GetPlayerActor()->getAllLinkedActors();
-    actors.push_back(this->GetPlayerActor());
+    std::vector<ActorPtr> actorsToBeamUp;
+    actorsToBeamUp.push_back(this->GetPlayerActor());
+    actorsToBeamUp.assign(this->GetPlayerActor()->ar_linked_actors.begin(), this->GetPlayerActor()->ar_linked_actors.end());
 
     float src_agl = std::numeric_limits<float>::max(); 
     float dst_agl = std::numeric_limits<float>::max(); 
-    for (ActorPtr actor : actors)
+    for (ActorPtr& actor : actorsToBeamUp)
     {
         for (int i = 0; i < actor->ar_num_nodes; i++)
         {
@@ -863,7 +863,7 @@ void GameContext::TeleportPlayer(float x, float z)
 
     translation += Ogre::Vector3::UNIT_Y * (std::max(0.0f, src_agl) - dst_agl);
 
-    for (ActorPtr actor : actors)
+    for (ActorPtr& actor : actorsToBeamUp)
     {
         actor->resetPosition(actor->ar_nodes[0].AbsPosition + translation, false);
     }
@@ -1329,7 +1329,7 @@ void GameContext::UpdateCommonInputEvents(float dt)
     if (App::GetInputEngine()->getEventBoolValueBounce(EV_COMMON_TOGGLE_DEBUG_VIEW))
     {
         m_player_actor->GetGfxActor()->ToggleDebugView();
-        for (ActorPtr actor : m_player_actor->getAllLinkedActors())
+        for (ActorPtr& actor : m_player_actor->ar_linked_actors)
         {
             actor->GetGfxActor()->SetDebugView(m_player_actor->GetGfxActor()->GetDebugView());
         }
@@ -1338,7 +1338,7 @@ void GameContext::UpdateCommonInputEvents(float dt)
     if (App::GetInputEngine()->getEventBoolValueBounce(EV_COMMON_CYCLE_DEBUG_VIEWS))
     {
         m_player_actor->GetGfxActor()->CycleDebugViews();
-        for (ActorPtr actor : m_player_actor->getAllLinkedActors())
+        for (ActorPtr& actor : m_player_actor->ar_linked_actors)
         {
             actor->GetGfxActor()->SetDebugView(m_player_actor->GetGfxActor()->GetDebugView());
         }
@@ -1400,7 +1400,7 @@ void GameContext::UpdateCommonInputEvents(float dt)
     // toggle physics
     if (RoR::App::GetInputEngine()->getEventBoolValueBounce(EV_TRUCK_TOGGLE_PHYSICS))
     {
-        for (ActorPtr actor : App::GetGameContext()->GetPlayerActor()->getAllLinkedActors())
+        for (ActorPtr& actor : App::GetGameContext()->GetPlayerActor()->ar_linked_actors)
         {
             actor->ar_physics_paused = !App::GetGameContext()->GetPlayerActor()->ar_physics_paused;
         }
@@ -1847,7 +1847,7 @@ void GameContext::UpdateTruckInputEvents(float dt)
     }
 
     m_player_actor->UpdatePropAnimInputEvents();
-    for (ActorPtr linked_actor : m_player_actor->getAllLinkedActors())
+    for (ActorPtr linked_actor : m_player_actor->ar_linked_actors)
     {
         linked_actor->UpdatePropAnimInputEvents();
     }

source/main/gameplay/Character.cpp
@@ -159,7 +159,7 @@ void Character::update(float dt)
         // Submesh "collision"
         {
             float depth = 0.0f;
-            for (ActorPtr actor : App::GetGameContext()->GetActorManager()->GetActors())
+            for (ActorPtr& actor : App::GetGameContext()->GetActorManager()->GetActors())
             {
                 if (actor->ar_bounding_box.contains(position))
                 {

source/main/gameplay/RecoveryMode.cpp
@@ -115,7 +115,7 @@ void RecoveryMode::UpdateInputEvents(float dt)
 
             if (App::sim_soft_reset_mode->getBool())
             {
-                for (ActorPtr actor : App::GetGameContext()->GetPlayerActor()->getAllLinkedActors())
+                for (ActorPtr& actor : App::GetGameContext()->GetPlayerActor()->ar_linked_actors)
                 {
                     actor->requestRotation(rotation, rotation_center);
                     actor->requestTranslation(translation);
@@ -129,7 +129,7 @@ void RecoveryMode::UpdateInputEvents(float dt)
             App::GetGameContext()->GetPlayerActor()->requestAngleSnap(45);
             if (App::sim_soft_reset_mode->getBool())
             {
-                for (ActorPtr actor : App::GetGameContext()->GetPlayerActor()->getAllLinkedActors())
+                for (ActorPtr& actor : App::GetGameContext()->GetPlayerActor()->ar_linked_actors)
                 {
                     actor->requestAngleSnap(45);
                 }
@@ -144,7 +144,7 @@ void RecoveryMode::UpdateInputEvents(float dt)
         if (App::sim_soft_reset_mode->getBool())
         {
             reset_type = ActorModifyRequest::Type::SOFT_RESET;
-            for (ActorPtr actor : App::GetGameContext()->GetPlayerActor()->getAllLinkedActors())
+            for (ActorPtr& actor : App::GetGameContext()->GetPlayerActor()->ar_linked_actors)
             {
                 ActorModifyRequest* rq = new ActorModifyRequest;
                 rq->amr_actor = actor->ar_instance_id;

source/main/gameplay/VehicleAI.cpp
@@ -419,7 +419,7 @@ void VehicleAI::update(float dt, int doUpdate)
             }
 
             // Collision avoidance with other actors
-            for (ActorPtr actor : App::GetGameContext()->GetActorManager()->GetActors())
+            for (ActorPtr& actor : App::GetGameContext()->GetActorManager()->GetActors())
             {
                 if (actor->ar_driveable == NOT_DRIVEABLE) // Ignore objects that may be actors
                     continue;
@@ -490,7 +490,7 @@ void VehicleAI::update(float dt, int doUpdate)
             }
 
             // Collision avoidance with other actors
-            for (ActorPtr actor : App::GetGameContext()->GetActorManager()->GetActors())
+            for (ActorPtr& actor : App::GetGameContext()->GetActorManager()->GetActors())
             {
                 if (actor->ar_driveable == NOT_DRIVEABLE) // Ignore objects that may be actors
                     continue;

source/main/gfx/GfxActor.cpp
@@ -1796,7 +1796,7 @@ void RoR::GfxActor::UpdateSimDataBuffer()
 
     // Linked Actors
     m_linked_gfx_actors.clear();
-    for (ActorPtr actor : m_actor->getAllLinkedActors())
+    for (ActorPtr& actor : m_actor->ar_linked_actors)
     {
         m_linked_gfx_actors.insert(actor->GetGfxActor());
     }

source/main/gfx/camera/CameraManager.cpp
@@ -1125,7 +1125,7 @@ void CameraManager::CameraBehaviorVehicleSplineUpdate()
         m_cam_target_pitch = -asin(dir.dotProduct(Vector3::UNIT_Y));
     }
 
-    if (m_cct_player_actor->getAllLinkedActors().size() != m_splinecam_num_linked_beams)
+    if (m_cct_player_actor->ar_linked_actors.size() != m_splinecam_num_linked_beams)
     {
         this->CameraBehaviorVehicleSplineCreateSpline();
     }
@@ -1243,13 +1243,13 @@ void CameraManager::CameraBehaviorVehicleSplineCreateSpline()
         m_splinecam_spline_nodes.push_back(&m_cct_player_actor->ar_nodes[m_cct_player_actor->ar_camera_rail[i]]);
     }
 
-    auto linkedBeams = m_cct_player_actor->getAllLinkedActors();
+    auto linkedBeams = m_cct_player_actor->ar_linked_actors;
 
     m_splinecam_num_linked_beams = static_cast<int>(linkedBeams.size());
 
     if (m_splinecam_num_linked_beams > 0)
     {
-        for (ActorPtr actor : linkedBeams)
+        for (ActorPtr& actor : linkedBeams)
         {
             if (actor->ar_num_camera_rails <= 0)
                 continue;

source/main/gui/panels/GUI_TopMenubar.cpp
@@ -336,7 +336,7 @@ void TopMenubar::Update()
                     ImGui::PushStyleColor(ImGuiCol_Text, ORANGE_TEXT);
                     if (ImGui::Button(_LC("TopMenubar", " [!] Confirm removal")))
                     {
-                        for (ActorPtr actor : App::GetGameContext()->GetActorManager()->GetLocalActors())
+                        for (ActorPtr& actor : App::GetGameContext()->GetActorManager()->GetLocalActors())
                         {
                             if (!actor->ar_hide_in_actor_list && !actor->isPreloadedWithTerrain() && 
                                     actor->ar_state != ActorState::NETWORKED_OK)
@@ -1188,7 +1188,7 @@ void TopMenubar::Update()
                     App::GetGuiManager()->SurveyMap.ai_waypoints.clear();
                 }
 
-                for (ActorPtr actor : App::GetGameContext()->GetActorManager()->GetLocalActors())
+                for (ActorPtr& actor : App::GetGameContext()->GetActorManager()->GetLocalActors())
                 {
                     if (actor->ar_driveable == AI)
                     {
@@ -1472,7 +1472,7 @@ void TopMenubar::DrawMpUserToActorList(RoRnet::UserInfo &user)
 void TopMenubar::DrawActorListSinglePlayer()
 {
     std::vector<ActorPtr> actor_list;
-    for (ActorPtr actor : App::GetGameContext()->GetActorManager()->GetActors())
+    for (ActorPtr& actor : App::GetGameContext()->GetActorManager()->GetActors())
     {
         if (!actor->ar_hide_in_actor_list)
         {
@@ -1490,7 +1490,7 @@ void TopMenubar::DrawActorListSinglePlayer()
     {
         ActorPtr player_actor = App::GetGameContext()->GetPlayerActor();
         int i = 0;
-        for (ActorPtr actor : actor_list)
+        for (ActorPtr& actor : actor_list)
         {
             std::string text_buf_rem = fmt::format("X ##[{}]", i);
             ImGui::PushStyleColor(ImGuiCol_Text, RED_TEXT);
@@ -1502,12 +1502,11 @@ void TopMenubar::DrawActorListSinglePlayer()
             ImGui::SameLine();
 
             std::string text_buf = fmt::format( "[{}] {}", i++, StripColorMarksFromText(actor->ar_design_name).c_str());
-            auto linked_actors = actor->getAllLinkedActors();
             if (actor == player_actor)
             {
                 ImGui::PushStyleColor(ImGuiCol_Text, GREEN_TEXT);
             }
-            else if (std::find(linked_actors.begin(), linked_actors.end(), player_actor) != linked_actors.end())
+            else if (std::find(actor->ar_linked_actors.begin(), actor->ar_linked_actors.end(), player_actor) != actor->ar_linked_actors.end())
             {
                 ImGui::PushStyleColor(ImGuiCol_Text, ORANGE_TEXT);
             }

source/main/gui/panels/GUI_VehicleButtons.cpp
@@ -627,7 +627,7 @@ void VehicleButtons::DrawActorPhysicsButton(RoR::GfxActor* actorx)
 
     if (ImGui::ImageButton(reinterpret_cast<ImTextureID>(m_actor_physics_icon->getHandle()), ImVec2(24, 24)))
     {
-        for (ActorPtr actor : actorx->GetActor()->getAllLinkedActors())
+        for (ActorPtr& actor : actorx->GetActor()->ar_linked_actors)
         {
             actor->ar_physics_paused = !actorx->GetActor()->ar_physics_paused;
         }

commit 7df5f66233771ea2d84496be9bb095d847ea81f9

Fixes #3079 - Minimap teleport doesn't work if in vehicle
Broken in https://github.com/RigsOfRods/rigs-of-rods/commit/3111ec97917d6c7eea461d6873687701f6cdb8b2 - adding linked actors to the worklist accidentally overwrites the player actor.

source/main/GameContext.cpp
@@ -845,8 +845,8 @@ void GameContext::TeleportPlayer(float x, float z)
     Ogre::Vector3 translation = Ogre::Vector3(x, y, z) - this->GetPlayerActor()->ar_nodes[0].AbsPosition;
 
     std::vector<ActorPtr> actorsToBeamUp;
-    actorsToBeamUp.push_back(this->GetPlayerActor());
     actorsToBeamUp.assign(this->GetPlayerActor()->ar_linked_actors.begin(), this->GetPlayerActor()->ar_linked_actors.end());
+    actorsToBeamUp.push_back(this->GetPlayerActor());
 
     float src_agl = std::numeric_limits<float>::max(); 
     float dst_agl = std::numeric_limits<float>::max(); 