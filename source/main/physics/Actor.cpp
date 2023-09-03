RigsOfRods/rigs-of-rods

commit 539a1b24f50bad880611f245c76b634dbde1ad84

Any node now activates script event boxes
Previously only the main camera node activated eventboxes. This was a bad fit for i.e. races vs. the finishing line.

The process of invoking the event boxes was inverted - instead of searching an event box for the camera node, all event boxes close to the actor are searched for a hit with any node. Hits are recorded and next time the recorded colliding node is checked first.

I checked performance impact on Auriga race under Debug. There seems to be no noticeable FPS drop.

Code changes:
* Actor::CalcForcesEulerCompute() - call to UpdateBoundingBoxes() moved here from CalcNodes(). Added call to CalcEventBoxes().
* Actor::CalcEventBoxes() - searches and records hits with eventboxes, as described above.
* Collisions::nodeCollision() - no longer processes event boxes, the parameter was removed.

source/main/physics/Actor.h
@@ -402,6 +402,8 @@ public:
     float             ar_collision_range;             //!< Physics attr
     float             ar_top_speed;                   //!< Sim state
     ground_model_t*   ar_last_fuzzy_ground_model;     //!< GUI state
+    CollisionBoxPtrVec m_potential_eventboxes;
+    std::vector<std::pair<collision_box_t*, NodeNum_t>> m_active_eventboxes;
 
     // Gameplay state
     ActorState        ar_state;
@@ -455,7 +457,8 @@ private:
     void              CalcHooks();                         
     void              CalcHydros();                        
     void              CalcMouse();                         
-    void              CalcNodes();                         
+    void              CalcNodes();
+    void              CalcEventBoxes();
     void              CalcReplay();                        
     void              CalcRopes();                         
     void              CalcShocks(bool doUpdate, int num_steps); 

source/main/physics/collision/Collisions.cpp
@@ -604,11 +604,21 @@ void Collisions::envokeScriptCallback(collision_box_t *cbox, node_t *node)
         return;
     
     std::lock_guard<std::mutex> lock(m_scriptcallback_mutex);
-    // this prevents that the same callback gets called at 2k FPS all the time, serious hit on FPS ...
-    if (std::find(std::begin(m_last_called_cboxes), std::end(m_last_called_cboxes), cbox) == m_last_called_cboxes.end())
+    if (node)
     {
+        // An actor is activating the eventbox
+        // Duplicate invocation is prevented by `Actor::m_active_eventboxes` cache.
         App::GetScriptEngine()->envokeCallback(eventsources[cbox->eventsourcenum].scripthandler, &eventsources[cbox->eventsourcenum], node);
-        m_last_called_cboxes.push_back(cbox);
+    }
+    else
+    {
+        // A character is activating the eventbox
+        // this prevents that the same callback gets called at 2k FPS all the time, serious hit on FPS ... 
+        if (std::find(std::begin(m_last_called_cboxes), std::end(m_last_called_cboxes), cbox) == m_last_called_cboxes.end())
+        {
+            App::GetScriptEngine()->envokeCallback(eventsources[cbox->eventsourcenum].scripthandler, &eventsources[cbox->eventsourcenum], node);
+            m_last_called_cboxes.push_back(cbox);
+        }
     }
 #endif //USE_ANGELSCRIPT
 }
@@ -914,7 +924,7 @@ bool Collisions::permitEvent(CollisionEventFilter filter)
     }
 }
 
-bool Collisions::nodeCollision(node_t *node, float dt, bool envokeScriptCallbacks)
+bool Collisions::nodeCollision(node_t *node, float dt)
 {
     // find the correct cell
     int refx = (int)(node->AbsPosition.x / CELL_SIZE);
@@ -965,17 +975,12 @@ bool Collisions::nodeCollision(node_t *node, float dt, bool envokeScriptCallback
                     // now test with the inner box
                     if (Pos > cbox->relo && Pos < cbox->rehi)
                     {
-                        if (cbox->eventsourcenum!=-1 && permitEvent(cbox->event_filter) && envokeScriptCallbacks)
-                        {
-                            envokeScriptCallback(cbox, node);
-                            isScriptCallbackEnvoked = true;
-                        }
                         if (cbox->camforced && !forcecam)
                         {
                             forcecam = true;
                             forcecampos = cbox->campos;
                         }
-                        if (!cbox->virt && !envokeScriptCallbacks)
+                        if (!cbox->virt)
                         {
                             // collision, process as usual
                             // we have a collision
@@ -1005,17 +1010,12 @@ bool Collisions::nodeCollision(node_t *node, float dt, bool envokeScriptCallback
                     }
                 } else
                 {
-                    if (cbox->eventsourcenum!=-1 && permitEvent(cbox->event_filter) && envokeScriptCallbacks)
-                    {
-                        envokeScriptCallback(cbox, node);
-                        isScriptCallbackEnvoked = true;
-                    }
                     if (cbox->camforced && !forcecam)
                     {
                         forcecam = true;
                         forcecampos = cbox->campos;
                     }
-                    if (!cbox->virt && !envokeScriptCallbacks)
+                    if (!cbox->virt)
                     {
                         // we have a collision
                         contacted=true;
@@ -1071,11 +1071,8 @@ bool Collisions::nodeCollision(node_t *node, float dt, bool envokeScriptCallback
         }
     }
 
-    if (envokeScriptCallbacks && !isScriptCallbackEnvoked)
-        clearEventCache();
-
     // process minctri collision
-    if (minctri && !envokeScriptCallbacks)
+    if (minctri)
     {
         // we have a contact
         contacted=true;
@@ -1089,6 +1086,46 @@ bool Collisions::nodeCollision(node_t *node, float dt, bool envokeScriptCallback
     return contacted;
 }
 
+void Collisions::findPotentialEventBoxes(Ogre::AxisAlignedBox const& aabb, CollisionBoxPtrVec& out_boxes)
+{
+    // Find collision cells occupied by the actor (remember 'Y' is 'up').
+    const int cell_lo_x = (int)(aabb.getMinimum().x / (float)CELL_SIZE);
+    const int cell_lo_z = (int)(aabb.getMinimum().z / (float)CELL_SIZE);
+    const int cell_hi_x = (int)(aabb.getMaximum().x / (float)CELL_SIZE);
+    const int cell_hi_z = (int)(aabb.getMaximum().z / (float)CELL_SIZE);
+
+    // Loop the collision cells
+    for (int refx = cell_lo_x; refx <= cell_hi_x; refx++)
+    {
+        for (int refz = cell_lo_z; refz <= cell_hi_z; refz++)
+        {
+            // Find current cell
+            const int hash = this->hash_find(refx, refz);
+            const unsigned int cell_id = (refx << 16) + refz;
+
+            // Find eligible event boxes in the cell
+            for (size_t k = 0; k < hashtable[hash].size(); k++)
+            {
+                if (hashtable[hash][k].cell_id != cell_id)
+                {
+                    continue;
+                }
+                else if (hashtable[hash][k].IsCollisionBox())
+                {
+                    collision_box_t* cbox = &m_collision_boxes[hashtable[hash][k].element_index];
+
+                    if (!cbox->enabled)
+                        continue;
+
+                    if (cbox->eventsourcenum != -1 && this->permitEvent(cbox->event_filter))
+                    {
+                        out_boxes.push_back(cbox);
+                    }
+                }
+            }
+        }
+    }
+}
 
 Vector3 Collisions::getPosition(const Ogre::String &inst, const Ogre::String &box)
 {

source/main/physics/ActorForcesEuler.cpp
@@ -45,6 +45,8 @@ using namespace RoR;
 void Actor::CalcForcesEulerCompute(bool doUpdate, int num_steps)
 {
     this->CalcNodes(); // must be done directly after the inter truck collisions are handled
+    this->UpdateBoundingBoxes();
+    this->CalcEventBoxes();
     this->CalcReplay();
     this->CalcAircraftForces(doUpdate);
     this->CalcFuseDrag();
@@ -1532,7 +1534,7 @@ void Actor::CalcNodes()
         {
             Vector3 oripos = ar_nodes[i].AbsPosition;
             bool contacted = App::GetGameContext()->GetTerrain()->GetCollisions()->groundCollision(&ar_nodes[i], PHYSICS_DT);
-            contacted = contacted | App::GetGameContext()->GetTerrain()->GetCollisions()->nodeCollision(&ar_nodes[i], PHYSICS_DT, false);
+            contacted = contacted | App::GetGameContext()->GetTerrain()->GetCollisions()->nodeCollision(&ar_nodes[i], PHYSICS_DT);
             ar_nodes[i].nd_has_ground_contact = contacted;
             if (ar_nodes[i].nd_has_ground_contact || ar_nodes[i].nd_has_mesh_contact)
             {
@@ -1548,8 +1550,6 @@ void Actor::CalcNodes()
         {
             // record g forces on cameras
             m_camera_gforces_accu += ar_nodes[i].Forces / ar_nodes[i].mass;
-            // trigger script callbacks
-            App::GetGameContext()->GetTerrain()->GetCollisions()->nodeCollision(&ar_nodes[i], PHYSICS_DT, true);
         }
 
         // integration
@@ -1616,8 +1616,67 @@ void Actor::CalcNodes()
             ar_nodes[i].nd_under_water = is_under_water;
         }
     }
+}
 
-    this->UpdateBoundingBoxes();
+void Actor::CalcEventBoxes()
+{
+    // Assumption: node positions and bounding boxes are up to date.
+    // First, find all collision boxes which this actor's bounding box touches (potential collisions)
+    // For each potential collision box:
+    // * if a collision was already recorded, test the recorded node. If still colliding, do nothing.
+    // * otherwise loop nodes until collision is found. If not, clear the collision record.
+    // ----------------------------------------------------------------------------------------------
+
+    m_potential_eventboxes.clear();
+    App::GetGameContext()->GetTerrain()->GetCollisions()->findPotentialEventBoxes(ar_bounding_box, m_potential_eventboxes);
+
+    for (collision_box_t* cbox : m_potential_eventboxes)
+    {
+        // Find existing collision record
+        bool has_collision = false;
+        bool do_callback = true;
+        auto itor = m_active_eventboxes.begin();
+        while (itor != m_active_eventboxes.end())
+        {
+            if (itor->first == cbox)
+            {
+                // Existing record found - check if the node still collides
+                has_collision = App::GetGameContext()->GetTerrain()->GetCollisions()->isInside(ar_nodes[itor->second].AbsPosition, cbox);
+                if (!has_collision)
+                {
+                    // Erase the collision record
+                    itor = m_active_eventboxes.erase(itor);
+                    // Prevent invoking the same callback again
+                    do_callback = false;
+                }
+                break;
+            }
+            else
+            {
+                itor++;
+            }
+        }
+
+        if (!has_collision)
+        {
+            // Find if any node collides
+            for (NodeNum_t i = 0; i < ar_num_nodes; i++)
+            {
+                has_collision = App::GetGameContext()->GetTerrain()->GetCollisions()->isInside(ar_nodes[i].AbsPosition, cbox);
+                if (has_collision)
+                {
+                    // Add new collision record
+                    m_active_eventboxes.push_back(std::make_pair(cbox, i));
+                    // Do the script callback
+                    if (do_callback)
+                    {
+                        App::GetGameContext()->GetTerrain()->GetCollisions()->envokeScriptCallback(cbox, &ar_nodes[i]);
+                    }
+                    break;
+                }
+            }
+        }
+    }
 }
 
 void Actor::CalcHooks()

source/main/physics/SimData.h
@@ -697,6 +697,7 @@ struct collision_box_t
     Ogre::Vector3 campos;       //!< camera position
     Ogre::Vector3 debug_verts[8];//!< box corners in absolute world position
 };
+typedef std::vector<collision_box_t*> CollisionBoxPtrVec;
 
 /// Surface friction properties.
 struct ground_model_t

source/main/physics/collision/Collisions.h
@@ -130,7 +130,7 @@ private:
 
     // collision boxes pool
     CollisionBoxVec m_collision_boxes; // Formerly MAX_COLLISION_BOXES = 5000
-    std::vector<collision_box_t*> m_last_called_cboxes;
+    std::vector<collision_box_t*> m_last_called_cboxes; // Only used for character, actors have their own cache `Actor::m_active_eventboxes`
 
     // collision tris pool
     CollisionTriVec m_collision_tris; // Formerly MAX_COLLISION_TRIS = 100000
@@ -150,7 +150,6 @@ private:
     int free_eventsource;
 
     bool permitEvent(CollisionEventFilter filter);
-    void envokeScriptCallback(collision_box_t* cbox, node_t* node = 0);
 
     Landusemap* landuse;
     int collision_version;
@@ -188,7 +187,9 @@ public:
     bool groundCollision(node_t* node, float dt);
     bool isInside(Ogre::Vector3 pos, const Ogre::String& inst, const Ogre::String& box, float border = 0);
     bool isInside(Ogre::Vector3 pos, collision_box_t* cbox, float border = 0);
-    bool nodeCollision(node_t* node, float dt, bool envokeScriptCallbacks = true);
+    bool nodeCollision(node_t* node, float dt);
+    void findPotentialEventBoxes(Ogre::AxisAlignedBox const& aabb, CollisionBoxPtrVec& out_boxes);
+    void envokeScriptCallback(collision_box_t* cbox, node_t* node = 0);
 
     void finishLoadingTerrain();
 

commit 008e77afce4d9e0ec5e8d517e5679ffae0ab74ba

(3rd edition) Fixed huge FPS drop when nodes move far away from vehicle
Fixed #3058 - caused by 539a1b24f50bad880611f245c76b634dbde1ad84

Changes:
* for eventbox triggering, only consider a bounding box around collision cab nodes. It makes more sense physically and also eliminates surprises, as loose cab nodes would be easily visible.
* ignore cabnodes farther than 15m

source/main/physics/Actor.cpp
@@ -1116,11 +1116,17 @@ void Actor::UpdateBoundingBoxes()
     // Reset
     ar_bounding_box = AxisAlignedBox::BOX_NULL;
     ar_predicted_bounding_box = AxisAlignedBox::BOX_NULL;
+    ar_cabnodes_bounding_box = AxisAlignedBox::BOX_NULL;
     for (size_t i = 0; i < ar_collision_bounding_boxes.size(); ++i)
     {
         ar_collision_bounding_boxes[i] = AxisAlignedBox::BOX_NULL;
         ar_predicted_coll_bounding_boxes[i] = AxisAlignedBox::BOX_NULL;
     }
+
+    // To avoid performance choking by overstretched bounding box (happens when vehicle drops some nodes),
+    // we set a maximum distance limit from the main camera.
+    const float CABNODE_MAX_CAMDIST = 15.f;
+    const Ogre::Vector3 mainCamPos = ar_nodes[ar_main_camera_node_pos].RelPosition;
 
     // Update
     for (int i = 0; i < ar_num_nodes; i++)
@@ -1130,6 +1136,11 @@ void Actor::UpdateBoundingBoxes()
         int16_t cid = ar_nodes[i].nd_coll_bbox_id;
 
         ar_bounding_box.merge(pos);                                  // Current box
+        if (ar_nodes[i].nd_cab_node                                 // Current cab-nodes box (for eventbox collisions)
+            && (mainCamPos.squaredDistance(ar_nodes[i].RelPosition)) < (CABNODE_MAX_CAMDIST*CABNODE_MAX_CAMDIST)) // ... we compare squared distance for performance
+        {
+            ar_cabnodes_bounding_box.merge(pos);
+        }
         ar_predicted_bounding_box.merge(pos);                        // Predicted box (current position)
         ar_predicted_bounding_box.merge(pos + vel);                  // Predicted box (future position)
         if (cid != node_t::INVALID_BBOX)

source/main/physics/Actor.h
@@ -290,6 +290,7 @@ public:
     std::vector<Airbrake*>    ar_airbrakes;
     CmdKeyArray               ar_command_key; //!< BEWARE: commandkeys are indexed 1-MAX_COMMANDS!
     Ogre::AxisAlignedBox      ar_bounding_box;     //!< standard bounding box (surrounds all nodes of an actor)
+    Ogre::AxisAlignedBox      ar_cabnodes_bounding_box; //!< bounding box around cab-triangle nodes only
     Ogre::AxisAlignedBox      ar_predicted_bounding_box;
     float                     ar_initial_total_mass = 0.f;
     std::vector<float>        ar_initial_node_masses;

source/main/physics/collision/Collisions.cpp
@@ -1088,8 +1088,11 @@ bool Collisions::nodeCollision(node_t *node, float dt)
 
 void Collisions::findPotentialEventBoxes(Actor* actor, CollisionBoxPtrVec& out_boxes)
 {
+    // NOTE: Only collision-cab nodes are considered for eventbox triggering!
+    // ----------------------------------------------------------------------
+
     // Find collision cells occupied by the actor (remember 'Y' is 'up').
-    const AxisAlignedBox aabb = actor->ar_bounding_box;
+    const AxisAlignedBox aabb = actor->ar_cabnodes_bounding_box;
     const int cell_lo_x = (int)(aabb.getMinimum().x / (float)CELL_SIZE);
     const int cell_lo_z = (int)(aabb.getMinimum().z / (float)CELL_SIZE);
     const int cell_hi_x = (int)(aabb.getMaximum().x / (float)CELL_SIZE);