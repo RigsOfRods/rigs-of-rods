36eaeb2e05b4bdf96a67616bb231c528002e7166
Edgar <Edgar@AnotherFoxGuy.com>
2023-09-16 11:22:29 +0200

    :arrow_up: Update dependency libcurl to v8

--- conanfile.py
+++ conanfile.py
@@ -11,8 +11,8 @@ class RoR(ConanFile):
     def requirements(self):
         self.requires("angelscript/2.35.1")
         self.requires("discord-rpc/3.4.0@anotherfoxguy/stable")
+        self.requires("libcurl/8.2.1")
         self.requires("fmt/10.1.1")
-        self.requires("libcurl/7.79.1")
         self.requires("mygui/3.4.0@anotherfoxguy/stable")
         self.requires("ogre3d-caelum/0.6.3.1@anotherfoxguy/stable")
         self.requires("ogre3d-pagedgeometry/1.2.0@anotherfoxguy/stable")
