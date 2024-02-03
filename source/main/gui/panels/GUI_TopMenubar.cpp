bbf483221849805adb6e1fb990edfb5dfacf0026
cryham <cryham@gmail.com>
2024-02-03 20:13:21 +0100

    ✨ Add option for Keyboard Steering Sensitivity

--- source/main/gui/panels/GUI_TopMenubar.cpp
+++ source/main/gui/panels/GUI_TopMenubar.cpp
@@ -661,6 +661,9 @@ void TopMenubar::Draw(float dt)
                 ImGui::Separator();
                 ImGui::TextColored(GRAY_HINT_TEXT, "%s", _LC("TopMenubar",  "Vehicle control options:"));
                 DrawGCheckbox(App::io_hydro_coupling, _LC("TopMenubar", "Keyboard steering speed coupling"));
+                ImGui::Separator();
+                ImGui::TextColored(GRAY_HINT_TEXT, "%s", _LC("TopMenubar",  "Keyboard steering:"));
+                DrawGFloatSlider(App::io_hydro_sensivity, _LC("TopMenubar", "Sensitivity"), 0.3, 5);
             }
             if (App::mp_state->getEnum<MpState>() == MpState::CONNECTED)
             {
