RigsOfRods/rigs-of-rods

commit 9551d053bfb2cc4527c87213ab95b43fc044993b

Tuning: fixups of prop/flexbody tweaks.
Code changes:
* TuneupFileFormat: just renamed `Xrot` to `Xrotation`
* CacheSystem.cpp: Fixed protected elements not being reset with Reset btn.
* AddonPartFileFormat: Fixed `addonpart_tweak_prop/_flexbody` not being parsed at all.
* FlexBody, FlexFactory: added 'orig mesh name' field to flexbody, for UIs.
* ActorSpawner: Pass ID and orig mesh name to placeholder-flexbody.
* GfxActor: fixed crash when rendering actor with a flexbody removed via tuneup/addonpart. The problem was `FinishFlexbodyTasks()` processing placeholder flexbodies. Now using `isVisible()`.

source/main/gfx/GfxActor.cpp
@@ -2996,8 +2996,13 @@ void RoR::GfxActor::UpdateFlexbodies()
 
     for (FlexBody* fb: m_flexbodies)
     {
-        const int camera_mode = fb->getCameraMode();
-        if ((camera_mode == -2) || (camera_mode == m_simbuf.simbuf_cur_cinecam))
+        // Update visibility
+        fb->setVisible(
+            fb->getCameraMode() == CAMERA_MODE_ALWAYS_VISIBLE
+            || fb->getCameraMode() == m_simbuf.simbuf_cur_cinecam);
+
+        // Update visible on background thread
+        if (fb->isVisible())
         {
             auto func = std::function<void()>([fb]()
                 {
@@ -3005,10 +3010,6 @@ void RoR::GfxActor::UpdateFlexbodies()
                 });
             auto task_handle = App::GetThreadPool()->RunTask(func);
             m_flexbody_tasks.push_back(task_handle);
-        }
-        else
-        {
-            fb->setVisible(false);
         }
     }
 }
@@ -3037,7 +3038,10 @@ void RoR::GfxActor::FinishFlexbodyTasks()
     }
     for (FlexBody* fb: m_flexbodies)
     {
-        fb->updateFlexbodyVertexBuffers();
+        if (fb->isVisible())
+        {
+            fb->updateFlexbodyVertexBuffers();
+        }
     }
 }
 

source/main/physics/ActorSpawner.cpp
@@ -1500,7 +1500,7 @@ void ActorSpawner::ProcessFlexbody(RigDef::Flexbody& def)
     if (TuneupUtil::isFlexbodyRemoved(m_actor, flexbody_id))
     {
         // Create placeholder
-        m_actor->m_gfx_actor->m_flexbodies.emplace_back(new FlexBody(FlexBody::TUNING_PLACEHOLDER));
+        m_actor->m_gfx_actor->m_flexbodies.emplace_back(new FlexBody(FlexBody::TUNING_PLACEHOLDER, flexbody_id, def.mesh_name));
         return;
     }
 

source/main/physics/flex/FlexBody.cpp
@@ -519,9 +519,11 @@ FlexBody::FlexBody(
     }
 }
 
-FlexBody::FlexBody(FlexBodyPlaceholder_t ticket)
+FlexBody::FlexBody(FlexBodyPlaceholder_t ticket, FlexbodyID_t id, const std::string& orig_meshname)
 {
     m_camera_mode = CAMERA_MODE_ALWAYS_HIDDEN;
+    m_id = id;
+    m_orig_mesh_name = orig_meshname;
 }
 
 FlexBody::~FlexBody()
@@ -552,6 +554,15 @@ FlexBody::~FlexBody()
         Ogre::MeshManager::getSingleton().remove(mesh->getHandle());
     }
     m_scene_entity = nullptr;
+}
+
+bool FlexBody::isVisible() const
+{
+    // Scene node is NULL if disabled via addonpart/tuneup.
+    return m_scene_node
+        && m_scene_node->isInSceneGraph()
+        && m_scene_node->getAttachedObject(0)->isVisible();
+        
 }
 
 void FlexBody::setVisible(bool visible)
@@ -608,6 +619,9 @@ void FlexBody::computeFlexbody()
 
 void FlexBody::updateFlexbodyVertexBuffers()
 {
+    if (!m_scene_node) // Disabled via addonpart/tuneup
+        return;
+
     Vector3 *ppt = m_dst_pos;
     Vector3 *npt = m_dst_normals;
     if (m_uses_shared_vertex_data)
@@ -677,19 +691,6 @@ void FlexBody::updateBlend() //so easy!
             m_blend_changed = true;
         }
     }
-}
-
-std::string FlexBody::getOrigMeshName()
-{
-    std::string meshname = m_scene_entity->getMesh()->getName();
-    // Cut off the generated '_FlexBody_#@Actor_#' part
-    // TODO: fix the resource management to create resource group for each actor (currently done for each repo ZIP) - then we won't need those unique tokens anymore.
-    size_t pos = meshname.find("_FlexBody_");
-    if (pos != std::string::npos)
-    {
-        meshname = meshname.substr(0, pos);
-    }
-    return meshname;
 }
 
 int evalNodeDistance(NodeNum_t a, NodeNum_t b)

source/main/physics/flex/FlexBody.h
@@ -61,7 +61,7 @@ public:
     typedef int FlexBodyPlaceholder_t;
     static const FlexBodyPlaceholder_t TUNING_PLACEHOLDER = -11;
 
-    FlexBody(FlexBodyPlaceholder_t);
+    FlexBody(FlexBodyPlaceholder_t, FlexbodyID_t id, const std::string& orig_meshname);
     ~FlexBody();
 
     void reset();
@@ -76,6 +76,7 @@ public:
     void computeFlexbody(); //!< Updates mesh deformation; works on CPU using local copy of vertex data.
     void updateFlexbodyVertexBuffers();
 
+    bool isVisible() const;
     void setVisible(bool visible);
 
     void setFlexbodyCastShadow(bool val);
@@ -84,7 +85,7 @@ public:
     Locator_t& getVertexLocator(int vert) { ROR_ASSERT((size_t)vert < m_vertex_count); return m_locators[vert]; }
     Ogre::Vector3 getVertexPos(int vert) { ROR_ASSERT((size_t)vert < m_vertex_count); return m_dst_pos[vert] + m_flexit_center; }
     Ogre::Entity* getEntity() { return m_scene_entity; }
-    std::string getOrigMeshName();
+    const std::string& getOrigMeshName() const { return m_orig_mesh_name; }
     std::vector<NodeNum_t>& getForsetNodes() { return m_forset_nodes; };
     std::string getOrigMeshInfo() { return m_orig_mesh_info; }
     std::string getLiveMeshInfo() { return RoR::PrintMeshInfo("Live", m_scene_entity->getMesh()); }
@@ -137,6 +138,7 @@ private:
     // Diagnostic data, not used for calculations
     std::vector<NodeNum_t> m_forset_nodes;
     std::string m_orig_mesh_info;
+    std::string m_orig_mesh_name;
 };
 
 /// @} // addtogroup Flex

source/main/gui/panels/GUI_TopMenubar.cpp
@@ -1652,7 +1652,7 @@ void TopMenubar::Draw(float dt)
                         if (p.pp_beacon_type == 'L' || p.pp_beacon_type == 'R' || p.pp_beacon_type == 'w')
                         {
                             ImGui::SameLine();
-                            ImGui::TextDisabled("(!)");
+                            ImGui::TextDisabled("(special!)");
                             if (ImGui::IsItemHovered())
                             {
                                 ImGui::BeginTooltip();
@@ -1663,7 +1663,7 @@ void TopMenubar::Draw(float dt)
                         else if (p.pp_wheel_mesh_obj)
                         {
                             ImGui::SameLine();
-                            ImGui::TextDisabled("(!)");
+                            ImGui::TextDisabled("(special!)");
                             if (ImGui::IsItemHovered())
                             {
                                 ImGui::BeginTooltip();

source/main/physics/ActorSpawner.h
@@ -475,7 +475,7 @@ private:
     int                            m_first_wing_index;
     std::vector<CabTexcoord>       m_oldstyle_cab_texcoords;
     std::vector<CabSubmesh>        m_oldstyle_cab_submeshes;    
-    RigDef::Keyword                m_current_keyword; //!< For error reports
+    RigDef::Keyword                m_current_keyword = RigDef::Keyword::INVALID; //!< For error reports
     std::shared_ptr<RigDef::Document::Module> m_current_module; //!< For resolving addonparts
     std::map<Ogre::String, unsigned int> m_named_nodes;
     /// @}

source/main/physics/flex/FlexFactory.cpp
@@ -107,6 +107,7 @@ FlexBody* FlexFactory::CreateFlexBody(
         m_flexbody_cache.AddItemToSave(new_flexbody);
     }
     new_flexbody->m_id = flexbody_id;
+    new_flexbody->m_orig_mesh_name = common_mesh->getName();
     return new_flexbody;
 }
 

source/main/resources/CacheSystem.cpp
@@ -1740,6 +1740,9 @@ void CacheSystem::ModifyProject(ModifyProjectRequest* request)
         tuneup_entry->tuneup_def->use_addonparts.clear();
         tuneup_entry->tuneup_def->wheel_tweaks.clear();
         tuneup_entry->tuneup_def->node_tweaks.clear();
+        tuneup_entry->tuneup_def->protected_flexbodies.clear();
+        tuneup_entry->tuneup_def->protected_props.clear();
+        tuneup_entry->tuneup_def->protected_wheels.clear();
         break;
 
     default:

commit a358909bc3f112c6653d5a3db0e7ce6a5e87e74f

:bug: Fixed FlexbodyDebug UI 'hide other' not working for flexbodies.
Broken in 9551d053bfb2cc4527c87213ab95b43fc044993b - since this commit flexbodies got a dynamic visibility control, exactly the same as props. From user (truck fileformat) perspective, it was the same already but the update pattern was different, and this commit unified it. The FlexbodyDebug UI didn't reflect this however.

In addition to fixing the glitch, this commit also unifies code for the visibility handling - same variables, same constants, just different prefixes.

Codechanges:
* GfxData: cosmetic changes to CAMERA_MODE constants - added typedef
* FlexBody: replaced single visibility mode field with _active+_orig fields, same as props have. Also using the same coding style as props do.
* GfxActor: cosmetic change - use the new variables in flexbody.
* ActorSpawner: set flexbody visibility via the new vars.

source/main/gfx/GfxActor.cpp
@@ -3010,10 +3010,9 @@ void RoR::GfxActor::UpdateFlexbodies()
 
     for (FlexBody* fb: m_flexbodies)
     {
-        // Update visibility
-        fb->setVisible(
-            fb->getCameraMode() == CAMERA_MODE_ALWAYS_VISIBLE
-            || fb->getCameraMode() == m_simbuf.simbuf_cur_cinecam);
+        // Update visibility (same logic as props)
+        const bool visible = (fb->fb_camera_mode_active == CAMERA_MODE_ALWAYS_VISIBLE || fb->fb_camera_mode_active == m_simbuf.simbuf_cur_cinecam);
+        fb->setVisible(visible);
 
         // Update visible on background thread
         if (fb->isVisible())

source/main/gfx/GfxData.h
@@ -120,9 +120,10 @@ enum class WheelSide: char
 };
 
 // Dynamic visibility control (value 0 and higher is cinecam index) - common to 'props' and 'flexbodies'
-static const int CAMERA_MODE_ALWAYS_HIDDEN = -3;
-static const int CAMERA_MODE_ALWAYS_VISIBLE = -2;
-static const int CAMERA_MODE_3RDPERSON_ONLY = -1;
+typedef int CameraMode_t;
+static CameraMode_t CAMERA_MODE_ALWAYS_HIDDEN = -3;
+static CameraMode_t CAMERA_MODE_ALWAYS_VISIBLE = -2;
+static CameraMode_t CAMERA_MODE_3RDPERSON_ONLY = -1;
 
 enum ShifterPropAnim
 {
@@ -171,9 +172,11 @@ struct Prop
     std::string           pp_media[2];                                   //!< Redundant, for Tuning UI. Media1 = prop mesh name, Media2 = steeringwheel mesh/beaconprop flare mat.
     std::vector<PropAnim> pp_animations;
 
-    // Visibility control
-    int                   pp_camera_mode_active = CAMERA_MODE_ALWAYS_VISIBLE; //!< Dynamic visibility mode {0 and higher = cinecam index}
-    int                   pp_camera_mode_orig = CAMERA_MODE_ALWAYS_VISIBLE;   //!< Dynamic visibility mode {0 and higher = cinecam index}
+    /// @name Visibility control (same as flexbody - see file FlexBody.h)
+    /// @{
+    CameraMode_t          pp_camera_mode_active = CAMERA_MODE_ALWAYS_VISIBLE; //!< Dynamic visibility mode {0 and higher = cinecam index}
+    CameraMode_t          pp_camera_mode_orig = CAMERA_MODE_ALWAYS_VISIBLE;   //!< Dynamic visibility mode {0 and higher = cinecam index}
+    /// @}
 
     // Special prop - steering wheel
     MeshObject*           pp_wheel_mesh_obj       = nullptr;

source/main/gui/panels/GUI_FlexbodyDebug.cpp
@@ -426,6 +426,9 @@ void FlexbodyDebug::DrawDebugView(FlexBody* flexbody, Prop* prop, NodeNum_t node
 
 void FlexbodyDebug::UpdateVisibility()
 {
+    // Both flexbodies and props use the same dynamic visibility mode, see `CameraMode_t` typedef and constants in file GfxData.h
+    // ---------------------------------------------------------------------------------------------------------------------------
+
     ActorPtr actor = App::GetGameContext()->GetPlayerActor();
     if (!actor)
     {
@@ -442,13 +445,18 @@ void FlexbodyDebug::UpdateVisibility()
         {
             prop.pp_camera_mode_active = CAMERA_MODE_ALWAYS_HIDDEN;
         }
+        // Override flexbody dynamic visibility mode
+        for (FlexBody* flexbody: actor->GetGfxActor()->GetFlexbodies())
+        {
+            flexbody->fb_camera_mode_active = CAMERA_MODE_ALWAYS_HIDDEN;
+        }
 
         // Then re-display what we need manually.
         auto& flexbody_vec = actor->GetGfxActor()->GetFlexbodies();
         const int combo_flexbody_selection = m_combo_selection;
         if (combo_flexbody_selection >= 0 && combo_flexbody_selection < (int)flexbody_vec.size())
         {
-            flexbody_vec[combo_flexbody_selection]->setVisible(true);
+            flexbody_vec[combo_flexbody_selection]->fb_camera_mode_active = CAMERA_MODE_ALWAYS_VISIBLE;
         }
 
         auto& prop_vec = actor->GetGfxActor()->getProps();
@@ -471,6 +479,11 @@ void FlexbodyDebug::UpdateVisibility()
         for (Prop& prop : actor->GetGfxActor()->getProps())
         {
             prop.pp_camera_mode_active = prop.pp_camera_mode_orig;
+        }
+        // Restore flexbody dynamic visibility mode
+        for (FlexBody* flexbody: actor->GetGfxActor()->GetFlexbodies())
+        {
+            flexbody->fb_camera_mode_active = flexbody->fb_camera_mode_orig;
         }
     }
 }

source/main/physics/ActorSpawner.cpp
@@ -1562,7 +1562,9 @@ void ActorSpawner::ProcessFlexbody(RigDef::Flexbody& def)
         if (flexbody == nullptr)
             return; // Error already logged
 
-        flexbody->setCameraMode(def.camera_settings.mode);
+        // Dynamic visibility - same as with props
+        flexbody->fb_camera_mode_orig = def.camera_settings.mode;
+        flexbody->fb_camera_mode_active = def.camera_settings.mode;
 
         m_actor->m_gfx_actor->m_flexbodies.emplace_back(flexbody);
     }

source/main/physics/flex/FlexBody.cpp
@@ -567,6 +567,7 @@ bool FlexBody::isVisible() const
 
 void FlexBody::setVisible(bool visible)
 {
+    // Scene node is NULL if disabled via addonpart/tuneup.
     if (m_scene_node)
         m_scene_node->setVisible(visible);
 }

source/main/physics/flex/FlexBody.h
@@ -25,6 +25,7 @@
 #include "Application.h"
 #include "Locator_t.h"
 #include "SimData.h"
+#include "GfxData.h"
 #include "RigDef_File.h"
 #include "Utils.h"
 
@@ -61,17 +62,18 @@ public:
     typedef int FlexBodyPlaceholder_t;
     static const FlexBodyPlaceholder_t TUNING_PLACEHOLDER = -11;
 
+    /// @name Visibility control (same as prop - see file GfxData.h)
+    /// @{
+    CameraMode_t          fb_camera_mode_active = CAMERA_MODE_ALWAYS_VISIBLE; //!< Dynamic visibility mode {0 and higher = cinecam index}
+    CameraMode_t          fb_camera_mode_orig = CAMERA_MODE_ALWAYS_VISIBLE;   //!< Dynamic visibility mode {0 and higher = cinecam index}
+    /// @}
+
     FlexBody(FlexBodyPlaceholder_t, FlexbodyID_t id, const std::string& orig_meshname);
     ~FlexBody();
 
     void reset();
     void updateBlend();
     void writeBlend();
-
-    /// Visibility control 
-    /// @param mode {-2 = always, -1 = 3rdPerson only, 0+ = cinecam index}
-    void setCameraMode(int mode) { m_camera_mode = mode; };
-    int getCameraMode() { return m_camera_mode; };
 
     void computeFlexbody(); //!< Updates mesh deformation; works on CPU using local copy of vertex data.
     void updateFlexbodyVertexBuffers();