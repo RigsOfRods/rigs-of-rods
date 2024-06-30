RigsOfRods/rigs-of-rods

commit 74ed1083cb9e53e38096ec96b6a7b1f0b237ce01

Made 'managedmaterials' work with addon parts. :sparkles:

source/main/resources/CacheSystem.cpp
@@ -1209,6 +1209,20 @@ bool CacheSystem::CheckResourceLoaded(Ogre::String & filename, Ogre::String& gro
     return false;
 }
 
+std::string CacheSystem::ComposeResourceGroupName(const CacheEntryPtr& entry)
+{
+    // Compose group name as "{bundle <local path>}", 
+    //  where 'local path' means under 'Documenst\My Games\Rigs of Rods'.
+    // -----------------------------------------------------------------
+    std::string name = entry->resource_bundle_path; // Start from full path
+    size_t prefix_pos = name.find_first_not_of(App::sys_user_dir->getStr());
+    if (prefix_pos != std::string::npos)
+    {
+        name = name.substr(App::sys_user_dir->getStr().length());
+    }
+    return fmt::format("{{bundle {}}}", name);
+}
+
 void CacheSystem::LoadResource(CacheEntryPtr& entry)
 {
     if (!entry)
@@ -1220,7 +1234,7 @@ void CacheSystem::LoadResource(CacheEntryPtr& entry)
         return;
     }
 
-    Ogre::String group = "bundle " + entry->resource_bundle_path; // Compose group name from full path.
+    Ogre::String group = CacheSystem::ComposeResourceGroupName(entry);
     bool readonly = entry->resource_bundle_type == "Zip"; // Make "FileSystem" (directory) bundles writable. Default is read-only.
     bool recursive = false;
 

source/main/physics/ActorSpawner.cpp
@@ -379,7 +379,7 @@ void ActorSpawner::InitializeRig()
 
     m_flex_factory.CheckAndLoadFlexbodyCache();
 
-    m_placeholder_managedmat = Ogre::MaterialManager::getSingleton().getByName("rigsofrods/managedmaterial-placeholder"); // Built-in
+    m_managedmat_placeholder_template = Ogre::MaterialManager::getSingleton().getByName("rigsofrods/managedmaterial-placeholder"); // Built-in
 
     m_apply_simple_materials = App::diag_simple_materials->getBool();
     if (m_apply_simple_materials)
@@ -1638,6 +1638,7 @@ void ActorSpawner::ProcessProp(RigDef::Prop & def)
 
     prop.pp_scene_node = App::GetGfxScene()->GetSceneManager()->getRootSceneNode()->createChildSceneNode();
     const std::string instance_name = this->ComposeName("PropEntity", prop_index);
+    LOG(fmt::format("[RoR] DBG ActorSpawner::ProcessProp(): creating MeshObject '{}' in RG '{}'", def.mesh_name, resource_group));
     prop.pp_mesh_obj = new MeshObject(def.mesh_name, resource_group, instance_name, prop.pp_scene_node);
 
     prop.pp_mesh_obj->setCastShadows(true); // Orig code {{ prop.pp_mesh_obj->setCastShadows(shadowmode != 0); }}, shadowmode has default value 1 and changes with undocumented directive 'set_shadows'
@@ -2295,7 +2296,7 @@ Ogre::MaterialPtr ActorSpawner::InstantiateManagedMaterial(Ogre::String const & 
         return Ogre::MaterialPtr();
     }
 
-    return src_mat->clone(clone_name, true, m_custom_resource_group);
+    return src_mat->clone(clone_name);
 }
 
 void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
@@ -2328,12 +2329,17 @@ void ActorSpawner::ProcessManagedMaterial(RigDef::ManagedMaterial & def)
         def.specular_map = "";
     }
 
-    // Create temporary placeholder
+    // Create fallback placeholders
     // This is necessary to load meshes with original material names (= unchanged managed mat names)
     // - if not found, OGRE substitutes them with 'BaseWhite' which breaks subsequent processing.
     if (Ogre::MaterialManager::getSingleton().getByName(def.name, resource_group).isNull())
     {
-        m_placeholder_managedmat->clone(def.name, /*changeGroup=*/true, resource_group);
+        LOG(fmt::format("[RoR] DBG ActorSpawner::ProcessManagedMaterial(): Creating placeholder for material '{}' in group '{}'", def.name, resource_group));
+        m_managedmat_placeholder_template->clone(def.name, /*changeGroup=*/true, resource_group);
+    }
+    else
+    {
+        LOG(fmt::format("[RoR] DBG ActorSpawner::ProcessManagedMaterial(): Placeholder already exists: '{}' in group '{}'", def.name, resource_group));
     }
 
     std::string custom_name = def.name + ACTOR_ID_TOKEN + TOSTRING(m_actor->ar_instance_id);

source/main/physics/ActorSpawner.h
@@ -473,7 +473,7 @@ private:
     RoR::FlexFactory                          m_flex_factory;
     std::map<std::string, CustomMaterial>     m_material_substitutions; //!< Maps original material names (shared) to their actor-specific substitutes; There's 1 substitute per 1 material, regardless of user count.
     std::map<std::string, Ogre::MaterialPtr>  m_managed_materials;
-    Ogre::MaterialPtr                         m_placeholder_managedmat;
+    Ogre::MaterialPtr                         m_managedmat_placeholder_template; //!< An 'error marker' material (bright magenta) to generate managedmaterial placeholders from.
     Ogre::SceneNode*                          m_particles_parent_scenenode;
     Ogre::MaterialPtr                         m_cab_trans_material;
     Ogre::MaterialPtr                         m_simple_material_base;

source/main/resources/CacheSystem.h
@@ -280,6 +280,7 @@ private:
 
     static Ogre::String StripUIDfromString(Ogre::String uidstr); 
     static Ogre::String StripSHA1fromString(Ogre::String sha1str);
+    static std::string ComposeResourceGroupName(const CacheEntryPtr& entry);
 
     void ParseZipArchives(Ogre::String group);
     bool ParseKnownFiles(Ogre::String group); // returns true if no known files are found

commit 62729bcd585011ab2e69fc9bb4bdef078c074e2c

:bug: Fixed mods not loading from `app_extra_mod_path`
Fixes #3149

Problem was caused by myself in 74ed1083cb9e53e38096ec96b6a7b1f0b237ce01 - apart from fixing the bug, I also made a cosmetic edit of resource group names - Instead of it being 'bundle $FULLPATH', I made it '{bundle $LOCALPATH}'. But I botched the code, causing exception "invalid string position" to be thrown.

Solution: keep pretifying the resource group names, but account for all locations. I added a location-label to the name to avoid RG name clashes.
* prepackaged mods, i.e. {bundle BIN:\content\simple2-terrain.zip}
* extra mod path, i.e. {bundle EXTRA:\DI_SportsCar.zip}
* mod dirs under user profile, i.e. {bundle USER:\mods\vehicles\Desperado.zip}

source/main/resources/CacheSystem.cpp
@@ -1339,18 +1339,60 @@ bool CacheSystem::CheckResourceLoaded(Ogre::String & filename, Ogre::String& gro
     return false;
 }
 
+static bool CheckAndReplacePathIgnoreCase(const CacheEntryPtr& entry, CVar* dir, const std::string& dir_label, std::string& out_rgname)
+{
+    // Helper for `ComposeResourceGroupName()`
+    // ---------------------------------------
+
+    // Sanity check - assert on Debug, minimize damage on Release
+    ROR_ASSERT(entry->resource_bundle_path != "");
+    if (entry->resource_bundle_path == "")
+    {
+        LOG(fmt::format("[RoR|CacheSystem] CheckAndReplacePathIgnoreCase(): INTERNAL ERROR - entry '{}' has no bundle path!", entry->fname));
+        return false;
+    }
+
+    // Lowercase everything
+    std::string lower_bundlepath = entry->resource_bundle_path;
+    Ogre::StringUtil::toLowerCase(lower_bundlepath);
+
+    std::string lower_dir = dir->getStr();
+    Ogre::StringUtil::toLowerCase(lower_dir);    
+
+    // Look for match and replace
+    if (Ogre::StringUtil::startsWith(lower_bundlepath, lower_dir, /*lowercase:*/true))
+    {
+        // Sanity check; Should be guaranteed by the `startsWith()` check, but just to be sure...
+        ROR_ASSERT(lower_bundlepath.size() > lower_dir.size());
+        if (lower_bundlepath.size() > lower_dir.size())
+        {
+            std::string localpath = entry->resource_bundle_path.substr(lower_dir.length());
+            out_rgname = fmt::format("{{bundle {}:{}}}", dir_label, localpath);
+            return true;
+        }
+    }
+    return false;
+}
+
 std::string CacheSystem::ComposeResourceGroupName(const CacheEntryPtr& entry)
 {
-    // Compose group name as "{bundle <local path>}", 
-    //  where 'local path' means under 'Documenst\My Games\Rigs of Rods'.
-    // -----------------------------------------------------------------
-    std::string name = entry->resource_bundle_path; // Start from full path
-    size_t prefix_pos = name.find_first_not_of(App::sys_user_dir->getStr());
-    if (prefix_pos != std::string::npos)
+    // Compose group name as "{bundle <local path>}", where 'local path' means either:
+    // - under `sys_user_dir` (by default 'Documenst\My Games\Rigs of Rods')
+    // - under `app_extra_mod_path` (empty by default)
+    // - under 'sys_process_dir' (autodetected)
+    // -------------------------------------------------------------------------------
+
+    std::string rg_name;
+    if (CheckAndReplacePathIgnoreCase(entry, App::sys_user_dir, "USER", rg_name) ||
+        CheckAndReplacePathIgnoreCase(entry, App::sys_process_dir, "BIN", rg_name) ||
+        CheckAndReplacePathIgnoreCase(entry, App::app_extra_mod_path, "EXTRA", rg_name))
     {
-        name = name.substr(App::sys_user_dir->getStr().length());
+        return rg_name;
     }
-    return fmt::format("{{bundle {}}}", name);
+    else
+    {
+        return fmt::format("{{bundle FULL:{}}}", entry->resource_bundle_path);
+    }
 }
 
 void CacheSystem::LoadSupplementaryDocuments(CacheEntryPtr& entry)