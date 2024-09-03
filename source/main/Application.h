ad133a6f184ecf02c6f108230ab44f8004b7ff2e
Rafael <rbgalvan@cougarnet.uh.edu>
2024-09-02 22:05:48 -0500

    Added login and user profile UI

--- source/main/Application.h
+++ source/main/Application.h
@@ -103,6 +103,11 @@ enum MsgType
     MSG_NET_DISCONNECT_REQUESTED,
     MSG_NET_USER_DISCONNECT,
     MSG_NET_RECV_ERROR,
+    MSG_NET_USERAUTH_SUCCESS,
+    MSG_NET_USERAUTH_FAILURE,
+    MSG_NET_USERAUTH_TFA_REQUESTED,
+    MSG_NET_USERAUTH_TFA_FAILURE,
+    MSG_NET_USERAUTH_TFA_TRIGGERED,
     MSG_NET_REFRESH_SERVERLIST_SUCCESS,    //!< Payload = GUI::MpServerInfoVec* (owner)
     MSG_NET_REFRESH_SERVERLIST_FAILURE,    //!< Payload = RoR::CurlFailInfo* (owner)
     MSG_NET_REFRESH_REPOLIST_SUCCESS,      //!< Payload = GUI::ResourcesCollection* (owner)
@@ -386,6 +391,7 @@ extern CVar* mp_cyclethru_net_actors; //!< Include remote actors when cycling th
 
 // New remote API
 extern CVar* remote_query_url;
+extern CVar* remote_login_token;
 
 // Diagnostic
 extern CVar* diag_auto_spawner_report;
