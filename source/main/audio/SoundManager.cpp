d762a6cad6b723711ec21ae4fcc9914d396fd571
Niklas Kersten <n.kersten@xyz.de>
2024-10-03 09:14:16 +0200

    :wrench: Move the source filtering code to the update() function

--- source/main/audio/SoundManager.cpp
+++ source/main/audio/SoundManager.cpp
@@ -317,7 +317,19 @@ void SoundManager::update()
 
     if(App::audio_enable_efx->getBool())
     {
-        this->updateListenerEffectSlot();
+        // apply filters to sources when appropriate
+        for(int source_index = 0; source_index < hardware_sources_in_use_count; source_index++)
+        {
+            // update air absorption factor
+            alSourcef(hardware_sources[source_index], AL_AIR_ABSORPTION_FACTOR, App::audio_air_absorption_factor->getFloat());
+
+            if(App::audio_enable_obstruction->getBool())
+            {
+                updateObstructionFilter(hardware_sources[source_index]);
+            }
+        }
+
+        updateListenerEffectSlot();
     }
 }
 
@@ -616,20 +628,6 @@ void SoundManager::recomputeAllSources()
 		}
 	}
 #endif
-
-    if(App::audio_enable_efx->getBool())
-    {
-        for(int source_index = 0; source_index < hardware_sources_in_use_count; source_index++)
-        {
-            // update air absorption factor
-            alSourcef(hardware_sources[source_index], AL_AIR_ABSORPTION_FACTOR, App::audio_air_absorption_factor->getFloat());
-
-            if(App::audio_enable_obstruction->getBool())
-            {
-                updateObstructionFilter(hardware_sources[source_index]);
-            }
-        }
-    }
 }
 
 void SoundManager::updateObstructionFilter(const ALuint hardware_source)
