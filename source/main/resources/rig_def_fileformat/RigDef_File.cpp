aa9a423472858286777691542ab05a471b8aaac0
ohlidalp <petr@ohlidal.cz>
2024-12-04 11:03:53 +0100

    `KeywordToString()` - added missing 'flares3'

--- source/main/resources/rig_def_fileformat/RigDef_File.cpp
+++ source/main/resources/rig_def_fileformat/RigDef_File.cpp
@@ -213,6 +213,7 @@ const char * KeywordToString(Keyword keyword)
         case Keyword::FIXES:                return "fixes";
         case Keyword::FLARES:               return "flares";
         case Keyword::FLARES2:              return "flares2";
+        case Keyword::FLARES3:              return "flares3";
         case Keyword::FLEXBODIES:           return "flexbodies";
         case Keyword::FLEXBODY_CAMERA_MODE: return "flexbody_camera_mode";
         case Keyword::FLEXBODYWHEELS:       return "flexbodywheels";
