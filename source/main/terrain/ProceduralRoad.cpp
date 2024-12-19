RigsOfRods/rigs-of-rods

commit faebbcd2d78eaf3b4600624a1371e962e1209030

Terrain: Grouped scene nodes together for nicer inspector view.
This has no effect on rendering, it just helps users to diagnose the scene graph.

source/main/terrain/ProceduralRoad.cpp
@@ -58,7 +58,7 @@ ProceduralRoad::~ProceduralRoad()
     }
 }
 
-void ProceduralRoad::finish()
+void ProceduralRoad::finish(Ogre::SceneNode* snode)
 {
     Vector3 pts[8];
     computePoints(pts, lastpos, lastrot, lasttype, lastwidth, lastbwidth, lastbheight);
@@ -70,7 +70,6 @@ void ProceduralRoad::finish()
     String entity_name = String("RoadSystem_Instance-").append(StringConverter::toString(mid));
     String mesh_name = String("RoadSystem-").append(StringConverter::toString(mid));
     Entity* ec = App::GetGfxScene()->GetSceneManager()->createEntity(entity_name, mesh_name);
-    snode = App::GetGfxScene()->GetSceneManager()->getRootSceneNode()->createChildSceneNode();
     snode->attachObject(ec);
 
     App::GetGameContext()->GetTerrain()->GetCollisions()->registerCollisionMesh(

source/main/terrain/ProceduralManager.cpp
@@ -61,6 +61,11 @@ void ProceduralObject::deletePoint(int pos)
 
 #pragma region ProceduralManager
 
+ProceduralManager::ProceduralManager(Ogre::SceneNode* groupingSceneNode)
+    : pGroupingSceneNode(groupingSceneNode)
+{
+}
+
 ProceduralManager::~ProceduralManager()
 {
     this->removeAllObjects();
@@ -163,7 +168,7 @@ void ProceduralManager::updateObject(ProceduralObjectPtr po)
             po->road->addBlock(pp->position, pp->rotation, pp->type, pp->width, pp->bwidth, pp->bheight, pp->pillartype);
         }
     }
-    po->road->finish();
+    po->road->finish(pGroupingSceneNode);
 }
 
 void ProceduralManager::addObject(ProceduralObjectPtr po)

source/main/terrain/ProceduralManager.h
@@ -79,6 +79,7 @@ struct ProceduralObject: public RefCountingObject<ProceduralObject>
 class ProceduralManager: public RefCountingObject<ProceduralManager>
 {
 public:
+    ProceduralManager(Ogre::SceneNode* groupingSceneNode);
     virtual ~ProceduralManager() override;
 
     /// Generates road mesh and adds to internal list
@@ -102,6 +103,7 @@ private:
     void deleteObject(ProceduralObjectPtr po);
 
     std::vector<ProceduralObjectPtr> pObjects;
+    Ogre::SceneNode* pGroupingSceneNode = nullptr;
 };
 
 /// @} // addtogroup Terrain

source/main/terrain/ProceduralRoad.h
@@ -73,7 +73,7 @@ public:
     void addCollisionQuad(Ogre::Vector3 p1, Ogre::Vector3 p2, Ogre::Vector3 p3, Ogre::Vector3 p4, ground_model_t* gm, bool flip = false);
     void addCollisionQuad(Ogre::Vector3 p1, Ogre::Vector3 p2, Ogre::Vector3 p3, Ogre::Vector3 p4, std::string const& gm_name, bool flip = false);
     void createMesh();
-    void finish();
+    void finish(Ogre::SceneNode* snode);
     void setCollisionEnabled(bool v) { collision = v; }
 
     static const unsigned int MAX_VERTEX = 50000;

source/main/terrain/Terrain.cpp
@@ -148,14 +148,14 @@ bool RoR::Terrain::initialize()
 
     this->setGravity(this->m_def.gravity);
 
-    loading_window->SetProgress(15, _L("Initializing Shadow Subsystem"));
+    loading_window->SetProgress(10, _L("Initializing Object Subsystem"));
+    this->initObjects(); // *.odef files
+
+    loading_window->SetProgress(14, _L("Initializing Shadow Subsystem"));
     this->initShadows();
 
     loading_window->SetProgress(17, _L("Initializing Geometry Subsystem"));
     this->m_geometry_manager = new TerrainGeometryManager(this);
-
-    loading_window->SetProgress(19, _L("Initializing Object Subsystem"));
-    this->initObjects(); // *.odef files
 
     loading_window->SetProgress(23, _L("Initializing Camera Subsystem"));
     this->initCamera();
@@ -464,14 +464,13 @@ void RoR::Terrain::initScripting()
 
     for (std::string as_filename : m_def.as_files)
     {
-        if (App::GetScriptEngine()->loadScript(as_filename) == 0)
-            loaded = true;
+        loaded |= this->getObjectManager()->LoadTerrainScript(as_filename);
     }
 
     if (!loaded)
     {
         // load a default script that does the most basic things
-        App::GetScriptEngine()->loadScript(DEFAULT_TERRAIN_SCRIPT);
+        this->getObjectManager()->LoadTerrainScript(DEFAULT_TERRAIN_SCRIPT);
     }
 
     // finally resume AS logging

source/main/terrain/TerrainObjectManager.cpp
@@ -21,6 +21,7 @@
 
 #include "TerrainObjectManager.h"
 
+#include "Actor.h"
 #include "Application.h"
 #include "AutoPilot.h"
 #include "CacheSystem.h"
@@ -36,6 +37,7 @@
 #include "ODefFileFormat.h"
 #include "PlatformUtils.h"
 #include "ProceduralRoad.h"
+#include "ScriptEngine.h"
 #include "SoundScriptManager.h"
 #include "TerrainGeometryManager.h"
 #include "Terrain.h"
@@ -65,7 +67,9 @@ inline float getTerrainHeight(Real x, Real z, void* unused = 0)
 TerrainObjectManager::TerrainObjectManager(Terrain* terrainManager) :
     terrainManager(terrainManager)
 {
-    m_procedural_manager = new ProceduralManager();
+    m_terrn2_grouping_node = App::GetGfxScene()->GetSceneManager()->getRootSceneNode()->createChildSceneNode(fmt::format("Terrain: {}", terrainManager->GetDef().name));
+
+    m_procedural_manager = new ProceduralManager(m_terrn2_grouping_node->createChildSceneNode("Procedural Roads"));
 }
 
 TerrainObjectManager::~TerrainObjectManager()
@@ -84,6 +88,8 @@ TerrainObjectManager::~TerrainObjectManager()
 #endif //USE_PAGED
 
     App::GetGfxScene()->GetSceneManager()->destroyAllEntities();
+
+    App::GetGfxScene()->GetSceneManager()->destroySceneNode(m_terrn2_grouping_node);
 }
 
 void GenerateGridAndPutToScene(Ogre::Vector3 position)
@@ -134,7 +140,7 @@ void GenerateGridAndPutToScene(Ogre::Vector3 position)
 
     mo->end();
     mo->setCastShadows(false);
-    Ogre::SceneNode *n = App::GetGfxScene()->GetSceneManager()->getRootSceneNode()->createChildSceneNode();
+    Ogre::SceneNode *n = App::GetGameContext()->GetTerrain()->getObjectManager()->getGroupingSceneNode()->createChildSceneNode();
     n->setPosition(position);
     n->attachObject(mo);
     n->setVisible(true);
@@ -142,11 +148,15 @@ void GenerateGridAndPutToScene(Ogre::Vector3 position)
 
 void TerrainObjectManager::LoadTObjFile(Ogre::String tobj_name)
 {
+    ROR_ASSERT(this->terrainManager);
+    ROR_ASSERT(this->terrainManager->getCacheEntry());
+    ROR_ASSERT(this->terrainManager->getCacheEntry()->resource_group != "");
+
     std::shared_ptr<TObjFile> tobj;
     try
     {
         DataStreamPtr stream_ptr = ResourceGroupManager::getSingleton().openResource(
-            tobj_name, Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME);
+            tobj_name, this->terrainManager->getCacheEntry()->resource_group);
         TObjParser parser;
         parser.Prepare();
         parser.ProcessOgreStream(stream_ptr.get());
@@ -162,6 +172,9 @@ void TerrainObjectManager::LoadTObjFile(Ogre::String tobj_name)
         LOG("[RoR|Terrain] Error reading TObj file: " + tobj_name + "\nMessage" + e.what());
         return;
     }
+
+    ROR_ASSERT(m_terrn2_grouping_node);
+    m_tobj_grouping_node = m_terrn2_grouping_node->createChildSceneNode(tobj_name);
 
     int mapsizex = terrainManager->getGeometryManager()->getMaxTerrainSize().x;
     int mapsizez = terrainManager->getGeometryManager()->getMaxTerrainSize().z;
@@ -243,6 +256,8 @@ void TerrainObjectManager::LoadTObjFile(Ogre::String tobj_name)
     {
         m_procedural_manager->logDiagnostics();
     }
+
+    m_tobj_grouping_node = nullptr;
 }
 
 void TerrainObjectManager::ProcessTree(
@@ -560,7 +575,7 @@ bool TerrainObjectManager::LoadTerrainObject(const Ogre::String& name, const Ogr
         return false;
     }
 
-    SceneNode* tenode = App::GetGfxScene()->GetSceneManager()->getRootSceneNode()->createChildSceneNode();
+    SceneNode* tenode = this->getGroupingSceneNode()->createChildSceneNode();
 
     MeshObject* mo = nullptr;
     if (odef->header.mesh_name != "none")
@@ -905,6 +920,17 @@ bool TerrainObjectManager::LoadTerrainObject(const Ogre::String& name, const Ogr
     return true;
 }
 
+bool TerrainObjectManager::LoadTerrainScript(const Ogre::String& filename)
+{
+    ROR_ASSERT(!m_angelscript_grouping_node);
+
+    m_angelscript_grouping_node = m_terrn2_grouping_node->createChildSceneNode(filename);
+    ScriptUnitId_t result = App::GetScriptEngine()->loadScript(filename);
+    m_angelscript_grouping_node = nullptr;
+    
+    return result != SCRIPTUNITID_INVALID;
+}
+
 bool TerrainObjectManager::UpdateAnimatedObjects(float dt)
 {
     if (m_animated_objects.size() == 0)
@@ -996,4 +1022,19 @@ void TerrainObjectManager::ProcessODefCollisionBoxes(StaticObject* obj, ODefFile
             obj->collBoxes.push_back(boxnum);
         }
     }
+}
+
+Ogre::SceneNode* TerrainObjectManager::getGroupingSceneNode()
+{
+    // This has no effect on rendering, it just helps users to diagnose the scene graph.
+    // --------------------------------------------------------------------------------
+
+    if (m_angelscript_grouping_node)
+        return m_angelscript_grouping_node;
+    else if (m_tobj_grouping_node)
+        return m_tobj_grouping_node;
+    else if (m_terrn2_grouping_node)
+        return m_terrn2_grouping_node;
+    else
+        return App::GetGfxScene()->GetSceneManager()->getRootSceneNode();
 }

source/main/terrain/TerrainObjectManager.h
@@ -71,6 +71,7 @@ public:
     std::vector<EditorObject>& GetEditorObjects() { return m_editor_objects; }
     void           LoadTObjFile(Ogre::String filename);
     bool           LoadTerrainObject(const Ogre::String& name, const Ogre::Vector3& pos, const Ogre::Vector3& rot, const Ogre::String& instancename, const Ogre::String& type, float rendering_distance = 0, bool enable_collisions = true, int scripthandler = -1, bool uniquifyMaterial = false);
+    bool           LoadTerrainScript(const Ogre::String& filename);
     void           MoveObjectVisuals(const Ogre::String& instancename, const Ogre::Vector3& pos);
     void           unloadObject(const Ogre::String& instancename);
     void           LoadTelepoints();
@@ -111,6 +112,7 @@ public:
     std::vector<localizer_t> GetLocalizers() { return localizers; }
 
     ProceduralManagerPtr& getProceduralManager() { return m_procedural_manager; }
+    Ogre::SceneNode* getGroupingSceneNode();
 
 protected:
 
@@ -164,6 +166,9 @@ protected:
     Terrain*           terrainManager;
     ProceduralManagerPtr      m_procedural_manager;
     int                       m_entity_counter = 0;
+    Ogre::SceneNode*          m_terrn2_grouping_node = nullptr; //!< For a readable scene graph (via inspector script)
+    Ogre::SceneNode*          m_tobj_grouping_node = nullptr; //!< For even more readable scene graph (via inspector script)
+    Ogre::SceneNode*          m_angelscript_grouping_node = nullptr; //!< For even more readable scene graph (via inspector script)
 
 #ifdef USE_PAGED
     std::vector<Forests::PagedGeometry*> m_paged_geometry;

commit fefe3924aab4af039f07abc1ae308183570a5117

:bug: Fixed road editor leaving outdated meshes in scene.
Problem: The road mesh could not be removed from scene, so road editor's [rebuild mesh] button just piled up new meshes.
Cause: The `ProceduralRoad::snode` variable was never set - it should have been written in `finish()` but it was shadowed by the new parameter. Broken in faebbcd2d78eaf3b4600624a1371e962e1209030
Fix: rename the offending param and correctly create+assign child scene node.

source/main/terrain/ProceduralRoad.cpp
@@ -58,7 +58,7 @@ ProceduralRoad::~ProceduralRoad()
     }
 }
 
-void ProceduralRoad::finish(Ogre::SceneNode* snode)
+void ProceduralRoad::finish(Ogre::SceneNode* groupingSceneNode)
 {
     Vector3 pts[8];
     computePoints(pts, lastpos, lastrot, lasttype, lastwidth, lastbwidth, lastbheight);
@@ -70,6 +70,7 @@ void ProceduralRoad::finish(Ogre::SceneNode* snode)
     String entity_name = String("RoadSystem_Instance-").append(StringConverter::toString(mid));
     String mesh_name = String("RoadSystem-").append(StringConverter::toString(mid));
     Entity* ec = App::GetGfxScene()->GetSceneManager()->createEntity(entity_name, mesh_name);
+    snode = groupingSceneNode->createChildSceneNode();
     snode->attachObject(ec);
 
     if (collision)