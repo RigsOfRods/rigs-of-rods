RigsOfRods/rigs-of-rods

commit d820a2279481f63b02937d1fac0e5ba5c68f6c35

OIS/mousegrab glitch: removed leftover debug messages.

source/main/utils/InputEngine.cpp
@@ -703,22 +703,11 @@ void InputEngine::processMouseMotionEvent(const OIS::MouseEvent& arg)
 
 void InputEngine::processMousePressEvent(const OIS::MouseEvent& arg, OIS::MouseButtonID _id)
 {
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse pressed; old state: LMB={}, MMB={}, RMB={}, framesSinceReset={}, lmbDownsSinceReset={}",
-            mouseState.buttonDown(OIS::MB_Left), mouseState.buttonDown(OIS::MB_Middle), mouseState.buttonDown(OIS::MB_Right),
-            m_oisworkaround_frames_since_reset, m_oisworkaround_lmbdowns_since_reset));
-
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse pressed; _arg: LMB={}, MMB={}, RMB={}",
-            arg.state.buttonDown(OIS::MB_Left), arg.state.buttonDown(OIS::MB_Middle), arg.state.buttonDown(OIS::MB_Right)));
-
     // Skip false 'LMB press' event after restoring window focus; see commentary in `resetKeysAndMouseButtons()`.
     if (_id == OIS::MB_Left)
     {
         if (m_oisworkaround_lmbdowns_since_reset == 0 && m_oisworkaround_frames_since_reset < 1)
         {
-            App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-                "InputEngine: mouse pressed, but it's a fake event, ignoring it.");
             return;
         }
         m_oisworkaround_lmbdowns_since_reset++;
@@ -727,35 +716,13 @@ void InputEngine::processMousePressEvent(const OIS::MouseEvent& arg, OIS::MouseB
     // Only update the one particular button, OIS's persistent state may be dirty, see commentary in `getMouseState()`
     BitMask_t btnmask = 1 << _id;
     BITMASK_SET_1(mouseState.buttons, btnmask);
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse pressed, btn={}", (int)_id));
-
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse pressed; new state: LMB={}, MMB={}, RMB={}",
-            mouseState.buttonDown(OIS::MB_Left), mouseState.buttonDown(OIS::MB_Middle), mouseState.buttonDown(OIS::MB_Right)));
-
-
 }
 
 void InputEngine::processMouseReleaseEvent(const OIS::MouseEvent& arg, OIS::MouseButtonID _id)
 {
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse released; old state: LMB={}, MMB={}, RMB={}",
-            mouseState.buttonDown(OIS::MB_Left), mouseState.buttonDown(OIS::MB_Middle), mouseState.buttonDown(OIS::MB_Right)));
-
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse released; _arg: LMB={}, MMB={}, RMB={}",
-            arg.state.buttonDown(OIS::MB_Left), arg.state.buttonDown(OIS::MB_Middle), arg.state.buttonDown(OIS::MB_Right)));
-
     // Only update the one particular button, OIS's persistent state may be dirty, see commentary in `getMouseState()`
     BitMask_t btnmask = 1 << _id;
     BITMASK_SET_0(mouseState.buttons, btnmask);
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse released, btn={}", (int)_id));
-
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("InputEngine: mouse released; new state: LMB={}, MMB={}, RMB={}",
-            mouseState.buttonDown(OIS::MB_Left), mouseState.buttonDown(OIS::MB_Right), mouseState.buttonDown(OIS::MB_Right)));
 }
 
 /* --- Custom Methods ------------------------------------------ */

source/main/AppContext.cpp
@@ -186,11 +186,6 @@ void AppContext::windowResized(Ogre::RenderWindow* rw)
 
 void AppContext::windowFocusChange(Ogre::RenderWindow* rw)
 {
-    const OIS::MouseState ms = App::GetInputEngine()->getMouseState();
-    App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_NOTICE,
-        fmt::format("Window focus change; isActive={}, LMB={}, MMB={}, RMB={}",
-            rw->isActive(), ms.buttonDown(OIS::MB_Left), ms.buttonDown(OIS::MB_Middle), ms.buttonDown(OIS::MB_Right)));
-    
     // If you alt+TAB out of the window while any mouse button is down, OIS will not release it until you click in the window again.
     // See https://github.com/RigsOfRods/rigs-of-rods/issues/2468
     // To work around, we reset all internal mouse button states here and pay attention not to get them polluted by OIS again.

commit d7f9199da282939d3119dd6c7468404ce3b54994

Fixes #3216 - Airplane dashboard not reacting to mouse
_problem: _
wrong viewport dimensions used when calculating relative mouse position to query for overlay element.
Broken in #3184. Before that, we queried both mouse position and viewport dimensions directly from OIS.
I had to change that because of OIS bug, and now the dimension values are not updated correctly.

_solution:_
Minimum viable fix - just keep the same dummy OIS event, just update the viewport dimensions.

source/main/utils/InputEngine.cpp
@@ -609,6 +609,10 @@ OIS::MouseState InputEngine::getMouseState()
     // See commentary in `resetKeysAndMouseButtons()`
     // To work around, we keep internal button states and pay attention not to get them polluted by OIS.
     // -----------------------------------------------------------------------------------------------------
+
+    mouseState.width = (int)App::GetAppContext()->GetRenderWindow()->getWidth();
+    mouseState.height = (int)App::GetAppContext()->GetRenderWindow()->getHeight();
+
     return mouseState;
 }
 