RigsOfRods/rigs-of-rods

commit 3b681f3e7bddad4e4a26a48be3d90db97738c02c

:angel:Script :bulb: `game.destroyObject(instanceName)` also works with TOBJ
Previously objects spawned via TOBJ file mostly didn't have instance names (optional), so this script function couldn't be used with them.
Now, all objects without custom instancename get one generated, as "auto^filename.tobj(line#)".
These auto^ names are not saved when exporting TOBJ from terrain editor.

The 'example_terrn_editorObjects.as' file was updated to showcase this.

source/main/resources/tobj_fileformat/TObjFileFormat.cpp
@@ -409,13 +409,13 @@ Ogre::Quaternion TObjParser::CalcRotation(Ogre::Vector3 const& rot) const
 
 bool TObjParser::ParseObjectLine(TObjEntry& object)
 {
-    Str<300> odef("generic");
-    char type[100] = {};
-    char name[100] = {};
+    Str<TObj::STR_LEN> odef("generic");
+    Str<TObj::STR_LEN> type("");
+    Str<TObj::STR_LEN> instance_name("");
     Ogre::Vector3 pos(Ogre::Vector3::ZERO);
     Ogre::Vector3 rot(Ogre::Vector3::ZERO);
     int r = sscanf(m_cur_line, "%f, %f, %f, %f, %f, %f, %s %s %s",
-        &pos.x, &pos.y, &pos.z, &rot.x, &rot.y, &rot.z, odef.GetBuffer(), type, name);
+        &pos.x, &pos.y, &pos.z, &rot.x, &rot.y, &rot.z, odef.GetBuffer(), type.GetBuffer(), instance_name.GetBuffer());
     if (r < 6)
     {
         return false;
@@ -435,7 +435,19 @@ bool TObjParser::ParseObjectLine(TObjEntry& object)
     else if (odef == "roadbridgenopillar") { special = TObj::SpecialObject::ROAD_BRIDGE_NO_PILLARS; }
     else if (odef == "roadbridge"        ) { special = TObj::SpecialObject::ROAD_BRIDGE           ; }
 
-    object = TObjEntry(pos, rot, odef.ToCStr(), special, type, name);
+    // If no instance name given, generate one, so that scripts can use `game.destroyObject(instanceName)` even for pre-placed objects.
+    // Don't use spaces because TOBJ parser doesn't support "" yet (game tries to strip the 'auto^' instancenames on save, but someone could export via custom script).
+    if (instance_name == "")
+    {
+        instance_name << "auto^" << m_filename << "(line:" << m_line_number << ")";
+        // Also set 'type' arg to non-empty (same reason).
+        if (special == TObj::SpecialObject::NONE)
+        {
+            type << "-";
+        }
+    }
+
+    object = TObjEntry(pos, rot, odef.ToCStr(), special, type, instance_name);
     object.rendering_distance = m_default_rendering_distance;
 
     // Attach comments
@@ -614,10 +626,17 @@ void TObj::WriteToStream(TObjDocumentPtr doc, Ogre::DataStreamPtr stream)
             }
         }
 
+        // Don't save autogenerated instance names
+        std::string valid_instance_name;
+        if (strncmp(entry.instance_name, "auto^", 5) != 0)
+        {
+            valid_instance_name = entry.instance_name;
+        }
+
         std::string line = fmt::format("{:8.3f}, {:8.3f}, {:8.3f}, {:9f}, {:9f}, {:9f}, {} {} {}\n",
             entry.position.x, entry.position.y, entry.position.z,
             entry.rotation.x, entry.rotation.y, entry.rotation.z,
-            entry.odef_name, entry.type, entry.instance_name);
+            entry.odef_name, entry.type, valid_instance_name);
         stream->write(line.c_str(), line.length());
     }
 }

source/main/terrain/TerrainEditor.h
@@ -69,6 +69,7 @@ public:
     void UpdateInputEvents(float dt);
     void WriteOutputFile();
     void ClearSelection();
+    int GetSelectedObjectIndex() const { return m_object_index; }
 
 private:
     bool                m_object_tracking = true;

resources/scripts/example_terrn_editorObjects.as
@@ -5,33 +5,46 @@
 #include "imgui_utils.as"
 imgui_utils::CloseWindowPrompt closeBtnHandler;
 
+void drawEditorObjectLine(array<TerrainEditorObjectClassPtr@>@ ediObjects, uint i)
+{
+    ImGui::PushID(i); // important - otherwise "destroy" button will interfere between objects, deleting multiple!
+    
+    TerrainEditorObjectClass@ curObj = ediObjects[i];
+    ImGui::TextDisabled("["+i+"/"+ediObjects.length()+"]");
+    ImGui::SameLine();
+    ImGui::Text(curObj.getName());
+    ImGui::SameLine();ImGui::TextDisabled(",");ImGui::SameLine();
+    ImGui::Text(curObj.getType()!="" ?curObj.getType(): "~");
+    ImGui::SameLine();ImGui::TextDisabled(",");ImGui::SameLine();
+    ImGui::Text('"'+curObj.getInstanceName()+'"');
+    ImGui::SameLine();
+    if (ImGui::SmallButton("destroy"))
+    {
+        game.destroyObject(curObj.getInstanceName());
+    }
+    
+    ImGui::PopID();  // i
+}
+
 void frameStep(float dt)
 {
     // Begin drawing window
     if (ImGui::Begin("Terrain Editor Objects", closeBtnHandler.windowOpen, 0))
     {
-        ImGui::TextDisabled("Enumerates all terrain objects\n using new API `game.getEditorObjects()`");
         // Draw the "Terminate this script?" prompt on the top (if not disabled by config).
         closeBtnHandler.draw();
         
-        // Direct access
+        // Header
+        ImGui::TextDisabled("Enumerates all terrain objects using new API `game.getEditorObjects()`.");
+        ImGui::TextDisabled("Also deletes objects via `game.destroyObject()` using autogenerated names.");
         ImGui::Text("Object count: " + game.getEditorObjects().length());
         ImGui::Separator();
         
-        // Local variable access
+        // Draw all instances
         array<TerrainEditorObjectClassPtr@>@ ediObjects = game.getEditorObjects();
         for (uint i = 0; i < ediObjects.length(); i++)
         {
-            TerrainEditorObjectClass@ curObj = ediObjects[i];
-            string label = "["+i+"/"+ediObjects.length()+"] " + curObj.getName(); 
-            
-            if (curObj.getInstanceName() != "") { 
-                label += " ~ instance name: '"+curObj.getInstanceName()+"'"; 
-            }
-            if (curObj.getType() != "") { 
-                label+= " (type: " + curObj.getType() + ")";
-            }
-            ImGui::TreeNode(label);
+            drawEditorObjectLine(ediObjects, i);
         }
         
         // End drawing window

commit 63e06b6a0c27488c7342f44e40794f532e27faa4

:bug: Fixed excess icons on survey map
Broken in 3b681f3 - the 'type' parameter from TOBJ file was given a default placeholder value "-" to export correctly, but surveymap expected empty string where icon wasn't desired.

source/main/resources/tobj_fileformat/TObjFileFormat.cpp
@@ -442,11 +442,6 @@ bool TObjParser::ParseObjectLine(TObjEntry& object)
     if (instance_name == "")
     {
         instance_name << "auto^" << m_filename << "(line:" << m_line_number << ")";
-        // Also set 'type' arg to non-empty (same reason).
-        if (special == TObjSpecialObject::NONE)
-        {
-            type << "-";
-        }
     }
 
     object = TObjEntry(pos, rot, odef.ToCStr(), special, type, instance_name);

source/main/terrain/TerrainEditor.h
@@ -38,7 +38,7 @@ public:
     // Variables are not accessible from AngelScript
     std::string name;
     std::string instance_name;
-    std::string type;
+    std::string type; //!< Accepts "-" as placeholder, otherwise a surveymap icon is registered.
     Ogre::Vector3 position = Ogre::Vector3::ZERO;
     Ogre::Vector3 rotation = Ogre::Vector3::ZERO;
     Ogre::Vector3 initial_position = Ogre::Vector3::ZERO;

source/main/terrain/TerrainObjectManager.cpp
@@ -729,7 +729,7 @@ bool TerrainObjectManager::LoadTerrainObject(const Ogre::String& name, const Ogr
         int race_id = res.size() > 1 ? StringConverter::parseInt(res[1], -1) : -1;
         m_map_entities.push_back(SurveyMapEntity(type, /*caption:*/type, fmt::format("icon_{}.dds", type), /*resource_group:*/"", object->position, Ogre::Radian(0), race_id));
     }
-    else if (!object->type.empty())
+    else if (object->type != "" && object->type != "-")
     {
         String caption = "";
         if (object->type == "station" || object->type == "hotel" || object->type == "village" ||