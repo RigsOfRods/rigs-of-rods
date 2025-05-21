aa7d13d0add4f857ab3818ef714f751dcb6e53ca
ohlidalp <petr@ohlidal.cz>
2025-05-21 21:10:01 +0200

    :video_game: Ressurected Envmap debug overlay.
    Toggle anytime with cvar 'diag_envmap' in console, or a checkbox in the RenderDiag UI.

--- source/main/system/CVar.cpp
+++ source/main/system/CVar.cpp
@@ -82,7 +82,7 @@ void Console::cVarSetupBuiltins()
     App::diag_rig_log_node_import= this->cVarCreate("diag_rig_log_node_import","RigImporter_LogAllNodes",    CVAR_ARCHIVE | CVAR_TYPE_BOOL,    "false");
     App::diag_rig_log_node_stats = this->cVarCreate("diag_rig_log_node_stats", "RigImporter_LogNodeStats",   CVAR_ARCHIVE | CVAR_TYPE_BOOL,    "false");
     App::diag_truck_mass         = this->cVarCreate("diag_truck_mass",         "Debug Truck Mass",           CVAR_ARCHIVE | CVAR_TYPE_BOOL,    "false");
-    App::diag_envmap             = this->cVarCreate("diag_envmap",             "EnvMapDebug",                CVAR_ARCHIVE | CVAR_TYPE_BOOL,    "false");
+    App::diag_envmap             = this->cVarCreate("diag_envmap",             "EnvMapDebug",                               CVAR_TYPE_BOOL,    "false");
     App::diag_videocameras       = this->cVarCreate("diag_videocameras",       "VideoCameraDebug",           CVAR_ARCHIVE | CVAR_TYPE_BOOL,    "false");
     App::diag_preset_terrain     = this->cVarCreate("diag_preset_terrain",     "Preselected Terrain",        CVAR_ARCHIVE);
     App::diag_preset_spawn_pos   = this->cVarCreate("diag_spawn_position",     "",                           CVAR_ARCHIVE);
