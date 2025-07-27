RigsOfRods/rigs-of-rods

commit f9a1f6e22b600a1dc4b3ba58747f846014795584

:construction: Update (export) truck file using N/B util UI.
FEATURE CHANGES TO N/B UTIL UI:
* There's a new checkbox: "Override all node masses?" - if unchecked, the TotalMass slider doesn't affec the export. If checked, all nodes will get the 'l' flag and weight override parameter. This is the only way to propagate the slider setting to the truck file.
* New checkbox: "Reduce dry mass by node mass overrides" - with explanatory text! Exporting the TotalMass slider value back to truck file is somewhat of a challenge, but when you check both the checkboxes, it works (with a minor discrepancy).

CODECHANGES TO FILE Actor.h:
new beam data layers:
    `std::vector<bool>    ar_beams_invisible;` //!< Tracks the various invisible flags across beam types (shocks/hydros/commands...)
    `std::vector<bool>    ar_beams_user_defined;` //!< True for 'beams', false for wheels/cinecam/hooknode/wings/rotators etc...
new node data layers:
    `std::vector<float>   ar_nodes_default_loadweights;`           //!< 'set_node_defaults': load weight.
    `std::vector<float>   ar_nodes_override_loadweights;`          //!< 'nodes': 'l' flag and number.
    `std::vector<BitMask_t> ar_nodes_options;`                     //!< merged options from 'nodes' and 'set_node_defaults'
new N/B util helper fields:
    `bool ar_nb_export_override_all_node_masses = false;`
    `bool ar_nb_export_reduce_drymass_by_nodemass_overrides = false;`

source/main/resources/CacheSystem.cpp
@@ -1902,6 +1902,17 @@ CacheEntryPtr CacheSystem::CreateProject(CreateProjectRequest* request)
         {
             Ogre::FileInfo fileinfo = filelist->at(i);
 
+            if (request->cpr_type == CreateProjectRequestType::ACTOR_PROJECT)
+            {
+                std::string basename, ext;
+                Ogre::StringUtil::splitBaseFilename(fileinfo.filename, basename, ext);
+                // Skip all actor files - the one we care about will be added manually.
+                if (ext == "truck" || ext == "car" || ext == "load" || ext == "fixed" || ext == "boat" || ext == "airplane" || ext == "train" || ext == "trailer")
+                {
+                    continue;
+                }
+            }
+
             // Render a frame with a progress window on it.
             App::GetGuiManager()->LoadingWindow.SetProgress(
                 (i+1)/filelist->size(),
@@ -1930,13 +1941,58 @@ CacheEntryPtr CacheSystem::CreateProject(CreateProjectRequest* request)
 
         App::GetGuiManager()->LoadingWindow.SetVisible(false);
         Ogre::ResourceGroupManager::getSingleton().destroyResourceGroup(temp_rg);
-
-        // Finally rename the mod file
-        Ogre::FileSystemLayer::renameFile(
-            /*oldPath:*/ PathCombine(project_path, request->cpr_source_entry->fname),
-            /*newPath:*/ PathCombine(project_path, project_entry->fname));
     }
 
+   if (request->cpr_type == CreateProjectRequestType::ACTOR_PROJECT)
+   {
+       // Load the project file to perform fixups (name & category)
+         GenericDocumentPtr doc = new GenericDocument();
+         int flags = GenericDocument::OPTION_ALLOW_NAKED_STRINGS
+                              | GenericDocument::OPTION_ALLOW_SLASH_COMMENTS
+                              | GenericDocument::OPTION_FIRST_LINE_IS_TITLE
+                              | GenericDocument::OPTION_ALLOW_SEPARATOR_COLON
+                              | GenericDocument::OPTION_PARENTHESES_CAPTURE_SPACES;
+           if (!doc->loadFromResource(request->cpr_source_entry->fname, request->cpr_source_entry->resource_group, flags))
+          {
+               App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_ERROR,
+                   fmt::format(_LC("CacheSystem", "Could not load project file '{}' to perform fixups."),
+                       project_entry->fname));
+               return nullptr;
+          }
+
+       // fixup the document.. 
+
+        GenericDocContextPtr ctx = new GenericDocContext(doc);
+        // >> seek the name
+        while (!ctx->isTokString()) 
+        {
+            ctx->seekNextLine(); 
+        }
+        // >> change the name
+        if (!ctx->endOfFile()) 
+        {
+            ctx->setTokString(0, project_entry->dname);
+        }
+        // >> seek fileinfo 
+        while (!ctx->endOfFile() && (!ctx->isTokKeyword() || ctx->getTokKeyword() != "fileinfo"))
+        {
+            ctx->seekNextLine(); 
+        }
+        // change the fileinfo param #2 categoryid (if found)
+        if (!ctx->endOfFile() && ctx->isTokKeyword() && ctx->getTokKeyword() == "fileinfo" && !ctx->endOfFile(2))
+        {
+            ctx->setTokFloat(2, CID_Projects);
+        }
+
+       // Write the document
+       if (!doc->saveToResource(project_entry->fname, project_entry->resource_group))
+       {
+            App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_ERROR,
+                fmt::format(_LC("CacheSystem", "Could not save fixed-up project file '{}'."),
+                    project_entry->fname));
+            return nullptr;
+       }
+   }
 
     if (project_entry_created)
     {
@@ -1949,6 +2005,9 @@ CacheEntryPtr CacheSystem::CreateProject(CreateProjectRequest* request)
     TRIGGER_EVENT_ASYNC(SE_GENERIC_MODCACHE_ACTIVITY,
         /*ints*/ activity_type, project_entry->number, 0, 0,
         /*strings*/ project_entry->fname, project_entry->fext);
+
+    // Unload the preliminary resource group - force proper load of complete bundle.
+    this->UnLoadResource(project_entry);
 
     return project_entry;
 }
@@ -2148,6 +2207,28 @@ void CacheSystem::ModifyProject(ModifyProjectRequest* request)
         request->mpr_target_actor->removeWorkingTuneupDef();
         ROR_ASSERT(!request->mpr_target_actor->getWorkingTuneupDef());
         break;
+
+    case ModifyProjectRequestType::ACTOR_UPDATE_DEF_DOCUMENT:
+    {
+        const bool actor_ok(request->mpr_target_actor && request->mpr_target_actor->ar_state != ActorState::DISPOSED);
+        if (!actor_ok)
+        {
+            App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_ERROR,
+                fmt::format(_LC("CacheSystem", "Error updating truck file: actor not found or disposed")));
+            return;
+        }
+        RoR::CacheEntryPtr entry = request->mpr_target_actor->getUsedActorEntry();
+        const bool entry_ok(entry && entry->resource_bundle_type == "FileSystem");
+        if (!entry_ok)
+        {
+            App::GetConsole()->putMessage(Console::CONSOLE_MSGTYPE_INFO, Console::CONSOLE_SYSTEM_ERROR,
+                fmt::format(_LC("CacheSystem", "Error updating truck file: cache entry missing or not a project")));
+            return;
+        }
+        request->mpr_target_actor->propagateNodeBeamChangesToDef();
+        App::GetGameContext()->GetActorManager()->ExportActorDef(entry->actor_def, entry->fname, entry->resource_group);
+        break;
+    }
 
     default:
         break;

source/main/CMakeLists.txt
@@ -145,6 +145,7 @@ set(SOURCE_FILES
         network/RoRnet.h
         physics/Actor.{h,cpp}
         physics/ApproxMath.h
+        physics/ActorExport.cpp
         physics/ActorForcesEuler.cpp
         physics/ActorManager.{h,cpp}
         physics/ActorSlideNode.cpp

source/main/gui/panels/GUI_NodeBeamUtils.cpp
@@ -37,15 +37,25 @@ void NodeBeamUtils::Draw()
         this->SetVisible(false);
         return;
     }
+    const bool is_project = actor->getUsedActorEntry()->resource_bundle_type != "Zip";
+
     ImGui::SetNextWindowPosCenter(ImGuiCond_FirstUseEver);
     ImGui::SetNextWindowSize(ImVec2(600.f, 675.f), ImGuiCond_FirstUseEver);
-    const int flags = ImGuiWindowFlags_NoCollapse;
+    int flags = ImGuiWindowFlags_NoCollapse;
+    if (is_project)
+    {
+        flags |= ImGuiWindowFlags_MenuBar;
+    }
     bool keep_open = true;
     ImGui::Begin(_LC("NodeBeamUtils", "Node/Beam Utils"), &keep_open, flags);
 
-    if (actor->getUsedActorEntry()->resource_bundle_type == "Zip")
+    if (!is_project)
     {
         this->DrawCreateProjectBanner(actor, keep_open);
+    }
+    else
+    {
+        this->DrawMenubar(actor);
     }
 
     ImGui::PushItemWidth(500.f); // Width includes [+/-] buttons
@@ -221,12 +231,10 @@ void NodeBeamUtils::DrawCreateProjectBanner(ActorPtr actor, bool& window_open)
     {
         // Unzip the mod
         RoR::CreateProjectRequest* req = new RoR::CreateProjectRequest();
-        std::string basefname, ext, path;
-        Ogre::StringUtil::splitFullFilename(actor->getUsedActorEntry()->fname_without_uid, basefname, ext, path);
-        req->cpr_name = basefname + "_nbutil." + actor->getUsedActorEntry()->fext;
+        req->cpr_name = "nbutil_" + actor->getUsedActorEntry()->fname_without_uid;
         req->cpr_description = "Node/Beam Utils project for " + actor->getUsedActorEntry()->dname;
         req->cpr_source_entry = actor->getUsedActorEntry();
-        req->cpr_type = RoR::CreateProjectRequestType::DEFAULT;
+        req->cpr_type = RoR::CreateProjectRequestType::ACTOR_PROJECT;
         App::GetGameContext()->PushMessage(Message(MSG_EDI_CREATE_PROJECT_REQUESTED, req));
 
         // Show a message box "please load the project"
@@ -243,4 +251,34 @@ void NodeBeamUtils::DrawCreateProjectBanner(ActorPtr actor, bool& window_open)
         window_open = false;
     }
     ImGui::Dummy(ImVec2(1.f, 6.f));
+}
+
+void NodeBeamUtils::DrawMenubar(ActorPtr actor)
+{
+    if (ImGui::BeginMenuBar())
+    {
+        ImGui::TextDisabled(_LC("NodeBeamUtils", "Project:"));
+        ImGui::SameLine();
+        ImGui::Text("%s", actor->getUsedActorEntry()->dname.c_str());
+        ImGui::SameLine();
+        if (ImGui::SmallButton(_LC("NodeBeamUtils", "Save active")))
+        {
+            RoR::ModifyProjectRequest* req = new RoR::ModifyProjectRequest();
+            req->mpr_type = RoR::ModifyProjectRequestType::ACTOR_UPDATE_DEF_DOCUMENT;
+            req->mpr_target_actor = actor;
+            App::GetGameContext()->PushMessage(Message(MSG_EDI_MODIFY_PROJECT_REQUESTED, req));
+        }
+        if (ImGui::BeginMenu(_LC("NodeBeamUtils", "Export options")))
+        {
+            ImGui::Checkbox(_LC("NodeBeamUtils", "Override all node masses"), &actor->ar_nb_export_override_all_node_masses);
+            ImGui::TextDisabled(_LC("NodeBeamUtils", "Gives all nodes the 'l' flag and weight override parameter.\nThis is the only way to propagate the slider setting to the truck file."));
+            ImGui::Separator();
+            ImGui::Checkbox(_LC("NodeBeamUtils", "Reduce dry mass by node mass overrides"), &actor->ar_nb_export_reduce_drymass_by_nodemass_overrides);
+            ImGui::TextDisabled(_LC("NodeBeamUtils", "When converting node from calculated-mass to override-mass, reduce dry mass accordingly."));
+            ImGui::EndMenu();
+        }
+        
+
+        ImGui::EndMenuBar();
+    }
 }

source/main/gui/panels/GUI_NodeBeamUtils.h
@@ -42,6 +42,7 @@ private:
     const ImVec4 GRAY_HINT_TEXT = ImVec4(0.62f, 0.62f, 0.61f, 1.f);
 
     void DrawCreateProjectBanner(ActorPtr actor, bool& window_open);
+    void DrawMenubar(ActorPtr actor);
 };
 
 } // namespace GUI

source/main/physics/Actor.cpp
@@ -659,8 +659,11 @@ void Actor::calcNetwork()
     m_net_initialized = true;
 }
 
-void Actor::RecalculateNodeMasses(Real total)
+void Actor::recalculateNodeMasses()
 {
+    // Originally `calc_masses2(Real total, bool reCalc)`, where `total` was always the dry mass.
+    // ------------------------------------------------------------------------------------------
+
     //reset
     for (int i = 0; i < ar_num_nodes; i++)
     {
@@ -694,7 +697,7 @@ void Actor::RecalculateNodeMasses(Real total)
     {
         if (ar_beams[i].bm_type != BEAM_VIRTUAL)
         {
-            Real half_mass = ar_beams[i].L * total / len / 2.0f;
+            Real half_mass = ar_beams[i].L * m_dry_mass / len / 2.0f;
             if (!ar_beams[i].p1->nd_tyre_node)
                 ar_beams[i].p1->mass += half_mass;
             if (!ar_beams[i].p2->nd_tyre_node)
@@ -4422,6 +4425,11 @@ std::vector<std::string> Actor::getDescription()
 void Actor::setMass(float m)
 {
     m_dry_mass = m;
+}
+
+void Actor::setLoadedMass(float m)
+{
+    m_load_mass = m;
 }
 
 bool Actor::getCustomLightVisible(int number)

source/main/physics/Actor.h
@@ -126,12 +126,15 @@ public:
     // PLEASE maintain the same order as in 'scripting/bindings/ActorAngelscript.cpp' and 'doc/angelscript/.../BeamClass.h'
     void              scaleTruck(float value);
     void              setMass(float m);
+    void              setLoadedMass(float m);
     void              setSimAttribute(ActorSimAttr attr, float val); //!< HAZARDOUS - values may not be checked; Pay attention to 'safe values' at each attribute description.
     float             getSimAttribute(ActorSimAttr attr);
     // not exported to scripting:
     void              applyNodeBeamScales();               //!< For GUI::NodeBeamUtils
     void              searchBeamDefaults();                //!< Searches for more stable beam defaults
     void              updateInitPosition();
+    void              propagateNodeBeamChangesToDef(); //!< Back-propagates changes done by N/B-utils UI to the def-document.
+    void              recalculateNodeMasses();
     /// @}
 
     /// @name User interaction
@@ -284,18 +287,39 @@ public:
 
     // -------------------- Public data -------------------- //
 
-    // Nodes
+    /// @name Mass
+    /// @{
+    float             m_dry_mass = 0.f;              //!< User-defined; from 'globals' arg#1 - default for all nodes
+    float             m_load_mass = 0.f;             //!< User-defined; from 'globals' arg#2 - only applies to nodes with 'l' flag
+    int               m_masscount = 0;               //!< Calculated; Number of nodes loaded with l option
+    float             m_total_mass = 0.f;            //!< Calculated; total mass in Kg
+    float             ar_initial_total_mass = 0.f;   //!< Calculated; total mass in Kg (snapshot at spawn)
+    /// @}
+
+    // Node data (split to layers)
     node_t*              ar_nodes = nullptr;
     int*                 ar_nodes_id = nullptr;                  //!< Number in truck file, -1 for nodes generated by wheels/cinecam
     std::string*         ar_nodes_name = nullptr;                //!< Name in truck file, only if defined with 'nodes2'
-    int                  ar_nodes_name_top_length = 0; //!< For nicely formatted diagnostic output
-    Ogre::Vector3*       ar_nodes_spawn_offsets = nullptr; //!< Relative positions (incl. Tuning system tweaks) from the definition file, for spawn-like resetting (i.e. with terrain editor on preloaded actor).
-    int                  ar_num_nodes = 0;
-    std::vector<float>   ar_minimass;                  //!< minimum node mass in Kg
-    bool                 ar_minimass_skip_loaded_nodes = false;
+    std::vector<float>   ar_nodes_default_loadweights;           //!< 'set_node_defaults': load weight.
+    std::vector<float>   ar_nodes_override_loadweights;          //!< 'nodes': 'l' flag and number.
+    Ogre::Vector3*       ar_nodes_spawn_offsets = nullptr;       //!< Relative positions (incl. Tuning system tweaks) from the definition file, for spawn-like resetting (i.e. with terrain editor on preloaded actor).
+    std::vector<BitMask_t> ar_nodes_options;                     //!< merged options from 'nodes' and 'set_node_defaults'
+    std::vector<float>   ar_minimass;                            //!< minimum node mass in Kg
+    std::vector<float>   ar_initial_node_masses;
+    std::vector<Ogre::Vector3> ar_initial_node_positions;        //!< Absolute world positions, for resetting to pristine state.
 
+    // Node additional info
+    bool                 ar_minimass_skip_loaded_nodes = false;
+    int                  ar_nodes_name_top_length = 0; //!< For nicely formatted diagnostic output
+    int                  ar_num_nodes = 0;
+
+    // Beam data (split to layers)
     beam_t*              ar_beams = nullptr;
     int                  ar_num_beams = 0;
+    std::vector<std::pair<float, float>> ar_initial_beam_defaults;
+    std::vector<bool>    ar_beams_invisible;    //!< Used only by the exporter (for rendering, invisible beams simply get no mesh).
+    std::vector<bool>    ar_beams_user_defined; //!< True for 'beams', false for wheels/cinecam/hooknode/wings/rotators etc...
+
     std::vector<beam_t*> ar_inter_beams;       //!< Beams connecting 2 actors
     shock_t*             ar_shocks = nullptr;            //!< Shock absorbers
     int                  ar_num_shocks = 0;        //!< Number of shock absorbers
@@ -315,10 +339,7 @@ public:
     Ogre::AxisAlignedBox      ar_bounding_box;     //!< standard bounding box (surrounds all nodes of an actor)
     Ogre::AxisAlignedBox      ar_evboxes_bounding_box; //!< bounding box around nodes eligible for eventbox triggering
     Ogre::AxisAlignedBox      ar_predicted_bounding_box;
-    float                     ar_initial_total_mass = 0.f;
-    std::vector<float>        ar_initial_node_masses;
-    std::vector<Ogre::Vector3>     ar_initial_node_positions; //!< Absolute world positions, for resetting to pristine state.
-    std::vector<std::pair<float, float>> ar_initial_beam_defaults;
+    
     std::vector<wheeldetacher_t>   ar_wheeldetachers;
     std::vector<std::vector<int>>  ar_node_to_node_connections;
     std::vector<std::vector<int>>  ar_node_to_beam_connections;
@@ -475,6 +496,8 @@ public:
 
     // Realtime node/beam structure editing helpers
     bool                    ar_nb_initialized = false;
+    bool                    ar_nb_export_override_all_node_masses = false;
+    bool                    ar_nb_export_reduce_drymass_by_nodemass_overrides = false;
     std::vector<float>      ar_nb_optimum;            //!< Temporary storage of the optimum search result
     std::vector<float>      ar_nb_reference;          //!< Temporary storage of the reference search result
     int                     ar_nb_skip_steps = 0;         //!< Amount of physics steps to be skipped before measuring
@@ -534,7 +557,6 @@ private:
     void              CalcWheels(bool doUpdate, int num_steps); 
 
     void              DetermineLinkedActors();
-    void              RecalculateNodeMasses(Ogre::Real total); //!< Previously 'calc_masses2()'
     void              calcNodeConnectivityGraph();
     void              AddInterActorBeam(beam_t* beam, ActorPtr other, ActorLinkingRequestType type);  //!< Do not call directly - use `MSG_SIM_ACTOR_LINKING_REQUESTED`
     void              RemoveInterActorBeam(beam_t* beam, ActorLinkingRequestType type);  //!< Do not call directly - use `MSG_SIM_ACTOR_LINKING_REQUESTED`
@@ -574,7 +596,6 @@ private:
     Ogre::Vector3     m_avg_node_velocity = Ogre::Vector3::ZERO;          //!< average node velocity (compared to the previous frame step)
     float             m_stabilizer_shock_sleep = 0.f;     //!< Sim state
     Replay*           m_replay_handler = nullptr;
-    float             m_total_mass = 0.f;            //!< Physics state; total mass in Kg
     NodeNum_t         m_mouse_grab_node = NODENUM_INVALID;  //!< Sim state; node currently being dragged by user
     Ogre::Vector3     m_mouse_grab_pos = Ogre::Vector3::ZERO;
     float             m_mouse_grab_move_force = 0.f;
@@ -601,9 +622,6 @@ private:
     float             m_odometer_total = 0.f;        //!< GUI state
     float             m_odometer_user = 0.f;         //!< GUI state
     int               m_num_command_beams = 0;     //!< TODO: Remove! Spawner context only; likely unused feature
-    float             m_load_mass = 0.f;             //!< Physics attr; predefined load mass in Kg
-    int               m_masscount = 0;             //!< Physics attr; Number of nodes loaded with l option
-    float             m_dry_mass = 0.f;              //!< Physics attr;
     std::unique_ptr<Buoyance> m_buoyance;              //!< Physics
     CacheEntryPtr     m_used_actor_entry;
     CacheEntryPtr     m_used_skin_entry;               //!< Graphics

commit d4dc11d25e52b7f341de193fb62fb996669febbb

Fixed 'terrain_project_importer.as' stuck in endless loop.
Underlying problem: when performing `MSG_EDI_CREATE_PROJECT_REQUESTED`, the resulting modcache entry is in unloaded state, which is something else than the script expects. Broken in f9a1f6e22b600a1dc4b3ba58747f846014795584.

source/main/resources/CacheSystem.cpp
@@ -2000,14 +2000,14 @@ CacheEntryPtr CacheSystem::CreateProject(CreateProjectRequest* request)
         m_entries.push_back(project_entry);
     }
 
+    // Reload the underlying OGRE resource group to properly pick up all added files.
+    this->ReLoadResource(project_entry); // OK to call here ~ processing `MSG_EDI_CREATE_PROJECT_REQUESTED`
+
     // notify script
     modCacheActivityType activity_type = (project_entry_created) ? MODCACHEACTIVITY_ENTRY_ADDED : MODCACHEACTIVITY_ENTRY_MODIFIED;
     TRIGGER_EVENT_ASYNC(SE_GENERIC_MODCACHE_ACTIVITY,
         /*ints*/ activity_type, project_entry->number, 0, 0,
         /*strings*/ project_entry->fname, project_entry->fext);
-
-    // Unload the preliminary resource group - force proper load of complete bundle.
-    this->UnLoadResource(project_entry);
 
     return project_entry;
 }