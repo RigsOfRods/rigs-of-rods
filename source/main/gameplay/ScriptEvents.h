9ea07733840d5d20f4ba8919cbe6544c72c3556c
ohlidalp <petr@ohlidal.cz>
2025-12-23 17:45:45 +0100

    :angel:Script: New subevent `ASMANIP_SCRIPT_LOAD_FAILED`
    This is needed in case the request came from another script, for example the script_editor.as.
    Until recently this was covered by ASMANIP_SCRIPT_LOADED which arrived anyway, but that changed recently in 3d8e44c1222960e194366279a79ff0097e64f35a.

--- source/main/gameplay/ScriptEvents.h
+++ source/main/gameplay/ScriptEvents.h
@@ -72,8 +72,9 @@ enum scriptEvents
 enum angelScriptManipulationType
 {
     ASMANIP_CONSOLE_SNIPPET_EXECUTED = 0, // 0 for Backwards compatibility.
-    ASMANIP_SCRIPT_LOADED,                //!< Triggered after the script's `main()` completed; may trigger additional processing (for example, it delivers the *.mission file to mission system script). Args: #2 ScriptUnitId_t, #3 RoR::ScriptCategory, #4 unused, #5 filename.
-    ASMANIP_SCRIPT_UNLOADING,             //!< Triggered before unloading the script to let it clean up (important for missions). Args: #2 ScriptUnitId_t, #3 RoR::ScriptCategory, #4 unused, #5 filename.
+    ASMANIP_SCRIPT_LOADED,                //!< Triggered after the script's `main()` completed; Args: #2 ScriptUnitId_t, #3 RoR::ScriptCategory, #4 unused, #5 filename.
+    ASMANIP_SCRIPT_LOAD_FAILED,           //!< Triggered if the script fails to start at all. Args: #2 unused, #3 RoR::ScriptCategory, #4 unused, #5 filename.
+    ASMANIP_SCRIPT_UNLOADING,             //!< Triggered before unloading the script to let it clean up. Args: #2 ScriptUnitId_t, #3 RoR::ScriptCategory, #4 unused, #5 filename.
     ASMANIP_ACTORSIMATTR_SET              //!< Triggered when `setSimAttribute()` is called; additional args: #2 `RoR::ActorSimAtrr`, #3 ---, #4 ---, #5 attr name, #6 value converted to string.
 };
 
