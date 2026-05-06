RigsOfRods/rigs-of-rods

commit b94cad2749aa415041fd2e00aa30eda43ae84c97

:wrench: Unified duplicate `WheelBraking` combo
Removed enum RigDef::WheelBraking which duplicated enum wheel_t::BrakeCombo in SimData.h. Using the new RoR::WheelBraking everywhere. Finally got rid of `wheel_t::BrakeCombo ActorSpawner::TranslateBrakingDef(RigDef::WheelBraking def);`
Needed to break circular header dependencies (a nasty C++ thing). Factored out from a more complex buggy commit.

source/main/physics/Actor.cpp
@@ -1421,14 +1421,14 @@ void Actor::toggleTransferCaseMode()
 
     if (m_transfer_case->tr_4wd_mode)
     {
-        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_1].wh_propulsed = true;
-        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_2].wh_propulsed = true;
+        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_1].wh_propulsed = WheelPropulsion::FORWARD;
+        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_2].wh_propulsed = WheelPropulsion::FORWARD;
         m_num_proped_wheels += 2;
     }
     else
     {
-        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_1].wh_propulsed = false;
-        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_2].wh_propulsed = false;
+        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_1].wh_propulsed = WheelPropulsion::FORWARD;
+        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_2].wh_propulsed = WheelPropulsion::FORWARD;
         m_num_proped_wheels -= 2;
     }
 }

source/main/Application.h
@@ -517,6 +517,36 @@ enum class ExtCameraMode
     NODE    = 2,
 };
 
+enum class WheelBraking: int /// Wheels are braked by three mechanisms: A footbrake, a handbrake/parkingbrake, and directional brakes used for skidsteer steering.
+{
+    // DO NOT MODIFY NUMBERS - de/serialized from/to truck definition file, see RigDef_File.h, `struct BaseWheel`
+
+    NONE                 = 0, //!< = no  footbrake, no  handbrake, no  direction control -- wheel is unbraked
+    FOOT_HAND            = 1, //!< = yes footbrake, yes handbrake, no  direction control
+    FOOT_HAND_SKID_LEFT  = 2, //!< = yes footbrake, yes handbrake, yes direction control (braked when vehicle steers to the left)
+    FOOT_HAND_SKID_RIGHT = 3, //!< = yes footbrake, yes handbrake, yes direction control (braked when vehicle steers to the right)
+    FOOT_ONLY            = 4, //!< = yes footbrake, no  handbrake, no  direction control -- footbrake only, such as with the front wheels of a passenger car
+};
+
+enum class WheelPropulsion: int
+{
+    // DO NOT MODIFY NUMBERS - de/serialized from/to truck definition file, see RigDef_File.h, `struct BaseWheel`
+
+    NONE                 = 0,
+    FORWARD              = 1,
+    BACKWARD             = 2,
+};
+
+/// Used by rig-def/addonpart/tuneup formats to specify wheel rim mesh orientation.
+enum class WheelSide: char
+{
+    // DO NOT MODIFY LETTERS - de/serialized from/to truck definition file, see 'RigDef_File.h' and 'ActorExport.cpp'
+
+    INVALID   = 'n',
+    RIGHT     = 'r',
+    LEFT      = 'l'
+};
+
 enum class TObjSpecialObject
 {
     // Exported to AngelScript:

source/main/gfx/GfxData.h
@@ -111,14 +111,6 @@ enum class DebugViewType
     DEBUGVIEW_SUBMESH,
 };
 
-/// Used by rig-def/addonpart/tuneup formats to specify wheel rim mesh orientation.
-enum class WheelSide: char
-{
-    INVALID   = 'n',
-    RIGHT     = 'r',
-    LEFT      = 'l'
-};
-
 // Dynamic visibility control (value 0 and higher is cinecam index) - common to 'props' and 'flexbodies'
 typedef int CameraMode_t;
 static CameraMode_t CAMERA_MODE_ALWAYS_HIDDEN = -3;

source/main/physics/ActorForcesEuler.cpp
@@ -171,8 +171,10 @@ void Actor::CalcDifferentials()
         }
         for (int i = 0; i < ar_num_wheels; i++)
         {
-            if (ar_wheels[i].wh_propulsed && !ar_wheels[i].wh_is_detached)
+            if (ar_wheels[i].wh_propulsed != WheelPropulsion::NONE && !ar_wheels[i].wh_is_detached)
+            {
                 ar_wheels[i].wh_torque += torque;
+            }
         }
     }
 
@@ -318,14 +320,14 @@ void Actor::CalcWheels(bool doUpdate, int num_steps)
             ar_wheels[i].wh_tc_coef = 1.0f;
         }
 
-        if (ar_wheels[i].wh_braking != wheel_t::BrakeCombo::NONE)
+        if (ar_wheels[i].wh_braking != WheelBraking::NONE)
         {
             // footbrake
             float abrake = ar_brake_force * ar_brake;
 
             // handbrake
             float hbrake = 0.0f;
-            if (ar_parking_brake && (ar_wheels[i].wh_braking != wheel_t::BrakeCombo::FOOT_ONLY))
+            if (ar_parking_brake && (ar_wheels[i].wh_braking != WheelBraking::FOOT_ONLY))
             {
                 hbrake = m_handbrake_force;
             }
@@ -333,8 +335,8 @@ void Actor::CalcWheels(bool doUpdate, int num_steps)
             // directional braking
             float dbrake = 0.0f;
             if ((ar_wheels[i].wh_speed < 20.0f)
-                && (((ar_wheels[i].wh_braking == wheel_t::BrakeCombo::FOOT_HAND_SKID_LEFT)  && (ar_hydro_dir_state > 0.0f))
-                 || ((ar_wheels[i].wh_braking == wheel_t::BrakeCombo::FOOT_HAND_SKID_RIGHT) && (ar_hydro_dir_state < 0.0f))))
+                && (((ar_wheels[i].wh_braking == WheelBraking::FOOT_HAND_SKID_LEFT)  && (ar_hydro_dir_state > 0.0f))
+                 || ((ar_wheels[i].wh_braking == WheelBraking::FOOT_HAND_SKID_RIGHT) && (ar_hydro_dir_state < 0.0f))))
             {
                 dbrake = ar_brake_force * abs(ar_hydro_dir_state);
             }
@@ -389,7 +391,7 @@ void Actor::CalcWheels(bool doUpdate, int num_steps)
             Vector3 radius = outer_node->RelPosition - inner_node->RelPosition;
             float inverted_rlen = 1.0f / radius.length();
 
-            if (ar_wheels[i].wh_propulsed == 2)
+            if (ar_wheels[i].wh_propulsed == WheelPropulsion::BACKWARD)
             {
                 radius = -radius;
             }
@@ -424,7 +426,7 @@ void Actor::CalcWheels(bool doUpdate, int num_steps)
         // We overestimate the average speed on purpose in order to improve the quality of the braking force estimate
         ar_wheels[i].wh_avg_speed = ar_wheels[i].wh_avg_speed * 0.99 + ar_wheels[i].wh_speed * 0.1;
         ar_wheels[i].debug_rpm += RAD_PER_SEC_TO_RPM * ar_wheels[i].wh_speed / ar_wheels[i].wh_radius / (float)num_steps;
-        if (ar_wheels[i].wh_propulsed == 1)
+        if (ar_wheels[i].wh_propulsed == WheelPropulsion::FORWARD)
         {
             float speedacc = ar_wheels[i].wh_speed / (float)m_num_proped_wheels;
             ar_wheel_speed += speedacc;                          // Accumulate the average wheel speed (m/s)

source/main/physics/ActorSpawner.cpp
@@ -475,7 +475,7 @@ void ActorSpawner::FinalizeRig()
         float proped_wheels_radius_sum = 0.0f;
         for (int i = 0; i < m_actor->ar_num_wheels; i++)
         {
-            if (m_actor->ar_wheels[i].wh_propulsed > 0)
+            if (m_actor->ar_wheels[i].wh_propulsed != WheelPropulsion::NONE)
             {
                 proped_wheels_radius_sum += m_actor->ar_wheels[i].wh_radius;
             }
@@ -2865,15 +2865,15 @@ void ActorSpawner::ProcessTransferCase(RigDef::TransferCase & def)
 
     for (int i = 0; i < m_actor->ar_num_wheels; i++)
     {
-        m_actor->ar_wheels[i].wh_propulsed = false;
+        m_actor->ar_wheels[i].wh_propulsed = WheelPropulsion::NONE;
     }
-    m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a1]->di_idx_1].wh_propulsed = true;
-    m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a1]->di_idx_2].wh_propulsed = true;
+    m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a1]->di_idx_1].wh_propulsed = WheelPropulsion::FORWARD;
+    m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a1]->di_idx_2].wh_propulsed = WheelPropulsion::FORWARD;
     m_actor->m_num_proped_wheels = 2;
     if (!def.has_2wd)
     {
-        m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a2]->di_idx_1].wh_propulsed = true;
-        m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a2]->di_idx_2].wh_propulsed = true;
+        m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a2]->di_idx_1].wh_propulsed = WheelPropulsion::FORWARD;
+        m_actor->ar_wheels[m_actor->m_wheel_diffs[def.a2]->di_idx_2].wh_propulsed = WheelPropulsion::FORWARD;
         m_actor->m_num_proped_wheels = 4;
         m_actor->m_transfer_case->tr_4wd_mode = true;
     }
@@ -4487,8 +4487,8 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
     }
 
     // Wheel object
-    wheel.wh_braking = this->TranslateBrakingDef(def.braking);
-    wheel.wh_propulsed = (int)def.propulsion;
+    wheel.wh_braking = def.braking;
+    wheel.wh_propulsed = def.propulsion;
     wheel.wh_num_nodes = 2 * def.num_rays;
     wheel.wh_num_rim_nodes = wheel.wh_num_nodes;
     wheel.wh_axis_node_0 = axis_node_1;
@@ -4497,7 +4497,7 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
     wheel.wh_rim_radius = override_rim_radius;
     wheel.wh_arm_node = this->GetNodePointer(def.reference_arm_node);
 
-    if (def.propulsion != RigDef::WheelPropulsion::NONE)
+    if (def.propulsion != WheelPropulsion::NONE)
     {
         // for inter-differential locking
         m_actor->m_proped_wheel_pairs[m_actor->m_num_proped_wheels] = m_actor->ar_num_wheels;
@@ -4524,19 +4524,6 @@ void ActorSpawner::ProcessFlexBodyWheel(RigDef::FlexBodyWheel & def)
 
     // Commit the wheel
     ++m_actor->ar_num_wheels;
-}
-
-wheel_t::BrakeCombo ActorSpawner::TranslateBrakingDef(RigDef::WheelBraking def)
-{
-    switch (def)
-    {
-    case RigDef::WheelBraking::NONE:                 return wheel_t::BrakeCombo::NONE;
-    case RigDef::WheelBraking::FOOT_HAND:            return wheel_t::BrakeCombo::FOOT_HAND;
-    case RigDef::WheelBraking::FOOT_HAND_SKID_LEFT:  return wheel_t::BrakeCombo::FOOT_HAND_SKID_LEFT;
-    case RigDef::WheelBraking::FOOT_HAND_SKID_RIGHT: return wheel_t::BrakeCombo::FOOT_HAND_SKID_RIGHT;
-    case RigDef::WheelBraking::FOOT_ONLY:            return wheel_t::BrakeCombo::FOOT_ONLY;
-    default:                                         return wheel_t::BrakeCombo::NONE;
-    }
 }
 
 void ActorSpawner::GetWheelAxisNodes(RigDef::BaseWheel& def, node_t*& out_node_1, node_t*& out_node_2)
@@ -4735,8 +4722,8 @@ void ActorSpawner::BuildWheelObjectAndNodes(
     unsigned int reserve_nodes,
     unsigned int reserve_beams,
     float wheel_radius,
-    RigDef::WheelPropulsion propulsion,
-    RigDef::WheelBraking braking,
+    WheelPropulsion propulsion,
+    WheelBraking braking,
     std::shared_ptr<RigDef::NodeDefaults> node_defaults,
     float wheel_mass,
     float wheel_width       /* Default: -1.f */
@@ -4750,8 +4737,8 @@ void ActorSpawner::BuildWheelObjectAndNodes(
     axis_vector.normalise();
 
     /* Wheel object */
-    wheel.wh_braking      = this->TranslateBrakingDef(braking);
-    wheel.wh_propulsed    = (int)propulsion;
+    wheel.wh_braking      = braking;
+    wheel.wh_propulsed    = propulsion;
     wheel.wh_num_nodes    = 2 * num_rays;
     wheel.wh_axis_node_0  = axis_node_1;
     wheel.wh_axis_node_1  = axis_node_2;
@@ -4764,7 +4751,7 @@ void ActorSpawner::BuildWheelObjectAndNodes(
     Ogre::Real length_2 = (axis_node_2->RelPosition - wheel.wh_arm_node->RelPosition).length();
     wheel.wh_near_attach_node = (length_1 < length_2) ? axis_node_1 : axis_node_2;
 
-    if (propulsion != RigDef::WheelPropulsion::NONE)
+    if (propulsion != WheelPropulsion::NONE)
     {
         /* for inter-differential locking */
         m_actor->m_proped_wheel_pairs[m_actor->m_num_proped_wheels] = m_actor->ar_num_wheels;
@@ -5126,8 +5113,8 @@ WheelID_t ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
     }
 
     /* Wheel object */
-    wheel.wh_braking       = this->TranslateBrakingDef(wheel_2_def.braking);
-    wheel.wh_propulsed     = (int)wheel_2_def.propulsion;
+    wheel.wh_braking       = wheel_2_def.braking;
+    wheel.wh_propulsed     = wheel_2_def.propulsion;
     wheel.wh_num_nodes     = 2 * wheel_2_def.num_rays;
     wheel.wh_num_rim_nodes = wheel.wh_num_nodes;
     wheel.wh_axis_node_0   = axis_node_1;
@@ -5136,7 +5123,7 @@ WheelID_t ActorSpawner::AddWheel2(RigDef::Wheel2 & wheel_2_def)
     wheel.wh_rim_radius    = override_rim_radius;
     wheel.wh_arm_node      = this->GetNodePointer(wheel_2_def.reference_arm_node);
 
-    if (wheel_2_def.propulsion != RigDef::WheelPropulsion::NONE)
+    if (wheel_2_def.propulsion != WheelPropulsion::NONE)
     {
         /* for inter-differential locking */
         m_actor->m_proped_wheel_pairs[m_actor->m_num_proped_wheels] = m_actor->ar_num_wheels;

source/main/physics/ActorSpawner.h
@@ -280,8 +280,8 @@ private:
         unsigned int reserve_nodes,
         unsigned int reserve_beams,
         float wheel_radius,
-        RigDef::WheelPropulsion propulsion,
-        RigDef::WheelBraking braking,
+        WheelPropulsion propulsion,
+        WheelBraking braking,
         std::shared_ptr<RigDef::NodeDefaults> node_defaults,
         float wheel_mass,
         float wheel_width = -1.f);
@@ -308,7 +308,6 @@ private:
     /// @{
     void                          CalcMemoryRequirements(ActorMemoryRequirements& req, RigDef::Document::Module* module_def);    
     void                          UpdateCollcabContacterNodes();
-    wheel_t::BrakeCombo           TranslateBrakingDef(RigDef::WheelBraking def);
     void                          WashCalculator();
     void                          AdjustNodeBuoyancy(node_t & node, RigDef::Node & node_def, std::shared_ptr<RigDef::NodeDefaults> defaults); //!< For user-defined nodes
     void                          AdjustNodeBuoyancy(node_t & node, std::shared_ptr<RigDef::NodeDefaults> defaults); //!< For generated nodes

source/main/physics/SimData.h
@@ -393,25 +393,16 @@ struct soundsource_t
 
 struct wheel_t
 {
-    enum class BrakeCombo /// Wheels are braked by three mechanisms: A footbrake, a handbrake/parkingbrake, and directional brakes used for skidsteer steering.
-    {
-        NONE,                 //!< - 0 = no  footbrake, no  handbrake, no  direction control -- wheel is unbraked
-        FOOT_HAND,            //!< - 1 = yes footbrake, yes handbrake, no  direction control
-        FOOT_HAND_SKID_LEFT,  //!< - 2 = yes footbrake, yes handbrake, yes direction control (braked when vehicle steers to the left)
-        FOOT_HAND_SKID_RIGHT, //!< - 3 = yes footbrake, yes handbrake, yes direction control (braked when vehicle steers to the right)
-        FOOT_ONLY             //!< - 4 = yes footbrake, no  handbrake, no  direction control -- footbrake only, such as with the front wheels of a passenger car
-    };
-
     int         wh_num_nodes;
     node_t*     wh_nodes[50];             // TODO: remove limit, make this dyn-allocated ~ only_a_ptr, 08/2017
     int         wh_num_rim_nodes;
     node_t*     wh_rim_nodes[50];         // TODO: remove limit, make this dyn-allocated ~ only_a_ptr, 08/2017
-    BrakeCombo  wh_braking;
+    WheelBraking wh_braking;
     node_t*     wh_arm_node;
     node_t*     wh_near_attach_node;
     node_t*     wh_axis_node_0;
     node_t*     wh_axis_node_1;
-    int         wh_propulsed;             // TODO: add enum ~ only_a_ptr, 08/2017
+    WheelPropulsion wh_propulsed;
     Ogre::Real  wh_radius;
     Ogre::Real  wh_rim_radius;
     Ogre::Real  wh_speed;             //!< Current wheel speed in m/s

source/main/resources/CacheSystem.cpp
@@ -1013,31 +1013,31 @@ void CacheSystem::FillTruckDetailInfo(CacheEntryPtr& entry, Ogre::DataStreamPtr 
     for (const auto& w : def->root_module->wheels)
     {
         entry->wheelcount++;
-        if (w.propulsion != RigDef::WheelPropulsion::NONE)
+        if (w.propulsion != WheelPropulsion::NONE)
             entry->propwheelcount++;
     }
     for (const auto& w : def->root_module->wheels2)
     {
         entry->wheelcount++;
-        if (w.propulsion != RigDef::WheelPropulsion::NONE)
+        if (w.propulsion != WheelPropulsion::NONE)
             entry->propwheelcount++;
     }
     for (const auto& w : def->root_module->meshwheels)
     {
         entry->wheelcount++;
-        if (w.propulsion != RigDef::WheelPropulsion::NONE)
+        if (w.propulsion != WheelPropulsion::NONE)
             entry->propwheelcount++;
     }
     for (const auto& w : def->root_module->meshwheels2)
     {
         entry->wheelcount++;
-        if (w.propulsion != RigDef::WheelPropulsion::NONE)
+        if (w.propulsion != WheelPropulsion::NONE)
             entry->propwheelcount++;
     }
     for (const auto& w : def->root_module->flexbodywheels)
     {
         entry->wheelcount++;
-        if (w.propulsion != RigDef::WheelPropulsion::NONE)
+        if (w.propulsion != WheelPropulsion::NONE)
             entry->propwheelcount++;
     }
 

commit e5578aef6cca36b7c053b50b3c508e4b5b0ba805

:bug: Fixed #3422 Transfercase unable to switch back into 2WD
Broken by myself in b94cad2749aa415041fd2e00aa30eda43ae84c97

source/main/physics/Actor.cpp
@@ -1513,8 +1513,8 @@ void Actor::toggleTransferCaseMode()
     }
     else
     {
-        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_1].wh_propulsed = WheelPropulsion::FORWARD;
-        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_2].wh_propulsed = WheelPropulsion::FORWARD;
+        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_1].wh_propulsed = WheelPropulsion::NONE;
+        ar_wheels[m_wheel_diffs[m_transfer_case->tr_ax_2]->di_idx_2].wh_propulsed = WheelPropulsion::NONE;
         m_num_proped_wheels -= 2;
     }
 }