c9394129f42f5f71bc32be20db25bf3d19d1b065
ohlidalp <petr@ohlidal.cz>
2026-06-15 21:23:45 +0200

    :broom: No more 'texture_alias is deprecated' warnings in RoR.log

--- resources/paged/Default_fp.glsl
+++ resources/paged/Default_fp.glsl
@@ -1,14 +1,16 @@
+OGRE_NATIVE_GLSL_VERSION_DIRECTIVE
 #include <OgreUnifiedShader.h>
 
-uniform SAMPLER2D(texMap, 0);
-
-uniform vec4 fogColour;
-uniform vec4 fogParams;
+OGRE_UNIFORMS(
+    SAMPLER2D(texMap, 0);
+    uniform vec4 fogColour;
+    uniform vec4 fogParams;
+)
 
 MAIN_PARAMETERS
-IN(vec4 oUV, TEXCOORD0)
-IN(vec4 oColour, COLOR)
-IN(float oFogCoord, FOG)
+    IN(vec4 oUV, TEXCOORD0)
+    IN(vec4 oColour, COLOR)
+    IN(float oFogCoord, FOG)
 MAIN_DECLARATION
 {
     gl_FragColor = texture2D(texMap, oUV.xy);
