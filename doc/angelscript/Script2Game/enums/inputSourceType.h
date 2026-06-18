3140c59dcf100e600cadf9ec82148b72e9efba0d
EMN-CSharp <ezemenent2017@gmail.com>
2026-06-18 17:17:56 -0300

    :books: Updated AngelScript docs and fixed minor issues

---
+++ doc/angelscript/Script2Game/enums/inputSourceType.h
@@ -0,0 +1,29 @@
+
+  // =================================================== //
+  // THIS IS NOT A C++ HEADER! Only a dummy for Doxygen. //
+  // =================================================== //
+
+/** \addtogroup ScriptSideAPIs
+ *  @{
+ */    
+
+/** \addtogroup Script2Game
+ *  @{
+ */   
+
+namespace Script2Game {
+
+/**
+* Binding of RoR::InputSourceType; Used with Script2Game::InputEngineClass.
+*/
+enum inputSourceType
+{
+    IST_ANY,        //!< Digital and analog devices
+    IST_DIGITAL,    //!< Only digital devices
+    IST_ANALOG      //!< Only analog devices
+}
+
+} // namespace Script2Game
+
+/// @}    //addtogroup Script2Game
+/// @}    //addtogroup ScriptSideAPIs
