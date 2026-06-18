72ef3b7378bc702431d7c8b83e1401cca8d06b7d
EMN-CSharp <ezemenent2017@gmail.com>
2026-06-18 19:11:04 -0300

    :books: Updated autopilot docs

--- doc/angelscript/Script2Game/enums/APHeadingMode.h
+++ doc/angelscript/Script2Game/enums/APHeadingMode.h
@@ -16,9 +16,9 @@ namespace Script2Game {
 /**
  * Heading modes for the autopilot system
  */
-enum APHeadingModes
+enum APHeadingMode
 {
-	HEADING_NONE,		//!< Disables all the heading modes.
+	HEADING_NONE,		//!< No heading modes are enabled.
 	HEADING_FIXED,		//!< The autopilot flies a heading set by `AutopilotClass.adjustHeading(int addedHeading)`
 	HEADING_NAV,		//!< The autopilot flies an instrument landing system (ILS) approach if available. If no ILS is available, the autopilot is disconnected.
 	HEADING_WLV			//!< The autopilot keeps the wings level.
